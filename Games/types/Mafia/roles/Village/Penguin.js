e5e69124cf97dfc538189d083a8399ed5d802a2c
pikulet <24848927+pikulet@users.noreply.github.com>
2023-05-29 23:26:41 +0800

    feat(mafia): add penguin role (#171)

---
+++ Games/types/Mafia/roles/Village/Penguin.js
@@ -0,0 +1,11 @@
+const Role = require("../../Role");
+
+module.exports = class Penguin extends Role {
+
+    constructor(player, data) {
+        super("Penguin", player, data);
+        this.alignment = "Village";
+        this.cards = ["VillageCore", "WinWithVillage", "WaddleAndTellSecret"];
+    }
+
+}
