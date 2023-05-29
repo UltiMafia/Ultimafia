72d7553636d356cc277873a3570aa9aaa8142572
joyce <24848927+pikulet@users.noreply.github.com>
2023-05-30 00:51:41 +0800

    fix(mafia): host role does not instantly end game

---
+++ Games/types/Mafia/roles/cards/EndGameWhenOnlyOneOtherAlive.js
@@ -0,0 +1,16 @@
+const Card = require("../../Card");
+
+module.exports = class EndGameAtAnyTime extends Card {
+
+    constructor(role) {
+        super(role);
+
+        this.listeners = {
+            "death": function() {
+                if (this.game.alivePlayers().length <= 2) {
+                    this.winCount = "Village";
+                }
+            }
+        }
+    }
+}
