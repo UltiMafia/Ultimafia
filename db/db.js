71f207d5fd03d5ec3bb3e4da66b9042ccce4004e
Andrei <>
2023-06-05 04:20:24 +0300

    chore: run prettier

--- db/db.js
+++ db/db.js
@@ -1,18 +1,21 @@
 const mongoose = require("mongoose");
 
 module.exports = {
-    promise: new Promise(async (resolve, reject) => {
-        mongoose.set('useCreateIndex', true);
-        console.log("Connecting to MongoDB...")
-        await mongoose.connect(`mongodb://${process.env.MONGO_URL}/${process.env.MONGO_DB}?authSource=admin`, {
-            user: process.env.MONGO_USER,
-            pass: process.env.MONGO_PW,
-            useUnifiedTopology: true,
-            useNewUrlParser: true
-        });
-        console.log("Connected to MongoDB!")
+  promise: new Promise(async (resolve, reject) => {
+    mongoose.set("useCreateIndex", true);
+    console.log("Connecting to MongoDB...");
+    await mongoose.connect(
+      `mongodb://${process.env.MONGO_URL}/${process.env.MONGO_DB}?authSource=admin`,
+      {
+        user: process.env.MONGO_USER,
+        pass: process.env.MONGO_PW,
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+      }
+    );
+    console.log("Connected to MongoDB!");
 
-        resolve(mongoose.connection);
-    }),
-    conn: mongoose.connection
+    resolve(mongoose.connection);
+  }),
+  conn: mongoose.connection,
 };
