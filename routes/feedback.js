ab970c78c15bbe80082b5336a9413e7a735ac0ba
joyce <24848927+pikulet@users.noreply.github.com>
2023-06-12 21:30:18 +0800

    fix(feedback): make submit button permanent

--- routes/feedback.js
+++ routes/feedback.js
@@ -11,10 +11,18 @@ router.post("/send", async function (req, res) {
     var user = await models.User.findOne({ id: userId, deleted: false }).select(
       "_id name"
     );
+
+    let feedbackTitle = req.body.title;
+    let feedback = req.body.value;
     //let feedbackType = req.body.type;
     //let feedbackCategory = req.body.category;
+
+    if (!feedbackTitle || feedbackTitle.length < 5 || !feedback || feedback.length < 15) {
+      res.status(500);
+      res.send("We would appreciate a more meaningful feedback. Please elaborate more.");
+      return;
+    }
     let title = `[${user.name}] ${req.body.title};`;
-    let feedback = req.body.value;
 
     let trelloUrl = `${process.env.TRELLO_API_URL}?key=${process.env.TRELLO_API_KEY}&token=${process.env.TRELLO_API_TOKEN}&idList=${process.env.TRELLO_API_LIST_ID}&name=${title}&desc=${feedback}`;
     var feedbackRes = await axios.post(trelloUrl);
