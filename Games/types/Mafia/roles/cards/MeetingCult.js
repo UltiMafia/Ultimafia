7028616f7a7aa088ab5be7d7eb37a81213490dd8
Golbolco <29699696+Golbolco@users.noreply.github.com>
2023-06-14 08:47:37 -0400

    Update and rename MeetingMonster.js to MeetingCult.js

--- Games/types/Mafia/roles/cards/MeetingCult.js
+++ Games/types/Mafia/roles/cards/MeetingCult.js
@@ -1,11 +1,11 @@
 const Card = require("../../Card");
 
-module.exports = class MeetingMonster extends Card {
+module.exports = class MeetingCult extends Card {
   constructor(role) {
     super(role);
 
     this.meetings = {
-      Monsters: {
+      Cult: {
         states: ["Night"],
         flags: ["group", "speech"],
         canVote: false,
