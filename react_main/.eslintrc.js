2017abb24475898798e0390a23458f861563393d
Andrei <>
2023-06-16 23:20:09 +0300

    fix: fix FE scripts & update some rules in eslintrc

--- react_main/.eslintrc.js
+++ react_main/.eslintrc.js
@@ -10,6 +10,15 @@ module.exports = {
   },
   rules: {
     "prettier/prettier": "error",
+
+    eqeqeq: "warn",
+    "no-unused-vars": "warn",
+    "no-plusplus": "warn",
+    "no-param-reassign": "warn",
+    "react/destructuring-assignment": "warn",
+
+    "no-bitwise": "off",
+    "react/prop-types": "off",
   },
   plugins: ["prettier"],
 };
