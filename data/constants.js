9765dd2590f76ebbe56b85364d934947ffcf9410
RH <104711111+R9H9@users.noreply.github.com>
2023-06-19 08:13:18 +1000

    Update data/constants.js

--- data/constants.js
+++ data/constants.js
@@ -117,7 +117,7 @@ module.exports = {
       Unwavering: ["ConvertImmune"],
       Frustrated: ["FrustratedExecution"],
       Loudmouthed: ["CryOutVisitors"],
-      Traitorous: ["TurnTraitorOnMafiaKill.js"],
+      Traitorous: ["TurnTraitorOnMafiaKill"],
     },
     "Split Decision": {},
     Resistance: {},
