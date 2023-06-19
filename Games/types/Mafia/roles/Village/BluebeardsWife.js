6425389a21c0e8d33f2d302fb13521590d4aeed4
RH <104711111+R9H9@users.noreply.github.com>
2023-06-19 17:59:27 +1000

    Update BluebeardsWife.js

--- Games/types/Mafia/roles/Village/BluebeardsWife.js
+++ Games/types/Mafia/roles/Village/BluebeardsWife.js
@@ -2,7 +2,7 @@ const Role = require("../../Role");
 
 module.exports = class BluebeardsWife extends Role {
   constructor(player, data) {
-    super("Bluebeards Wife", player, data);
+    super("Bluebeard's Wife", player, data);
     this.alignment = "Village";
     this.cards = ["VillageCore", "WinWithVillage", "OpenTheDoor"];
   }
