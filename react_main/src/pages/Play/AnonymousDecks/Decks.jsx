b2a85370d34ac86f6e731f24f5fc3d407ab3ee10
pikulet <24848927+pikulet@users.noreply.github.com>
2023-06-19 16:07:20 +0800

    add search

--- react_main/src/pages/Play/AnonymousDecks/Decks.jsx
+++ react_main/src/pages/Play/AnonymousDecks/Decks.jsx
@@ -1,30 +1,136 @@
-import React, { useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "../../../css/host.css";
 import { TopBarLink } from "../Play";
+import { useLocation } from "react-router-dom/cjs/react-router-dom";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import { useErrorAlert } from "../../../components/Alerts";
+import { UserContext } from "../../../Contexts";
+import axios from "axios";
+import { ItemList, filterProfanity } from "../../../components/Basic";
+import { camelCase } from "../../../utils";
+import { PageNav, SearchBar } from "../../../components/Nav";
 
 export default function AnonymousDecks(props) {
-  const defaultOption = "Featured";
-  const [deckNavOption, setDeckNavOption] = useState(defaultOption);
+  const [listType, setListType] = useState("featured");
+  const [page, setPage] = useState(1);
+  const [pageCount, setPageCount] = useState(1);
+  const [searchVal, setSearchVal] = useState("");
+  const [decks, setDecks] = useState([]);
+  const [selDeck, setSelDeck] = useState({});
 
-  const deckMenuLabels = [
+  const location = useLocation();
+  const history = useHistory();
+  const errorAlert = useErrorAlert();
+
+  const user = useContext(UserContext);
+
+  useEffect(() => {
+    const params = new URLSearchParams(location.search);
+
+    if (params.get("deck")) {
+      getDeckList("id", 1, params.get("deck"));
+
+      axios
+        .get(`/deck/${params.get("deck")}`)
+        .then((res) => {
+          res.data.name = filterProfanity(res.data.name, user.settings);
+          setSelDeck(res.data);
+        })
+        .catch(errorAlert);
+    } else getDeckList(listType, page);
+  }, []);
+
+  function getDeckList(listType, page, query) {
+    axios
+      .get(
+        `/deck/${camelCase(listType)}?&page=${page}&query=${query || ""}`
+      )
+      .then((res) => {
+        setListType(listType);
+        setPage(page);
+        setDecks(res.data.decks);
+        setPageCount(res.data.pages);
+      });
+  }
+
+  function onHostNavClick(listType) {
+    setSearchVal("");
+    getDeckList(listType, 1);
+  }
+
+  function onSearchInput(query) {
+    setSearchVal(query);
+
+    if (query.length) getDeckList("search", 1, query);
+    else getDeckList("featured", 1);
+  }
+
+  function onPageNav(page) {
+    var args = [listType, page];
+
+    if (searchVal.length) args.push(searchVal);
+
+    getDeckList(...args);
+  }
+
+  function onEditDeck(deck) {
+    history.push(`/deck/create?edit=${deck.id}`);
+  }
+
+  function onDelDeck(deck) {
+    axios
+      .post("/deck/delete", { id: deck.id })
+      .then(() => {
+        getDeckList(listType, page);
+      })
+      .catch(errorAlert);
+  }
+
+  const hostButtonLabels = [
     "Featured",
     "Popular",
     "Yours",
-    "CreateDeck"
   ];
-
-  function onDeckNavClick() {
-
-  }
-
-  const deckMenu = deckMenuLabels.map((label) => (
+  const hostButtons = hostButtonLabels.map((label) => (
     <TopBarLink
       text={label}
-      sel={label}
-      onClick={() => onDeckNavClick(label)}
+      sel={listType}
+      onClick={() => onHostNavClick(label)}
       key={label}
     />
   ));
 
-  return <>hi</>
+  return (
+    <div className="span-panel main host">
+      <div className="top-bar">
+        {hostButtons}
+        <SearchBar
+          value={searchVal}
+          placeholder="Deck Name"
+          onInput={onSearchInput}
+        />
+      </div>
+      <ItemList
+        items={decks}
+        map={(deck) => (
+          <DeckRow
+            deck={deck}
+            sel={selDeck}
+            listType={listType}
+            onSelect={setSelDeck}
+            onEdit={onEditDeck}
+            onDel={onDelDeck}
+            odd={decks.indexOf(deck) % 2 == 1}
+            key={deck.id}
+          />
+        )}
+        empty="No decks"
+      />
+      <PageNav page={page} maxPage={pageCount} onNav={onPageNav} />
+    </div>
+  );
+}
+
+function DeckRow(props) {
+  return <h1>{props.deck.name}</h1>
 }
