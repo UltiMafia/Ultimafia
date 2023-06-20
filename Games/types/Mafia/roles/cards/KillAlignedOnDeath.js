e42b5aa1e00c9d2a6a682683c04c5b30635170a4
RH <104711111+R9H9@users.noreply.github.com>
2023-06-20 17:35:52 +1000

    Update KillAlignedOnDeath.js

--- Games/types/Mafia/roles/cards/KillAlignedOnDeath.js
+++ Games/types/Mafia/roles/cards/KillAlignedOnDeath.js
@@ -5,6 +5,17 @@ module.exports = class KillAlignedOnDeath extends Card {
     super(role);
 
     this.listeners = {
+      roleAssigned: function (player) {
+        if (player !== this.player) {
+          return;
+        }
+
+        this.game.queueAlert(
+          `${this.player.name} is the ${this.target.role.name} (${this.target.role.modifier})!`,
+          0,
+          this.game.players.filter((p) => p.role.alignment === this.player.role.alignment)
+        );
+      },
       death: function (player, killer, killType, instant) {
         if (player !== this.player) {
           return;
