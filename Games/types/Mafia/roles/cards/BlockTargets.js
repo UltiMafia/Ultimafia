73ed72a836d96c76a7c1e3fd964951a1d90a4608
RH <104711111+R9H9@users.noreply.github.com>
2023-06-24 08:58:27 +1000

    Update BlockTargets.js to my suggestion

--- Games/types/Mafia/roles/cards/BlockTargets.js
+++ Games/types/Mafia/roles/cards/BlockTargets.js
@@ -1,27 +1,24 @@
 const Card = require("../../Card");
+const { PRIORITY_NIGHT_ROLE_BLOCKER } = require("../../const/Priority");
 
 module.exports = class BlockTargets extends Card {
-
   constructor(role) {
     super(role);
 
-    this.meetingMods = {
-        "*": {
-        states: ["Night"],
-        flags: ["voting"],
-        action: {
-          labels: ["block"],
-          priority: PRIORITY_NIGHT_ROLE_BLOCKER,
-          run: function () {
-            if (this.game.getStateName() != "Night") return;
-  
-            for (let action of this.game.actions[0]) {
-                this.blockActions();
+    this.actions = [
+      {
+        priority: PRIORITY_NIGHT_ROLE_BLOCKER,
+        labels: ["block", "hidden"],
+        run: function () {
+          if (this.game.getStateName() != "Night") return;
+
+          for (let action of this.game.actions[0]) {
+            if (action.actor == this.actor && !action.hasLabel("hidden")) {
+              this.blockActions(this.actor);
             }
-          },
-  
+          }
         },
       },
-    };
+    ];
   }
 };
