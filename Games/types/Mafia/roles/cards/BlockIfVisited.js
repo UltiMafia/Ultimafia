92cf0680bd0ac698ef60f1426640b9874838c984
RH <104711111+R9H9@users.noreply.github.com>
2023-06-24 08:58:05 +1000

    Update BlockIfVisited.js to my suggestion

--- Games/types/Mafia/roles/cards/BlockIfVisited.js
+++ Games/types/Mafia/roles/cards/BlockIfVisited.js
@@ -1,19 +1,22 @@
 const Card = require("../../Card");
+const { PRIORITY_NIGHT_ROLE_BLOCKER } = require("../../const/Priority");
 
 module.exports = class BlockIfVisited extends Card {
-
   constructor(role) {
     super(role);
 
-    this.meetingMods = {
-        "*": {
-          run: function () {
-          let visitors = this.getVisitors();
-          if (visitors.length > 0) {
-            return;
+    this.actions = [
+      {
+        priority: PRIORITY_NIGHT_ROLE_BLOCKER,
+        labels: ["block", "hidden"],
+        run: function () {
+          if (this.game.getStateName() != "Night") return;
+
+          if (this.hasVisitors() === true) {
+            this.blockActions(this.actor);
           }
         },
       },
-    };
+    ];
   }
 };
