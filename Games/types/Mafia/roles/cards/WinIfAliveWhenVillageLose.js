e8ca797e060516d5026153caef54e4ca3fc485d9
RH <104711111+R9H9@users.noreply.github.com>
2023-06-24 21:37:42 +1000

    Create WinIfAliveWhenVillageLose.js

---
+++ Games/types/Mafia/roles/cards/WinIfAliveWhenVillageLose.js
@@ -0,0 +1,23 @@
+const Card = require("../../Card");
+const { PRIORITY_WIN_CHECK_DEFAULT } = require("../../const/Priority");
+
+module.exports = class WinIfAliveWhenVillageLose extends Card {
+  constructor(role) {
+    super(role);
+
+    this.winCount = "Village";
+    this.winCheck = {
+      priority: PRIORITY_WIN_CHECK_DEFAULT,
+      againOnFinished: true,
+      check: function (winners, confirmedFinished) {
+        if (
+          this.player.alive &&
+          confirmedFinished &&
+          !winners.groups["Village"]
+        ) {
+          winners.addPlayer(this.player, this.name);
+        }
+      },
+    };
+  }
+};
