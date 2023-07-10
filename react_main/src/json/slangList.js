eb934ad5087407a7a87635aa1a2e0ca706d84f01
Golbolco <29699696+Golbolco@users.noreply.github.com>
2023-07-10 18:32:12 -0400

    Update slangList.js

--- react_main/src/json/slangList.js
+++ react_main/src/json/slangList.js
@@ -336,12 +336,12 @@ export const slangList = {
   sheep: {
     definition:
       "Sheep. To vote with another player without using logic to arrive at an independent conclusion, but is instead following a leader on blind faith.",
-    emoji: ["🐗", "🐷", "🐖"],
+    emoji: ["🐑", "🐏"],
   },
   hog: {
     definition:
       "Hog. To make an outrageous or surprising play that goes against the grain, without violating rules. Opposite of sheep.",
-    emoji: ["🐑", "🐏"],
+    emoji: ["🐗", "🐷", "🐖"],
   },
   Paired: {
     definition: "Players who the speaker believes share a win condition.",
