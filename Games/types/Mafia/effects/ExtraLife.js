fe5423e794aabff953a80a2bb649f7b26caacdd4
Golbolco <Golbolco@gmail.com>
2023-07-13 16:53:18 -0400

    fix: leech immunity

--- Games/types/Mafia/effects/ExtraLife.js
+++ Games/types/Mafia/effects/ExtraLife.js
@@ -4,16 +4,19 @@ module.exports = class ExtraLife extends Effect {
   constructor() {
     super("Extra Life");
 
-    this.immunity["kill"] = Infinity;
+    this.immunity["lynch"] = Infinity;
     this.listeners = {
       immune: function (action) {
-        if (
-          action.target === this.player &&
-          action.hasLabel("kill") &&
-          !this.player.role.immunity["kill"] &&
-          !this.player.tempImmunity["kill"]
-        ) {
-          this.remove();
+        if (action.target === this.player) {
+          if (action.hasLabel("kill") &&
+              !this.player.role.immunity["kill"] &&
+              !this.player.tempImmunity["kill"]) {
+              this.remove();
+          } else if (action.hasLabel("lynch") &&
+              !this.player.role.immunity["lynch"] &&
+              !this.player.tempImmunity["lynch"]) {
+              this.remove();
+          }
         }
       },
     };
