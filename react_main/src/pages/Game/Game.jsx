eace6fa07ccc415a4ef646256c9cbfecfbbfd74c
joyce <24848927+pikulet@users.noreply.github.com>
2023-07-13 23:02:53 +0800

    fix: change to save on enter

--- react_main/src/pages/Game/Game.jsx
+++ react_main/src/pages/Game/Game.jsx
@@ -2210,7 +2210,12 @@ export function LastWillEntry(props) {
   function onWillChange(e) {
     var newWill = e.target.value.slice(0, MaxWillLength);
     setLastWill(newWill);
-    props.socket.send("lastWill", newWill);
+  }
+
+  function onWillSave(e) {
+    if (e.key === "Enter" && e.shiftKey == false) {
+      props.socket.send("lastWill", lastWill);
+    }
   }
 
   return (
@@ -2222,6 +2227,7 @@ export function LastWillEntry(props) {
             className="last-will-entry"
             value={lastWill}
             onChange={onWillChange}
+            onKeyDown={onWillSave}
           />
         </div>
       }
