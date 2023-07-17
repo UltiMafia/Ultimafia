94a67ed61e83ce943dfba1cf7a9f00f0c21a0e8f
Golbolco <Golbolco@gmail.com>
2023-07-16 21:29:04 -0400

    fix: gambler win

--- Games/types/Mafia/roles/cards/WinWithTwoGambleWins.js
+++ Games/types/Mafia/roles/cards/WinWithTwoGambleWins.js
@@ -2,21 +2,21 @@ const Card = require("../../Card");
 const { PRIORITY_WIN_CHECK_DEFAULT } = require("../../const/Priority");
 
 module.exports = class WinWithTwoGambleWins extends Card {
-  constructor(role) {
-    super(role);
 
-    role.gambleWins = 0;
-    this.winCheck = {
-      priority: PRIORITY_WIN_CHECK_DEFAULT,
-      check: function (counts, winners, aliveCount) {
-        if (
-          this.player.alive &&
-          !winners.groups[this.name] &&
-          (this.gambleWins >= 2 || aliveCount == 2)
-        ) {
-          winners.addPlayer(this.player, this.name);
-        }
-      },
-    };
-  }
-};
+    constructor(role) {
+        super(role);
+
+        this.killer = null;
+
+        this.winCheck = {
+            priority: PRIORITY_WIN_CHECK_DEFAULT,
+            againOnFinished: true,
+            check: function (counts, winners, aliveCount, confirmedFinished) {
+              if (this.player.alive && (this.player.role.data.gamblerWins >= 2 || aliveCount === 2)) {
+                winners.addPlayer(this.player, this.name);
+                }
+            }
+        };
+    }
+
+}
