b2d868f1df0178d74d917f2a28e95d37a5cfaf8d
joyce <24848927+pikulet@users.noreply.github.com>
2023-07-20 00:57:54 +0800

    style fixes, and change phase to 1

--- Games/types/Mafia/roles/cards/WinWithCurrentAlignment.js
+++ Games/types/Mafia/roles/cards/WinWithCurrentAlignment.js
@@ -2,19 +2,17 @@ const Card = require("../../Card");
 const { PRIORITY_WIN_CHECK_DEFAULT } = require("../../const/Priority");
 
 module.exports = class WinWithCurrentAlignment extends Card {
+  constructor(role) {
+    super(role);
 
-    constructor(role) {
-        super(role);
-
-        this.winCheck = {
-            priority: PRIORITY_WIN_CHECK_DEFAULT + 1,
-            againOnFinished: true,
-            check: function (counts, winners) {
-                if (this.player.alive && winners.groups[this.player.role.alignment]) {
-                  winners.addPlayer(this.player, this.name);
-                }
-            }
-        };
-    }
-
-}
+    this.winCheck = {
+      priority: PRIORITY_WIN_CHECK_DEFAULT + 1,
+      againOnFinished: true,
+      check: function (counts, winners) {
+        if (this.player.alive && winners.groups[this.player.role.alignment]) {
+          winners.addPlayer(this.player, this.name);
+        }
+      },
+    };
+  }
+};
