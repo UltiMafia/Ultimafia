5092af078ad17beda399c7aa0a09856f09168ac6
joyce <24848927+pikulet@users.noreply.github.com>
2023-07-20 00:49:18 +0800

    style fix

--- Games/types/Mafia/roles/cards/WinWithCurrentAlignment.js
+++ Games/types/Mafia/roles/cards/WinWithCurrentAlignment.js
@@ -9,8 +9,8 @@ module.exports = class WinWithCurrentAlignment extends Card {
         this.winCheck = {
             priority: PRIORITY_WIN_CHECK_DEFAULT + 1,
             againOnFinished: true,
-            check: function (counts, winners, aliveCount, confirmedFinished) {
-                if (this.player.alive && Object.keys(winners.groups).includes(this.player.role.alignment)) {
+            check: function (counts, winners) {
+                if (this.player.alive && winners.groups[this.player.role.alignment]) {
                   winners.addPlayer(this.player, this.name);
                 }
             }
