cf18d119fa7bee2460c623052be6ad3ca5df167a
sushie <24848927+pikulet@users.noreply.github.com>
2023-07-23 20:56:16 +0800

    fix(mafia): poison kills have power 2 (#735)
    Fixes #731

--- Games/types/Mafia/effects/Poison.js
+++ Games/types/Mafia/effects/Poison.js
@@ -18,6 +18,7 @@ module.exports = class Poison extends Effect {
       labels: ["kill", "poison", "hidden", "absolute", "uncontrollable"],
       delay: 1,
       effect: this,
+      power: 2,
       run: function () {
         if (this.dominates()) this.target.kill("poison", this.actor);
 
