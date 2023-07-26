50cc90b81f5ebf91929a03f733e099ebd97b7a74
Golbolco <Golbolco@gmail.com>
2023-07-26 10:34:24 -0400

    fix: cult death rework

--- Games/types/Mafia/roles/cards/KillSameRoleOnDeath.js
+++ Games/types/Mafia/roles/cards/KillSameRoleOnDeath.js
@@ -1,3 +1,4 @@
+
 const Card = require("../../Card");
 
 module.exports = class KillSameRoleOnDeath extends Card {
@@ -5,14 +6,24 @@ module.exports = class KillSameRoleOnDeath extends Card {
     super(role);
 
     this.listeners = {
-      state: function (stateInfo) {
-        if (stateInfo.id == 0) this.data.leader = true;
+      start: function () {
+        if (
+          !Object.values(this.game.players).find(
+            (e) => e.role.data.cultLeader === true
+          )
+        ) {
+          this.data.cultLeader = true;
+          this.player.queueAlert("You are the Cult Leader.");
+        }
       },
       death: function (player, killer, deathType, instant) {
-        if (player == this.player && this.data.leader)
-          for (let _player of this.game.players)
-            if (_player.alive && _player.role.name == this.name)
-              _player.kill("basic", this.player, instant);
+        if (player === this.player && this.data.cultLeader) {
+          for (const player of this.game.players) {
+            if (player.alive && player.role.name === this.name) {
+              player.kill("basic", killer, instant);
+            }
+          }
+        }
       },
     };
   }
