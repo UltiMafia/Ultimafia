0f84d9c3a62830c51dd69eede08f1d058c0fa74a
Golbolco <Golbolco@gmail.com>
2023-07-26 10:47:09 -0400

    fix: cupid can win if dead

--- Games/types/Mafia/roles/cards/WinIfPairedLoversAlive.js
+++ Games/types/Mafia/roles/cards/WinIfPairedLoversAlive.js
@@ -9,18 +9,18 @@ module.exports = class WinIfPairedLoversAlive extends Card {
       priority: PRIORITY_WIN_CHECK_DEFAULT,
       againOnFinished: true,
       check: function (counts, winners, aliveCount, confirmedFinished) {
-        if (!this.player.alive || !this.pairedLovers) {
+        if (!this.pairedLovers) {
           return;
         }
 
-        for (let lover of this.pairedLovers) {
+        for (const lover of this.pairedLovers) {
           if (!lover.alive) {
             return;
           }
         }
 
         if (
-          (!confirmedFinished && counts["Village"] == aliveCount) ||
+          (!confirmedFinished && counts.Village === aliveCount) ||
           (confirmedFinished && !winners.groups[this.name])
         ) {
           winners.addPlayer(this.player, this.name);
