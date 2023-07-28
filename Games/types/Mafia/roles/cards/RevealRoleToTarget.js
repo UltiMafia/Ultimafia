4baa805528b253ae6c3a9409c3d5cb145ed42ed5
RH <104711111+R9H9@users.noreply.github.com>
2023-07-29 09:24:03 +1000

    Add Noisy/Clumsy (#806)

---
+++ Games/types/Mafia/roles/cards/RevealRoleToTarget.js
@@ -0,0 +1,23 @@
+const Card = require("../../Card");
+const { PRIORITY_INVESTIGATIVE_AFTER_RESOLVE_DEFAULT } = require("../../const/Priority");
+
+module.exports = class RevealRoleToTarget extends Card {
+  constructor(role) {
+    super(role);
+
+    this.actions = [
+      {
+        priority: PRIORITY_INVESTIGATIVE_AFTER_RESOLVE_DEFAULT,
+        labels: ["investigate", "hidden"],
+        run: function () {
+          if (this.game.getStateName() != "Night") return;
+
+          var alert = `:sy2i: You learn that you were targeted by ${this.actor.getAppearance("investigate", true)}.`;
+
+          let visits = this.getVisits(this.actor);
+          visits.map((v) => v.queueAlert(alert));
+        },
+      },
+    ];
+  }
+};
