ee6f65947c8b7879d65a21eca1afa2da2c8d06be
RH <104711111+R9H9@users.noreply.github.com>
2023-07-29 09:17:24 +1000

    Create RevealNameToTarget.js

---
+++ Games/types/Mafia/roles/cards/RevealNameToTarget.js
@@ -0,0 +1,23 @@
+const Card = require("../../Card");
+const { PRIORITY_INVESTIGATIVE_AFTER_RESOLVE_DEFAULT } = require("../../const/Priority");
+
+module.exports = class RevealNameToTarget extends Card {
+  constructor(role) {
+    super(role);
+
+    this.actions = [
+      {
+        priority: PRIORITY_INVESTIGATIVE_AFTER_RESOLVE_DEFAULT,
+        labels: ["investigate", "hidden"],
+        run: function () {
+          if (this.game.getStateName() != "Night") return;
+
+          var alert = `:sy2i: You learn that you were visited by ${this.actor.name}.`;
+
+          let visits = this.getVisits(this.actor);
+          visits.map((v) => v.queueAlert(alert));
+        },
+      },
+    ];
+  }
+};
