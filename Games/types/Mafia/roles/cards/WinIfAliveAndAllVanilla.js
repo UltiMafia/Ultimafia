792f45d78f0400d17841b33664662ba7d441af0c
RH <104711111+R9H9@users.noreply.github.com>
2023-07-29 13:31:19 +1000

    Update WinIfAliveAndAllVanilla.js

--- Games/types/Mafia/roles/cards/WinIfAliveAndAllVanilla.js
+++ Games/types/Mafia/roles/cards/WinIfAliveAndAllVanilla.js
@@ -5,19 +5,6 @@ const { PRIORITY_ITEM_TAKER_DEFAULT } = require("../../const/Priority");
 module.exports = class WinIfAliveAndAllVanilla extends Card {
   constructor(role) {
     super(role);
-
-    this.actions = [
-      {
-        priority: PRIORITY_ITEM_TAKER_DEFAULT,
-        labels: ["hidden"],
-        run: function () {
-          if (this.game.getStateName() != "Night") return;
-
-          let visits = this.getVisits(this.actor);
-          visits.map((v) => this.removeAllItems(v));
-        },
-      },
-    ];
     
     this.winCheck = {
       priority: PRIORITY_WIN_CHECK_DEFAULT,
