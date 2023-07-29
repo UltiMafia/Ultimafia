502eab11bb2a21e8ccedfc12badf1fa33fdc1fa1
RH <104711111+R9H9@users.noreply.github.com>
2023-07-29 12:14:45 +1000

    Create Vanillaise.js

---
+++ Games/types/Mafia/roles/cards/Vanillaise.js
@@ -0,0 +1,37 @@
+const Card = require("../../Card");
+const { PRIORITY_CONVERT_DEFAULT } = require("../../const/Priority");
+
+module.exports = class Vanillaise extends Card {
+  constructor(role) {
+    super(role);
+
+    this.meetings = {
+      "Vanillaise": {
+        states: ["Night"],
+        flags: ["voting"],
+        action: {
+          labels: ["convert"],
+          priority: PRIORITY_CONVERT_DEFAULT,
+          run: function () {
+            var vanilla;
+            if (this.target.alignment === "Village" || this.target.winCount === "Village") {
+              vanilla = "Villager";
+            } else if (this.target.alignment === "Mafia") {
+              vanilla = "Mafioso";
+            } else if (this.target.alignment === "Cult") {
+              vanilla = "Cultist";
+            } else if (this.target.alignment === "Independent") {
+              vanilla = "Grouch";
+            }
+            if (this.dominates()) {
+              this.target.setRole(vanilla);
+              this.actor.queueAlert(
+                `You have converted ${this.target.name} to a ${vanilla}!`
+              );
+            }
+          },
+        },
+      },
+    };
+  }
+};
