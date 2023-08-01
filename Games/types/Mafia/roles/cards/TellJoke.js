d1a0269606105a6bc216c84722644ac5d6593e37
RH <104711111+R9H9@users.noreply.github.com>
2023-08-01 21:11:28 +1000

    Update TellJoke.js

--- Games/types/Mafia/roles/cards/TellJoke.js
+++ Games/types/Mafia/roles/cards/TellJoke.js
@@ -1,5 +1,5 @@
-const Card = require("../../Card");
 const Random = require("../../../../../lib/Random");
+const Card = require("../../Card");
 const { PRIORITY_INVESTIGATIVE_DEFAULT } = require("../../const/Priority");
 
 module.exports = class TellJoke extends Card {
@@ -24,7 +24,7 @@ module.exports = class TellJoke extends Card {
 
             let tellJokeAbout = Random.randArrayVal(chosen).name;
             let shuffledChosen = Random.randomizeArray(chosen).map(
-              (p) => p.role.name
+              (p) => p.getRoleAppearance();
             );
             let roles = `A ${shuffledChosen[0]}, a ${shuffledChosen[1]} and a ${shuffledChosen[2]}`;
 
