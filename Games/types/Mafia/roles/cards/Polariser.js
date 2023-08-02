236f421fedb93ba3c12d0e66d94e65884c109cd6
joyce <24848927+pikulet@users.noreply.github.com>
2023-08-03 00:03:02 +0800

    let bears win by majority

--- Games/types/Mafia/roles/cards/Polariser.js
+++ Games/types/Mafia/roles/cards/Polariser.js
@@ -8,7 +8,7 @@ module.exports = class Polariser extends Card {
     this.meetings = {
       Polarise: {
         states: ["Night"],
-        flags: ["group", "speech", "voting", "multiActor"],
+        flags: ["voting", "multi"],
         targets: { include: ["alive"], exclude: ["members"] },
         multiMin: 2,
         multiMax: 2,
