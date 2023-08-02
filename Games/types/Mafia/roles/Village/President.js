04df70bb4b241742e93fc10fdfe90552e7ca2343
joyce <24848927+pikulet@users.noreply.github.com>
2023-08-02 23:48:28 +0800

    fix president role reveal

--- Games/types/Mafia/roles/Village/President.js
+++ Games/types/Mafia/roles/Village/President.js
@@ -6,24 +6,21 @@ module.exports = class President extends Role {
     this.alignment = "Village";
     this.cards = ["VillageCore", "WinWithVillage",];
     this.listeners = {
-      roleAssigned: function (player) {
-        if (player !== this.player) {
-          return;
-        }
-
-        this.player.role.data.position = this.player.role.modifier;
-        if (this.player.role.name === "President") {
-          this.player.role.data.position = this.player.role.name;
-        }
-
-        this.game.queueAlert(
-          `${this.player.name} has been elected as the ${this.player.role.data.position}! Protect them at all costs!`,
-          0,
-          this.game.players.filter(
-            (p) => p.role.alignment === this.player.role.alignment
-          )
-        );
-      },
+      roleAssigned: [
+        function (player) {
+          if (player !== this.player) {
+            return;
+          }
+  
+          this.game.queueAlert(
+            `${this.player.name} has been elected as the ${this.player.getRoleAppearance("reveal")}! Protect them at all costs!`,
+            0,
+            this.game.players.filter(
+              (p) => p.role.alignment === this.player.role.alignment && p != this.player
+            )
+          );
+        },
+      ]
     };
   }
 };
