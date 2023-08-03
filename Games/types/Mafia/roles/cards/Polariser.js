f0bf2a042cc0e9833a8d61821b6e2e143844b20c
Golbolco <Golbolco@gmail.com>
2023-08-02 21:08:38 -0400

    fix: polar bears meet and vote together

--- Games/types/Mafia/roles/cards/Polariser.js
+++ Games/types/Mafia/roles/cards/Polariser.js
@@ -8,7 +8,7 @@ module.exports = class Polariser extends Card {
     this.meetings = {
       Polarise: {
         states: ["Night"],
-        flags: ["voting", "multi"],
+        flags: ["voting", "multi", "group", "speech", "multiActor"],
         targets: { include: ["alive"], exclude: ["members"] },
         multiMin: 2,
         multiMax: 2,
