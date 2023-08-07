9526d9b823bb8e6bfa59f12cf5dc3a4b8a0d17f6
joyce <24848927+pikulet@users.noreply.github.com>
2023-08-07 13:35:19 +0800

    fix(sh): fix secret hitler execution bug when play has no custom death message

--- Games/types/Secret Hitler/Player.js
+++ Games/types/Secret Hitler/Player.js
@@ -1,7 +1,10 @@
 const Player = require("../../core/Player");
+const deathMessages = require("./templates/death");
 
 module.exports = class SecretHitlerPlayer extends Player {
   constructor(user, game, isBot) {
     super(user, game, isBot);
+    
+    this.deathMessages = deathMessages;
   }
 };
