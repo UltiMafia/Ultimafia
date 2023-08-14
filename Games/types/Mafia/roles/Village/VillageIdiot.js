43294ace6b11f9c3048e5df9e8b358d5c61df1b6
joyce <24848927+pikulet@users.noreply.github.com>
2023-08-15 00:46:27 +0800

    fix vi card

--- Games/types/Mafia/roles/Village/VillageIdiot.js
+++ Games/types/Mafia/roles/Village/VillageIdiot.js
@@ -3,12 +3,12 @@ const Role = require("../../Role");
 module.exports = class VillageIdiot extends Role {
   constructor(player, data) {
     super("Village Idiot", player, data);
-    
+
     this.alignment = "Village";
     this.cards = [
       "VillageCore",
       "WinWithVillage",
-      "SeeRandomSpeakers",
+      "Clueless",
     ];  
   }
 };
