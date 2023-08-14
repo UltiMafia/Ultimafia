75f006588ff0967d8ce45c02ffc583e9d552d0c6
joyce <24848927+pikulet@users.noreply.github.com>
2023-08-15 00:53:07 +0800

    fix druid and revive

--- Games/types/Mafia/roles/cards/Treevive.js
+++ Games/types/Mafia/roles/cards/Treevive.js
@@ -11,12 +11,20 @@ module.exports = class Treevive extends Card {
         states: ["Night"],
         flags: ["voting"],
         targets: { include: ["dead"], exclude: ["alive", "self"] },
+        shouldMeet: function () {
+          return !this.data.revived;
+        },
         action: {
           labels: ["revive"],
           priority: PRIORITY_NIGHT_REVIVER,
           run: function () {
-            if (this.dominates()) this.actor.role.data.revived = true;
-            this.target.setRole(`Tree`, this.target.role.data, true);
+            if (!this.dominates()) {
+              return;
+            } 
+            
+            this.actor.role.data.revived = true;
+            this.target.revive("basic", this.actor);
+            this.target.setRole("Tree", this.target.role.data, true);
             this.target.queueAlert(":sy2e: You grow into a tree!");
             this.target.role.revealToAll();
           },
