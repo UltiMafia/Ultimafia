d1eefc4454d164c02e512f31cfbbd54e53f4d77e
joyce <24848927+pikulet@users.noreply.github.com>
2023-08-16 21:28:44 +0800

    remove old roles

--- Games/types/Mafia/roles/Mafia/Framer.js
+++
@@ -1,7 +0,0 @@
-const Lawyer = require("./Lawyer");
-
-module.exports = class Framer extends Lawyer {
-  constructor(player, data) {
-    super("Framer", player, data);
-  }
-};
--- Games/types/Mafia/roles/Village/Veteran.js
+++
@@ -1,7 +0,0 @@
-const Villager = require("./Villager");
-
-module.exports = class Veteran extends Villager {
-  constructor(player, data) {
-    super("Veteran", player, data);
-  }
-};
