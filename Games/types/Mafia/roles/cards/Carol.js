39d5deb1b071d147acfd4610cd1d8d9ff9b23901
Golbolco <Golbolco@gmail.com>
2023-08-17 14:04:07 -0400

    fix: dreamer and caroler detect cult/hostiles (#1008)
    nobody ever thought to make dreamer/caroler compatible with cult
    
    this is why making 1:1 copy of EM mechanics is painful ):
    
    ---------
    
    Co-authored-by: joyce <24848927+pikulet@users.noreply.github.com>

--- Games/types/Mafia/roles/cards/Carol.js
+++ Games/types/Mafia/roles/cards/Carol.js
@@ -17,45 +17,28 @@ module.exports = class Carol extends Card {
           labels: ["carol"],
           priority: PRIORITY_INVESTIGATIVE_AFTER_RESOLVE_DEFAULT,
           run: function () {
-            if (this.game.players.length < 3) return;
-
             this.actor.role.data.prevTarget = this.target;
-
-            for (let action of this.game.actions[0]) {
-              if (
-                action.actors.indexOf(this.target) != -1 &&
-                action.priority < this.priority &&
-                !action.hasLabel("hidden") &&
-                action.target != "No"
-              ) {
-                return;
-              }
-            }
-
+            
             var alive = this.game.players.filter((p) => p.alive);
-
             if (alive.length < 3) return;
 
+            const visitors = this.getVisitors(this.target).filter(p => p != this.actor);
+            if (visitors.length > 0) return;
+            
             var carol;
-            var mafia = alive.filter((p) => p.role.alignment == "Mafia");
+            var evilPlayers = alive.filter((p) => p.role.alignment == "Mafia" || p.role.alignment == "Cult" || p.role.alignment == "Hostile");
             var chosenThree = [
               Random.randArrayVal(alive, true),
               Random.randArrayVal(alive, true),
               Random.randArrayVal(alive, true),
             ];
 
-            if (mafia.length == 0)
-              carol = `:sy3c: You see a merry Caroler outside your house! They sing you a happy song about all of the Mafia being dead!`;
-            else {
-              if (
-                chosenThree.filter((p) => p.role.alignment == "Mafia").length ==
-                0
-              ) {
-                chosenThree[0] = Random.randArrayVal(mafia);
-                chosenThree = Random.randomizeArray(chosenThree);
-              }
-
-              carol = `:sy3c: You see a merry Caroler outside your house! They sing you a Carol about ${chosenThree[0].name}, ${chosenThree[1].name}, ${chosenThree[2].name}, at least one of whom is the Mafia!`;
+            if (evilPlayers.length == 0) {
+              carol = `:sy3c: You see a merry Caroler outside your house! They sing you a happy song about all of the evil players being dead!`;
+            } else {
+              chosenThree[0] = Random.randArrayVal(evilPlayers);
+              chosenThree = Random.randomizeArray(chosenThree);
+              carol = `:sy3c: You see a merry Caroler outside your house! They sing you a Carol about ${chosenThree[0].name}, ${chosenThree[1].name}, ${chosenThree[2].name}, at least one of whom is evil!`;
             }
 
             this.target.queueAlert(carol);
