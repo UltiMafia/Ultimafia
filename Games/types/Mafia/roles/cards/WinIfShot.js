209064c6dc845dd7fdcc49f706b60748881ac8a8
Golbolco <Golbolco@gmail.com>
2023-08-17 14:10:27 -0400

    fix: dodo can't joint with maf majority (#1007)
    fixed ability for dodo serving as an easy path for mafia win if they
    would normall have majority by shooting dodo
    
    ---------
    
    Co-authored-by: joyce <24848927+pikulet@users.noreply.github.com>

--- Games/types/Mafia/roles/cards/WinIfShot.js
+++ Games/types/Mafia/roles/cards/WinIfShot.js
@@ -1,16 +1,17 @@
 const Card = require("../../Card");
-const { PRIORITY_WIN_CHECK_DEFAULT } = require("../../const/Priority");
+const { PRIORITY_WIN_BY_GETTING_SHOT } = require("../../const/Priority");
 
 module.exports = class WinIfShot extends Card {
   constructor(role) {
     super(role);
 
     this.winCheck = {
-      priority: PRIORITY_WIN_CHECK_DEFAULT,
+      priority: PRIORITY_WIN_BY_GETTING_SHOT,
       againOnFinished: true,
       check: function (counts, winners, aliveCount) {
         if (this.data.shot && !winners.groups[this.name]) {
           winners.addPlayer(this.player, this.name);
+          return true;
         }
       },
     };
