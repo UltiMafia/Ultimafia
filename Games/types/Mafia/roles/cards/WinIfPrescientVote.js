e8aec06993168932e46631c8a97b449b799164af
sushie <24848927+pikulet@users.noreply.github.com>
2023-08-20 10:46:32 +0800

    fix(mafia): bugged flavour text for warlock and monk (#1035)

--- Games/types/Mafia/roles/cards/WinIfPrescientVote.js
+++ Games/types/Mafia/roles/cards/WinIfPrescientVote.js
@@ -29,8 +29,8 @@ module.exports = class WinfIfPrescientVote extends Card {
           this.player.alive
         ) {
           this.predictedCorrect += 1;
-          this.actor.queueAlert(
-            `The Village has condemned ${this.target.name} to death, enhancing your clairvoyant abilities.`
+          this.player.queueAlert(
+            `The Village has condemned ${this.predictedVote.name} to death, enhancing your clairvoyant abilities.`
           );
         }
       },
