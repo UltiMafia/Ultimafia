4119d9084fb5b794fdfd2ab40a777be280a6a61b
joyce <24848927+pikulet@users.noreply.github.com>
2023-08-22 09:14:21 +0800

    fix(mafia): rework yakuza to separate priorities

--- Games/types/Mafia/roles/cards/CommitSeppuku.js
+++ Games/types/Mafia/roles/cards/CommitSeppuku.js
@@ -1,5 +1,5 @@
 const Card = require("../../Card");
-const { PRIORITY_CONVERT_DEFAULT } = require("../../const/Priority");
+const { PRIORITY_CONVERT_DEFAULT, PRIORITY_KILL_DEFAULT } = require("../../const/Priority");
 
 module.exports = class CommitSeppuku extends Card {
   constructor(role) {
@@ -11,16 +11,37 @@ module.exports = class CommitSeppuku extends Card {
         flags: ["voting"],
         targets: { include: ["alive"], exclude: ["Mafia"] },
         action: {
-          labels: ["kill", "seppuku", "convert"],
+          labels: ["convert", "seppuku"],
           priority: PRIORITY_CONVERT_DEFAULT,
           power: 2,
           run: function () {
-            if (this.dominates) this.target.setRole("Mafioso");
-            if (this.dominates(this.actor))
-              this.actor.kill("basic", this.actor);
+            if (this.dominates()) this.target.setRole("Mafioso");
+            this.actor.role.hasSeppukuTonight = true;
           },
         },
       },
     };
+
+    this.listeners = {
+      state: function() {
+        this.hasSeppukuTonight = false;
+      }
+    }
+
+    this.actions = [
+      {
+        labels: ["kill", "seppuku"],
+        priority: PRIORITY_KILL_DEFAULT,
+        run: function() {
+          if (!this.actor.role.hasSeppukuTonight) {
+            return;
+          }
+
+          if (this.dominates(this.actor)) {
+            this.actor.kill("basic", this.actor);
+          }
+        }
+      }
+    ]
   }
 };
