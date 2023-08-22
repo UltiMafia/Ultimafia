2eecaedf73d43b01c6c5c67929c5e012934e4cf7
sushie <24848927+pikulet@users.noreply.github.com>
2023-08-22 10:03:30 +0800

    fix(mafia): mafia kills are no longer multiactor (#1058)

--- Games/types/Mafia/roles/cards/MeetingMafia.js
+++ Games/types/Mafia/roles/cards/MeetingMafia.js
@@ -9,7 +9,7 @@ module.exports = class MeetingMafia extends Card {
       Mafia: {
         actionName: "Mafia Kill",
         states: ["Night"],
-        flags: ["group", "speech", "voting", "multiActor"],
+        flags: ["group", "speech", "voting"],
         targets: {
           include: ["alive"],
           exclude: [excludeMafiaOnlyIfNotAnonymous],
