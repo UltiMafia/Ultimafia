a86fe983e1cb2f494b7a0192dc26f3b3fc572e76
ammico <ammygrace@gmail.com>
2023-08-23 19:17:34 -0700

    update contributers page with all role icons so far (#1068)

--- data/contributors.js
+++ data/contributors.js
@@ -1,33 +1,55 @@
 const developmentContributors = [
-  "Fsalker",
-  "Ruviz",
-  "Darkishzapo",
-  "kirigiri",
-  "RH9",
-  "Starry",
-  "Golbolco",
   "DrSharky",
   "eggsushi",
+  "Golbolco",
+  "RH9",
+  "Fsalker",
+  "Starry",
+  "kirigiri",
+  "Ruviz",
+  "Darkishzapo",
+ 
 ];
 
 const artContributors = {
   ammico: {
-    Mafia: ["Hunter", "Druid"],
-  },
-  Kobeni: {
-    Mafia: ["Fungoid", "Selkie"],
-  },
-  Maxie: {
-    Mafia: ["Leech"],
+    Mafia: ["Doctor", "Cop", "Insane Cop", "Naive Cop", "Paranoid Cop", "Confused Cop", "Hunter", "Tracker", "Witness", "Monkey", "Medic", "Agent", "Nurse", "Surgeon", "Pharmacist", "Invisible Man", "Chauffeur", "Party Host", "Ghost", "Resurrectionist", "Robin Hood", "Coroner", "Mourner", "Santa", "Sleepwalker", "Waitress", "Soldier", "Empath", "Guard", "Martyr", "Marathoner", "Stalker", "Hooker", "Driver", "Lawyer", "Spy", "Gramps", "Prosecutor", "Fabricator", "Poltergeist", "Jinx", "Quack", "Gossiper", "Werewolf", "Witch", "Druid", "Admirer", "Old Maid", "Occultist", "Creepy Girl", "Siren", "Angel", "Yandere", "Nyarlathotep", "Turkey"]  ],
   },
   bee: {
-    Mafia: ["Queen Bee", "Cannibal"],
+    Mafia: ["Vigilante", "Detective", "Town Crier", "Capybara", "Lightkeeper", "Demolitionist", "Turncoat", "Arsonist", "Queen Bee", "Mummy", "Jinx", "Sharpshooter", "Cannibal", "Vengeful Spirit", "Serial Killer", "Mastermind", "Alien"],
+  },
+  Draculaura: {
+    Mafia: ["Watcher", "Freemason", "Sapling", "Tree", "Trickster", "Lookout", "Cultist", "Nomad"],
+  },
+  Golbolco: {
+    Mafia: ["Funsmith", "Journalist", "President", "Bleeder", "Poisoner", "Caporegime", "Gondolier", "Inquisitor", "Gunrunner", "Palladist", "Prophet", "Yandere", "Nyarlathotep", "Leprechaun"],
+  },
+  grr: {
+    Mafia: ["Granny"],
   },
   hima: {
-    Mafia: ["Heartbreaker"],
+    Mafia: ["Oracle", "Celebrity", "Dreamer", "Medium", "Suitress", "Gunslinger", "Actress", "Heartbreaker", "Lover"],
+  },
+  kirigiri: {
+    Mafia: ["Gunsmith", "Blacksmith", "Governor", "Sheriff", "Templar", "Miller", "Psyche", "Mafioso", "Godfather", "Ninja", "Janitor", "Paralyzer", "Traitor"],
+  },
+  Kobeni: {
+    Mafia: ["Chef", "Baker", "Fungoid", "Medusa", "Selkie", "Diabolist"],
+  },
+  Lucaskart: {
+    Mafia: ["Secretary"],
+  },
+  Maxie: {
+    Mafia: ["Virgin", "Leech"],
+  },
+  Noi: {
+    Mafia: ["Penguin", "Butterfly", "Mindwarper", "Anarchist", "Polar Bear"],
+  },
+  Sw33t: {
+    Mafia: ["Celebrity", "Waitress", "Gunslinger", "Hooker", "Actress", "Necromancer", "Housekeeper"],
   },
   trout: {
-    Mafia: ["Villager", "Fool", "Gambler"],
+    Mafia: ["Villager", "King", "Coward", "Bluebeard's Wife", "Eve", "Accursed Doll", "Fool", "Gambler"],
   },
 };
 
