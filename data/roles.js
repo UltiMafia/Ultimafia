960c2eabd99652a5d7d0b20c6c1b0188f9199a4d
Golbolco <Golbolco@gmail.com>
2023-08-25 19:37:25 -0400

    feat: doomsayer role

--- data/roles.js
+++ data/roles.js
@@ -1405,6 +1405,14 @@ const roleData = {
         "If the victim votes for the target in the village meeting the following day, the victim will die.",
       ],
     },
+    Doomsayer: {
+      alignment: "Cult",
+      newlyAdded: true,
+      description: [
+        "Convert all players who visit during the night.",
+        "All Cultists die if the Doomsayer dies.",
+      ],
+    },
 
     //Independent
     Fool: {
