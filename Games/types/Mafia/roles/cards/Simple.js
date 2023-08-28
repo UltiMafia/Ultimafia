a0b6d7bb5394d8b547739713afb2ecb18fffa1bb
RH <104711111+R9H9@users.noreply.github.com>
2023-08-29 09:00:00 +1000

    Create Simple.js

---
+++ Games/types/Mafia/roles/cards/Simple.js
@@ -0,0 +1,24 @@
+const Card = require("../../Card");
+const { PRIORITY_NIGHT_ROLE_BLOCKER } = require("../../const/Priority");
+
+module.exports = class Simple extends Card {
+  constructor(role) {
+    super(role);
+
+    this.actions = [
+      {
+        priority: PRIORITY_NIGHT_ROLE_BLOCKER,
+        labels: ["block", "hidden"],
+        run: function () {
+          if (this.game.getStateName() != "Night") return;
+
+          if (!this.actor.alive) return;
+
+          if (this.isVanillaRole(this.actor) === false) {
+            this.blockActions(this.actor);
+          }
+        },
+      },
+    ];
+  }
+};
