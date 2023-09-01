e92cb204c4957bc63cb813c87ac49de272aa2565
sushie <24848927+pikulet@users.noreply.github.com>
2023-09-01 20:22:09 +0800

    fix(mafia): fix bug with anarch bomb exploding in postgame (#1121)

--- Games/types/Mafia/items/Timebomb.js
+++ Games/types/Mafia/items/Timebomb.js
@@ -24,6 +24,10 @@ module.exports = class Timebomb extends Item {
         // bomb detonates between 10 and 30 seconds
         let toDetonate = Random.randInt(10000, 30000);
         this.timer = setTimeout(() => {
+          if (this.game.finished) {
+            return;
+          }
+
           this.drop();
           if (!this.holder.alive) {
             return;
