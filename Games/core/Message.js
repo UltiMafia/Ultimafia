92eaca5985d5e56059272a27c020f07d58d28bb4
DrSharky <keleblanc24@gmail.com>
2023-09-03 13:18:23 -0400

    Moved text color adjustment to utils
    Moved the text color adjustment functions from message.js to utils.js.
    Also added a region to hopefully organize better. :^)

--- Games/core/Message.js
+++ Games/core/Message.js
@@ -1,6 +1,7 @@
 const shortid = require("shortid");
 const constants = require("../../data/constants");
 const colorContrast = require("color-contrast");
+const utils = require("./Utils");
 
 module.exports = class Message {
   constructor(info) {
@@ -94,8 +95,8 @@ module.exports = class Message {
       senderId = version.sender.id;
 
       if (version.sender.anonId !== undefined) {
-        version.textColor = version.sender.user.textColor !== undefined ? this.adjustColor(version.sender.user.textColor) : "";  
-        version.nameColor = version.sender.user.nameColor !== undefined ? this.adjustColor(version.sender.user.nameColor) : "";
+        version.textColor = version.sender.user.textColor !== undefined ? utils.adjustColor(version.sender.user.textColor) : "";  
+        version.nameColor = version.sender.user.nameColor !== undefined ? utils.adjustColor(version.sender.user.nameColor) : "";
       }
       else{
         version.textColor = version.sender.user.settings.textColor !== undefined ? this.adjustColor(version.sender.user.settings.textColor) : "";
@@ -105,83 +106,6 @@ module.exports = class Message {
     else return;
 
     return this.parseMessageInfoObj(version, senderId);
-  }
-
-  getIncreasedBrightness(color1, color2) {
-    let contrastVal = colorContrast(color1, color2);
-    if (contrastVal < 1.5) {
-      return this.increaseBrightness(color1, 60);
-    }
-    else if (contrastVal <= 2.5) {
-      return this.increaseBrightness(color1, 45);
-    }
-    else if (contrastVal <= 4.5) {
-      return this.increaseBrightness(color1, 30);
-    }
-    else {
-      return color1;
-    }
-  }
-
-  getDecreasedBrightness(color1, color2) {
-    let contrastVal = colorContrast(color1, color2);
-    if (contrastVal < 1.5) {
-      return this.decreaseBrightness(color1, 50);
-    }
-    else if (contrastVal <= 2.5) {
-      return this.decreaseBrightness(color1, 40);
-    }
-    else if (contrastVal <= 4.5) {
-      return this.decreaseBrightness(color1, 30);
-    }
-    else {
-      return color1;
-    }
-  }
-
-  adjustColor(color) {
-      return  {
-        darkTheme: this.getIncreasedBrightness(color, "#181a1b"),
-        lightTheme: this.getDecreasedBrightness(color, "#ffffff")
-      };
-  }
-
-  increaseBrightness(color, percent) {
-    let num = parseInt(color.replace("#", ""), 16),
-      amt = Math.round(2.55 * percent),
-      R = (num >> 16) + amt,
-      B = ((num >> 8) & 0x00ff) + amt,
-      G = (num & 0x0000ff) + amt;
-    return (
-      "#" +
-      (
-        0x1000000 +
-        (R < 255 ? (R < 1 ? 0 : R) : 255) * 0x10000 +
-        (B < 255 ? (B < 1 ? 0 : B) : 255) * 0x100 +
-        (G < 255 ? (G < 1 ? 0 : G) : 255)
-      )
-        .toString(16)
-        .slice(1)
-    );
-  }
-
-  decreaseBrightness(color, percent) {
-    let num = parseInt(color.replace("#", ""), 16),
-      amt = Math.round(2.55 * percent),
-      R = (num >> 16) - amt,
-      B = ((num >> 8) & 0x00ff) - amt,
-      G = (num & 0x0000ff) - amt;
-    return (
-      "#" +
-      (
-        0x1000000 +
-        (R < 255 ? (R < 1 ? 0 : R) : 255) * 0x10000 +
-        (B < 255 ? (B < 1 ? 0 : B) : 255) * 0x100 +
-        (G < 255 ? (G < 1 ? 0 : G) : 255)
-      )
-        .toString(16)
-        .slice(1)
-    );
   }
 
   parseMessageInfoObj(version, senderId) {
