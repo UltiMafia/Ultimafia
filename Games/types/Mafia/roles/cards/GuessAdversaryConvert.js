e5006084573ebee7b36724f54bc506f612f66e18
Golbolco <Golbolco@gmail.com>
2023-09-04 12:30:25 -0400

    fix

--- Games/types/Mafia/roles/cards/GuessAdversaryConvert.js
+++ Games/types/Mafia/roles/cards/GuessAdversaryConvert.js
@@ -11,13 +11,16 @@ module.exports = class GuessAdversaryConvert extends Card {
         action: {
           labels: ["Convert"],
           run: function () {
-            if (this.target.role.name != this.actor.role.rolesToGuess) {
+            if (this.actor.role.roleToGuess.isArray) {
+                if (roleToGuess.indexOf(this.target.role.name) < 0) {
+                  this.cancel();
+                  return;
+                }
+            } else if (this.target.role.name != this.actor.role.roleToGuess) {
               this.cancel();
-              return; 
+              return;
             }
-
-            if (this.dominates()) this.target.setRole("Cultist");
-          },
+          }
         },
       },
     };
