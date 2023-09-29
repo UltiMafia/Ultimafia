3eaf95f5de3fb2502ea6674771f223e8ec28d5a6
Golbolco <Golbolco@gmail.com>
2023-09-29 10:22:49 -0400

    fix: telepathic is anonmous

--- Games/types/Mafia/roles/cards/Telepathic.js
+++ Games/types/Mafia/roles/cards/Telepathic.js
@@ -17,4 +17,29 @@ module.exports = class Telepathic extends Card {
       },
     };
   }
+  speak(message) {
+    if (message.abilityName != "Contact") return;
+
+    message.modified = true;
+    message.anonymous = true;
+    message.quotable = false;
+    message.prefix = `says to ${message.abilityTarget}`;
+    message.recipients = [message.sender];
+    message.parseForReview = this.parseForReview;
+
+    for (let player of message.game.players)
+      if (player.role.name == message.abilityTarget)
+        message.recipients.push(player);
+
+    if (message.recipients.length == 1) {
+      message.cancel = true;
+      return;
+    }
+  }
+
+  parseForReview(message) {
+    message.recipients = message.versions["*"].recipients;
+    message.prefix = message.versions["*"].prefix;
+    return message;
+  }
 };
