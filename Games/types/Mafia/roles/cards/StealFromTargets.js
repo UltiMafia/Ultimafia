aca6e92c5b7f3d6da4f0e36be302abe770cd98b5
Golbolco <Golbolco@gmail.com>
2023-10-02 09:46:19 -0400

    feat: kleptomaniac modifier, rewritten

---
+++ Games/types/Mafia/roles/cards/StealFromTargets.js
@@ -0,0 +1,21 @@
+const Card = require("../../Card");
+const { PRIORITY_ITEM_TAKER_DEFAULT } = require("../../const/Priority");
+
+module.exports = class StealFromTargets extends Card {
+  constructor(role) {
+    super(role);
+
+    this.actions = [
+      {
+        priority: PRIORITY_ITEM_TAKER_DEFAULT,
+        labels: ["stealItem"],
+        run: function () {
+          if (this.game.getStateName() != "Night") return;
+
+          let visits = this.getVisitors(this.actor);
+          visitors.map((v) => this.stealAllItems(v));
+        },
+      },
+    ];
+  }
+};
