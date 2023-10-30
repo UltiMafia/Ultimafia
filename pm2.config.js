47dd70085968110c156386eadbe7c5ec6dcc87d6
DrSharky <keleblanc24@gmail.com>
2023-10-30 15:44:32 -0400

    Revert "MORE TEMP CHANGES"
    This reverts commit e5f487b3292933623b47527e098d3d86d627b702.

--- pm2.config.js
+++ pm2.config.js
@@ -5,16 +5,19 @@ module.exports = {
       "script": "./Games/games.js",
       "autorestart": false,
       "time": true,
+      "node-args": ["--debug=7000"],
     },
     {
       "name": "www",
       "script": "./bin/www",
       "time": true,
+      "node-args": ["--debug=7000"],
     },
     {
       "name": "chat",
       "script": "./modules/chat.js",
       "time": true,
+      "node-args": ["--debug=7000"],
     },
   ],
 };
