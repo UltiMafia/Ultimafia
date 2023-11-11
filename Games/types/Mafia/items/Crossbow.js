a85ab67397c0780ffdb877ffff500356c58831da
Golbolco <Golbolco@gmail.com>
2023-11-11 18:10:35 -0500

    possible crossbow fix 1

--- Games/types/Mafia/items/Crossbow.js
+++ Games/types/Mafia/items/Crossbow.js
@@ -1,17 +1,13 @@
-const Item = require("../Item");
-const Action = require("../Action");
+const Item = require("../../Item");
+const { PRIORITY_SUNSET_DEFAULT } = require("../../const/Priority");
 
 module.exports = class Crossbow extends Item {
-  constructor(options) {
-    super("Crossbow");
+  constructor(role) {
+    super(role);
 
     this.cursed = options?.cursed;
-
-    this.baseMeetingName = "Get Revenge";
-    this.currentMeetingIndex = 0;
-
     this.meetings = {
-      [this.baseMeetingName]: {
+      "Get Revenge": {
         states: ["Sunset"],
         flags: ["voting"],
         shouldMeet: function () {
@@ -24,24 +20,24 @@ module.exports = class Crossbow extends Item {
         action: {
           labels: ["kill"],
           priority: PRIORITY_SUNSET_DEFAULT,
+          item: this,
           run: function () {
             this.item.drop();
 
-            var cursed = this.item.cursed;
-
             if (cursed) {
-                let action = new Action({
-                  actor: this.actor,
-                  target: this.target,
-                  game: this.game,
-                  labels: ["reveal"],
-                  run: function () {
-                    if (this.dominates()) this.target.role.revealToAll();
-                  },
-                });
-                action.do();
-                return;
-              }
+              let action = new Action({
+                actor: this.actor,
+                target: this.target,
+                game: this.game,
+                labels: ["reveal"],
+                run: function () {
+                  if (this.dominates()) 
+                  this.target.role.revealToAll();
+                },
+              });
+              action.do();
+              return;
+            }
 
             if (this.dominates())
               this.target.kill("condemnRevenge", this.actor);
@@ -50,54 +46,26 @@ module.exports = class Crossbow extends Item {
       },
     };
     this.stateMods = {
-        Day: {
-          type: "delayActions",
-          delayActions: true,
+      Day: {
+        type: "delayActions",
+        delayActions: true,
+      },
+      Overturn: {
+        type: "delayActions",
+        delayActions: true,
+      },
+      Sunset: {
+        type: "add",
+        index: 5,
+        length: 1000 * 30,
+        shouldSkip: function () {
+          for (let action of this.game.actions[0])
+            if (action.target == this.player && action.hasLabel("condemn"))
+              return false;
+
+          return true;
         },
-        Overturn: {
-          type: "delayActions",
-          delayActions: true,
-        },
-        Sunset: {
-          type: "add",
-          index: 5,
-          length: 1000 * 30,
-          shouldSkip: function () {
-            for (let action of this.game.actions[0])
-              if (action.target == this.player && action.hasLabel("condemn"))
-                return false;
-  
-            return true;
-          },
-        },
-      };
-  }
-
-  get snoopName() {
-    if (this.cursed) {
-      return "Crossbow (Cursed)";
-    }
-
-    return "Crossbow";
-  }
-
-  getMeetingName(idx) {
-    return `${this.id} ${idx}`;
-  }
-
-  getCurrentMeetingName() {
-    if (this.currentMeetingIndex === 0) {
-      return this.baseMeetingName;
-    }
-
-    return this.getMeetingName(this.currentMeetingIndex);
-  }
-
-  // increase meeting name index to ensure each meeting name is unique
-  incrementMeetingName() {
-    let mtg = this.meetings[this.getCurrentMeetingName()];
-    delete this.meetings[this.getCurrentMeetingName()];
-    this.currentMeetingIndex += 1;
-    this.meetings[this.getCurrentMeetingName()] = mtg;
+      },
+    };
   }
 };
