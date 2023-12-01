f81eeda2958017373a0a9b9e5138eefcbf46bc2c
DrSharky <keleblanc24@gmail.com>
2023-12-01 15:53:47 -0500

    Update Armor.js

--- Games/types/Mafia/items/Armor.js
+++ Games/types/Mafia/items/Armor.js
@@ -11,7 +11,7 @@ module.exports = class Armor extends Item {
 
     this.listeners = {
       immune: function (action, player) {
-        let killer = this.getVisitors(this.target, "kill");
+        //let killer = this.getVisitors(this.target, "kill");
 
         if (player == this.holder && action.hasLabel("kill")) {
           if (this.holder.tempImmunity["kill"]) return;
