184df7be02e312a3a6166572a9b05307821dc702
RH <104711111+R9H9@users.noreply.github.com>
2023-12-02 11:45:40 +1100

    add alive check

--- Games/types/Mafia/roles/cards/StealFromTargets.js
+++ Games/types/Mafia/roles/cards/StealFromTargets.js
@@ -11,6 +11,7 @@ module.exports = class StealFromTargets extends Card {
         labels: ["stealItem"],
         run: function () {
           if (this.game.getStateName() != "Night") return;
+          if (!this.actor.alive) return;
 
           let visits = this.getVisitors(this.actor);
           visitors.map((v) => this.stealAllItems(v));
