e662afb1b3ec5611920f46e5636601b91aafaf6b
RH <104711111+R9H9@users.noreply.github.com>
2023-12-09 11:11:45 +1100

    fixed naming

--- Games/types/Mafia/roles/cards/WinIfAllVanilla.js
+++ Games/types/Mafia/roles/cards/WinIfAllVanilla.js
@@ -1,7 +1,7 @@
 const Card = require("../../Card");
 const { PRIORITY_WIN_CHECK_DEFAULT } = require("../../const/Priority");
 
-module.exports = class WinIfAliveAndOtherAllVanilla extends Card {
+module.exports = class WinIfAllVanilla extends Card {
   constructor(role) {
     super(role);
 
