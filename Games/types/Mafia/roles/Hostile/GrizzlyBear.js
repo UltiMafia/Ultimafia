4761b11b899910a88b1871da4c1f196102314fc5
Golbolco <Golbolco@gmail.com>
2023-12-09 23:02:08 -0500

    Starcraft 2 Hostile Roles (#1283)
    Experimental hostile roles inspired by Starcraft 2
    
    Special thanks to Darkishzapo for bringing these to my attention
    
    ---------
    
    Co-authored-by: RH <104711111+R9H9@users.noreply.github.com>

---
+++ Games/types/Mafia/roles/Hostile/GrizzlyBear.js
@@ -0,0 +1,13 @@
+const Role = require("../../Role");
+
+module.exports = class GrizzlyBear extends Role {
+  constructor(player, data) {
+    super("Grizzly Bear", player, data);
+    this.alignment = "Hostile";
+    this.cards = [
+      "VillageCore",
+      "WinAmongLastTwo",
+      "MassMurderer",
+    ];
+  }
+};
