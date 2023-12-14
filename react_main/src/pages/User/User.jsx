5393ad4e79708223f4f68bf8be2c03c7495d92e0
DrSharky <keleblanc24@gmail.com>
2023-12-14 13:09:15 -0500

    UI fixes (#1315)

--- react_main/src/pages/User/User.jsx
+++ react_main/src/pages/User/User.jsx
@@ -193,10 +193,13 @@ export function Avatar(props) {
   else size = "";
 
   if (hasImage && !imageUrl && id && avatarId) {
-    if (id === avatarId && !deckProfile) {
-      style.backgroundImage = `url(/uploads/${id}_avatar.webp?t=${siteInfo.cacheVal})`;
-    } else {
-      style.backgroundImage = `url(/uploads/decks/${avatarId}.webp?t=${siteInfo.cacheVal})`;
+    if (id === avatarId) {
+      if (!deckProfile) {
+        style.backgroundImage = `url(/uploads/${id}_avatar.webp?t=${siteInfo.cacheVal})`;
+      }
+      else {
+        style.backgroundImage = `url(/uploads/decks/${avatarId}.webp?t=${siteInfo.cacheVal})`;
+      }
     }
   } else if (hasImage && !imageUrl && id) {
     style.backgroundImage = `url(/uploads/${id}_avatar.webp?t=${siteInfo.cacheVal})`;
@@ -214,6 +217,12 @@ export function Avatar(props) {
     rand = Math.abs(rand) / Math.pow(2, 31);
 
     style.backgroundColor = colors[Math.floor(rand * colors.length)];
+  }
+  if (typeof hasImage == "string") {
+    if (hasImage.includes('decks')) {
+      style.backgroundImage = `url(/uploads${hasImage}?t=${siteInfo.cacheVal})`;
+      style.backgroundColor = "#00000000";
+    }
   }
 
   var santaWidth;
