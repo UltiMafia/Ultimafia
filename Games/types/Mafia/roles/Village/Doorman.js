c7c1858b98dacf6fd9da41b269a3cc186bc040e9
RH <104711111+R9H9@users.noreply.github.com>
2023-12-14 15:12:31 +1100

    Update and rename Guard.js to Doorman.js

--- Games/types/Mafia/roles/Village/Doorman.js
+++ Games/types/Mafia/roles/Village/Doorman.js
@@ -1,8 +1,8 @@
 const Role = require("../../Role");
 
-module.exports = class Guard extends Role {
+module.exports = class Doorman extends Role {
   constructor(player, data) {
-    super("Guard", player, data);
+    super("Doorman", player, data);
 
     this.alignment = "Village";
     this.cards = ["VillageCore", "WinWithVillage", "Rolestopper"];
