5df2301350d866e9316dc98e8f054929fc4cc20e
RH <104711111+R9H9@users.noreply.github.com>
2023-12-14 14:40:10 +1100

    Create BecomeFirstDeadAligned.js

---
+++ Games/types/Mafia/roles/cards/BecomeFirstDeadAligned.js
@@ -0,0 +1,26 @@
+const Card = require("../../Card");
+
+module.exports = class BecomeFirstDeadAligned extends Card {
+  constructor(role) {
+    super(role);
+
+    this.listeners = {
+      death: function (player) {
+        if (
+          player !== this.player &&
+          !isVanillaRole(player) &&
+          this.player.alive
+        ) {
+          this.actor.setRole(
+              `${this.target.role.name}:${this.target.role.modifier}`,
+              this.target.role.data,
+              false,
+              false,
+              true
+          );
+          this.game.events.emit("roleAssigned", this.actor);
+        }
+      },
+    };
+  }
+};
