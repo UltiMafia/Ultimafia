ed73b99ecfda743b18525649fbc3ff03c43935cb
RH <104711111+R9H9@users.noreply.github.com>
2023-12-14 18:06:52 +1100

    Create GuessRole.js

---
+++ Games/types/Mafia/roles/cards/GuessRole.js
@@ -0,0 +1,43 @@
+const Card = require("../../Card");
+const { PRIORITY_INVESTIGATIVE_DEFAULT } = require("../../const/Priority");
+
+module.exports = class GuessRole extends Card {
+  constructor(role) {
+    super(role);
+
+    this.meetings = {
+      "Guess Player": {
+        states: ["Night"],
+        flags: ["voting"],
+        action: {
+          priority: PRIORITY_INVESTIGATIVE_DEFAULT - 1,
+          run: function () {
+            this.actor.role.data.targetRole = this.target.role.name;
+          },
+        },
+      },
+
+      "Guess Role": {
+        states: ["Night"],
+        flags: ["voting"],
+        inputType: "role",
+        targets: { include: ["all"] },
+        action: {
+          labels: ["role"],
+          priority: PRIORITY_INVESTIGATIVE_DEFAULT,
+          run: function () {
+            let targetRole = this.actor.role.data.targetRole;
+            if (targetRole) {
+              if (this.target === targetRole) {
+                this.actor.queueAlert(`:invest: You were not mistaken in pursuing {this.target} for they turned out to be {addArticle(targetRole)}.`);
+              } else {
+                this.actor.queueAlert(`:invest: You were mistaken in pursuing {this.target} for they turned out not to be {addArticle(targetRole)}.`);
+              }
+              delete this.actor.role.data.targetRole;
+            }
+          },
+        },
+      },
+    };
+  }
+};
