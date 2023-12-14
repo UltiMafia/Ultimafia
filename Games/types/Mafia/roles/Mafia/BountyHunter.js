a564d481310c52d952e5ed1f8cbc3411e77c677f
RH <104711111+R9H9@users.noreply.github.com>
2023-12-14 19:06:49 +1100

    Update and rename Recruiter.js to BountyHunter.js

--- Games/types/Mafia/roles/Mafia/BountyHunter.js
+++ Games/types/Mafia/roles/Mafia/BountyHunter.js
@@ -1,8 +1,8 @@
 const Role = require("../../Role");
 
-module.exports = class Recruiter extends Role {
+module.exports = class BountyHunter extends Role {
   constructor(player, data) {
-    super("Recruiter", player, data);
+    super("Bounty Hunter", player, data);
 
     this.alignment = "Mafia";
     this.cards = ["VillageCore", "WinWithMafia", "MeetingMafia", "GuessRole"];
