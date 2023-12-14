5d72439d0e3a12512571abf93bf8a1ebd3a020fa
RH <104711111+R9H9@users.noreply.github.com>
2023-12-14 13:42:21 +1100

    Update MeetWithTemplars.js

--- Games/types/Mafia/roles/cards/MeetWithTemplars.js
+++ Games/types/Mafia/roles/cards/MeetWithTemplars.js
@@ -12,5 +12,20 @@ module.exports = class MeetWithTemplars extends Card {
         inputType: "boolean",
       },
     };
+    this.listeners = {
+      roleAssigned: function (player) {
+        if (player !== this.player) return;
+
+        for (let player of this.game.players) {
+          if (
+            player.role.name === "Templar" &&
+            player !== this.player &&
+            !player.role.oblivious["self"]
+          ) {
+            this.revealToPlayer(player);
+          }
+        }
+      },
+    };
   }
 };
