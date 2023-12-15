505feeaee3738cc6c25bda2e3b2b5d2a2d73e930
RH <104711111+R9H9@users.noreply.github.com>
2023-12-15 15:59:30 +1100

    Update RoamingAlignment.js

--- Games/types/Mafia/roles/cards/RoamingAlignment.js
+++ Games/types/Mafia/roles/cards/RoamingAlignment.js
@@ -10,24 +10,22 @@ module.exports = class RoamingAlignment extends Card {
 
     this.meetings = {
       "Align With": {
-        actionName: "Follow the ways of",
         states: ["Night"],
-        flags: ["voting"],
+        flags: ["voting", "mustAct"],
+        targets: { include: ["alive"], exclude: ["self", isPrevTarget] },
         action: {
           priority: PRIORITY_MODIFY_INVESTIGATIVE_RESULT_DEFAULT,
           run: function () {
+            this.actor.role.data.prevTarget = this.target;
             let alignment = this.target.role.alignment;
             if (alignment == "Independent" || alignment == "Hostile") {
-              this.actor.queueAlert(
-                `You follow ${this.target.name} but could not find somewhere that you could call your own.`
-              );
-              delete this.actor.role.data.alignment;
+              let alignment = this.target.role.name;
               return;
             }
 
             this.actor.role.data.alignment = alignment;
             this.actor.queueAlert(
-              `You follow ${this.target.name} and learn the ways of the ${alignment}.`
+              `You follow ${this.target.name} and will win if they win.`
             );
           },
         },
@@ -61,3 +59,7 @@ module.exports = class RoamingAlignment extends Card {
     };
   }
 };
+
+function isPrevTarget(player) {
+  return this.role && player == this.role.data.prevTarget;
+}
