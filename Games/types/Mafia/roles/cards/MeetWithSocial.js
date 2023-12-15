f3982bc5134125b8b25844dde99ea0c65d53351b
RH <104711111+R9H9@users.noreply.github.com>
2023-12-15 15:04:35 +1100

    Update MeetWithSocial.js

--- Games/types/Mafia/roles/cards/MeetWithSocial.js
+++ Games/types/Mafia/roles/cards/MeetWithSocial.js
@@ -1,5 +1,4 @@
 const Card = require("../../Card");
-const { MEETING_PRIORITY_SOCIAL } = require("../const/MeetingPriority");
 
 module.exports = class MeetWithSocial extends Card {
   constructor(role) {
@@ -9,8 +8,7 @@ module.exports = class MeetWithSocial extends Card {
       "Social Gathering": {
         actionName: "End Social Gathering?",
         states: ["Night"],
-        flags: ["exclusive", "group", "speech", "voting", "mustAct", "noVeg"],
-        priority: MEETING_PRIORITY_SOCIAL,
+        flags: ["group", "speech", "voting", "mustAct", "noVeg"],
         inputType: "boolean",
       },
     };
