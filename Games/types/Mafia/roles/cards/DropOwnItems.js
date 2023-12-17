4632b6b6fa792823f7efc79eb4623043d52b8dc8
RH <104711111+R9H9@users.noreply.github.com>
2023-12-17 17:08:22 +1100

    Create DropOwnItems.js

---
+++ Games/types/Mafia/roles/cards/DropOwnItems.js
@@ -0,0 +1,20 @@
+const Card = require("../../Card");
+const { PRIORITY_ITEM_GIVER_DEFAULT } = require("../../const/Priority");
+
+module.exports = class FixOwnItems extends Card {
+  constructor(role) {
+    super(role);
+
+    this.actions = [
+      {
+        labels: ["dropItems", "hidden"],
+        priority: PRIORITY_ITEM_GIVER_DEFAULT + 1,
+        run: function () {
+          for (let item of this.actor.items) {
+            item.drop();
+          }
+        },
+      },
+    ];
+  }
+};
