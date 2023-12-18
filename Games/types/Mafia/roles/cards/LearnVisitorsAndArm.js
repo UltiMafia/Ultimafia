961ebeeed998fc699e75d4693fb9380a21e29569
RH <104711111+R9H9@users.noreply.github.com>
2023-12-18 16:09:45 +1100

    Update LearnVisitorsAndArm.js

--- Games/types/Mafia/roles/cards/LearnVisitorsAndArm.js
+++ Games/types/Mafia/roles/cards/LearnVisitorsAndArm.js
@@ -1,5 +1,5 @@
 const Card = require("../../Card");
-const { PRIORITY_INVESTIGATIVE_DEFAULT } = require("../../const/Priority");
+const { PRIORITY_ITEM_GIVER_DEFAULT } = require("../../const/Priority");
 
 module.exports = class LearnVisitorsAndArm extends Card {
   constructor(role) {
@@ -7,7 +7,7 @@ module.exports = class LearnVisitorsAndArm extends Card {
 
     this.actions = [
       {
-        priority: PRIORITY_INVESTIGATIVE_DEFAULT,
+        priority: PRIORITY_ITEM_GIVER_DEFAULT,
         labels: ["investigate", "role", "hidden", "absolute"],
         run: function () {
           if (this.game.getStateName() != "Night") return;
