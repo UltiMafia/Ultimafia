6a147bae763e0afe4d3e8a4223c4fe7b7978154d
RH <104711111+R9H9@users.noreply.github.com>
2023-12-18 11:13:02 +1100

    hotfix: undertaker (#1336)

--- Games/types/Mafia/roles/cards/CleanCondemnation.js
+++ Games/types/Mafia/roles/cards/CleanCondemnation.js
@@ -18,7 +18,7 @@ module.exports = class CleanCondemnation extends Card {
 
             var condemnedTarget;
             for (let action of this.game.actions[0]) {
-              if (action.hasLabel("condemn"))) {
+              if (action.hasLabel("condemn")) {
                 condemnedTarget = action.target;
                 break;
               }
@@ -27,7 +27,7 @@ module.exports = class CleanCondemnation extends Card {
 
             const roleName = condemnedTarget.getRoleAppearance("condemn");
             this.actor.role.lastCleanedAppearance = roleName;
-            condemnedTarget.role.appearance.death = null;
+            condemnedTarget.role.appearance.condemn = null;
             this.actor.role.lastCleanedWill = condemnedTarget.lastWill;
             condemnedTarget.lastWill = null;
 
@@ -41,7 +41,7 @@ module.exports = class CleanCondemnation extends Card {
     };
     this.listeners = {
       state: function () {
-        if (this.game.getStateName() != "Day") return;
+        if (this.game.getStateName() != "Night") return;
 
         const cleanedPlayer = this.cleanedPlayer;
         if (!cleanedPlayer) return;
