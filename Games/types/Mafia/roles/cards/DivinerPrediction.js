0fd6733c96e8caa75ffab469207a095d228d834b
RH <104711111+R9H9@users.noreply.github.com>
2023-12-21 09:39:48 +1100

    Update DivinerPrediction.js

--- Games/types/Mafia/roles/cards/DivinerPrediction.js
+++ Games/types/Mafia/roles/cards/DivinerPrediction.js
@@ -61,22 +61,33 @@ module.exports = class DivinerPrediction extends Card {
           return;
         }
 
-        if (!action.hasLabel("condemn")) {
-          return;
+        if (action.hasLabel("condemn")) {
+          let action = new Action({
+            actor: this.player,
+            target: this.predictedVote,
+            game: this.player.game,
+            power: 5,
+            labels: ["kill", "condemn", "overthrow", "diviner"],
+            run: function () {
+              if (this.dominates()) this.target.kill("condemn", this.actor);
+              this.predictedCorrect = false;
+            },
+          });
+          action.do();
+        } else if (action.hasLabel("kill")) {
+          let action = new Action({
+            actor: this.player,
+            target: this.predictedVote,
+            game: this.player.game,
+            power: 5,
+            labels: ["kill", "diviner"],
+            run: function () {
+              if (this.dominates()) this.target.kill("basic", this.actor);
+              this.predictedCorrect = false;
+            },
+          });
+          action.do();
         }
-
-        let action = new Action({
-          actor: this.player,
-          target: this.predictedVote,
-          game: this.player.game,
-          power: 5,
-          labels: ["kill", "condemn", "overthrow"],
-          run: function () {
-            if (this.dominates()) this.target.kill("condemn", this.actor);
-            this.predictedCorrect = false;
-          },
-        });
-        action.do();
       },
     };
   }
