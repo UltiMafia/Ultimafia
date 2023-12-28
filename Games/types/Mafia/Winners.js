7438c829090ddd094d75741f3b1419497d59ceaa
Golbolco <Golbolco@gmail.com>
2023-12-28 15:22:33 -0500

    dentist win message

--- Games/types/Mafia/Winners.js
+++ Games/types/Mafia/Winners.js
@@ -31,6 +31,8 @@ module.exports = class MafiaWinners extends Winners {
         return "After burying the last body, the haunted doll was put to rest. The Creepy Girl was free to lead her own life.";
       case "Cupid":
         return "Ordained by Mount Olympus, Cupid ensured that the star-crossed lovers survived the chaos.";
+      case "Dentist":
+        return ":) A toxic cloud of nitrous rolled over the town leaving none alive... the mad Dentist's hideous laugh echoed through empty streets, every corpse left with a rictus grin.";
       case "Diviner":
         return "At last, with all of those irritating townsfolk gone, the Diviner can finally dowse for orichalcum in peace.";
       case "Dodo":
