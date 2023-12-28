50d61911265e1667560825581415c476d12b46b6
RH <104711111+R9H9@users.noreply.github.com>
2023-12-29 07:43:35 +1100

    fix Dentist file

--- Games/types/Mafia/roles/Hostile/Dentist.js
+++ Games/types/Mafia/roles/Hostile/Dentist.js
@@ -2,14 +2,9 @@ const Role = require("../../Role");
 
 module.exports = class Dentist extends Role {
   constructor(player, data) {
-    super("Serial Killer", player, data);
+    super("Dentist", player, data);
 
     this.alignment = "Hostile";
     this.cards = ["VillageCore", "WinAmongLastTwo", "NightGasser"];
-    this.meetingMods = {
-      "Solo Kill": {
-        flags: ["voting", "mustAct"],
-      },
-    };
   }
 };
