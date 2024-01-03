be1bc4a5832259acac860e832e0987531ba4bc81
RH <104711111+R9H9@users.noreply.github.com>
2024-01-03 18:36:29 +1100

    Update and rename WinIfDead.js to NightPhantom.js

---
+++ Games/types/Mafia/roles/cards/NightPhantom.js
@@ -0,0 +1,28 @@
+const Card = require("../../Card");
+const { PRIORITY_KILL_DEFAULT } = require("../../const/Priority");
+
+module.exports = class NightPhantom extends Card {
+  constructor(role) {
+    super(role);
+
+    this.meetings = {
+      "Phantom Kill": {
+        actionName: "Phantom Kill",
+        states: ["Night"],
+        flags: ["voting"],
+        action: {
+          labels: ["kill"],
+          priority: PRIORITY_KILL_DEFAULT,
+          run: function () {
+            let target = this.target;
+            target.role.appearance.death = null;
+            if (this.dominates()) {
+              target.kill("basic", this.actor); 
+              target.role.revealAlignmentToAll(false, this.getRevealType("death"));
+            }
+          },
+        },
+      },
+    };
+  }
+};
