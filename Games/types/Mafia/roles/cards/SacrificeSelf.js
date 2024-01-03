0dd80e99c8c67669256645df354884dbd1e958c8
RH <104711111+R9H9@users.noreply.github.com>
2024-01-03 15:41:31 +1100

    Update SacrificeSelf.js

--- Games/types/Mafia/roles/cards/SacrificeSelf.js
+++ Games/types/Mafia/roles/cards/SacrificeSelf.js
@@ -1,5 +1,5 @@
 const Card = require("../../Card");
-const { PRIORITY_DAY_DEFAULT } = require("../../const/Priority");
+const { PRIORITY_OVERTHROW_VOTE } = require("../../const/Priority");
 
 module.exports = class SacrificeSelf extends Card {
   constructor(role) {
@@ -7,33 +7,93 @@ module.exports = class SacrificeSelf extends Card {
 
     this.meetings = {
       "Sacrifice Self": {
-        states: ["Day"],
-        flags: ["voting"],
+        states: ["Overturn"],
+        flags: ["group", "speech", "voting", "anonymousVotes"],
+        targets: { include: ["alive"], exclude: ["dead", "self"] },
+        leader: true,
         action: {
-          labels: ["save"],
-          priority: PRIORITY_DAY_DEFAULT,
+          power: 3,
+          labels: ["kill", "condemn", "overthrow"],
+          priority: PRIORITY_OVERTHROW_VOTE,
           run: function () {
-            this.actor.role.protectingTarget = this.target;
-            this.target.giveEffect("CondemnImmune", Infinity, 1);
+            for (let action of this.game.actions[0]) {
+              if (action.hasLabel("condemn") && !action.hasLabel("overthrow")) {
+                if (action.target === this.target) {
+                  return;
+                }
+
+                // Only one village vote can be overthrown
+                action.cancel(true);
+                break;
+              }
+            }
+
+            if (this.dominates()) {
+              this.target.kill("condemn", this.actor);
+            }
+
+            --this.actor.role.overturnsLeft;
           },
         },
       },
     };
 
-    this.listeners = {
-      state: function () {
-        if (this.game.getStateName() == "Night") {
-          delete this.protectingTarget;
-        }
+    this.stateMods = {
+      Day: {
+        type: "delayActions",
+        delayActions: true,
       },
-      immune: function (action, player) {
-        if (player != this.protectingTarget) {
-          return;
-        }
+      Overturn: {
+        type: "add",
+        index: 4,
+        length: 1000 * 30,
+        shouldSkip: function () {
+          //skip if town is trying to condemn mafia under don
+          if (this.player.alive && this.player.role.name == "Don") {
+            for (let action of this.game.actions[0]) {
+              if (
+                action.hasLabel("condemn") &&
+                action.target.alignment == "Mafia"
+              ) {
+                return true;
+              }
+              //don cannot OT away from don
+              if (
+                action.hasLabel("condemn") &&
+                action.target.role.name == "Don"
+              ) {
+                return true;
+              }
+            }
+            return false;
+          }
 
-        if (action.hasLabel("condemn")) {
-          this.player.kill("sacrifice", this.player);
-        }
+          if (!this.overturnsLeft) {
+            return true;
+          }
+          if (!this.player.alive) {
+            return true;
+          }
+
+          let isNoVote = true;
+          for (let action of this.game.actions[0]) {
+            if (action.hasLabel("condemn")) {
+              isNoVote = false;
+              break;
+            }
+          }
+
+          if (isNoVote) {
+            return true;
+          }
+
+          for (let player of this.game.players) {
+            if (!player.hasItem("OverturnSpectator")) {
+              player.holdItem("OverturnSpectator");
+            }
+          }
+          return false;
+        },
       },
     };
   }
