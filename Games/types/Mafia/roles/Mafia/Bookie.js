7fea166c33946007935d8477e59687c4ed7ae1f9
RH <104711111+R9H9@users.noreply.github.com>
2024-01-10 10:17:31 +1100

    Create Bookie.js

---
+++ Games/types/Mafia/roles/Mafia/Bookie.js
@@ -0,0 +1,9 @@
+const Role = require("../../Role");
+
+module.exports = class Bookie extends Role {
+  constructor(player, data) {
+    super("Bookie", player, data);
+    this.alignment = "Mafia";
+    this.cards = ["VillageCore", "WinWithMafia", "MeetingMafia", "BookieWager"];
+  }
+};
