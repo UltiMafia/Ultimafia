fc54b4de3762dfe5e524dd3b1bfbecd152f3942b
RH <104711111+R9H9@users.noreply.github.com>
2024-01-11 11:55:31 +1100

    Update Pyromaniac.js

--- Games/types/Mafia/roles/Hostile/Pyromaniac.js
+++ Games/types/Mafia/roles/Hostile/Pyromaniac.js
@@ -4,7 +4,7 @@ module.exports = class Pyromaniac extends Role {
   constructor(player, data) {
     super("Pyromaniac", player, data);
     this.alignment = "Village";
-    this.cards = ["VillageCore", "WinUponMajority", "DouseInGasoline"];
+    this.cards = ["VillageCore", "WinUponPyromaniacMajority", "DouseInGasoline"];
     this.meetingMods = {
       "Douse Player": {
         flags: ["group", "speech", "voting"],
