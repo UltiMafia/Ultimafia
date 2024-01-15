7f1f74d04f2d3720567a30c30b82ee49c02bd57e
RH <104711111+R9H9@users.noreply.github.com>
2024-01-15 11:26:14 +1100

    Create ImperialDecree.js

---
+++ Games/types/Mafia/roles/cards/ImperialDecree.js
@@ -0,0 +1,103 @@
+const Card = require("../../Card");
+const {
+  PRIORITY_EFFECT_GIVER_DEFAULT,
+  PRIORITY_WIN_CHECK_DEFAULT,
+} = require("../../const/Priority");
+
+module.exports = class ImperialDecree extends Card {
+  constructor(role) {
+    super(role);
+
+    role.duelists = [];
+    role.predictedCorrect = 0;
+
+    this.meetings = {
+      "Declare Duelists (2)": {
+        states: ["Night"],
+        flags: ["voting", "multi"],
+        multiMin: 2,
+        multiMax: 2,
+        action: {
+          labels: ["effect", "dueling"],
+          priority: PRIORITY_EFFECT_GIVER_DEFAULT,
+          run: function () {
+            this.target.forEach((p) => {
+              p.giveEffect("Dueling");
+              this.duelists.push(p);
+            });
+          },
+        },
+      },
+      "Predict Winner": {
+        states: ["Sunrise"],
+        flags: ["voting", "mustAct", "instant"],
+        action: {
+          run: function () {
+            this.actor.role.predictedVote = this.target;
+          },
+        },
+      },
+    };
+
+    this.winCheck = {
+      priority: PRIORITY_WIN_CHECK_DEFAULT,
+      againOnFinished: true,
+      check: function (counts, winners) {
+        if (
+          this.player.alive &&
+          !winners.groups[this.name] &&
+          this.predictedCorrect >= 2
+        ) {
+          winners.addPlayer(this.player, this.name);
+        }
+      },
+    };
+
+    this.listeners = {
+      death: function (player, killer, deathType) {
+        if (
+          player === this.predictedVote &&
+          deathType === "condemn" &&
+          this.player.alive
+        ) {
+          this.predictedCorrect += 1;
+          this.player.queueAlert(
+            `The Village has condemned ${this.predictedVote.name} to death, proving that you were correct in your warrior's abilities.`
+          );
+        }
+      },
+      state: function (stateInfo) {
+        if (!this.player.alive) {
+          return;
+        }
+
+        if (!stateInfo.name.match(/Sunrise/)) {
+          return;
+        }
+
+        this.meetings["Predict Winner"].targets = this.duelists;
+        delete this.predictedVote;
+      },
+    };
+    
+    this.stateMods = {
+      Night: {
+        type: "delayActions",
+        delayActions: true,
+      },
+      Sunrise: {
+        type: "add",
+        index: 7,
+        length: 1000 * 60,
+        shouldSkip: function () {
+          for (let player of this.game.players) {
+            if (player.role.name === "Emperor") {
+              return false;
+            }
+          }
+          return true;
+        },
+      },
+    };
+  }
+};
