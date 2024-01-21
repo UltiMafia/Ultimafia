101f5b08048e01b0df3f4dd1c01af854cd94257f
Andrei <>
2024-01-21 11:54:13 +0200

    fix: use fallback LOADING PAGE instead of 'haha ur net iz szlow'

--- react_main/src/NewMain.jsx
+++ react_main/src/NewMain.jsx
@@ -1,6 +1,7 @@
 import { Route, Switch } from "react-router-dom";
 import React, { Suspense } from "react";
 import { Welcome } from "./pages/Welcome/Welcome";
+import LoadingPage from "./pages/Loading";
 // LazyMain is not very useful... but it was an attempt at increasing the Welcome page's loading time.
 const LazyMain = React.lazy(() => import("./Main"));
 
@@ -11,11 +12,7 @@ export const NewMain = () => {
         <Welcome />
       </Route>
       <Route>
-        <Suspense
-          fallback={
-            "Loading... (damn, can you really see this? either your internet is slow or the server is dead)"
-          }
-        >
+        <Suspense fallback={LoadingPage}>
           <LazyMain />
         </Suspense>
       </Route>
