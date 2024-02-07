c1a788c3ac64cfc485682c1f8a5936efecce9ed3
DrSharky <keleblanc24@gmail.com>
2024-02-07 16:14:38 -0500

    Discord login (#1434)

--- routes/auth.js
+++ routes/auth.js
@@ -9,6 +9,61 @@ const models = require("../db/models");
 const fbServiceAccount = require("../" + process.env.FIREBASE_JSON_FILE);
 const logger = require("../modules/logging")(".");
 const router = express.Router();
+const passport = require("passport");
+////TESTING THIS IN PROD, DO NOT TOUCH
+const DiscordStrategy = require("passport-discord").Strategy;
+////TESTING THIS IN PROD, DO NOT TOUCH
+
+////TESTING THIS IN PROD, DO NOT TOUCH
+passport.use(new DiscordStrategy({
+  clientID: process.env.DISCORD_CLIENT_ID,
+  clientSecret: process.env.DISCORD_CLIENT_SECRET,
+  callbackURL: "https://ultimafia.com/auth/discord/redirect",
+  scope: ["identify", "email"]
+}, async (accessToken, refreshToken, profile, done) => {
+  try {
+    if (profile) {
+      if (profile.email) {
+        done(null, profile);
+      }
+    }
+    let user = await models.User.findOne({ discordId: profile.id });
+      if (user) {
+        console.log("User exists");
+        await user.updateOne({ discordUsername: `${profile.username}`, discordName: `${profile.global_name}`, email: `${profile.email}` });
+        done(null, user);
+      }
+      user = await models.User.findOne({ email: profile.email });
+      if (user) {
+        await user.updateOne({ discordId: `${profile.id}`, discordUsername: `${profile.username}`, discordName: `${profile.global_name}` });
+        done(null, user);
+      }
+      else {
+        const newUser = await models.User.create({
+          discordId: profile.id,
+          username: profile.username,
+          global_name: profile.global_name,
+          email: profile.email
+        });
+        const savedUser = await newUser.save();
+        done(null, savedUser);
+      }
+  }
+  catch (err) {
+    console.log(err);
+    done(err, null);
+  }
+}));
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  // done(null, user);
+});
+////TESTING THIS IN PROD, DO NOT TOUCH
+
 
 const allowedEmailDomans = JSON.parse(process.env.EMAIL_DOMAINS);
 
@@ -19,17 +74,28 @@ credential: fbAdmin.credential.cert(fbServiceAccount),
 router.post("/", async function (req, res) {
   try {
     var idToken = String(req.body.idToken);
-    var userData = await fbAdmin.auth().verifyIdToken(idToken);
-    var verified = userData.email_verified;
+    if (idToken) {
+      var userData = await fbAdmin.auth().verifyIdToken(idToken);
+      var verified = userData.email_verified;
 
-    if (verified) {
-      await authSuccess(req, userData.uid, userData.email);
-      res.sendStatus(200);
-    } else {
-      res.status(403);
-      res.send(
-        "Please verify your email address before logging in. Be sure to check your spam folder."
-      );
+      if (verified) {
+        await authSuccess(req, userData.uid, userData.email);
+        res.sendStatus(200);
+      } else {
+        res.status(403);
+        res.send(
+          "Please verify your email address before logging in. Be sure to check your spam folder."
+        );
+      }
+    }
+    else {
+      if (req.body.discordProfile) {
+        await authSuccess(req, null, req.body.email, req.body.discordProfile);
+        res.sendStatus(200);
+      }
+      else {
+        res.sendStatus(403);
+      }
     }
   } catch (e) {
     logger.error(e);
@@ -37,6 +103,19 @@ router.post("/", async function (req, res) {
     res.send("Error authenticating.");
   }
 });
+
+////TESTING THIS IN PROD, DO NOT TOUCH
+router.get("/discord", passport.authenticate("discord"));
+
+router.get("/discord/redirect", passport.authenticate("discord", 
+{
+  failureRedirect: "https://ultimafia.com",
+  successRedirect: "https://ultimafia.com/play"
+}), async (req, res) => {
+  await authSuccess(req, null, discordUser.email, discordUser);
+});
+////TESTING THIS IN PROD, DO NOT TOUCH
+
 
 router.post("/verifyCaptcha", async function (req, res) {
   try {
@@ -67,7 +146,7 @@ router.post("/verifyCaptcha", async function (req, res) {
   }
 });
 
-async function authSuccess(req, uid, email) {
+async function authSuccess(req, uid, email, discordProfile) {
   try {
     /* *** Scenarios ***
             - Signed in
@@ -114,6 +193,11 @@ async function authSuccess(req, uid, email) {
         joined: Date.now(),
         lastActive: Date.now(),
         ip: [ip],
+        ////TESTING THIS IN PROD, DO NOT TOUCH
+        discordId: discordProfile?.id,
+        discordUsername: discordProfile?.username,
+        discordName: discordProfile?.global_name,
+        ////TESTING THIS IN PROD, DO NOT TOUCH
       });
 
       if (process.env.NODE_ENV.includes("development")) {
@@ -230,6 +314,19 @@ async function authSuccess(req, uid, email) {
       }
 
       await models.User.updateOne({ id: id }, { $addToSet: { ip: ip } });
+
+      ////TESTING THIS IN PROD, DO NOT TOUCH
+      // Link Discord profile if logging in with Discord.
+      if (discordProfile) {
+        await models.User.updateOne({ id: id }, { $set: 
+          {
+            discordId: discordProfile.id,
+            discordUsername: discordProfile.username,
+            discordName: discordProfile.global_name
+          } });
+      }
+      ////TESTING THIS IN PROD, DO NOT TOUCH
+
     }
 
     req.session.user = {
