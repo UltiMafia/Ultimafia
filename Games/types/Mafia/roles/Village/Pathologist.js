0f0c99e2e9696ca4a076ce021dd31f1222d2f474
Golbolco <Golbolco@gmail.com>
2024-03-28 14:39:18 -0400

    fix: patho bug

--- Games/types/Mafia/roles/Village/Pathologist.js
+++ Games/types/Mafia/roles/Village/Pathologist.js
@@ -4,9 +4,9 @@ module.exports = class Pathologist extends Role {
   constructor(player, data) {
     super("Pathologist", player, data);
     this.alignment = "Village";
-    this.cards = ["VillageCore", "WinWithVillage", "ReceiveAllVisitors"];
+    this.cards = ["VillageCore", "WinWithVillage", "ReceiveAllReports"];
     this.meetingMods = {
-      ReceiveAllVisitors: {
+      ReceiveAllReports: {
         actionName: "Autopsy",
       },
     };
