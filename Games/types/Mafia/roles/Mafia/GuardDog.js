ac48bea69325d7eddcb3cb3ec13cc3f11799dda3
ammico <ammygrace@gmail.com>
2024-04-02 23:20:18 -0700

    icon overhaul 120-145 (#1467)
    Co-authored-by: RH <104711111+R9H9@users.noreply.github.com>

--- Games/types/Mafia/roles/Mafia/GuardDog.js
+++ Games/types/Mafia/roles/Mafia/GuardDog.js
@@ -1,8 +1,8 @@
 const Role = require("../../Role");
 
-module.exports = class Rottweiler extends Role {
+module.exports = class GuardDog extends Role {
   constructor(player, data) {
-    super("Rottweiler", player, data);
+    super("Guard Dog", player, data);
 
     this.alignment = "Mafia";
     this.cards = [
