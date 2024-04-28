048b46e91ec1451089e60294987668344dae8398
Golbolco <Golbolco@gmail.com>
2024-04-28 18:19:02 -0400

    feat: reverted warlock code

--- Games/types/Mafia/roles/cards/Warlockracy.js
+++ Games/types/Mafia/roles/cards/Warlockracy.js
@@ -1,45 +1,32 @@
 const Card = require("../../Card");
-const { PRIORITY_NIGHT_SAVER } = require("../../const/Priority");
 
 module.exports = class Warlockracy extends Card {
   constructor(role) {
     super(role);
 
     this.meetings = {
-      "Break Oath": {
-        actionName: "Break Oath",
+      "Predict Vote": {
         states: ["Night"],
         flags: ["voting"],
         action: {
-          labels: ["save"],
-          priority: PRIORITY_NIGHT_SAVER,
           run: function () {
-            this.actor.role.savedPlayer = this.target;
-
-            // power 5, lifespan 1
-            this.target.giveEffect("CondemnImmune", 5, 1);
+            this.actor.role.predictedVote = this.target;
           },
         },
       },
     };
 
     this.listeners = {
-      state: function () {
-        if (this.game.getStateName() == "Night") {
-          delete this.savedPlayer;
-        }
-      },
-      immune: function (action, player) {
-        if (player != this.savedPlayer) {
+      state: function (stateInfo) {
+        if (!this.player.alive) {
           return;
         }
 
-        if (action.hasLabel("condemn")) {
-          this.player.queueAlert(
-            `You make a pact with ${this.savedPlayer.name} at the gallows and induct them into your coven.`
-          );
-          this.savedPlayer.setRole("Cultist");
+        if (!stateInfo.name.match(/Night/)) {
+          return;
         }
+
+        delete this.predictedVote;
       },
     };
   }
