e48db32ffb8dfa8f79224f365daee1c8f089095d
Golbolco <Golbolco@gmail.com>
2024-06-26 09:15:17 -0400

    Revert "mUI components applied to all pages save for user profiles" (#1551)
    Reverts UltiMafia/Ultimafia#1550

--- react_main/src/pages/Community/Forums/Board.jsx
+++ react_main/src/pages/Community/Forums/Board.jsx
@@ -1,22 +1,11 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Redirect, useParams, Link } from "react-router-dom";
 import axios from "axios";
-import { useTheme } from "@mui/styles";
-import {
-  Container,
-  Box,
-  Typography,
-  Button,
-  Grid,
-  IconButton,
-  Paper,
-  Modal,
-  TextField,
-  Divider,
-} from "@mui/material";
+
 import { useErrorAlert } from "../../../components/Alerts";
 import { getPageNavFilterArg, PageNav } from "../../../components/Nav";
 import { NameWithAvatar } from "../../User/User";
+import { Modal } from "../../../components/Modal";
 import { VoteWidget, ViewsAndReplies } from "./Forums";
 import { TextEditor } from "../../../components/Form";
 import { Time } from "../../../components/Basic";
@@ -30,16 +19,16 @@ export default function Board(props) {
   const [newThreadContent, setNewThreadContent] = useState("");
   const [boardPage, setBoardPage] = useState(1);
   const sortType = "bumpDate";
+  // const [sortType, setSortType] = useState("bumpDate");
   const [loaded, setLoaded] = useState(false);
   const [redirect, setRedirect] = useState();
 
   const { boardId } = useParams();
   const user = useContext(UserContext);
   const errorAlert = useErrorAlert();
-  const theme = useTheme();
 
   useEffect(() => {
-    document.title = "Board | UltiMafia";
+    document.title = "Create Mafia Setup | UltiMafia";
   }, []);
 
   useEffect(() => {
@@ -48,7 +37,9 @@ export default function Board(props) {
       .then((res) => {
         setBoardInfo(res.data);
         setLoaded(true);
+
         document.title = `${res.data.name} | UltiMafia`;
+
         props.updateForumNavInfo({
           type: "board",
           id: boardId,
@@ -76,7 +67,13 @@ export default function Board(props) {
   }
 
   function onBoardPageNav(page) {
-    var filterArg = getPageNavFilterArg(page, boardPage, boardInfo.threads, sortType);
+    var filterArg = getPageNavFilterArg(
+      page,
+      boardPage,
+      boardInfo.threads,
+      sortType
+    );
+
     if (filterArg == null) return;
 
     axios
@@ -92,54 +89,93 @@ export default function Board(props) {
 
   function threadRowsMap(thread) {
     const recentReplies = thread.recentReplies.map((reply) => (
-      <Box key={reply.id} display="flex" alignItems="center">
-        <NameWithAvatar small id={reply.author.id} name={reply.author.name} avatar={reply.author.avatar} />
-        <Link to={`/community/forums/thread/${thread.id}?reply=${reply.id}`} style={{ marginLeft: theme.spacing(1) }}>
-          <Time millisec={Date.now() - reply.postDate} /> ago
+      <div className="column-item" key={reply.id}>
+        <NameWithAvatar
+          small
+          id={reply.author.id}
+          name={reply.author.name}
+          avatar={reply.author.avatar}
+        />
+        <Link
+          className="reply-age"
+          to={`/community/forums/thread/${thread.id}?reply=${reply.id}`}
+        >
+          <Time millisec={Date.now() - reply.postDate} />
+          {` ago`}
         </Link>
-      </Box>
+      </div>
     ));
 
     return (
-      <Paper key={thread.id} style={{ padding: theme.spacing(2), marginBottom: theme.spacing(2) }}>
-        <Box display="flex" alignItems="center" mb={2}>
-          <VoteWidget item={thread} itemType="thread" itemHolder={boardInfo} setItemHolder={setBoardInfo} itemKey="threads" />
-          <Box ml={2} flexGrow={1}>
-            <Link to={`/community/forums/thread/${thread.id}`}>
+      <div
+        className={`thread ${thread.deleted ? "deleted" : ""}`}
+        key={thread.id}
+      >
+        <VoteWidget
+          item={thread}
+          itemType="thread"
+          itemHolder={boardInfo}
+          setItemHolder={setBoardInfo}
+          itemKey="threads"
+        />
+        <div className="thread-info">
+          <Link
+            className="thread-title"
+            to={`/community/forums/thread/${thread.id}`}
+          >
             {thread.locked && <i className="fas fa-lock" />}
             {thread.pinned && <i className="fas fa-thumbtack" />}
-              {thread.title}
-            </Link>
-            <NameWithAvatar small id={thread.author.id} avatar={thread.author.avatar} name={thread.author.name} groups={thread.author.groups} />
-            <ViewsAndReplies viewCount={thread.viewCount || 0} replyCount={thread.replyCount || 0} />
-          </Box>
-        </Box>
-        <Grid container spacing={2}>
-          <Grid item xs={12} sm={6}>
-            <Typography variant="subtitle2">Post Date</Typography>
-            <Typography>{<Time millisec={Date.now() - thread.postDate} />} ago</Typography>
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <Typography variant="subtitle2">Recent Replies</Typography>
-            {recentReplies.length === 0 ? (
-              <Typography>No replies yet</Typography>
-            ) : (
-              recentReplies
+            {thread.title}
+          </Link>
+          <NameWithAvatar
+            small
+            id={thread.author.id}
+            avatar={thread.author.avatar}
+            name={thread.author.name}
+            groups={thread.author.groups}
+          />
+          <div className="counts">
+            <ViewsAndReplies
+              viewCount={thread.viewCount || 0}
+              replyCount={thread.replyCount || 0}
+            />
+          </div>
+        </div>
+        <div className="forum-column">
+          <div className="column-title">Post Date</div>
+          <div className="column-content">
+            <div className="column-item center-item">
+              <Time millisec={Date.now() - thread.postDate} />
+              {` ago`}
+            </div>
+          </div>
+        </div>
+        <div className="forum-column three">
+          <div className="column-title">Recent Replies</div>
+          <div
+            className={`column-content ${
+              recentReplies.length === 0 ? "center-content" : ""
+            }`}
+          >
+            {recentReplies.length === 0 && (
+              <div className="column-item center-item">No replies yet</div>
             )}
-          </Grid>
-        </Grid>
-      </Paper>
+            {recentReplies}
+          </div>
+        </div>
+      </div>
     );
   }
 
   if (redirect) return <Redirect to={redirect} />;
+
   if (!loaded) return <NewLoading small />;
 
   const threads = boardInfo.threads.map(threadRowsMap);
   const pinnedThreads = boardInfo.pinnedThreads.map(threadRowsMap);
 
   return (
-    <Container maxWidth="md" style={{ marginTop: theme.spacing(4) }}>
+    <div className="board-wrapper">
       <CreateThreadModal
         boardId={boardId}
         show={showCreateModal}
@@ -150,43 +186,70 @@ export default function Board(props) {
         setThreadContent={setNewThreadContent}
         setRedirect={setRedirect}
       />
-      <Box display="flex" justifyContent="space-between" alignItems="center" mb={4}>
-        <Box display="flex" alignItems="center">
-          <IconButton>
-            <i className={`fas fa-${boardInfo.icon || "comments"}`} />
-          </IconButton>
-          <Typography variant="h5" style={{ marginLeft: theme.spacing(2) }}>
-            {boardInfo.name}
-          </Typography>
-        </Box>
-        <Button variant="contained" color="primary" onClick={onCreateThreadClick} disabled={!user.perms.createThread}>
+      <div className="board-info">
+        <div className="board-title-wrapper">
+          <i className={`fas fa-${boardInfo.icon || "comments"} board-icon`} />
+          <div className="board-title">{boardInfo.name}</div>
+        </div>
+        <div
+          className="create-thread btn btn-theme"
+          onClick={onCreateThreadClick}
+          disabled={!user.perms.createThread}
+        >
           <i className="fas fa-plus" />
           Create Thread
-        </Button>
-      </Box>
+        </div>
+      </div>
       {pinnedThreads.length > 0 && (
-        <Box mb={4}>
-          <Typography variant="h6">Pinned Threads</Typography>
-          <Divider />
-          {pinnedThreads}
-        </Box>
+        <div className="threads pinned-threads span-panel">{pinnedThreads}</div>
       )}
       <PageNav inverted page={boardPage} onNav={onBoardPageNav} />
-      <Box mb={4}>
-        {threads.length > 0 ? threads : <Typography>No threads yet</Typography>}
-      </Box>
+      <div className="threads span-panel">
+        {threads.length > 0 && threads}
+        {threads.length === 0 && "No threads yet"}
+      </div>
       <PageNav inverted page={boardPage} onNav={onBoardPageNav} />
-    </Container>
+    </div>
   );
 }
 
 function CreateThreadModal(props) {
   const errorAlert = useErrorAlert();
-  const theme = useTheme();
+  const header = "Create Thread";
 
-  const onTitleChange = (e) => props.setThreadTitle(e.target.value);
-  const onCancel = () => props.setShow(false);
-  const onPostThread = () => {
+  const content = (
+    <div className="form">
+      <div className="field-wrapper thread-title">
+        <div className="label">Title</div>
+        <input type="text" value={props.threadTitle} onChange={onTitleChange} />
+      </div>
+      <TextEditor
+        value={props.threadContent}
+        onChange={props.setThreadContent}
+      />
+    </div>
+  );
+
+  const footer = (
+    <div className="control">
+      <div className="post btn btn-theme" onClick={onPostThread}>
+        Post
+      </div>
+      <div className="cancel btn btn-theme-third" onClick={onCancel}>
+        Cancel
+      </div>
+    </div>
+  );
+
+  function onTitleChange(e) {
+    props.setThreadTitle(e.target.value);
+  }
+
+  function onCancel() {
+    props.setShow(false);
+  }
+
+  function onPostThread() {
     axios
       .post("/forums/thread", {
         board: props.boardId,
@@ -198,32 +261,16 @@ function CreateThreadModal(props) {
         props.setRedirect(`/community/forums/thread/${res.data}`);
       })
       .catch(errorAlert);
-  };
+  }
 
   return (
-    <Modal open={props.show} onClose={onCancel}>
-      <Paper style={{ padding: theme.spacing(4), maxWidth: "500px", margin: "auto" }}>
-        <Typography variant="h6">Create Thread</Typography>
-        <Box mt={2}>
-          <TextField
-            label="Title"
-            fullWidth
-            value={props.threadTitle}
-            onChange={onTitleChange}
-          />
-        </Box>
-        <Box mt={2}>
-          <TextEditor value={props.threadContent} onChange={props.setThreadContent} />
-        </Box>
-        <Box display="flex" justifyContent="flex-end" mt={2}>
-          <Button onClick={onCancel} style={{ marginRight: theme.spacing(2) }}>
-            Cancel
-          </Button>
-          <Button variant="contained" color="primary" onClick={onPostThread}>
-            Post
-          </Button>
-        </Box>
-      </Paper>
-    </Modal>
+    <Modal
+      className="create-thread"
+      show={props.show}
+      onBgClick={onCancel}
+      header={header}
+      content={content}
+      footer={footer}
+    />
   );
 }
