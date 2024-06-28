7534832db4785792ce2384d0ff2d73519299dc43
ruthless <frailxup@gmail.com>
2024-06-28 20:53:38 +0000

    Revert "fix: another attempt at fixing the report bot"
    This reverts commit 9874ad9898fe9c86712f1a7b19c3bef3ba8f4126.

--- routes/report.js
+++ routes/report.js
@@ -24,30 +24,29 @@ router.post("/send", async function (req, res) {
       !report ||
       report.length < 15 
     ) {
-      // Send a 400 error code if the report contents don't meet our requirements
+      // Should send a 400 error code if the report contents doesn't meet our requirements
       res.status(400).send("ERROR: Report title and evidence must be greater than 5 characters. Description must be greater than 15 characters.");
       return;
     }
 
-    let ping = "<@&1107343293848768622>";
-    let title = `[${user.name}] reporting ${reportTitle}`;
+    let ping = "<@&1107343293848768622>\n";
+    let title = `[${user.name}] reporting ${req.body.title}`;
 
     // Ensures the report goes to mod chat
     let webhookURL = 'https://discord.com/api/webhooks/1255571211950489662/tUAchIDAs1gQ6MU6F0Vfp5vyojPTHcaEu_JK6xZdHMKUzCenX6CMbBoqPDUFbaNC4_Wk';
 
-    // Constructs a dynamic message with the report content to send to Discord
-    let messageContent = `${ping}\n${title} for ${reportEvidence}\n**Description:** ${report}`;
+    // Constructs a dynamic message with the report content to send to discord
     await axios({
       method: "POST",
       url: process.env.DISCORD_GAME_HOOK,
       data: {
-        content: messageContent,
+        content: `${ping} ${title} for ${reportEvidence}. "${report}"`,
         username: "SnitchBot",
       },
     });
-
     // Confirms the report has been successfully sent
     res.status(200).send("Report has been sent to mod chat!");
+    // Handles an error with sending the report
   } catch (e) {
     logger.error(e);
     res.status(500);
