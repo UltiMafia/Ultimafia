1977fabf9d206e8ea312a797f670d54932863501
Golbolco <Golbolco@gmail.com>
2024-07-03 23:24:25 +0000

    chore: first pass at donors data

---
+++ data/donors.js
@@ -0,0 +1,15 @@
+const donors = [
+    "Eudia",
+    "godswife",
+    "Arials",
+    "PizzaEnjoyer",
+    "Ego",
+    "MetalAf",
+  ];
+  
+  const donorData = {
+    donor: donors,
+  };
+  
+  module.exports = donorData;
+  
