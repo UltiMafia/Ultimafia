8325e0fd8850baed2b37888f2f5965272622966b
NouSixXD <nousixgh@protonmail.com>
2024-07-03 21:00:32 +0200

    commie fix (#1572)
    Co-authored-by: Golbolco <Golbolco@gmail.com>

--- Games/types/Mafia/roles/cards/WinIfAllVanilla.js
+++ Games/types/Mafia/roles/cards/WinIfAllVanilla.js
@@ -12,10 +12,10 @@ module.exports = class WinIfAllVanilla extends Card {
 
         for (let player of this.game.players) {
           if (
-            (player.alive && player.role.name != "Villager") ||
-            player.role.name != "Mafioso" ||
-            player.role.name != "Cultist" ||
-            player.role.name != "Grouch" ||
+            player.alive && player.role.name != "Villager" &&
+            player.role.name != "Mafioso" &&
+            player.role.name != "Cultist" &&
+            player.role.name != "Grouch" &&
             player.role.name != "Communist"
           ) {
             return;
