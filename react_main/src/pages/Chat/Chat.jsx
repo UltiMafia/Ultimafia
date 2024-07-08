4db643249525b7178a0ba83940e01bae021ba18a
melod-e <91733517+melodic-e@users.noreply.github.com>
2024-07-08 11:29:19 +1000

    debug (games): height must be explicitly set for #root (#1586)
    ## How the issue occurred
    
    While fixing the frontend code, it was observed that `height` was set
    explicitly across several elements where it was not necessary.
    
    Given that the codebase needs to be optimised, I went ahead and removed
    all instances of explicitly set `height` properties that did not appear
    to be necessary. This was problematic.
    
    There is likely inner logic in the games components where the main chat
    area inherits the height through a chain that leads to the `#root`
    element.

--- react_main/src/pages/Chat/Chat.jsx
+++ react_main/src/pages/Chat/Chat.jsx
@@ -12,6 +12,7 @@ import update from "immutability-helper";
 import { ClientSocket as Socket } from "../../Socket";
 import { useErrorAlert } from "../../components/Alerts";
 import { NameWithAvatar, StatusIcon } from "../User/User";
+import UserNotifications from "../User/UserNotifications";
 import { UserContext } from "../../Contexts";
 import { MaxChatMessageLength } from "../../Constants";
 import { Time, UserText } from "../../components/Basic";
@@ -355,12 +356,18 @@ export default function Chat({ setShowChatTab }) {
         className="chat-wrapper"
         notifCount={chatInfo.notifs.all}
       >
-        <div className="top-bar" onClick={onTopBarClick}>
-          <i
-            className="fas fa-comment"
-            style={{ marginLeft: "20px", marginRight: "10px" }}
-          />
-          <span className="label">Chat</span>
+        <div className="top-bar">
+          {/* <UserNotifications 
+            user={user}
+          /> */}
+          {/* testMafia */}
+          <div style={{flexGrow: 1}} onClick={onTopBarClick}>
+            <i
+              className="fas fa-comment"
+              style={{ marginLeft: "20px", marginRight: "10px" }}
+            />
+            <span className="label">Chat</span>
+          </div>
           <i
             className="fas fa-times chat-close-btn"
             onClick={() => closeChatTab()}
