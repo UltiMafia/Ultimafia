372954ee5b9d751719626bc5fc64d879bbb2da5a
Golbolco <Golbolco@gmail.com>
2024-07-09 21:35:33 +0000

    chore: terminology page almost done

--- react_main/src/constants/commandList.js
+++ react_main/src/constants/commandList.js
@@ -1,33 +1,28 @@
 // TODO / note: slang containing spaces currently does not work (although this list may contain it)
 
 export const commandList = {
-  kick: {
+  "/kick": {
     input: "Username",
     description:
-      "Kicklim (Kick-condemn). condemning a player which has not voted yet (in order to maintain the game ranked).",
+      "Used during pregame to temporarily remove someone from a game lobby.",
   },
-  ban: {
+  "/ban": {
     input: "Username",
     description:
-      "Away From Keyboard: when a player stop taking part in a discussion in a chat room for a short time. This can be scummy sometimes.",
+      "Used during pregame to permanently remove someone from a game lobby.",
   },
-  me: {
+  "/me": {
     input: "Text",
-    description: "Alternate Account.",
+    description: "Used to speak in italics as if roleplaying.",
   },
-  extend: {
+  "/extend": {
     input: "N/A",
     description:
-      "Bandwagon. Occurs when someone decides to vote someone during the day phase as their condemn target, and the whole town follows, without having substantial evidence.",
+      "Used during the day phase to give more time to players while deliberating on the vote.",
   },
-  role: {
+  "/role": {
     input: "Role name",
     description:
-      "A Blitz: a rapid vote, usually the hammer, as by the time the blitz is noticed, the mafia has already won or it is too late. It is usually employed by the mafia in MYLO or LYLO situations to quickly condemn someone if an inattentive town has a stray vote, and the mafia has the hammer to themselves.",
-  },
-  hack: {
-    input: "N/A",
-    description:
-      "Bussing: when a mafia member backstabs a fellow mafia to get the trust of the villagers, usually by supporting (and possibly even leading) a condemn against their fellow mafia.",
+      "Used to display role descriptions in chat.",
   },
 };
--- react_main/src/pages/Learn/Terminology.jsx
+++ react_main/src/pages/Learn/Terminology.jsx
@@ -17,11 +17,56 @@ import {
 import { useTheme } from '@mui/styles';
 
 import { slangList } from "../../constants/slangList";
+import { commandList } from "../../constants/commandList";
 
 export default function Terminology(props) {
     const theme = useTheme();
 
-const slangTableRows = Object.keys(slangList).map((key) => {
+    const commandTableRows = Object.keys(commandList).map((key) => {
+        let { input, description } = commandList[key];
+    
+        return {
+          term: key,
+          input,
+          description,
+        };
+      });
+
+    const commandTable = (
+      <TableContainer component={Paper}>
+        <Table aria-label="a dense table">
+          <TableHead>
+            <TableRow>
+              <TableCell>
+                Command
+              </TableCell>
+              <TableCell>
+                Input
+              </TableCell>
+              <TableCell>
+                Description
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {commandTableRows.map((row) => (
+              <TableRow
+                key={row.name}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row" align="center">
+                  {row.term}
+                </TableCell>
+                <TableCell align="center">{row.input}</TableCell>
+                <TableCell align="center">{row.description}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    );
+
+    const slangTableRows = Object.keys(slangList).map((key) => {
     let { definition, emoji } = slangList[key];
     if (Array.isArray(emoji)) {
       emoji = emoji.join(", ");
@@ -70,15 +115,29 @@ const slangTableRows = Object.keys(slangList).map((key) => {
 
 return (
     <>
+  <Typography variant="h4" gutterBottom>Terminology</Typography>
+  <Typography variant="body1" paragraph>
+      The game features a comprehensive list of terms and chat commands. Understanding these commands and terms is crucial for mastering the game's mechanics and strategies.
+      To improve the definitions or add new ones, we encourage you to get involved through our Discord or Github Repo.
+  </Typography>
   <Accordion>
   <AccordionSummary>
-    <Typography variant="h6">Terminology</Typography>
+    <Typography variant="h6">Commands</Typography>
   </AccordionSummary>
   <AccordionDetails>
     <Typography paragraph>
-      The game features a comprehensive list of terms that are automatically detected during gameplay. Understanding these terms is crucial for mastering the game's mechanics and strategies.
-      To improve the definitions or contribute to the emoji pool, we encourage you to get involved through our Discord or Github Repo.
-      Your contributions are invaluable in keeping the terminology current and engaging.
+      Below is a list of chat commands able to be used in games.
+    </Typography>
+    <Box className="paragraph">{commandTable}</Box>
+  </AccordionDetails>
+  </Accordion>
+  <Accordion>
+  <AccordionSummary>
+    <Typography variant="h6">Slang</Typography>
+  </AccordionSummary>
+  <AccordionDetails>
+    <Typography paragraph>
+      Below is a list of terms that are automatically detected during gameplay.
       Players can choose to toggle the visibility of these terms through their profile settings.
     </Typography>
     <Box className="paragraph">{slangTable}</Box>
