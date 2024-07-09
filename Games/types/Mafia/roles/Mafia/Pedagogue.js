bf6059ff0d143de91ce820cd71101d63ca1b08c2
Golbolco <Golbolco@gmail.com>
2024-07-09 07:20:32 -0400

    chore: quickfix

--- Games/types/Mafia/roles/Mafia/Pedagogue.js
+++ Games/types/Mafia/roles/Mafia/Pedagogue.js
@@ -1,8 +1,8 @@
 const Role = require("../../Role");
 
-module.exports = class Prizefighter extends Role {
+module.exports = class Pedagogue extends Role {
   constructor(player, data) {
-    super("Prizefighter", player, data);
+    super("Pedagogue", player, data);
 
     this.alignment = "Mafia";
     this.cards = [
