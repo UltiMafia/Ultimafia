c3ad75981c5cbfcc22a47c771768465e416c3426
SawJester <anglelord270@gmail.com>
2024-07-10 19:36:44 -0400

    Create LearnOneOfTwoPlayersIsEvil.js

---
+++ Games/types/Mafia/roles/cards/LearnOneOfTwoPlayersIsEvil.js
@@ -0,0 +1,40 @@
+const Card = require("../../Card");
+const Random = require("../../../../../lib/Random");
+const { PRIORITY_INVESTIGATIVE_DEFAULT } = require("../../const/Priority");
+
+module.exports = class LearnOneOfTwoPlayersIsEvil extends Card {
+  constructor(role) {
+    super(role);
+
+    this.actions = [
+      {
+        priority: PRIORITY_INVESTIGATIVE_DEFAULT,
+        run: function () {
+          if (!this.actor.alive) return;
+          if (this.game.getStateName() != "Night") return;
+          if(this.actor.role.hasInfo) return;
+
+          
+          var alive = this.game.players.filter((p) => p.alive && p != this.actor);
+          var townPlayers = alive.filter((p) => this.game.getRoleAlignment(p.getRoleAppearance().split(" (")[0]) == "Cult" || this.game.getRoleAlignment(p.getRoleAppearance().split(" (")[0]) == "Mafia");
+
+          if(townPlayers.length == 0){
+             this.actor.queueAlert(` You learn that no Cult or Mafia are present.`);
+          }
+          else{
+            const chosenTown = Random.randArrayVal(townPlayers);
+            var aliveRemoveTarget = alive.filter((p) => p != chosenTown);
+            const chosenRandom = Random.randArrayVal(aliveRemoveTarget);
+            let chosenRole = chosenTown.getRoleAppearance();
+
+            let chosenNames = [chosenTown,chosenRandom];
+            let chosenNamesRan = Random.randomizeArray(chosenNames);
+
+            this.actor.queueAlert(` You learn that ${chosenNamesRan[0].name} or ${chosenNamesRan[1].name} is a ${chosenRole}.` );
+            this.actor.role.hasInfo = true;
+          }
+        },
+      },
+    ];
+  }
+};
