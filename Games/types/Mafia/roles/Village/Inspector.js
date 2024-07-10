48e7efa5cde46c2f58ac64d1aebe1be6a679c35e
SawJester <anglelord270@gmail.com>
2024-07-10 19:29:00 -0400

    Create Inspector.js

---
+++ Games/types/Mafia/roles/Village/Inspector.js
@@ -0,0 +1,10 @@
+const Role = require("../../Role");
+
+module.exports = class Inspector extends Role {
+  constructor(player, data) {
+    super("Inspector", player, data);
+
+    this.alignment = "Village";
+    this.cards = ["VillageCore", "WinWithVillage", "LearnOneOfTwoPlayersIsTown"];
+  }
+};
