e91705dbfacd08815499dbaaf5352564e53e7b95
NaShBe <37202417+NaShBe@users.noreply.github.com>
2024-07-10 20:32:11 +0000

    remove 'Block Name' functionality

--- db/schemas.js
+++ db/schemas.js
@@ -435,9 +435,6 @@ var schemas = {
     image: String,
     creator: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   }),
-  BlockedName: new mongoose.Schema({
-    name: { type: String, index: true },
-  }),
   Restart: new mongoose.Schema({
     when: Number,
   }),
