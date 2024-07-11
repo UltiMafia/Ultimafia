6ac1cf15d72cadd8d57f69f979fd3815a20b2c56
SawJester <anglelord270@gmail.com>
2024-07-11 18:24:18 -0400

    Create KillorPass.js

---
+++ Games/types/Mafia/roles/cards/KillorPass.js
@@ -0,0 +1,38 @@
+const Card = require("../../Card");
+const Random = require("../../../../../lib/Random");
+const { PRIORITY_KILL_DEFAULT } = require("../../const/Priority");
+
+module.exports = class KillorPass extends Card {
+  constructor(role) {
+    super(role);
+
+    this.meetings = {
+      Kill: {
+        actionName: "Kill",
+        states: ["Night"],
+        flags: ["voting"],
+        targets: { include: ["alive","self"]},
+        action: {
+          labels: ["kill"],
+          priority: PRIORITY_KILL_DEFAULT + 1,
+          run: function () {
+            let savers = this.getVisitors(this.target, "save");
+            if (savers.length == 0 && this.target === this.actor) {
+              var aliveTargets = this.game.players.filter((p) => p.alive && p != this.actor);
+              var cultTargets = aliveTargets.filter((p) => p.role.alignment == "Cult");
+              
+              if(cultTargets.length > 0){
+              const randomTarget = Random.randArrayVal(cultTargets);
+              randomTarget.setRole("Snallygaster", this.actor);
+              this.actor.kill("basic");
+              }
+            }
+            
+            if (this.dominates()) this.target.kill("basic", this.actor);
+
+          },
+        },
+      },
+    };
+  }
+};
