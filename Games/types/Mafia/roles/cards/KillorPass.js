714226e448e89714ccf492c26288d0fd3f4b72a9
SawJester <anglelord270@gmail.com>
2024-07-11 22:15:08 -0400

    Update KillorPass.js

--- Games/types/Mafia/roles/cards/KillorPass.js
+++ Games/types/Mafia/roles/cards/KillorPass.js
@@ -23,7 +23,7 @@ module.exports = class KillorPass extends Card {
               
               if(cultTargets.length > 0){
               const randomTarget = Random.randArrayVal(cultTargets);
-              randomTarget.setRole("Snallygaster", this.actor);
+                randomTarget.setRole(${this.player.role.name}:${this.player.role.modifier}`,this.player.role.data);
               this.actor.kill("basic");
               }
             }
