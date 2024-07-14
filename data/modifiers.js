2e2baeb9009154b88884ef0fbd4bd15f94ad10fa
SawJester <SawJester@users.noreply.github.com>
2024-07-14 08:59:45 +0000

    Prettified Code!

--- data/modifiers.js
+++ data/modifiers.js
@@ -353,7 +353,8 @@ const modifierData = {
     },
     Bouncy: {
       internal: ["Bouncy"],
-      description: "If possible kills will redirected to another player of the same alignment.",
+      description:
+        "If possible kills will redirected to another player of the same alignment.",
     },
   },
   "Split Decision": {},
