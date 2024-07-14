35a5a610561c42c81dc1f146d4a294bd330d3362
SawJester <SawJester@users.noreply.github.com>
2024-07-14 19:35:35 +0000

    Prettified Code!

--- Games/types/Mafia/roles/cards/Bouncy.js
+++ Games/types/Mafia/roles/cards/Bouncy.js
@@ -8,28 +8,26 @@ module.exports = class Bouncy extends Card {
 
     this.actions = [
       {
-          labels: ["redirect"],
-          priority: PRIORITY_MODIFY_ACTION,
-          run: function () {
-
+        labels: ["redirect"],
+        priority: PRIORITY_MODIFY_ACTION,
+        run: function () {
           if (this.game.getStateName() != "Night") return;
           if (!this.actor.alive) return;
-              var alive = this.game.players.filter(
-                (p) =>
-                  p.alive &&
-                  p != this.actor &&
-                  p.role.alignment == this.actor.role.alignment
-              );
-              if (alive.length > 0) {
-                var randomTarget = Random.randArrayVal(alive);
-                for (const action of this.game.actions[0]) {
-                  if (action.target === this.actor && action.hasLabel("kill")) {
-                    action.target = randomTarget;
-                  }
-                }
+          var alive = this.game.players.filter(
+            (p) =>
+              p.alive &&
+              p != this.actor &&
+              p.role.alignment == this.actor.role.alignment
+          );
+          if (alive.length > 0) {
+            var randomTarget = Random.randArrayVal(alive);
+            for (const action of this.game.actions[0]) {
+              if (action.target === this.actor && action.hasLabel("kill")) {
+                action.target = randomTarget;
               }
-            
-          },
+            }
+          }
+        },
       },
     ];
   }
