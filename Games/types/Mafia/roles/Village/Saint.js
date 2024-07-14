b348a408a27bad32ef49939ebc849d08d805a426
SawJester <anglelord270@gmail.com>
2024-07-13 21:23:13 -0400

    Update Saint.js

--- Games/types/Mafia/roles/Village/Saint.js
+++ Games/types/Mafia/roles/Village/Saint.js
@@ -2,7 +2,7 @@ const Role = require("../../Role");
 
 module.exports = class Saint extends Role {
   constructor(player, data) {
-    super("Accountant", player, data);
+    super("Saint", player, data);
 
     this.alignment = "Village";
     this.cards = ["VillageCore", "WinWithVillage", "KillAlignedOnCondemn"];
