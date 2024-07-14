94478812e37d2b76e5a100ae11d3d8bb718c9b7e
SawJester <SawJester@users.noreply.github.com>
2024-07-14 10:00:03 +0000

    Prettified Code!

--- Games/types/Mafia/roles/cards/Bouncy.js
+++ Games/types/Mafia/roles/cards/Bouncy.js
@@ -6,27 +6,31 @@ module.exports = class Bouncy extends Card {
   constructor(role) {
     super(role);
 
-    this.actions = [{
+    this.actions = [
+      {
         action: {
           labels: ["redirect"],
           priority: PRIORITY_MODIFY_ACTION,
           run: function () {
             if (this.dominates()) {
-               var alive = this.game.players.filter(
-            (p) => p.alive && p != this.actor && p.role.alignment == this.actor.role.alignment
-            );
-              if(alive.length > 0){
-              var randomTarget = Random.randArrayVal(alive);
-              for (const action of this.game.actions[0]) {
-                if (action.target === this.actor && action.hasLabel("kill")) {
-                  action.target = randomTarget;
+              var alive = this.game.players.filter(
+                (p) =>
+                  p.alive &&
+                  p != this.actor &&
+                  p.role.alignment == this.actor.role.alignment
+              );
+              if (alive.length > 0) {
+                var randomTarget = Random.randArrayVal(alive);
+                for (const action of this.game.actions[0]) {
+                  if (action.target === this.actor && action.hasLabel("kill")) {
+                    action.target = randomTarget;
+                  }
                 }
               }
             }
-            }
           },
         },
-    },
+      },
     ];
   }
 };
