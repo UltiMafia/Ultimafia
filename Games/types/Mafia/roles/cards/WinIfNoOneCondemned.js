bd761403c934b7c78de72b694c11792d98b6c188
SawJester <anglelord270@gmail.com>
2024-07-14 03:55:15 -0400

    Update MayorWin.js

--- Games/types/Mafia/roles/cards/WinIfNoOneCondemned.js
+++ Games/types/Mafia/roles/cards/WinIfNoOneCondemned.js
@@ -1,5 +1,5 @@
 const Card = require("../../Card");
-const { PRIORITY_INVESTIGATIVE_AFTER_RESOLVE_DEFAULT } = require("../../const/Priority");
+const { PRIORITY_DAY_EFFECT_DEFAULT } = require("../../const/Priority");
 
 module.exports = class WinIfNoOneCondemned extends Card {
   constructor(role) {
@@ -7,18 +7,28 @@ module.exports = class WinIfNoOneCondemned extends Card {
 
      this.actions = [
       {
-        priority: PRIORITY_INVESTIGATIVE_AFTER_RESOLVE_DEFAULT -3,
+        priority: PRIORITY_DAY_EFFECT_DEFAULT;,
         run: function () {
           if (!this.actor.alive) return;
           if (this.game.alivePlayers().length != 3 ){
-            this.actor.role.data.MayorWin = 0;
+            this.actor.role.data.MayorWin = false;
             return;
           };
-          if (this.game.getStateName() == "Night" && this.actor.role.data.MayorWin == 0){
+          if (this.game.getStateName() == "Day"){
+            let alivePlayers = this.game.alivePlayers();
+
+            for(let x = 0; x<this.game.alivePlayers().length;x++){
+            for (let action of this.game.actions[0]){
+            if (action.target == alivePlayers [x] && action.hasLabel("condemn"))
+              return;
+            }
+            }
+            /*
             this.actor.queueAlert(
               `Now that only 3 players are alive today, Town will win if no one is executed Today!`
             );
-            this.actor.role.data.MayorWin = 1;
+            */
+            this.actor.role.data.MayorWin = true;
             return;
           }
 
@@ -28,18 +38,17 @@ module.exports = class WinIfNoOneCondemned extends Card {
     ];
 
     this.listeners = {
-      
       death: function (player, killer, deathType) {
           if (this.game.alivePlayers().length != 3 || !this.player.alive || player == this.player){
             return;
           }
-          if(deathType == "condemn" || this.game.getStateName() != "Day"){
+          if(deathType == "condemn" || this.game.getStateName() != "Night"){
           return;
           }
             this.actor.queueAlert(
             `Now that only 3 players are alive today, Town will win if no one is executed Today!`
           );
-            this.actor.role.data.MayorWin = 2;
+            this.actor.role.data.MayorWin = true;
       },
     };
   }
