fec8bf2dc4e0bd8c8908bfb949d07eb54a05535d
SawJester <anglelord270@gmail.com>
2024-07-15 01:09:06 -0400

    Create RemoveModifers.js

---
+++ Games/types/Mafia/roles/cards/RemoveModifers.js
@@ -0,0 +1,32 @@
+const Card = require("../../Card");
+const { PRIORITY_CONVERT_DEFAULT } = require("../../const/Priority");
+
+module.exports = class RemoveModifers extends Card {
+  constructor(role) {
+    super(role);
+
+    this.meetings = {
+      "Purge Modifers": {
+        states: ["Night"],
+        flags: ["voting"],
+        action: {
+          labels: ["convert"],
+          priority: PRIORITY_CONVERT_DEFAULT,
+          run: function () {
+            if (!this.dominates()) {
+              return;
+            }
+
+            let currRoleName = this.target.role.name;
+            let currRoleModifier = this.target.role.modifier;
+            let currRoleData = this.target.role.data;
+
+
+            this.target.setRole(${currRoleName}`,currRoleData);
+            this.game.events.emit("roleAssigned", this.actor);
+          },
+        },
+      },
+    };
+  }
+};
