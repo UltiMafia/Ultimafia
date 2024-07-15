5522aa891e0a1978d79ff9711b6ca134263e80f7
SawJester <anglelord270@gmail.com>
2024-07-15 00:26:52 -0400

    Update IfVotedForceCondemn.js

--- Games/types/Mafia/roles/cards/IfVotedForceCondemn.js
+++ Games/types/Mafia/roles/cards/IfVotedForceCondemn.js
@@ -5,42 +5,23 @@ module.exports = class IfVotedForceCondemn extends Card {
   constructor(role) {
     super(role);
 
-        this.listeners = {
+
+
+      this.listeners = {
       vote: function (vote) {
-        if (
-          vote.meeting.name === "Village" &&
-          vote.target === this.player.id
-        ) {
+        if (vote.meeting.name === "Village" && vote.target === this.player.id) {
           if(this.player.role.data.hasBeenVoted) return;
-          player.role.data.hasBeenVoted = true;
+
+          this.player.role.data.hasBeenVoted = true;
+
           if(this.game.getRoleAlignment(vote.voter.getRoleAppearance().split(" (")[0]) != "Village"){
             return;
           }
-          let action = new Action({
-            actor: this.player,
-            target: vote.voter,
-            game: this.game,
-            labels: ["overthrow"],
-            priority: PRIORITY_OVERTHROW_VOTE,
-            run: function () {
-              if (this.dominates()){ 
-               //this.target.kill("curse", this.actor, true);
 
-              for (let action of this.game.actions[0]) {
-              if (action.hasLabel("condemn") && !action.hasLabel("overthrow")) {
-                if (action.target === this.target) {
-                  return;
-                }
-              }
-            }
-                for (const player of this.game.alivePlayers()) {
-                  player.giveEffect("CannotChangeVote", -1);
-                }
-              }
-            },
-          });
-
-          this.game.instantAction(action);
+          for (const player of this.game.alivePlayers()) {
+            player.giveEffect("CannotBeVoted", 1);
+          }
+          vote.voter.kill("condemn", this.player, true);
         }
       },
     };
