a8f339245c06a22a3fc0904807ea67b1d5988432
cjack04 <cjack366@students.kennesaw.edu>
2024-07-16 14:59:53 -0400

    commit (#1648)
    Co-authored-by: Golbolco <Golbolco@gmail.com>

---
+++ Games/types/Mafia/roles/cards/PGRevealRole.js
@@ -0,0 +1,27 @@
+const Card = require("../../Card");
+const { PRIORITY_REVEAL_DEFAULT } = require("../../const/Priority");
+
+module.exports = class RevealTargetOnDeath extends Card {
+  constructor(role) {
+    super(role);
+
+    this.meetings = {
+      "Photograph Player": {
+        actionName: "Photograph Player",
+        states: ["Night"],
+        flags: ["voting"],
+        action: {
+          priority: PRIORITY_REVEAL_DEFAULT,
+          run: function () {
+            if (this.dominates()) {
+              const displayRoleName = this.target.role.getDisplayName ? this.target.role.getDisplayName() : this.target.role.name;
+              const flavorText = `The Photographer has taken a picture of ${this.target.name}.`;
+              this.game.queueAlert(flavorText);
+              this.target.role.revealToAll();
+            }
+          },
+        },
+      },
+    };
+  }
+};
