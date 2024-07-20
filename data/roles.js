3dfd24b447382b3298e40176c3e818d1709d8a80
SawJester <anglelord270@gmail.com>
2024-07-20 09:44:39 -0400

    Update roles.js

--- data/roles.js
+++ data/roles.js
@@ -1838,6 +1838,16 @@ const roleData = {
         "If there is no Living Imp, All Cult-aligned players die.",
       ],
     },
+    Snallygaster: {
+      alignment: "Cult",
+      newlyAdded: true,
+      description: [
+        "Each night, may choose a player to kill.",
+        "If a Snallygaster chooses a player with a Banished Role, the Snallygaster dies and the Banished Player becomes a Snallygaster.",
+        "If there is no Living Snallygaster, All Cult-aligned players die.",
+        "Snallygaster adds 1 Banished role in closed setups.",
+      ],
+    },
     Devotee: {
       alignment: "Cult",
       newlyAdded: true,
