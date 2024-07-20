b62fa52bf8f9437b41b6b8b7ac4eddaba5e0dadb
SawJester <anglelord270@gmail.com>
2024-07-20 08:17:55 -0400

    Create BanishedRole.js

---
+++ Games/types/Mafia/roles/cards/BanishedRole.js
@@ -0,0 +1,9 @@
+onst Card = require("../../Card");
+const Random = require("../../../../../lib/Random");
+
+module.exports = class BanishedRole extends Card {
+  constructor(role) {
+    super(role);
+    this.role.data.banished = true;
+  }
+};
