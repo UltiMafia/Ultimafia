8c6a8bc31d09450ab201ebe74215982e9fec620b
Golbolco <Golbolco@gmail.com>
2024-07-20 07:49:48 -0400

    role rename

--- data/roles.js
+++ data/roles.js
@@ -1845,12 +1845,12 @@ const roleData = {
         "If a Cult role that kills the team on death dies, the Devotee will prevent those deaths and converts to that role.",
       ],
     },
-    Conjuror: {
+    Count: {
       alignment: "Cult",
       newlyAdded: true,
       description: [
         "Adds 2 Banished roles in Closed Setups.",
-        "If a Conjuror is created mid-game, 2 Village/Independant players will be converted to Banished Roles."
+        "If a Count is created mid-game, 2 Village/Independant players will be converted to Banished Roles."
       ],
     },
 
