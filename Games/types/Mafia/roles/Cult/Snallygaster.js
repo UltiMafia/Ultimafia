46d9d527f1557da6ee547864f929d343244dd50c
SawJester <anglelord270@gmail.com>
2024-07-20 08:00:21 -0400

    Create Snallygaster.js

---
+++ Games/types/Mafia/roles/Cult/Snallygaster.js
@@ -0,0 +1,16 @@
+const Role = require("../../Role");
+
+module.exports = class Snallygaster extends Role {
+  constructor(player, data) {
+    super("Snallygaster", player, data);
+
+    this.alignment = "Cult";
+    this.cards = [
+      "VillageCore",
+      "WinWithCult",
+      "MeetingCult",
+      "Add1Banished",
+      "KillOrBanishedJump",
+    ];
+  }
+};
