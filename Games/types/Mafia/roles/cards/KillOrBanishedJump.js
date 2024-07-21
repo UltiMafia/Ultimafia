ac81627e4e54c206897e7b3aabb1bc9d65d768dd
SawJester <anglelord270@gmail.com>
2024-07-21 17:13:16 -0400

    Added Snallygaster (Fang Gu from BOTC) and Nerfed Empath (#1662)
    Snallygaster adds 1 Banished role during closed setups. Snallygaster can
    kill a player each night. The first time a Snallygaster chooses to kill
    a Banished role. The Snallygaster dies and the Banished role becomes a
    new Snallygaster.
    
    Empath can now be fooled by Miller, Tailor, Lawyer, etc.
    
    ---------
    
    Co-authored-by: Golbolco <Golbolco@gmail.com>

---
+++ Games/types/Mafia/roles/cards/KillOrBanishedJump.js
@@ -0,0 +1,39 @@
+const Card = require("../../Card");
+const Random = require("../../../../../lib/Random");
+const { PRIORITY_KILL_DEFAULT } = require("../../const/Priority");
+
+module.exports = class KillorBanishedJump extends Card {
+  constructor(role) {
+    super(role);
+
+    this.meetings = {
+      Kill: {
+        actionName: "Kill",
+        states: ["Night"],
+        flags: ["voting"],
+        targets: { include: ["alive", "self"] },
+        action: {
+          labels: ["kill"],
+          priority: PRIORITY_KILL_DEFAULT + 1,
+          run: function () {
+            let savers = this.getVisitors(this.target, "save");
+            let saversSelf = this.getVisitors(this.actor, "save");
+            if (savers.length == 0 && this.target.role.data.banished  && !this.actor.role.data.hasJumped) {
+              if(saversSelf.length == 0){
+                this.actor.role.data.hasJumped = true;
+                this.target.setRole(
+                  `${this.actor.role.name}:${this.actor.role.modifier}`,
+                  this.actor.role.data
+                );
+                this.actor.kill("basic");
+              }
+              return;
+            }
+
+            if (this.dominates()) this.target.kill("basic", this.actor);
+          },
+        },
+      },
+    };
+  }
+};
