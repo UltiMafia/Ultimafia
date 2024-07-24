abfd0982e994a2d03eb45caf9b5866e17b16e281
SawJester <anglelord270@gmail.com>
2024-07-24 06:53:33 -0400

    Update ShavingCream.js

--- Games/types/Mafia/items/ShavingCream.js
+++ Games/types/Mafia/items/ShavingCream.js
@@ -1,4 +1,5 @@
 const Item = require("../Item");
+const Action = require("../Action");
 const { PRIORITY_SWAP_ROLES } = require("../const/Priority");
 
 module.exports = class ShavingCream extends Item {
@@ -8,43 +9,39 @@ module.exports = class ShavingCream extends Item {
     this.cursed = options?.cursed;
     this.meetings = {
       "Swap Players": {
-        actionName: "Player A",
+        actionName: "Swap Players (Choose 2)",
         states: ["Night"],
-        flags: ["voting"],
+        flags: ["voting", "multi"],
+        actor: this.holder,
         targets: { include: ["alive", "self"] },
-        action: {
-          labels: ["hidden","convert"],
-          priority: PRIORITY_SWAP_ROLES-1,
-          run: function () {
-            this.item.targetA = this.target;
-          },
-        },
-      },
-      "Swap Players": {
-        actionName: "Player B",
-        states: ["Night"],
-        flags: ["voting"],
-        targets: { include: ["alive", "self"] },
+        multiMin: 2,
+        multiMax: 2,
         action: {
           labels: ["hidden","convert"],
           priority: PRIORITY_SWAP_ROLES,
+          item: this,
           run: function () {
             var cursed = this.item.cursed;
             this.item.drop();
-            
-            if (!this.dominates()) return;
 
             if (cursed) {
               return;
             }
             
-            var targetA = this.item.targetA;
-            var targetB = this.target;
-            var oldARole = `${targetA.role.name}:${targetA.role.modifier}`;
-            var oldARoleData = targetA.role.data.
+            var targetA = this.target [0];
+            var targetB = this.target [1];
 
-            targetA.setRole(`${targetB.role.name}:${targetB.role.modifier}`,targetB.role.data,true);
-            targetB.setRole(oldARole, oldARoleData, true);
+            if (!targetA || !targetB) return;
+
+            if (this.dominates(this.target [0]) && this.dominates(this.target [1])){
+
+            var oldARole = `${targetA.role.name}:${targetA.role.modifier}`;
+            var oldARoleData = targetA.role.data;
+
+            targetA.setRole(`${targetB.role.name}:${targetB.role.modifier}`,targetB.role.data);
+            targetB.setRole(oldARole, oldARoleData);
+            }
+            
           },
         },
       },
