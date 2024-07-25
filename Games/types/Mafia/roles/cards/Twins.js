bf39eba620b148bb16c0959bab4d1324dc00378c
SawJester <anglelord270@gmail.com>
2024-07-24 20:54:08 -0400

    Create Twins.js

---
+++ Games/types/Mafia/roles/cards/Twins.js
@@ -0,0 +1,41 @@
+const Card = require("../../Card");
+const Random = require("../../../../../lib/Random");
+const { PRIORITY_WIN_BY_CONDEMNING } = require("../../const/Priority");
+
+module.exports = class Twins extends Card {
+  constructor(role) {
+    super(role);
+    this.target = "";
+
+    this.listeners = {
+      roleAssigned: function (player) {
+        if (player !== this.player) {
+          return;
+        }
+
+        const nonMafia = this.game.players.filter(
+          (p) =>
+            (p.role.alignment === "Village" || p.role.winCount === "Village") &&
+            p.alive &&
+            p !== this.player
+        );
+        this.target = Random.randArrayVal(nonMafia);
+        this.player.queueAlert(
+          `${this.target.name} is your Twin and their role is ${this.target.role.name}. Get them condemned for Cult to Win!`
+        );
+        this.target.queueAlert(
+          `${this.player.name} is your Twin and their role is ${this.player.role.name}. If you get condemned Cult Wins!`
+        );
+      },
+      death: function (player, killer, deathType) {
+        if (
+          player === this.target &&
+          deathType === "condemn" &&
+          this.player.alive
+        ) {
+          this.player.role.data.twincondemned = true;
+        }
+      },
+    };
+  }
+};
