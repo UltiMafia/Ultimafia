83fed5f4b9054df190ac1392604abe5b919a78fb
SawJester <anglelord270@gmail.com>
2024-07-24 21:06:06 -0400

    Create EvilTwin.js

---
+++ Games/types/Mafia/roles/Cult/EvilTwin.js
@@ -0,0 +1,15 @@
+const Role = require("../../Role");
+
+module.exports = class EvilTwin extends Role {
+  constructor(player, data) {
+    super("Evil Twin", player, data);
+
+    this.alignment = "Cult";
+    this.cards = [
+      "VillageCore",
+      "WinWithCult",
+      "MeetingCult",
+      "Twins",
+    ];
+  }
+};
