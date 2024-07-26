807728f667720fdb18f8e1fbd30d3a123cfe9823
SawJester <anglelord270@gmail.com>
2024-07-25 21:05:11 -0400

    Update and rename Cephalopod.js to Miasma.js

--- Games/types/Mafia/roles/Cult/Miasma.js
+++ Games/types/Mafia/roles/Cult/Miasma.js
@@ -1,8 +1,8 @@
 const Role = require("../../Role");
 
-module.exports = class Cephalopod extends Role {
+module.exports = class Miasma extends Role {
   constructor(player, data) {
-    super("Cephalopod", player, data);
+    super("Miasma", player, data);
 
     this.alignment = "Cult";
     this.cards = [
