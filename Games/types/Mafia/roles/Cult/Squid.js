1b0a8ce0f84082940053b4005c0f959ac6576af9
SawJester <anglelord270@gmail.com>
2024-07-25 21:14:39 -0400

    Update and rename Miasma.js to Squid.js

--- Games/types/Mafia/roles/Cult/Squid.js
+++ Games/types/Mafia/roles/Cult/Squid.js
@@ -1,8 +1,8 @@
 const Role = require("../../Role");
 
-module.exports = class Miasma extends Role {
+module.exports = class Squid extends Role {
   constructor(player, data) {
-    super("Miasma", player, data);
+    super("Squid", player, data);
 
     this.alignment = "Cult";
     this.cards = [
