1477799becc67a90c6a74800b7c08a4b4ca4312f
SawJester <anglelord270@gmail.com>
2024-07-28 04:06:26 -0400

    Update ConvertToChosenRole.js

--- Games/types/Mafia/roles/cards/ConvertToChosenRole.js
+++ Games/types/Mafia/roles/cards/ConvertToChosenRole.js
@@ -1,10 +1,10 @@
 const Card = require("../../Card");
 const { PRIORITY_CONVERT_DEFAULT } = require("../../const/Priority");
 const { addArticle } = require("../../../../core/Utils");
-
 module.exports = class ConvertToChosenRole extends Card {
   constructor(role) {
     super(role);
+//const targetOptions = this.game.PossibleRoles.filter((r) => r);
     this.meetings = {
       "Select Player": {
         states: ["Night"],
@@ -17,90 +17,17 @@ module.exports = class ConvertToChosenRole extends Card {
           },
         },
       },
-      "Type a Role that can spawn in the Setup (List Role Name Only)": {
+      "Convert To": {
         states: ["Night"],
         flags: ["voting"],
-        inputType: "text",
-        textOptions: {
-          minLength: 1,
-          maxLength: 50,
-          alphaOnly: true,
-          toLowerCase: true,
-          submit: "Submit",
-        },
+        inputType: "AllRoles",
+        //targets: { targetOptions },
         action: {
           labels: ["convert", "role"],
           priority: PRIORITY_CONVERT_DEFAULT,
           run: function () {
-              this.actor.role.data.targetRoleName = this.target;
-        
-          },
-        },
-      },
-      "Type any the names of Any Modifers the Role has. (Leave Blank and hit submit if it has No Modifers) ": {
-        states: ["Night"],
-        flags: ["voting"],
-        inputType: "text",
-        textOptions: {
-          minLength: 0,
-          maxLength: 50,
-          alphaOnly: true,
-          toLowerCase: true,
-          submit: "Submit",
-        },
-        action: {
-          labels: ["convert", "role"],
-          priority: PRIORITY_CONVERT_DEFAULT,
-          run: function () {
-              this.actor.role.data.targetRoleName = this.target;
-
-            let roleSelected = this.actor.role.data.targetRoleName;
             let targetPlayer = this.actor.role.data.targetPlayer;
-            delete this.actor.role.data.targetPlayer;
-            delete this.actor.role.data.targetRoleName;
-            let modifersSelected = this.target;
-            if (targetPlayer && roleSelected.length > 0) {
-
-              let roleSelected = roleSelected.trim();
-              let allRoles = this.game.PossibleRoles.filter((r) => r);
-              let matchedRoles = [];
-
-              for(let x = 0; x < allRoles; x++){
-                if(allRoles [x].split(":")[0].toLowerCase() == roleSelected.toLowerCase()){
-                  matchedRoles.push(allRoles [x]);
-                }
-              }
-              
-              let perfectMatch = 0
-              for(let x = 0; x < matchedRoles; x++){
-              let modifiers = matchedRoles[x].split(":")[1];
-              let isPerfect = true;
-              if(!modifiers || modifiers.length == 0){
-                if(modifersSelected.length <= 1) perfectMatch = matchedRoles[x];
-                else isPerfect = false;
-              }
-              else {
-              let modifierNames = modifiers.split("/");
-              for(let y = 0; y < modifierNames.length; y++){
-                if(!modifersSelected.toLowerCase().includes(modifierNames [y])){
-                  isPerfect = false;
-                }
-              }
-              }
-                if(isPerfect){
-                  perfectMatch = matchedRoles[x];
-                }
-                if(perfectMatch != 0) break;
-              }
-              let finalRole;
-              if(perfectMatch != 0){
-                finalRole = perfectMatch;
-              }
-              else{
-                finalRole = matchedRoles [0];
-              }
-              
-              
+            if (targetPlayer) {
               let players = this.game.players.filter((p) => p.role);
               let currentRoles = [];
               
@@ -108,19 +35,38 @@ module.exports = class ConvertToChosenRole extends Card {
                 currentRoles.push(players [x].role);
               }
               for(let y = 0; y < currentRoles.length;y++){
-                if(finalRole.split(":")[0] == currentRoles[y].name){
+                if(this.target.split(":")[0] == currentRoles[y].name){
                   return;
                 }
               }
               
-              if (this.game.getRoleAlignment(finalRole) == targetPlayer.role.alignment) {
-                    targetPlayer.setRole(`${finalRole}`);
+              if (this.game.getRoleAlignment(this.target) == targetPlayer.role.alignment) {
+                    targetPlayer.setRole(`${this.target}`);
               } 
+              delete this.actor.role.data.targetPlayer;
             }
           },
         },
       },
     };
 
+        this.listeners = {
+      roleAssigned: function (player) {
+        if (player !== this.player) {
+          return;
+        }
+
+        this.data.ConvertOptions = this.game.PossibleRoles.filter((r) => r);
+      },
+      // refresh cooldown
+      state: function (stateInfo) {
+        if (!stateInfo.name.match(/Night/)) {
+          return;
+        }
+        var ConvertOptions = this.data.ConvertOptions;
+
+        this.meetings["Convert To"].targets = ConvertOptions;
+      },
+    };
   }
 };
