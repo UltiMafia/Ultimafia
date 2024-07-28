13bfeba0774235932731f65de60e459f3c99ce6d
Golbolco <Golbolco@gmail.com>
2024-07-27 21:25:34 -0400

    name changes because I am a SOCIOPATH

--- Games/types/Mafia/roles/Village/Tarotist.js
+++ Games/types/Mafia/roles/Village/Tarotist.js
@@ -1,8 +1,8 @@
 const Role = require("../../Role");
 
-module.exports = class Mage extends Role {
+module.exports = class Tarotist extends Role {
   constructor(player, data) {
-    super("Mage", player, data);
+    super("Tarotist", player, data);
 
     this.alignment = "Village";
     this.cards = ["VillageCore", "WinWithVillage", "Learn2ExcessOr1Role"];
