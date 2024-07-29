b1ea542920678b612e5aceea89ad3007a10cbb3c
SawJester <anglelord270@gmail.com>
2024-07-29 01:16:09 -0400

    Update IfVotedForceCondemn.js

--- Games/types/Mafia/roles/cards/IfVotedForceCondemn.js
+++ Games/types/Mafia/roles/cards/IfVotedForceCondemn.js
@@ -5,12 +5,79 @@ module.exports = class IfVotedForceCondemn extends Card {
   constructor(role) {
     super(role);
 
+        this.actions = [
+      {
+        priority: PRIORITY_OVERTHROW_VOTE - 1,
+        labels: ["hidden", "absolute"],
+        run: function () {
+          if (this.game.getStateName() != "Day") return;
+
+          let villageMeeting = this.game.getMeetingByName("Village");
+
+          if()
+
+          //New code
+              for (let action of this.game.actions[0]) {
+              if (action.hasLabel("condemn") && !action.hasLabel("overthrow")) {
+                if (action.target === this.target) {
+                  return;
+                }
+
+                // Only one village vote can be overthrown
+                action.cancel(true);
+                break;
+              }
+            }
+
+            if (this.dominates()) {
+              this.target.kill("condemn", this.actor);
+            }
+
+
+          /* Old code in case new causes any problems
+          if (villageMeeting.finalTarget === this.actor) {
+            return;
+          }
+
+          // check if it was a target
+          let targeted = false;
+          for (let key in villageMeeting.votes) {
+            let target = villageMeeting.votes[key];
+            if (target === this.actor.id) {
+              targeted = true;
+              break;
+            }
+          }
+          if (!targeted) {
+            return;
+          }
+
+          let action = new Action({
+            actor: this.actor,
+            target: this.actor,
+            game: this.game,
+            labels: ["kill", "frustration", "hidden"],
+            power: 3,
+            run: function () {
+              this.game.sendAlert(
+                `${this.target.name} feels immensely frustrated!`
+              );
+              if (this.dominates()) this.target.kill("basic", this.actor);
+            },
+          });
+          action.do();
+          */
+        },
+      },
+    ];
+
     this.listeners = {
       vote: function (vote) {
         if (vote.meeting.name === "Village" && vote.target === this.player.id) {
           if (this.player.role.data.hasBeenVoted) return;
 
           this.player.role.data.hasBeenVoted = true;
+          this.player.role.data.playerVoter = 0;
 
           if (
             this.game.getRoleAlignment(
@@ -19,6 +86,7 @@ module.exports = class IfVotedForceCondemn extends Card {
           ) {
             return;
           }
+           this.player.role.data.playerVoter = vote.voter;
 
           for (const player of this.game.alivePlayers()) {
             player.giveEffect("CannotBeVoted", 1);
