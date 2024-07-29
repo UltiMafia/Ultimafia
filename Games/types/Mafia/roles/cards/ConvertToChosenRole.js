995dedb7801462ba6d497bc02b63e88ddf6b2542
SawJester <anglelord270@gmail.com>
2024-07-28 20:59:23 -0400

    added Pit hag from BOTC (#1671)
    added MiGo role. It chooses a player and a role. If the role's alignment
    matches the player's alignment and the role does not already exist. The
    player becomes that role.
    
    Golbolco wanted the role to be called "Mi-Go" but the "-" in the name
    broke the code.
    
    ---------
    
    Co-authored-by: SawJester <SawJester@users.noreply.github.com>

---
+++ Games/types/Mafia/roles/cards/ConvertToChosenRole.js
@@ -0,0 +1,72 @@
+const Card = require("../../Card");
+const { PRIORITY_CONVERT_DEFAULT } = require("../../const/Priority");
+const { addArticle } = require("../../../../core/Utils");
+module.exports = class ConvertToChosenRole extends Card {
+  constructor(role) {
+    super(role);
+//const targetOptions = this.game.PossibleRoles.filter((r) => r);
+    this.meetings = {
+      "Select Player": {
+        states: ["Night"],
+        flags: ["voting"],
+        targets: { include: ["alive", "self"] },
+        action: {
+          priority: PRIORITY_CONVERT_DEFAULT - 1,
+          run: function () {
+            this.actor.role.data.targetPlayer = this.target;
+          },
+        },
+      },
+      "Convert To": {
+        states: ["Night"],
+        flags: ["voting"],
+        inputType: "custom",
+        //targets: { targetOptions },
+        action: {
+          labels: ["convert", "role"],
+          priority: PRIORITY_CONVERT_DEFAULT,
+          run: function () {
+            let targetPlayer = this.actor.role.data.targetPlayer;
+            if (targetPlayer) {
+              let players = this.game.players.filter((p) => p.role);
+              let currentRoles = [];
+              
+              for (let x = 0; x < players.length; x++) {
+                currentRoles.push(players [x].role);
+              }
+              for(let y = 0; y < currentRoles.length;y++){
+                if(this.target.split(":")[0] == currentRoles[y].name){
+                  return;
+                }
+              }
+              
+              if (this.game.getRoleAlignment(this.target) == targetPlayer.role.alignment) {
+                    targetPlayer.setRole(`${this.target}`);
+              } 
+              delete this.actor.role.data.targetPlayer;
+            }
+          },
+        },
+      },
+    };
+
+        this.listeners = {
+      roleAssigned: function (player) {
+        if (player !== this.player) {
+          return;
+        }
+
+        this.data.ConvertOptions = this.game.PossibleRoles.filter((r) => r);
+      },
+      // refresh cooldown
+      state: function (stateInfo) {
+        if (!stateInfo.name.match(/Night/)) {
+          return;
+        }
+        var ConvertOptions = this.data.ConvertOptions;
+
+        this.meetings["Convert To"].targets = ConvertOptions;
+      },
+    };
+  }
+};
