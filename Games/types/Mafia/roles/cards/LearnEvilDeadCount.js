c568f14c2d61a51540f169de7f5294c893ad7c7e
SawJester <anglelord270@gmail.com>
2024-07-30 02:48:19 -0400

    Create LearnEvilDeadCount.js

---
+++ Games/types/Mafia/roles/cards/LearnEvilDeadCount.js
@@ -0,0 +1,35 @@
+const Card = require("../../Card");
+const { const PRIORITY_INVESTIGATIVE_AFTER_RESOLVE_DEFAULT } = require("../../const/Priority");
+
+module.exports = class LearnEvilDeadCount extends Card {
+  constructor(role) {
+    super(role);
+
+    this.actions = [
+      {
+        priority: const PRIORITY_INVESTIGATIVE_AFTER_RESOLVE_DEFAULT-1,
+        labels: ["investigate"],
+        run: function () {
+          if (this.game.getStateName() != "Night") return;
+          if (!this.actor.alive) return;
+          let evilCount;
+          let players = this.game.deadPlayers();
+          var evilPlayers = players.filter(
+            (p) =>
+              this.game.getRoleAlignment(
+                p.getRoleAppearance().split(" (")[0]
+              ) == "Cult" ||
+              this.game.getRoleAlignment(
+                p.getRoleAppearance().split(" (")[0]
+              ) == "Mafia"
+          );
+          evilCount = evilPlayers.length;
+            this.actor.queueAlert(
+              `You learn that ${evilCount} dead players are Evil.`
+            );
+          
+        },
+      },
+    ];
+  }
+};
