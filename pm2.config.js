f3f70168c9e34a7c564048f63d9ecaa43dd2328c
DrSharky <keleblanc24@gmail.com>
2024-08-01 10:36:29 -0400

    Pm2 fixes (#1679)

--- pm2.config.js
+++ pm2.config.js
@@ -6,18 +6,21 @@ module.exports = {
       autorestart: false,
       time: true,
       "node-args": ["--debug=7000"],
+      watch: false
     },
     {
       name: "www",
       script: "./bin/www",
       time: true,
       "node-args": ["--debug=7000"],
+      watch: false
     },
     {
       name: "chat",
       script: "./modules/chat.js",
       time: true,
       "node-args": ["--debug=7000"],
+      watch: false
     },
   ],
 };
