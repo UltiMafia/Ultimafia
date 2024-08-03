d52954765f9bb7a81c69b92479838b76b5737cc3
SawJester <anglelord270@gmail.com>
2024-08-03 13:43:30 -0400

    Update CheckSuccessfulVisit.js

--- Games/types/Mafia/roles/cards/CheckSuccessfulVisit.js
+++ Games/types/Mafia/roles/cards/CheckSuccessfulVisit.js
@@ -15,6 +15,12 @@ module.exports = class CheckSuccessfulVisit extends Card {
           let targets = this.getVisits(this.actor);
           let targetNames = targets.map((t) => t.name);
 
+          if(this.actor.hasEffect("FalseMode")){
+
+            if (targetNames.length >= 1) return;
+            
+          }
+
           if (targetNames.length >= 1) {
             this.actor.queueAlert(
               `:invest: You learn that your visit to ${targetNames.join(
