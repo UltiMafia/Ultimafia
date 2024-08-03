1c76ffbef3584f2cb7f32473786e92d0953f8620
SawJester <anglelord270@gmail.com>
2024-08-03 11:18:59 -0400

    Create Vortox.js

---
+++ Games/types/Mafia/roles/Cult/Vortox.js
@@ -0,0 +1,22 @@
+const Role = require("../../Role");
+
+module.exports = class Vortox extends Role {
+  constructor(player, data) {
+    super("Vortox", player, data);
+
+    this.alignment = "Cult";
+    this.cards = [
+      "VillageCore",
+      "WinWithCult",
+      "MeetingCult",
+      "Endangered",
+      "MakeAllVillageInfoFalse",
+      "NightKiller",
+    ];
+    this.meetingMods = {
+      "Solo Kill": {
+        targets: { include: ["alive"], exclude: ["Cult"] },
+      },
+    };
+  }
+};
