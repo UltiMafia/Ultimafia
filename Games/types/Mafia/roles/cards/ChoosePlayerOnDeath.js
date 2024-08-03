f8eb2eb3b3462dd85352be22ee69a52a017bfcb6
SawJester <anglelord270@gmail.com>
2024-08-03 07:20:26 -0400

    Update ChoosePlayerOnDeath.js

--- Games/types/Mafia/roles/cards/ChoosePlayerOnDeath.js
+++ Games/types/Mafia/roles/cards/ChoosePlayerOnDeath.js
@@ -9,23 +9,25 @@ module.exports = class ChoosePlayerOnDeath extends Card {
       "Choose Player": {
         actionName: "Lose unless this player is Village Aligned",
         states: ["Day"],
-        flags: ["voting"],
-         shouldMeet: function () {
-          return !this.hasChosen;
+        flags: ["voting", "mustAct"],
+        shouldMeet: function () {
+          return !this.revived;
         },
         action: {
-          priority: PRIORITY_DAY_DEFAULT,
+          priority: PRIORITY_DAY_DEFAULT -1,
           run: function () {
             this.game.queueAlert(
               `${this.actor.name} the ${this.actor.role.name} has selected ${this.target.name}`
             );
-            this.actor.role.hasChoosen = true;
+            //this.hasChoosen = true;
+            this.actor.role.revived = true;
+
             if(this.target.role.alignment != "Village"){
             for (let p of this.game.alivePlayers()) {
-            if (p.role.alignment === this.actor.role.alignment) {
-            p.kill("basic", this.actor, instant);
-          }
-        }
+            if (p.role.alignment === "Village") {
+            p.kill("basic", this.actor, true);
+              } 
+            } 
             }
             
             
