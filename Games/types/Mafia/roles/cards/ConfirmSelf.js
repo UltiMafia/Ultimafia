c0644729f8e77594908901f93beda7e0a3dad311
SawJester <anglelord270@gmail.com>
2024-08-03 14:10:38 -0400

    Update ConfirmSelf.js

--- Games/types/Mafia/roles/cards/ConfirmSelf.js
+++ Games/types/Mafia/roles/cards/ConfirmSelf.js
@@ -16,6 +16,14 @@ module.exports = class ConfirmSelf extends Card {
             var alert = `:mask: You learn that ${
               this.actor.name
             }'s role is ${this.actor.getRoleAppearance()}.`;
+
+            if(this.actor.hasEffect("FalseMode")){
+               let players = this.game.alivePlayers().filter((p) => p != this.target);
+              players = players.filter((p) => p != this.actor);
+              let playerNames = players.map((p) => p.name);
+              alert = `:mask: You learn that ${Random.randArrayVal(playerNames)}'s role is ${this.actor.getRoleAppearance()}.`;
+            }
+            
             this.target.queueAlert(alert);
           },
         },
