f1e358e1b73babc557ccb4828c94ab1c970c433d
SawJester <anglelord270@gmail.com>
2024-08-04 07:26:10 -0400

    Create ChoosePlayerOnDeath.js

---
+++ Games/types/Mafia/roles/cards/ChoosePlayerOnDeath.js
@@ -0,0 +1,39 @@
+const Card = require("../../Card");
+const { PRIORITY_DAY_DEFAULT } = require("../../const/Priority");
+
+module.exports = class ChoosePlayerOnDeath extends Card {
+  constructor(role) {
+    super(role);
+
+    this.meetings = {
+      "Choose Player": {
+        actionName: "Lose unless this player is Village Aligned",
+        states: ["Day"],
+        flags: ["voting", "mustAct"],
+        shouldMeet: function () {
+          return !this.revived;
+        },
+        action: {
+          priority: PRIORITY_DAY_DEFAULT -1,
+          run: function () {
+            this.game.queueAlert(
+              `${this.actor.name} the ${this.actor.role.name} has selected ${this.target.name}`
+            );
+            //this.hasChoosen = true;
+            this.actor.role.revived = true;
+
+            if(this.target.role.alignment != "Village"){
+            for (let p of this.game.alivePlayers()) {
+            if (p.role.alignment === "Village") {
+            p.kill("basic", this.actor, true);
+              } 
+            } 
+            }
+            
+            
+          },
+        },
+      },
+    };
+  }
+};
