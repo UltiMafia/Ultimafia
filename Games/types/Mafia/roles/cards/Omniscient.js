75d8c2ee218df4043d5e6c18950baa01e15ee603
SawJester <anglelord270@gmail.com>
2024-08-04 01:13:34 -0400

    Update Omniscient.js

--- Games/types/Mafia/roles/cards/Omniscient.js
+++ Games/types/Mafia/roles/cards/Omniscient.js
@@ -21,6 +21,11 @@ module.exports = class Omiscient extends Card {
             visits = this.getVisits(players[x]);
             visitNames = visits.map((p) => p.name);
             role = players[x].role.name;
+            if(this.actor.hasEffect("FalseMode")){
+              visits = this.getVisits(Random.randArrayVal(players));
+              visitNames = visits.map((p) => p.name);
+              role = Random.randArrayVal(players).role.name;
+            }
             name = players[x].name;
             if (visitNames.length == 0) visitNames.push("no one");
             this.actor.queueAlert(
