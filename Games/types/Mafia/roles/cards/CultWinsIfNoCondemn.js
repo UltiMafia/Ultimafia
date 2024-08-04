c3f9f0d9adc7ae9a1db094956496d9cac97f089a
SawJester <anglelord270@gmail.com>
2024-08-04 03:23:00 -0400

    Create CultWinsIfNoCondemn.js

---
+++ Games/types/Mafia/roles/cards/CultWinsIfNoCondemn.js
@@ -0,0 +1,39 @@
+const Card = require("../../Card");
+const { PRIORITY_DAY_EFFECT_DEFAULT } = require("../../const/Priority");
+
+module.exports = class CultWinsIfNoCondemn extends Card {
+  constructor(role) {
+    super(role);
+
+    this.actions = [
+      {
+        priority: PRIORITY_DAY_EFFECT_DEFAULT,
+        run: function () {
+          if (!this.actor.alive) return;
+          if (this.game.getStateName() == "Day") {
+            let alivePlayers = this.game.alivePlayers();
+
+            for (let x = 0; x < this.game.alivePlayers().length; x++) {
+              for (let action of this.game.actions[0]) {
+                if (
+                  action.target == alivePlayers[x] &&
+                  action.hasLabel("condemn")
+                ){
+                  this.actor.role.data.VortoxWin = false;
+                  return;
+                }
+              }
+            }
+            /*
+            this.actor.queueAlert(
+              `Now that only 3 players are alive today, Town will win if no one is executed Today!`
+            );
+            */
+            this.actor.role.data.VortoxWin = true;
+            return;
+          }
+        },
+      },
+    ];
+  }
+};
