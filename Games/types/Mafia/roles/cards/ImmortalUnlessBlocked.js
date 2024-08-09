1d9668198c4da30d62fbd9fdc1fdded825bc21e1
SawJester <anglelord270@gmail.com>
2024-08-09 19:27:24 -0400

    Create ImmortalUnlessBlocked.js

---
+++ Games/types/Mafia/roles/cards/ImmortalUnlessBlocked.js
@@ -0,0 +1,23 @@
+const Card = require("../../Card");
+const Random = require("../../../../../lib/Random");
+const { PRIORITY_NIGHT_SAVER } = require("../../const/Priority");
+
+module.exports = class ImmortalUnlessBlocked extends Card {
+  constructor(role) {
+    super(role);
+
+    this.actions = [
+      {
+        priority: PRIORITY_NIGHT_SAVER,
+        labels: ["save", "hidden", "absolute"],
+        run: function () {
+          if (this.game.getStateName() != "Night") return;
+
+          if (!this.actor.alive) return;
+
+              this.actor.giveEffect("Immortal",5,1);
+        },
+      },
+    ];
+  }
+};
