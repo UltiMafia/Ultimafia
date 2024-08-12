f1e20f75ed49d136165a9e0324e65558043e2057
SawJester <anglelord270@gmail.com>
2024-08-12 09:32:59 -0400

    Update Key.js

--- Games/types/Mafia/items/Key.js
+++ Games/types/Mafia/items/Key.js
@@ -2,8 +2,11 @@ const Item = require("../Item");
 const { PRIORITY_DAY_DEFAULT } = require("../const/Priority");
 
 module.exports = class Key extends Item {
-  constructor() {
+  constructor(options) {
     super("Key");
+
+    this.magicCult = options?.magicCult;
+    this.broken = options?.broken;
 
     this.meetings = {
       "Lock yourself in?": {
@@ -16,7 +19,7 @@ module.exports = class Key extends Item {
           item: this,
           run: function () {
             if (this.target == "Yes") {
-              this.item.holder.holdItem("Lock");
+              this.item.holder.holdItem("Lock",{ broken: this.broken, magicCult: this.magicCult});
               this.item.drop();
             }
           },
