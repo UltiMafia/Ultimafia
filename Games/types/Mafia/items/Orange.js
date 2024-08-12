ef829cf606467d6cf56b725ab2ec09398904ec9d
SawJester <anglelord270@gmail.com>
2024-08-12 08:12:33 -0400

    Update Orange.js

--- Games/types/Mafia/items/Orange.js
+++ Games/types/Mafia/items/Orange.js
@@ -44,4 +44,34 @@ module.exports = class Orange extends Item {
       },
     };
   }
+
+  eat() {
+    if (this.magicCult == true && this.holder.alignment != "Cult") {
+      let action = new Action({
+        actor: this.holder,
+        target: this.holder,
+        game: this.game,
+        labels: [
+          "giveEffect",
+          "poison",
+          "hidden",
+          "absolute",
+          "uncontrollable",
+        ],
+        priority: PRIORITY_EFFECT_GIVER_DEFAULT,
+        run: function () {
+          if (this.dominates()) {
+            this.target.queueAlert(
+              "You have been poisoned by the Cult's Magic Food!"
+            );
+            this.target.giveEffect("poison", this.actor);
+          }
+        },
+      });
+
+      action.do();
+    }
+  }
+
+  
 };
