71bb8ec8ab9d30efb5080b99428717a47bbb5d77
SawJester <anglelord270@gmail.com>
2024-08-16 21:12:46 -0400

    Create BecomeMindRottedRole.js

---
+++ Games/types/Mafia/roles/cards/BecomeMindRottedRole.js
@@ -0,0 +1,44 @@
+const Card = require("../../Card");
+const Random = require("../../../../../lib/Random");
+
+module.exports = class BecomeMindRottedRole extends Card {
+  constructor(role) {
+    super(role);
+
+    this.listeners = {
+      pregameSwitchRole: function (player) {
+        if (player != this.player) return;
+        this.player.role.data.reroll = true;
+        this.player.holdItem("IsTheCarrier");
+
+        let banishedRoles = this.game.banishedRoles;
+        let roles = this.game.PossibleRoles.filter((r) => r);
+         let currentRoles = [];
+        let playersAll = this.game.players.filter((p) => p.role);
+        for (let x = 0; x < playersAll.length; x++) {
+          //currentRoles.push(playersAll[x].role);
+          let tempName = playersAll[x].role.name;
+          let tempModifier = playersAll[x].role.modifier;
+          currentRoles.push(
+            `${tempName}:${tempModifier}`
+          );
+        }
+        for (let y = 0; y < currentRoles.length; y++) {
+          roles = roles.filter((r) => (r != currentRoles[y]) && !(currentRoles[y].includes(r)) );
+        }
+        roles = roles.filter((r) => !(r.toLowerCase().includes("banished")));
+        roles = roles.filter((r) => this.game.getRoleAlignment(r) == "Village");
+
+        if(roles.length <= 0){
+          roles = currentRoles;
+          roles = roles.filter((r) => this.game.getRoleAlignment(r) == "Village");
+          roles = roles.filter((r) => r.split(":")[0] != "Carrier");
+        }
+
+        let newRole = Random.randArrayVal(roles);
+        this.player.setRole(newRole, undefined, false, true);
+    
+      },
+    };
+  }
+};
