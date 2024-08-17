90f0648cff5b9b037417542c8c77921038a3fd52
SawJester <anglelord270@gmail.com>
2024-08-16 22:29:13 -0400

    Create MakeCondemnImmune.js

---
+++ Games/types/Mafia/roles/cards/MakeCondemnImmune.js
@@ -0,0 +1,30 @@
+const Card = require("../../Card");
+const { PRIORITY_EFFECT_GIVER_DEFAULT } = require("../../const/Priority");
+
+module.exports = class MakeCondemnImmune extends Card {
+  constructor(role) {
+    super(role);
+
+    this.meetings = {
+      "Protect from Condemn": {
+        states: ["Night"],
+        flags: ["voting"],
+        targets: { include: ["alive","self"], exclude: [isPrevTarget] },
+        action: {
+          labels: ["effect"],
+          priority: PRIORITY_EFFECT_GIVER_DEFAULT,
+          run: function () {
+            this.actor.role.data.prevTarget = this.target;
+            if (this.dominates()) {
+              this.target.giveEffect("Condemn Immune", 3,1);
+            }
+          },
+        },
+      },
+    };
+  }
+};
+
+function isPrevTarget(player) {
+  return this.role && player == this.role.data.prevTarget;
+}
