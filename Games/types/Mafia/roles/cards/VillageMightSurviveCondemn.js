9efaace97357ca857b127fe1804bfc2e500e1177
SawJester <anglelord270@gmail.com>
2024-08-17 09:03:51 -0400

    Create VillageMightSurviveCondemn.js

---
+++ Games/types/Mafia/roles/cards/VillageMightSurviveCondemn.js
@@ -0,0 +1,28 @@
+const Card = require("../../Card");
+const { PRIORITY_EFFECT_GIVER_DEFAULT } = require("../../const/Priority");
+const Random = require("../../../../../lib/Random");
+
+module.exports = class VillageMightSurviveCondemn extends Card {
+  constructor(role) {
+    super(role);
+
+    this.actions = [
+      {
+        priority: PRIORITY_EFFECT_GIVER_DEFAULT,
+        labels: ["save"],
+        run: function () {
+          if (!this.actor.alive) return;
+
+          const villagePlayers = this.game.alivePlayers().filter((p) => p.role.alignment == "Village" || p.role.winCount == "Village");
+
+          let shuffledPlayers = Random.randomizeArray(villagePlayers);
+
+          
+          
+            shuffledPlayers[0].giveEffect("Condemn Immune", 5, 1);
+          
+        },
+      },
+    ];
+  }
+};
