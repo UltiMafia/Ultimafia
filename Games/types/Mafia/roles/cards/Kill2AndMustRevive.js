fdb14092527b82843cb4898ff05f6ab3f00c0ec3
SawJester <anglelord270@gmail.com>
2024-08-18 19:18:09 -0400

    Update Kill2AndMustRevive.js

--- Games/types/Mafia/roles/cards/Kill2AndMustRevive.js
+++ Games/types/Mafia/roles/cards/Kill2AndMustRevive.js
@@ -6,6 +6,9 @@ module.exports = class KillorCharge extends Card {
   constructor(role) {
     super(role);
 
+    this.role.EatenPlayers = ["No One"];
+    this.role.revived = false;
+
     this.meetings = {
       "Kill 2 Players": {
         actionName: "Kill 2 Players",
@@ -14,9 +17,6 @@ module.exports = class KillorCharge extends Card {
         targets: { include: ["alive"] },
         multiMin: 2,
         multiMax: 2,
-        shouldMeet: function () {
-          return this.revived;
-        },
         action: {
           labels: ["kill"],
           priority: PRIORITY_KILL_DEFAULT + 1,
@@ -26,13 +26,59 @@ module.exports = class KillorCharge extends Card {
             for(let x = 0; x < this.target.length; x++){
               if (this.dominates(this.target[x])){
                 this.target[x].kill("basic", this.actor);
-                
+                this.actor.role.EatenPlayers.push(this.target[x]);
               }
             }
             
           },
         },
       },
+      Revive: {
+        actionName: "Revive",
+        states: ["Night"],
+        flags: ["voting"],
+        shouldMeet: function () {
+          return (this.role.EatenPlayers.length > 0);
+        },
+        action: {
+          labels: ["revive"],
+          priority: PRIORITY_NIGHT_REVIVER,
+          run: function () {
+            if (!this.dominates()) {
+              return;
+            }
+            if(this.target == "No one") return;
+
+            this.actor.role.EatenPlayers.filter((p) => p != this.target);
+
+            this.actor.role.revived = true;
+            this.target.revive("basic", this.actor);
+          },
+        },
+      },
+      
+    };
+    this.listeners = {
+      roleAssigned: function (player) {
+        if (player !== this.player) {
+          return;
+        }
+
+        this.data.ConvertOptions = this.game.PossibleRoles.filter(
+          (r) => this.game.getRoleAlignment(r) == "Village"
+        );
+      },
+      // refresh cooldown
+      state: function (stateInfo) {
+        if (!stateInfo.name.match(/Night/)) {
+          return;
+        }
+        var ConvertOptions = this.data.ConvertOptions;
+        ConvertOptions.push("None");
+
+        this.meetings["Revive"].targets = this.player.EatenPlayers;
+      },
     };
   }
 };
+
