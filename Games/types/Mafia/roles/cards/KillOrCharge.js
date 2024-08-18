56057496362c78c48e5fb1e418530d7082969479
SawJester <anglelord270@gmail.com>
2024-08-18 17:23:07 -0400

    Create KillOrCharge.js

---
+++ Games/types/Mafia/roles/cards/KillOrCharge.js
@@ -0,0 +1,55 @@
+const Card = require("../../Card");
+const Random = require("../../../../../lib/Random");
+const { PRIORITY_KILL_DEFAULT } = require("../../const/Priority");
+
+module.exports = class KillorCharge extends Card {
+  constructor(role) {
+    super(role);
+
+    this.meetings = {
+      Kill: {
+        actionName: "Kill",
+        states: ["Night"],
+        flags: ["voting"],
+        targets: { include: ["alive", "self"] },
+        shouldMeet: function () {
+          return !this.revived;
+        },
+        action: {
+          labels: ["kill"],
+          priority: PRIORITY_KILL_DEFAULT + 1,
+          run: function () {
+           if(this.target == "No one"){
+             this.actor.role.revived = true;
+           }
+
+            if (this.dominates()) this.target.kill("basic", this.actor);
+          },
+        },
+      },
+      "Kill 3 Players": {
+        actionName: "Kill 3 Players",
+        states: ["Night"],
+        flags: ["voting", "multi"],
+        targets: { include: ["alive", "self"] },
+        multiMin: 3,
+        multiMax: 3,
+        shouldMeet: function () {
+          return this.revived;
+        },
+        action: {
+          labels: ["kill"],
+          priority: PRIORITY_KILL_DEFAULT + 1,
+          run: function () {
+           
+             this.actor.role.revived = false;
+            for(let x = 0; x < this.target.length; x++){
+              if (this.dominates(this.target[x])) this.target[x].kill("basic", this.actor);
+            }
+            
+          },
+        },
+      },
+    };
+  }
+};
