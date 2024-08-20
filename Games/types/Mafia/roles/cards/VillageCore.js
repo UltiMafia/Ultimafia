8344dd7b7d58e73dfcf3643936a10d7a6c3216d7
SawJester <anglelord270@gmail.com>
2024-08-19 21:01:56 -0400

    Update VillageCore.js

--- Games/types/Mafia/roles/cards/VillageCore.js
+++ Games/types/Mafia/roles/cards/VillageCore.js
@@ -19,6 +19,26 @@ module.exports = class VillageCore extends Card {
           priority: PRIORITY_VILLAGE,
           power: 3,
           run: function () {
+
+            if(this.target == "Proclaim Atheist Game"){
+              let players = this.game.players.filter((p) => p.role.alignment == "Mafia" || p.role.alignment == "Cult");
+              if(players.length > 0){
+              for (let p of this.game.alivePlayers()) {
+                if (p.role.alignment === "Village" || p.role.name === "Atheist") {
+                  p.kill("basic", this.actor, true);
+                }
+              }
+              }
+              else{
+              for (let p of this.game.alivePlayers()) {
+                if (p.role.alignment === "Village" || p.role.name === "Atheist") {
+                  p.role.data.AtheistWin = true;
+                }
+              }
+              }
+            }
+
+            
             if (this.dominates()) this.target.kill("condemn", this.actor);
           },
         },
@@ -30,6 +50,16 @@ module.exports = class VillageCore extends Card {
         whileDead: true,
         passiveDead: false,
         speakDead: true,
+      },
+    };
+    this.listeners = {
+      state: function (stateInfo) {
+        if (!stateInfo.name.match(/Day/)) {
+          return;
+        }
+        if(!this.game.AtheistPossible) return;
+
+        this.meetings["Village"].targets.push("Proclaim Atheist Game");
       },
     };
   }
