7fb2a866cdbc449b92fcb788c787c51356638e0c
NouSixXD <nousixgh@protonmail.com>
2024-09-02 02:14:33 +0000

    Drama Queen

---
+++ Games/types/Mafia/roles/cards/StartDrama.js
@@ -0,0 +1,42 @@
+const Card = require("../../Card");
+
+module.exports = class StartDrama extends Card {
+  constructor(role) {
+    super(role);
+    role.data.canStartDrama = true;
+
+    this.listeners = {
+      roleAssigned: function (player) {
+        if (player !== this.player) {
+          return;
+        }
+        this.data.meetingName = "Start drama";
+      },
+    };
+
+    this.meetings = {
+      "Start Drama": {
+        states: ["Night"],
+        flags: ["voting"],
+        action: {
+          labels: ["drama"],
+          run: function () {
+            this.target.holdItem(
+              "Drama",
+              this.actor.role.data.meetingName,
+              this.actor,
+              this.target
+            );
+            this.target.queueAlert(
+              "Drama queen started drama with you!"
+            );
+          },
+        },
+        shouldMeet: function (meetingName) {
+          if (role.data.canStartDrama === true)
+            return true;
+        },
+      },
+    };
+  }
+};
