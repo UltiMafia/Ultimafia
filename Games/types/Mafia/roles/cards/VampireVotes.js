d6562d2e1106a777ee19db5049aae52c7af0996c
SawJester <SawJester@users.noreply.github.com>
2024-09-10 03:59:29 +0000

    Prettified Code!

--- Games/types/Mafia/roles/cards/VampireVotes.js
+++ Games/types/Mafia/roles/cards/VampireVotes.js
@@ -16,39 +16,46 @@ module.exports = class VampireVotes extends Card {
       vote: function (vote) {
         if (!this.player.alive) return;
         if (vote.meeting.name === "Village" && vote.target === vote.voter) {
-          if(vote.voter.role.alignment == "Village" &&  Random.randInt(0, 100) <= 30){
+          if (
+            vote.voter.role.alignment == "Village" &&
+            Random.randInt(0, 100) <= 30
+          ) {
             let action = new Action({
-            actor: this.player,
-            target: vote.voter,
-            game: this.game,
-            labels: ["kill", "curse", "hidden"],
-            power: 2,
-            run: function () {
-              if (this.dominates()) this.target.kill("curse", this.actor, true);
-            },
-          });
+              actor: this.player,
+              target: vote.voter,
+              game: this.game,
+              labels: ["kill", "curse", "hidden"],
+              power: 2,
+              run: function () {
+                if (this.dominates())
+                  this.target.kill("curse", this.actor, true);
+              },
+            });
 
-          this.game.instantAction(action);
+            this.game.instantAction(action);
           }
-        }
-        else if(vote.meeting.name === "Village"){
-           let votes = this.player.role.data.VotingLog;
+        } else if (vote.meeting.name === "Village") {
+          let votes = this.player.role.data.VotingLog;
 
           for (let y = 0; y < votes.length; y++) {
             if (votes[y].voter == vote.voter) {
-                if(vote.voter.role.alignment == "Village" &&  Random.randInt(0, 100) <= 30){
+              if (
+                vote.voter.role.alignment == "Village" &&
+                Random.randInt(0, 100) <= 30
+              ) {
                 let action = new Action({
-                actor: this.player,
-                target: vote.voter,
-                game: this.game,
-                labels: ["kill", "curse", "hidden"],
-                power: 2,
-                run: function () {
-                  if (this.dominates()) this.target.kill("curse", this.actor, true);
-                },
-              });
+                  actor: this.player,
+                  target: vote.voter,
+                  game: this.game,
+                  labels: ["kill", "curse", "hidden"],
+                  power: 2,
+                  run: function () {
+                    if (this.dominates())
+                      this.target.kill("curse", this.actor, true);
+                  },
+                });
 
-              this.game.instantAction(action);
+                this.game.instantAction(action);
               }
               return;
             }
