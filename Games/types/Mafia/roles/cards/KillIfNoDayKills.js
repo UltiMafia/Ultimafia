ccd487807d6b00bc17ad896d1682f9984b43b12d
SawJester <anglelord270@gmail.com>
2024-09-09 23:56:20 -0400

    Create KillIfNoDayKills.js

---
+++ Games/types/Mafia/roles/cards/KillIfNoDayKills.js
@@ -0,0 +1,56 @@
+const Card = require("../../Card");
+const { PRIORITY_KILL_DEFAULT } = require("../../const/Priority");
+
+module.exports = class KillIfNoDayKills extends Card {
+  constructor(role) {
+    super(role);
+
+    role.banishedDied = false;
+
+    this.meetings = {
+      "Kill": {
+        actionName: "Kill",
+        states: ["Night"],
+        flags: ["voting"],
+        targets: { include: ["alive", "self"] },
+        whileDead: true,
+        whileAlive: true,
+        shouldMeet: function () {
+          return this.banishedDied == false;
+        },
+        action: {
+          labels: ["kill"],
+          priority: PRIORITY_KILL_DEFAULT,
+          run: function () {
+            if (this.dominates()) this.target.kill("basic", this.actor);
+            this.banishedDied = false;
+          },
+        },
+      },
+    };
+
+    this.listeners = {
+      state: function (stateInfo) {
+        if (!this.player.alive) {
+          return;
+        }
+
+        if (stateInfo.name.match(/Day/)) {
+          this.banishedDied = false;
+          return;
+        }
+      },
+      death: function (player, killer, deathType) {
+          if (this.game.getStateName() == "Night") return;
+          this.banishedDied = true;
+          this.player.queueAlert(
+            `A Player has died today, You can not kill tonight.`
+          );
+      },
+      roleAssigned: function (player) {
+        if (player !== this.player) return;
+        this.player.banishedDied = true;
+      },
+    };
+  }
+};
