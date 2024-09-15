67d7e5c4465725585fbdd2d7a995399b0064e4a5
SawJester <SawJester@users.noreply.github.com>
2024-09-15 05:55:16 +0000

    Prettified Code!

--- Games/types/Mafia/const/FactionList.js
+++ Games/types/Mafia/const/FactionList.js
@@ -1,10 +1,10 @@
-const EVIL_FACTIONS = ["Cult","Mafia"];
-const NOT_EVIL_FACTIONS = ["Village","Independent"];
+const EVIL_FACTIONS = ["Cult", "Mafia"];
+const NOT_EVIL_FACTIONS = ["Village", "Independent"];
 const CULT_FACTIONS = ["Cult"];
 const MAFIA_FACTIONS = ["Mafia"];
-const FACTION_LEARN_TEAM = ["Mafia","Cult"];
-const FACTION_WIN_WITH_MAJORITY = ["Mafia","Cult"];
-const FACTION_WITH_MEETING = ["Mafia","Cult"];
+const FACTION_LEARN_TEAM = ["Mafia", "Cult"];
+const FACTION_WIN_WITH_MAJORITY = ["Mafia", "Cult"];
+const FACTION_WITH_MEETING = ["Mafia", "Cult"];
 const FACTION_KILL = ["Mafia"];
 
 module.exports = {
