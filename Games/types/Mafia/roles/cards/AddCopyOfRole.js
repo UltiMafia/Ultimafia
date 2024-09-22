82d9cea6165acbb8efbb7ce67c2f02978762728d
SawJester <anglelord270@gmail.com>
2024-09-22 12:49:00 -0400

    Update AddCopyOfRole.js

--- Games/types/Mafia/roles/cards/AddCopyOfRole.js
+++ Games/types/Mafia/roles/cards/AddCopyOfRole.js
@@ -10,16 +10,37 @@ module.exports = class AddCopyOfRole extends Card {
         if (player != this.player) return;
         if (this.player.role.data.hasCopied) return;
         this.player.role.data.reroll = true;
-        
+        this.player.role.data.hasCopied = true;
 
         if (this.player.role.name == "Baphomet") {
+          let players = this.game.players.filter(
+            (p) => (p.role.alignment == "Village" || p.role.alignment == "Independent")
+          );
+          let shuffledPlayers = Random.randomizeArray(players);
+          for (let x = 0; x < shuffledPlayers.length; x++) {
+            if (
+              shuffledPlayers[x].role.name == "Templar"
+            ) {
+              return;
+            }
+          }
+          shuffledPlayers = shuffledPlayers.filter((p) => !p.role.data.reroll);
+          if (shuffledPlayers.length <= 0) return;
+            for (let item of shuffledPlayers[0].items) {
+              item.drop();
+            }
+            shuffledPlayers[0].setRole("Templar", undefined, false, true);
+            shuffledPlayers[0].role.data.reroll = true;
+            shuffledPlayers[0].role.data.hasCopied = true;
+        }
+        else if (this.player.role.name == "Vice President") {
           let players = this.game.players.filter(
             (p) => p.role.alignment == "Village" || p.role.alignment == "Independent"
           );
           let shuffledPlayers = Random.randomizeArray(players);
           for (let x = 0; x < shuffledPlayers.length; x++) {
             if (
-              shuffledPlayers[x].role.name == "Templar"
+              shuffledPlayers[x].role.name == "President"
             ) {
               return;
             }
