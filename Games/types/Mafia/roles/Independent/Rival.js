2ca4c076334e171164e6e3d6ebb8191eb613809c
SawJester <anglelord270@gmail.com>
2024-09-22 12:30:35 -0400

    Update Rival.js

--- Games/types/Mafia/roles/Independent/Rival.js
+++ Games/types/Mafia/roles/Independent/Rival.js
@@ -6,6 +6,6 @@ module.exports = class Rival extends Role {
 
     this.alignment = "Independent";
     this.winCount = "Village";
-    this.cards = ["VillageCore", "WinIfRivalIsDead"];
+    this.cards = ["VillageCore", "WinIfRivalIsDead","AddCopyOfRole"];
   }
 };
