3bea2126682be18b7f7b8891f566cff3ac859f32
SawJester <anglelord270@gmail.com>
2024-10-02 19:04:30 -0400

    Snake Charmer fix (#1734)

--- Games/types/Mafia/roles/Village/SnakeCharmer.js
+++ Games/types/Mafia/roles/Village/SnakeCharmer.js
@@ -9,7 +9,7 @@ module.exports = class SnakeCharmer extends Role {
       "VillageCore",
       "WinWithFaction",
       "MeetingFaction",
-      "SwapRolesIfEvil",
+      "SwapRolesOnce",
     ];
   }
 };
