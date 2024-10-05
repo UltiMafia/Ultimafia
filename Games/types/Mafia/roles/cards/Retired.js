92d6818d926614dc4f2f325699ae58f3040fac0e
SawJester <anglelord270@gmail.com>
2024-10-05 18:16:23 -0400

    Create Retired.js

---
+++ Games/types/Mafia/roles/cards/Retired.js
@@ -0,0 +1,40 @@
+const Card = require("../../Card");
+const Random = require("../../../../../lib/Random");
+
+module.exports = class Retired extends Card {
+  constructor(role) {
+    super(role);
+
+    this.listeners = {
+      SwitchRoleBefore: function (player) {
+        if (player != this.player) return;
+        this.player.role.data.reroll = true;
+        this.player.holdItem("Retirement",this.player.role.name);
+        
+      
+
+        let newRole = "Grouch";
+
+        if(this.player.role.alignment == "Village"){
+          newRole = "Villager";
+        }
+        else if(this.player.role.alignment == "Mafia"){
+          newRole = "Mafioso";
+        }
+        else if(this.player.role.alignment == "Cult"){
+          newRole = "Cultist";
+        }
+
+        
+        this.player.setRole(
+          newRole,
+          undefined,
+          false,
+          true,
+          false,
+          "No Change"
+        );
+      },
+    };
+  }
+};
