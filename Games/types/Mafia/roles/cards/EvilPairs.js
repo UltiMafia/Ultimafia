7490203b3c18943d4cbcd043b035da7b4451012e
SawJester <SawJester@users.noreply.github.com>
2024-10-08 04:13:33 +0000

    Prettified Code!

--- Games/types/Mafia/roles/cards/EvilPairs.js
+++ Games/types/Mafia/roles/cards/EvilPairs.js
@@ -1,5 +1,7 @@
 const Card = require("../../Card");
-const { PRIORITY_INVESTIGATIVE_AFTER_RESOLVE_DEFAULT } = require("../../const/Priority");
+const {
+  PRIORITY_INVESTIGATIVE_AFTER_RESOLVE_DEFAULT,
+} = require("../../const/Priority");
 
 module.exports = class EvilPairs extends Card {
   constructor(role) {
@@ -7,7 +9,7 @@ module.exports = class EvilPairs extends Card {
 
     this.actions = [
       {
-        priority: PRIORITY_INVESTIGATIVE_AFTER_RESOLVE_DEFAULT-10,
+        priority: PRIORITY_INVESTIGATIVE_AFTER_RESOLVE_DEFAULT - 10,
         labels: ["investigate"],
         run: function () {
           if (this.game.getStateName() != "Night") return;
