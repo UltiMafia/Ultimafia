ffd518bd7e4b92b3dfbf9efa216013cabc18f8cf
SawJester <anglelord270@gmail.com>
2024-10-10 22:55:56 -0400

    Update SwapTwoOtherRoles.js

--- Games/types/Mafia/roles/cards/SwapTwoOtherRoles.js
+++ Games/types/Mafia/roles/cards/SwapTwoOtherRoles.js
@@ -10,7 +10,7 @@ module.exports = class SwapTwoOtherRoles extends Card {
         states: ["Night"],
         flags: ["voting"],
         action: {
-          priority: PRIORITY_SWAP_ROLES - 1,
+          priority: PRIORITY_SWAP_ROLES + 1,
           run: function () {
             this.actor.role.data.targetA = this.target;
           },
