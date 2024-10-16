d68fcb723db1fa64170c41e800e2f6591dd38aa8
SawJester <anglelord270@gmail.com>
2024-10-16 14:29:05 -0400

    Create Superhero.js

---
+++ Games/types/Mafia/roles/Independent/Superhero.js
@@ -0,0 +1,10 @@
+const Role = require("../../Role");
+
+module.exports = class Superhero extends Role {
+  constructor(player, data) {
+    super("Superhero", player, data);
+
+    this.alignment = "Independent";
+    this.cards = ["VillageCore", "WinWithIndependentMajority", "GiveSuperpowers"];
+  }
+};
