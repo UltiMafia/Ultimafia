4e24e86b61b7de648891f4c4041678a16c865849
SawJester <anglelord270@gmail.com>
2024-10-19 12:11:25 -0400

    Update MeetingFaction.js

--- Games/types/Mafia/roles/cards/MeetingFaction.js
+++ Games/types/Mafia/roles/cards/MeetingFaction.js
@@ -15,7 +15,81 @@ module.exports = class MeetingFaction extends Card {
   constructor(role) {
     super(role);
 
+    let meetingName = this.player.faction;
+    let meetingNameKill = `${this.role.player.faction} Kill`;
+
+
+      this.meetings[meetingName] = {
+      actionName: "End Meeting?",
+        states: ["Night"],
+        flags: ["group", "speech", "voting", "mustAct", "noVeg", "Important"],
+        inputType: "boolean",
+        shouldMeet: function (meetingName) {
+          //let lunatics = this.game.players.filter((p) => p.hasItem("IsTheLunatic"));
+          let meetingPlayers = this.game.players.filter((p) =>
+            FACTION_WITH_MEETING.includes(p.faction)
+          );
+
+          if (
+            this.game
+              .getRoleTags(this.player.role.name)
+              .join("")
+              .includes("AnonymizeMeeting") &&
+            meetingPlayers.length > 0
+          ) {
+            return true;
+          }
+
+          return (
+            !this.player.hasItem("IsTheTelevangelist") &&
+            FACTION_WITH_MEETING.includes(this.player.faction) &&
+            !FACTION_KILL.includes(this.player.faction)
+          );
+        },
+    };
+
+    this.meetings[meetingNameKill] = {
+        actionName: `${this.role.player.faction} Kill`,
+        states: ["Night"],
+        flags: ["group", "speech", "voting", "multiActor", "Important"],
+        targets: {
+          include: ["alive"],
+          exclude: [excludeMafiaOnlyIfNotAnonymous],
+        },
+        shouldMeet: function (meetingName) {
+          //let lunatics = this.game.players.filter((p) => p.hasItem("IsTheLunatic"));
+
+          let meetingPlayers = this.game.players.filter((p) =>
+            FACTION_KILL.includes(p.faction)
+          );
+          if (
+            this.game
+              .getRoleTags(this.player.role.name)
+              .join("")
+              .includes("AnonymizeMeeting") &&
+            meetingPlayers.length > 0
+          ) {
+            return true;
+          }
+
+          return (
+            FACTION_WITH_MEETING.includes(this.player.faction) &&
+            FACTION_KILL.includes(this.player.faction)
+          );
+        },
+        action: {
+          labels: ["kill", "mafia"],
+          priority: PRIORITY_MAFIA_KILL,
+          run: function () {
+            if (this.dominates()) {
+              this.target.kill("basic", this.actor);
+            }
+          },
+        },
+    };
+
     this.meetings = {
+      /*
       Faction: {
         actionName: "End Meeting?",
         states: ["Night"],
@@ -44,6 +118,7 @@ module.exports = class MeetingFaction extends Card {
           );
         },
       },
+      */
       "Faction Fake": {
         actionName: "End Meeting?",
         states: ["Night"],
@@ -83,6 +158,7 @@ module.exports = class MeetingFaction extends Card {
           );
         },
       },
+      /*
       "Faction Kill": {
         actionName: "Mafia Kill",
         states: ["Night"],
@@ -122,6 +198,7 @@ module.exports = class MeetingFaction extends Card {
           },
         },
       },
+      */
     };
     this.meetingMods = {
       "Faction Fake": {
