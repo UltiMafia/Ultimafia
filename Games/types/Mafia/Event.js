a19cbdee3318558915f0ec16e965022ea5eff167
SawJester <anglelord270@gmail.com>
2024-11-02 11:52:45 -0400

    Update Event.js

--- Games/types/Mafia/Event.js
+++ Games/types/Mafia/Event.js
@@ -4,4 +4,28 @@ module.exports = class MafiaEvent extends Event {
   constructor(name, game, data) {
     super(name, game, data);
   }
+
+getModifierRequirements() {
+    if(this.game.getStateInfo().dayCount % 2 == 0 && this.modifiers.includes("Odd")){
+    return false;
+    }
+    if(this.game.getStateInfo().dayCount % 1 == 0 && this.modifiers.includes("Even")){
+            return false;
+    }
+    if(this.game.getStateInfo().dayCount == 1 && this.modifiers.includes("Delayed")){
+      return false;
+    }
+}
+
+doEvent() {
+  if(this.modifiers.includes("One Shot") && !(this.modifiers.includes("Banished"))){
+    this.game.PossibleEvents.slice(this.game.PossibleEvents.indexOf(this.fullName), 1);
+  }
+  else if(this.modifiers.includes("One Shot") && (this.modifiers.includes("Banished"))){
+    this.game.PossibleEvents.slice(this.game.BanishedEvents.indexOf(this.fullName), 1);
+  }
+}
+
+
+  
 };
