56d22661e0d18679937b27b7c19afc013aad8a5d
SawJester <SawJester@users.noreply.github.com>
2024-11-02 16:03:01 +0000

    Prettified Code!

--- Games/types/Mafia/Events/TimeLoop.js
+++ Games/types/Mafia/Events/TimeLoop.js
@@ -13,30 +13,30 @@ module.exports = class TimeLoop extends Event {
 
   doEvent() {
     super.doEvent();
-          let victim = Random.randArrayVal(this.game.alivePlayers());
-          let L = function () {
-            if (this.role.data.doTimeLoop == true) {
-              this.role.data.doTimeLoop = false;
-              return true;
-            } else {
-              return false;
-            }
-          };
-          L = L.bind(this.holder);
-          this.holder.role.data.doTimeLoop = true;
-          this.game.setStateShouldSkip("Day", L);
-          this.action = new Action({
-            actor: victim,
-            target: victim,
-            game: this.game,
-            priority: PRIORITY_BECOME_DEAD_ROLE,
-            labels: ["hidden", "absolute"],
-            run: function () {
-              if (this.game.SilentEvents != false) {
-                this.game.queueAlert(`Event: Time Loop, It's Night Again!`);
-              }
-            },
-          });
-          this.game.queueAction(this.action);
+    let victim = Random.randArrayVal(this.game.alivePlayers());
+    let L = function () {
+      if (this.role.data.doTimeLoop == true) {
+        this.role.data.doTimeLoop = false;
+        return true;
+      } else {
+        return false;
+      }
+    };
+    L = L.bind(this.holder);
+    this.holder.role.data.doTimeLoop = true;
+    this.game.setStateShouldSkip("Day", L);
+    this.action = new Action({
+      actor: victim,
+      target: victim,
+      game: this.game,
+      priority: PRIORITY_BECOME_DEAD_ROLE,
+      labels: ["hidden", "absolute"],
+      run: function () {
+        if (this.game.SilentEvents != false) {
+          this.game.queueAlert(`Event: Time Loop, It's Night Again!`);
+        }
+      },
+    });
+    this.game.queueAction(this.action);
   }
 };
