9a81a8080ead4b9ceea31521407ba7a1220d982a
SawJester <SawJester@users.noreply.github.com>
2024-11-02 15:53:08 +0000

    Prettified Code!

--- Games/types/Mafia/Event.js
+++ Games/types/Mafia/Event.js
@@ -5,27 +5,44 @@ module.exports = class MafiaEvent extends Event {
     super(name, game, data);
   }
 
-getModifierRequirements() {
-    if(this.game.getStateInfo().dayCount % 2 == 0 && this.modifiers.includes("Odd")){
-    return false;
-    }
-    if(this.game.getStateInfo().dayCount % 1 == 0 && this.modifiers.includes("Even")){
-            return false;
-    }
-    if(this.game.getStateInfo().dayCount == 1 && this.modifiers.includes("Delayed")){
+  getModifierRequirements() {
+    if (
+      this.game.getStateInfo().dayCount % 2 == 0 &&
+      this.modifiers.includes("Odd")
+    ) {
       return false;
     }
-}
-
-doEvent() {
-  if(this.modifiers.includes("One Shot") && !(this.modifiers.includes("Banished"))){
-    this.game.PossibleEvents.slice(this.game.PossibleEvents.indexOf(this.fullName), 1);
+    if (
+      this.game.getStateInfo().dayCount % 1 == 0 &&
+      this.modifiers.includes("Even")
+    ) {
+      return false;
+    }
+    if (
+      this.game.getStateInfo().dayCount == 1 &&
+      this.modifiers.includes("Delayed")
+    ) {
+      return false;
+    }
   }
-  else if(this.modifiers.includes("One Shot") && (this.modifiers.includes("Banished"))){
-    this.game.PossibleEvents.slice(this.game.BanishedEvents.indexOf(this.fullName), 1);
+
+  doEvent() {
+    if (
+      this.modifiers.includes("One Shot") &&
+      !this.modifiers.includes("Banished")
+    ) {
+      this.game.PossibleEvents.slice(
+        this.game.PossibleEvents.indexOf(this.fullName),
+        1
+      );
+    } else if (
+      this.modifiers.includes("One Shot") &&
+      this.modifiers.includes("Banished")
+    ) {
+      this.game.PossibleEvents.slice(
+        this.game.BanishedEvents.indexOf(this.fullName),
+        1
+      );
+    }
   }
-}
-
-
-  
 };
