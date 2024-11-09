66312ee64fa29a37e5a63ba1db2b9f6cbd862e51
SawJester <anglelord270@gmail.com>
2024-11-08 23:30:38 -0500

    Create Exhausted.js

---
+++ Games/types/Mafia/roles/cards/Exhausted.js
@@ -0,0 +1,33 @@
+const Card = require("../../Card");
+const {
+  IMPORTANT_MEETINGS_NIGHT,
+  INVITED_MEETINGS,
+  STARTS_WITH_MEETINGS,
+  IMPORTANT_MEETINGS_DAY,
+} = require("../../const/ImportantMeetings");
+
+module.exports = class Exhausted extends Card {
+  constructor(role) {
+    super(role);
+
+    this.meetingMods = {
+      "*": {
+        shouldMeetMod: function (meetingName) {
+          for (let w = 0; w < IMPORTANT_MEETINGS_NIGHT.length; w++) {
+            if (meetingName == IMPORTANT_MEETINGS_NIGHT[w] || !meetingName) {
+              return true;
+            }
+          }
+          for (let w = 0; w < IMPORTANT_MEETINGS_DAY.length; w++) {
+            if (meetingName == IMPORTANT_MEETINGS_DAY[w] || !meetingName) {
+              return true;
+            }
+          }
+          if (meetingName == "Graveyard") return true;
+
+          return this.game.getStateInfo().id == 1;
+        },
+      },
+    };
+  }
+};
