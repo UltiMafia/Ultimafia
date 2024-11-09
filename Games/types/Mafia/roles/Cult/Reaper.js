34fef7b72d67f6bdd0dc33bfa54a8b0d5619d76b
SawJester <anglelord270@gmail.com>
2024-11-09 00:34:41 -0500

    Create Reaper.js

---
+++ Games/types/Mafia/roles/Cult/Reaper.js
@@ -0,0 +1,10 @@
+const Role = require("../../Role");
+
+module.exports = class Reaper extends Role {
+  constructor(player, data) {
+    super("Reaper", player, data);
+
+    this.alignment = "Cult";
+    this.cards = ["VillageCore", "WinWithFaction", "MeetingFaction", "StrongKiller"];
+  }
+};
