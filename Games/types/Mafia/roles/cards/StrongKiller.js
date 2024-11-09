d724642cf3f02ca62bd60e6db4d8440f7a76145e
SawJester <anglelord270@gmail.com>
2024-11-09 00:33:09 -0500

    Create StrongKiller.js

---
+++ Games/types/Mafia/roles/cards/StrongKiller.js
@@ -0,0 +1,24 @@
+const Card = require("../../Card");
+const { PRIORITY_KILL_DEFAULT } = require("../../const/Priority");
+
+module.exports = class StrongKiller extends Card {
+  constructor(role) {
+    super(role);
+
+    this.meetings = {
+      "Strong Kill": {
+        actionName: "Kill",
+        states: ["Night"],
+        flags: ["voting"],
+        action: {
+          labels: ["kill","strong"],
+          power: 999999,
+          priority: PRIORITY_KILL_DEFAULT + 1,
+          run: function () {
+            if (this.dominates()) this.target.kill("basic", this.actor);
+          },
+        },
+      },
+    };
+  }
+};
