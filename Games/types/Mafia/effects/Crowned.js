6ce8b681bffea7433e9f4646d0f4004cb6337b31
SawJester <anglelord270@gmail.com>
2024-11-14 21:51:17 -0500

    Update Crowned.js

--- Games/types/Mafia/effects/Crowned.js
+++ Games/types/Mafia/effects/Crowned.js
@@ -11,15 +11,28 @@ module.exports = class Crowned extends Effect {
     super.apply(player);
 
     let villageMeeting;
+    let room1;
+    let room2;
     for (const meeting of player.game.meetings) {
       if (meeting.name === "Village") {
         villageMeeting = meeting;
-        break;
+      }
+      if(meeting.name = "Room 1"){
+        room1 = meeting;
+      }
+      if(meeting.name = "Room 2"){
+        room2 = meeting;
       }
     }
 
     if (villageMeeting) {
       villageMeeting.members[player.id].voteWeight = Infinity;
+    }
+    if (room1) {
+      room1.members[player.id].voteWeight = Infinity;
+    }
+    if (room2) {
+      room2.members[player.id].voteWeight = Infinity;
     }
 
     /*
