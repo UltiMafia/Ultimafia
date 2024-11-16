1c7b01d1d22e57728437f7b47d0e13fba6038266
SawJester <SawJester@users.noreply.github.com>
2024-11-16 05:01:07 +0000

    Prettified Code!

--- Games/types/Mafia/effects/Whistleblown.js
+++ Games/types/Mafia/effects/Whistleblown.js
@@ -8,22 +8,16 @@ module.exports = class Whistleblown extends Effect {
 
   apply(player) {
     super.apply(player);
-                if (
-                this.game.RoomOne.length > 0 &&
-                this.game.RoomTwo.length > 0
-              ) {
-                if(this.game.RoomOne.includes(player)) {
-                  this.cannotVoteEffect = player.giveEffect("CannotVote", 1,"Room 1");
-                }
-                else if(this.game.RoomTwo.includes(player)) {
-                  this.cannotVoteEffect = player.giveEffect("CannotVote", 1,"Room 2");
-                }
-                else{
-                  this.cannotVoteEffect = player.giveEffect("CannotVote", 1);
-                }
-              }
-    else {
-    this.cannotVoteEffect = player.giveEffect("CannotVote", 1);
+    if (this.game.RoomOne.length > 0 && this.game.RoomTwo.length > 0) {
+      if (this.game.RoomOne.includes(player)) {
+        this.cannotVoteEffect = player.giveEffect("CannotVote", 1, "Room 1");
+      } else if (this.game.RoomTwo.includes(player)) {
+        this.cannotVoteEffect = player.giveEffect("CannotVote", 1, "Room 2");
+      } else {
+        this.cannotVoteEffect = player.giveEffect("CannotVote", 1);
+      }
+    } else {
+      this.cannotVoteEffect = player.giveEffect("CannotVote", 1);
     }
     this.cannotBeVotedEffect = player.giveEffect("CannotBeVoted", 1);
   }
