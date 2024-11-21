9596857467c74fbda748213f1eaf599cbbeb7561
SawJester <anglelord270@gmail.com>
2024-11-20 21:21:51 -0500

    Update Vain.js

--- Games/types/Mafia/roles/cards/Vain.js
+++ Games/types/Mafia/roles/cards/Vain.js
@@ -1,4 +1,5 @@
 const Card = require("../../Card");
+const Action = require("../../Action");
 const { PRIORITY_KILL_DEFAULT } = require("../../const/Priority");
 
 module.exports = class Vain extends Card {
@@ -10,21 +11,53 @@ module.exports = class Vain extends Card {
         priority: PRIORITY_KILL_DEFAULT,
         labels: ["kill", "hidden", "absolute"],
         run: function () {
-          if (this.game.getStateName() != "Night") return;
-          if (
-            this.actor.getMeetingByName("Mafia") ||
-            this.actor.getMeetingByName("Cultists")
+        
+                   if (
+            this.game.getStateName() != "Night" &&
+            this.game.getStateName() != "Dawn"
           )
             return;
 
           if (!this.actor.alive) return;
 
-          let visits = this.getVisits(this.actor);
-          let sameAlignmentVisits = visits.filter(
-            (v) => v.role.alignment == this.actor.role.alignment
-          );
-          if (sameAlignmentVisits.length > 0 && this.dominates(this.actor)) {
-            this.actor.kill("basic", this.actor);
+          for (let action of this.game.actions[0]) {
+            if (action.hasLabel("absolute")) {
+              continue;
+            }
+            if (action.hasLabel("mafia")) {
+              continue;
+            }
+            if (action.hasLabel("hidden")) {
+              continue;
+            }
+
+            let toCheck = action.target;
+            if (!Array.isArray(action.target)) {
+              toCheck = [action.target];
+            }
+
+            if (
+              action.actors.indexOf(this.actor) != -1 &&
+              !action.hasLabel("hidden") &&
+              action.target &&
+              toCheck[0] instanceof Player
+            ) {
+              for (let y = 0; y < toCheck.length; y++) {
+              if (toCheck[y].role.alignment == this.actor.alignment)) {
+          let action = new Action({
+            actor: this.actor,
+            target: this.actor,
+            game: this.game,
+            labels: ["kill", "hidden"],
+            run: function () {
+              if (this.dominates()) this.target.kill("basic", this.actor, true);
+            },
+          });
+          this.game.instantAction(action);
+                  return;
+                }
+              }
+            }
           }
         },
       },
