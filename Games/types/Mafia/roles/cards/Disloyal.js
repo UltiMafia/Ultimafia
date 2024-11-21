46efb747050f74c8687a354a00cbff7c740687cb
SawJester <SawJester@users.noreply.github.com>
2024-11-21 01:46:55 +0000

    Prettified Code!

--- Games/types/Mafia/roles/cards/Disloyal.js
+++ Games/types/Mafia/roles/cards/Disloyal.js
@@ -11,7 +11,11 @@ module.exports = class Disloyal extends Card {
         priority: PRIORITY_NIGHT_ROLE_BLOCKER - 1,
         labels: ["block", "hidden", "absolute"],
         run: function () {
-          if (this.game.getStateName() != "Night" && this.game.getStateName() != "Dawn") return;
+          if (
+            this.game.getStateName() != "Night" &&
+            this.game.getStateName() != "Dawn"
+          )
+            return;
 
           if (!this.actor.alive) return;
 
