3568468c89504615e0a770efeea4818df8617a97
SawJester <SawJester@users.noreply.github.com>
2024-11-22 21:59:17 +0000

    Prettified Code!

--- Games/types/Mafia/roles/cards/MakeSkittishOnRoleShare.js
+++ Games/types/Mafia/roles/cards/MakeSkittishOnRoleShare.js
@@ -7,35 +7,38 @@ module.exports = class MakeSkittishOnRoleShare extends Card {
     super(role);
 
     this.listeners = {
-      ShareRole: function (PlayerA, PlayerB, isAlignmentShare){
-        if((PlayerA == this.player || PlayerB == this.player) && !isAlignmentShare){
+      ShareRole: function (PlayerA, PlayerB, isAlignmentShare) {
+        if (
+          (PlayerA == this.player || PlayerB == this.player) &&
+          !isAlignmentShare
+        ) {
           let thisPlayer;
           let otherPlayer;
-          if(PlayerA == this.player){
+          if (PlayerA == this.player) {
             thisPlayer = PlayerA;
             otherPlayer = PlayerB;
-          }
-          else{
+          } else {
             thisPlayer = PlayerB;
             otherPlayer = PlayerA;
           }
-              var action = new Action({
-              actor: this.player,
-              target: otherPlayer,
-              game: this.game,
-              item: this,
-              labels: ["hidden"],
-              run: function () {
-                if (this.dominates(this.target)) {
-                  this.target.queueAlert(`Your feeling Skittish after Role Sharing with ${this.actor.name}`);
-                    this.target.giveEffect("MustRoleShare");
-                  }
-              },
-            });
-            this.game.instantAction(action);
+          var action = new Action({
+            actor: this.player,
+            target: otherPlayer,
+            game: this.game,
+            item: this,
+            labels: ["hidden"],
+            run: function () {
+              if (this.dominates(this.target)) {
+                this.target.queueAlert(
+                  `Your feeling Skittish after Role Sharing with ${this.actor.name}`
+                );
+                this.target.giveEffect("MustRoleShare");
+              }
+            },
+          });
+          this.game.instantAction(action);
         }
       },
-      
     };
   }
 };
