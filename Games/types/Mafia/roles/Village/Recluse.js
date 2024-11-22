80073b7a2bcf102cd924a643c38c4c9925c932f4
SawJester <anglelord270@gmail.com>
2024-11-22 18:15:55 -0500

    Create Recluse.js

---
+++ Games/types/Mafia/roles/Village/Recluse.js
@@ -0,0 +1,16 @@
+const Role = require("../../Role");
+
+module.exports = class Recluse extends Role {
+  constructor(player, data) {
+    super("Recluse", player, data);
+
+    this.alignment = "Village";
+    this.cards = [
+      "VillageCore",
+      "WinWithFaction",
+      "MeetingFaction",
+      "MakeShyOnRoleShare",
+    ];
+    
+  }
+};
