e2a4ea9bca4d9eb5b2d5719a014980cbeffd8dae
SawJester <SawJester@users.noreply.github.com>
2024-11-28 05:10:40 +0000

    Prettified Code!

--- Games/types/Mafia/roles/cards/Commuting.js
+++ Games/types/Mafia/roles/cards/Commuting.js
@@ -4,7 +4,7 @@ const { PRIORITY_BLOCK_VISITORS } = require("../../const/Priority");
 module.exports = class Commuting extends Card {
   constructor(role) {
     super(role);
-/*
+    /*
     this.actions = [
       {
         priority: PRIORITY_BLOCK_VISITORS,
@@ -37,32 +37,29 @@ module.exports = class Commuting extends Card {
           return;
         }
 
-
         var action = new Action({
-        actor: this.player,
-        game: this.player.game,
-        priority: PRIORITY_BLOCK_VISITORS,
-        labels: ["block", "hidden"],
-        run: function () {
-
-          for (let action of this.game.actions[0]) {
-            if (action.target == this.actor && !action.hasLabel("hidden")) {
-              for (let _action of this.game.actions[0]) {
-                if (
-                  _action.priority > this.priority &&
-                  !_action.hasLabel("absolute")
-                ) {
-                  _action.cancelActor(action.actor);
+          actor: this.player,
+          game: this.player.game,
+          priority: PRIORITY_BLOCK_VISITORS,
+          labels: ["block", "hidden"],
+          run: function () {
+            for (let action of this.game.actions[0]) {
+              if (action.target == this.actor && !action.hasLabel("hidden")) {
+                for (let _action of this.game.actions[0]) {
+                  if (
+                    _action.priority > this.priority &&
+                    !_action.hasLabel("absolute")
+                  ) {
+                    _action.cancelActor(action.actor);
+                  }
                 }
               }
             }
-          }
-        },
+          },
         });
 
         this.game.queueAction(action);
       },
     };
-    
   }
 };
