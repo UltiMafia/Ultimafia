af7431e3de211a02dc38f0ad34a3615c90a3975a
SawJester <anglelord270@gmail.com>
2024-11-27 21:39:33 -0500

    Update BlockIfVisited.js

--- Games/types/Mafia/roles/cards/BlockIfVisited.js
+++ Games/types/Mafia/roles/cards/BlockIfVisited.js
@@ -4,7 +4,7 @@ const { PRIORITY_NIGHT_ROLE_BLOCKER } = require("../../const/Priority");
 module.exports = class BlockIfVisited extends Card {
   constructor(role) {
     super(role);
-
+/*
     this.actions = [
       {
         priority: PRIORITY_NIGHT_ROLE_BLOCKER,
@@ -18,5 +18,27 @@ module.exports = class BlockIfVisited extends Card {
         },
       },
     ];
+*/
+      this.listeners = {
+      state: function (stateInfo) {
+        if (!stateInfo.name.match(/Night/)) {
+          return;
+        }
+        var action = new Action({
+          actor: this.player,
+          game: this.player.game,
+        priority: PRIORITY_NIGHT_ROLE_BLOCKER,
+        labels: ["block", "hidden"],
+        run: function () {
+          if (this.hasVisitors() === true) {
+            this.blockActions(this.actor);
+          }
+        },
+        });
+
+        this.game.queueAction(action);
+      },
+    };
+    
   }
 };
