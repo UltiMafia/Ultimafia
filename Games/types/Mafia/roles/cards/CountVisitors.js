d16f9f146c13c0f7e37381fcd021ba8452666284
SawJester <SawJester@users.noreply.github.com>
2024-11-28 16:22:37 +0000

    Prettified Code!

--- Games/types/Mafia/roles/cards/CountVisitors.js
+++ Games/types/Mafia/roles/cards/CountVisitors.js
@@ -5,7 +5,7 @@ const { PRIORITY_INVESTIGATIVE_DEFAULT } = require("../../const/Priority");
 module.exports = class CountVisitors extends Card {
   constructor(role) {
     super(role);
-/*
+    /*
     this.actions = [
       {
         priority: PRIORITY_INVESTIGATIVE_DEFAULT,
@@ -45,36 +45,34 @@ module.exports = class CountVisitors extends Card {
         }
 
         var action = new Action({
-        actor: this.player,
-        game: this.player.game,
-        priority: PRIORITY_INVESTIGATIVE_DEFAULT,
-        labels: ["hidden", "absolute"],
-        run: function () {
-          let visitors = this.actor.role.data.visitors;
-          if (visitors) {
-            let unique = new Set(visitors);
+          actor: this.player,
+          game: this.player.game,
+          priority: PRIORITY_INVESTIGATIVE_DEFAULT,
+          labels: ["hidden", "absolute"],
+          run: function () {
+            let visitors = this.actor.role.data.visitors;
+            if (visitors) {
+              let unique = new Set(visitors);
 
-            if (this.actor.hasEffect("FalseMode")) {
-              let num = 0;
-              if (unique.size == 0) num = 1;
-              else num = 0;
+              if (this.actor.hasEffect("FalseMode")) {
+                let num = 0;
+                if (unique.size == 0) num = 1;
+                else num = 0;
+                this.actor.queueAlert(
+                  `:visited: You were visited by ${num} people last night.`
+                );
+                return;
+              }
+
               this.actor.queueAlert(
-                `:visited: You were visited by ${num} people last night.`
+                `:visited: You were visited by ${unique.size} people last night.`
               );
-              return;
             }
-
-            this.actor.queueAlert(
-              `:visited: You were visited by ${unique.size} people last night.`
-            );
-          }
-        },
+          },
         });
 
         this.game.queueAction(action);
       },
     };
-
-    
   }
 };
