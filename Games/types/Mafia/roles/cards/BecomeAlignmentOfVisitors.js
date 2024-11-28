5f94f71ac34267476b4d0f810da879e159fc4458
SawJester <anglelord270@gmail.com>
2024-11-27 21:22:30 -0500

    Update BecomeAlignmentOfVisitors.js

--- Games/types/Mafia/roles/cards/BecomeAlignmentOfVisitors.js
+++ Games/types/Mafia/roles/cards/BecomeAlignmentOfVisitors.js
@@ -7,7 +7,7 @@ const {
 module.exports = class BecomeAlignmentOfVisitors extends Card {
   constructor(role) {
     super(role);
-
+/*
     this.actions = [
       {
         priority: PRIORITY_BLOCK_VISITORS - 1,
@@ -27,25 +27,44 @@ module.exports = class BecomeAlignmentOfVisitors extends Card {
             this.actor.faction = visit.faction;
             return;
           }
-          /*
-          for (let action of this.game.actions[0]) {
-            if (action.target == this.actor && !action.hasLabel("hidden")) {
-
-              if (
-                   action.priority > this.priority
-                ) {
-                if (this.dominates(action.actor)) {
-                  this.blockWithMindRot(action.actor);
-                }
-              }
-                this.actor.queueAlert(`After Hitchhiking with a player you feel like Supporting the ${action.actors [0].role.alignment}.`);
-                this.actor.role.alignment = action.actor.role.alignment;     
-                return;
-            }
-          }
-          */
         },
       },
     ];
+    */
+    this.listeners = {
+      state: function (stateInfo) {
+        if (!this.player.alive) {
+          return;
+        }
+        if (!stateInfo.name.match(/Night/)) {
+          return;
+        }
+        var action = new Action({
+          actor: this.player,
+          target: target,
+          game: this.player.game,
+                  priority: PRIORITY_BLOCK_VISITORS - 1,
+        labels: ["block", "hidden"],
+        run: function () {
+          if (!this.actor.alive) return;
+
+          for (let visit of this.getVisitors(this.actor)) {
+            if (this.dominates(visit)) {
+              this.blockWithMindRot(visit);
+            }
+
+            this.actor.queueAlert(
+              `After Hitchhiking with a player you feel like Supporting the ${visit.faction}.`
+            );
+            this.actor.faction = visit.faction;
+            return;
+          }
+        },
+        });
+
+        this.game.queueAction(action);
+      },
+    };
+    
   }
 };
