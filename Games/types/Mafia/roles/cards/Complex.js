d385d3ff38ca5414470237936649077c50aab4db
SawJester <SawJester@users.noreply.github.com>
2024-11-28 15:37:17 +0000

    Prettified Code!

--- Games/types/Mafia/roles/cards/Complex.js
+++ Games/types/Mafia/roles/cards/Complex.js
@@ -5,7 +5,7 @@ const { PRIORITY_NIGHT_ROLE_BLOCKER } = require("../../const/Priority");
 module.exports = class Complex extends Card {
   constructor(role) {
     super(role);
-/*
+    /*
     this.actions = [
       {
         priority: PRIORITY_NIGHT_ROLE_BLOCKER,
@@ -66,54 +66,51 @@ module.exports = class Complex extends Card {
         var action = new Action({
           actor: this.player,
           game: this.player.game,
-        priority: PRIORITY_NIGHT_ROLE_BLOCKER,
-        labels: ["block", "hidden"],
-        run: function () {
-          if (!this.actor.alive) return;
+          priority: PRIORITY_NIGHT_ROLE_BLOCKER,
+          labels: ["block", "hidden"],
+          run: function () {
+            if (!this.actor.alive) return;
 
-          for (let action of this.game.actions[0]) {
-            if (action.hasLabel("absolute")) {
-              continue;
-            }
-            if (action.hasLabel("mafia")) {
-              continue;
-            }
-            if (action.hasLabel("hidden")) {
-              continue;
-            }
+            for (let action of this.game.actions[0]) {
+              if (action.hasLabel("absolute")) {
+                continue;
+              }
+              if (action.hasLabel("mafia")) {
+                continue;
+              }
+              if (action.hasLabel("hidden")) {
+                continue;
+              }
 
-            let toCheck = action.target;
-            if (!Array.isArray(action.target)) {
-              toCheck = [action.target];
-            }
+              let toCheck = action.target;
+              if (!Array.isArray(action.target)) {
+                toCheck = [action.target];
+              }
 
-            if (
-              action.actors.indexOf(this.actor) != -1 &&
-              !action.hasLabel("hidden") &&
-              action.target &&
-              toCheck[0] instanceof Player
-            ) {
-              for (let y = 0; y < toCheck.length; y++) {
-                if (this.isVanillaRole(toCheck[y])) {
-                  if (
-                    action.priority > this.priority &&
-                    !action.hasLabel("absolute")
-                  ) {
-                    action.cancelActor(this.actor);
-                    break;
+              if (
+                action.actors.indexOf(this.actor) != -1 &&
+                !action.hasLabel("hidden") &&
+                action.target &&
+                toCheck[0] instanceof Player
+              ) {
+                for (let y = 0; y < toCheck.length; y++) {
+                  if (this.isVanillaRole(toCheck[y])) {
+                    if (
+                      action.priority > this.priority &&
+                      !action.hasLabel("absolute")
+                    ) {
+                      action.cancelActor(this.actor);
+                      break;
+                    }
                   }
                 }
               }
             }
-          }
-        },
+          },
         });
 
         this.game.queueAction(action);
       },
     };
-
-
-    
   }
 };
