8f3298679ffe113358c655cfc6ffafc4b7b85d37
SawJester <SawJester@users.noreply.github.com>
2024-11-28 02:47:34 +0000

    Prettified Code!

--- Games/types/Mafia/roles/cards/BlockTargets.js
+++ Games/types/Mafia/roles/cards/BlockTargets.js
@@ -4,7 +4,7 @@ const { PRIORITY_NIGHT_ROLE_BLOCKER } = require("../../const/Priority");
 module.exports = class BlockTargets extends Card {
   constructor(role) {
     super(role);
-/*
+    /*
     this.actions = [
       {
         priority: PRIORITY_NIGHT_ROLE_BLOCKER,
@@ -26,17 +26,16 @@ module.exports = class BlockTargets extends Card {
         var action = new Action({
           actor: this.player,
           game: this.player.game,
-        priority: PRIORITY_NIGHT_ROLE_BLOCKER,
-        labels: ["block", "hidden"],
-        run: function () {
-          let visits = this.getVisits(this.actor);
-          visits.map((v) => this.blockActions(v));
-        },
+          priority: PRIORITY_NIGHT_ROLE_BLOCKER,
+          labels: ["block", "hidden"],
+          run: function () {
+            let visits = this.getVisits(this.actor);
+            visits.map((v) => this.blockActions(v));
+          },
         });
 
         this.game.queueAction(action);
       },
     };
-    
   }
 };
