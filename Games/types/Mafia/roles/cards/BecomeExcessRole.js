c006f87ba331e6b736b1c56c7f453e222558b400
SawJester <SawJester@users.noreply.github.com>
2024-11-28 02:27:30 +0000

    Prettified Code!

--- Games/types/Mafia/roles/cards/BecomeExcessRole.js
+++ Games/types/Mafia/roles/cards/BecomeExcessRole.js
@@ -6,7 +6,7 @@ const { PRIORITY_BECOME_DEAD_ROLE } = require("../../const/Priority");
 module.exports = class BecomeExcessRole extends Card {
   constructor(role) {
     super(role);
-/*
+    /*
     this.actions = [
       {
         priority: PRIORITY_BECOME_DEAD_ROLE,
@@ -79,30 +79,30 @@ module.exports = class BecomeExcessRole extends Card {
           actor: this.player,
           game: this.player.game,
           priority: PRIORITY_BECOME_DEAD_ROLE,
-        labels: ["convert"],
-        run: function () {
-          if (this.game.getStateName() != "Night") return;
+          labels: ["convert"],
+          run: function () {
+            if (this.game.getStateName() != "Night") return;
 
-          if (!this.actor.alive) return;
+            if (!this.actor.alive) return;
 
-          let roles = this.game.PossibleRoles.filter((r) => r);
-          let players = this.game.players.filter((p) => p.role);
-          let currentRoles = [];
+            let roles = this.game.PossibleRoles.filter((r) => r);
+            let players = this.game.players.filter((p) => p.role);
+            let currentRoles = [];
 
-          for (let x = 0; x < players.length; x++) {
-            currentRoles.push(players[x].role);
-          }
-          for (let y = 0; y < currentRoles.length; y++) {
-            roles = roles.filter(
-              (r) =>
-                r.split(":")[0] != currentRoles[y].name &&
-                (this.game.getRoleAlignment(r) ==
-                  this.game.getRoleAlignment(this.actor.role.name) ||
-                  this.game.getRoleAlignment(this.actor.role.name) ==
-                    "Independent")
-            );
-          }
-          if (roles.length <= 0) {
+            for (let x = 0; x < players.length; x++) {
+              currentRoles.push(players[x].role);
+            }
+            for (let y = 0; y < currentRoles.length; y++) {
+              roles = roles.filter(
+                (r) =>
+                  r.split(":")[0] != currentRoles[y].name &&
+                  (this.game.getRoleAlignment(r) ==
+                    this.game.getRoleAlignment(this.actor.role.name) ||
+                    this.game.getRoleAlignment(this.actor.role.name) ==
+                      "Independent")
+              );
+            }
+            if (roles.length <= 0) {
               roles = Object.entries(AllRoles.Mafia)
                 .filter(
                   (roleData) =>
@@ -110,25 +110,23 @@ module.exports = class BecomeExcessRole extends Card {
                     this.game.getRoleAlignment(this.actor.role.name)
                 )
                 .map((roleData) => roleData[0]);
-          }
+            }
 
-          let newRole = Random.randArrayVal(roles);
+            let newRole = Random.randArrayVal(roles);
 
-          this.actor.setRole(
-            newRole,
-            undefined,
-            false,
-            false,
-            false,
-            "No Change"
-          );
-        },
+            this.actor.setRole(
+              newRole,
+              undefined,
+              false,
+              false,
+              false,
+              "No Change"
+            );
+          },
         });
 
         this.game.queueAction(action);
       },
     };
-
-    
   }
 };
