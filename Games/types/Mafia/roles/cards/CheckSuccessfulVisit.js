e57a963b1cf539ef8028a26ce417800c86614e52
SawJester <SawJester@users.noreply.github.com>
2024-11-28 02:55:47 +0000

    Prettified Code!

--- Games/types/Mafia/roles/cards/CheckSuccessfulVisit.js
+++ Games/types/Mafia/roles/cards/CheckSuccessfulVisit.js
@@ -4,7 +4,7 @@ const { PRIORITY_INVESTIGATIVE_DEFAULT } = require("../../const/Priority");
 module.exports = class CheckSuccessfulVisit extends Card {
   constructor(role) {
     super(role);
-/*
+    /*
     this.actions = [
       {
         priority: PRIORITY_INVESTIGATIVE_DEFAULT,
@@ -38,30 +38,29 @@ module.exports = class CheckSuccessfulVisit extends Card {
 
         var action = new Action({
           actor: this.player,
-        priority: PRIORITY_INVESTIGATIVE_DEFAULT,
-        labels: ["investigate", "hidden"],
+          priority: PRIORITY_INVESTIGATIVE_DEFAULT,
+          labels: ["investigate", "hidden"],
           game: this.player.game,
-        run: function () {
-          let targets = this.getVisits(this.actor);
-          let targetNames = targets.map((t) => t.name);
+          run: function () {
+            let targets = this.getVisits(this.actor);
+            let targetNames = targets.map((t) => t.name);
 
-          if (this.actor.hasEffect("FalseMode")) {
-            if (targetNames.length >= 1) return;
-          }
+            if (this.actor.hasEffect("FalseMode")) {
+              if (targetNames.length >= 1) return;
+            }
 
-          if (targetNames.length >= 1) {
-            this.actor.queueAlert(
-              `:invest: You learn that your visit to ${targetNames.join(
-                ", "
-              )} was successful.`
-            );
-          }
-        }, 
+            if (targetNames.length >= 1) {
+              this.actor.queueAlert(
+                `:invest: You learn that your visit to ${targetNames.join(
+                  ", "
+                )} was successful.`
+              );
+            }
+          },
         });
 
         this.game.queueAction(action);
       },
     };
-    
   }
 };
