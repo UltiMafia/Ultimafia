16559f36842222cc31b28ed6b297771d3cd23aa1
SawJester <anglelord270@gmail.com>
2024-12-11 14:29:08 -0500

    Gambler fix (#1780)
    Gambler Works

--- Games/types/Mafia/roles/Independent/Gambler.js
+++ Games/types/Mafia/roles/Independent/Gambler.js
@@ -6,17 +6,5 @@ module.exports = class Gambler extends Role {
 
     this.alignment = "Independent";
     this.cards = ["VillageCore", "ChallengeTarget", "WinWithTwoGambleWins"];
-
-    this.listeners = {
-      roleAssigned: function (player) {
-        if (player !== this.player) {
-          return;
-        }
-
-        this.player.queueAlert(
-          ":dice6: Everything this Village does is based on chance. Show them the odds and prove that the house doesn't always win."
-        );
-      },
-    };
   }
 };
