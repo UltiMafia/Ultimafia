2899985af66815572a8bbd324d1935f784894419
SawJester <anglelord270@gmail.com>
2024-12-11 14:24:24 -0500

    Update ChallengeTarget.js

--- Games/types/Mafia/roles/cards/ChallengeTarget.js
+++ Games/types/Mafia/roles/cards/ChallengeTarget.js
@@ -9,6 +9,9 @@ module.exports = class ChallengeTarget extends Card {
         if (player !== this.player) {
           return;
         }
+        this.player.queueAlert(
+          ":dice6: Everything this Village does is based on chance. Show them the odds and prove that the house doesn't always win."
+        );
         this.data.meetingName = "Gamble with " + this.player.name;
       },
     };
