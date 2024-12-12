50c3e44256ff7c2c9a0088c9b35efc559545a275
SawJester <anglelord270@gmail.com>
2024-12-11 21:55:34 -0500

    Update BinaryAlignmentInfo.js

--- Games/types/Mafia/information/BinaryAlignmentInfo.js
+++ Games/types/Mafia/information/BinaryAlignmentInfo.js
@@ -43,10 +43,10 @@ module.exports = class BinaryAlignmentInfo extends Information{
 
   getInfoFormated(){
     super.getInfoRaw();
-        if(this.randomTarget == true){
+        
       return `You Learn that your ${this.target.name} is ${this.mainInfo}`
-    }
-    return `You Learn that your Target is ${this.mainInfo}`
+    
+    //return `You Learn that your Target is ${this.mainInfo}`
   }
 
   isTrue() {
