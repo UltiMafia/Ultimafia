2e2d9bef6110e83686cf32b70fe662c243532517
SawJester <SawJester@users.noreply.github.com>
2024-12-14 14:29:54 +0000

    Prettified Code!

--- Games/types/Mafia/roles/Independent/Grouch.js
+++ Games/types/Mafia/roles/Independent/Grouch.js
@@ -3,7 +3,7 @@ const Role = require("../../Role");
 module.exports = class Grouch extends Role {
   constructor(player, data) {
     super("Grouch", player, data);
-    
+
     this.alignment = "Independent";
     //Grouch is not Hostile so it should not contest win.
     this.winCount = "Village";
