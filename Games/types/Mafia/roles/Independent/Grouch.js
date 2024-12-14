a1bc0321600dda1c7623f0dac9d8aba6e49e287b
SawJester <anglelord270@gmail.com>
2024-12-14 09:31:44 -0500

    Grouch Fix (#1784)
    Co-authored-by: SawJester <SawJester@users.noreply.github.com>

--- Games/types/Mafia/roles/Independent/Grouch.js
+++ Games/types/Mafia/roles/Independent/Grouch.js
@@ -5,6 +5,8 @@ module.exports = class Grouch extends Role {
     super("Grouch", player, data);
 
     this.alignment = "Independent";
+    //Grouch is not Hostile so it should not contest win.
+    this.winCount = "Village";
     this.cards = ["VillageCore", "WinIfAliveWhenVillageLose"];
   }
 };
