401e4fce191c615e047aa9fc7deffac5e112cd80
SawJester <anglelord270@gmail.com>
2024-12-13 21:18:40 -0500

    Update NightCaroler.js

--- Games/types/Mafia/roles/cards/NightCaroler.js
+++ Games/types/Mafia/roles/cards/NightCaroler.js
@@ -21,21 +21,8 @@ module.exports = class NightCaroler extends Card {
 
             var alive = this.game.players.filter((p) => p.alive);
             if (alive.length < 3) return;
-
-            var carol;
-            var evilPlayers = alive.filter(
-              (p) => p.role.alignment == "Mafia" || p.role.alignment == "Cult"
-            );
-
-            if (this.actor.hasEffect("FalseMode")) {
-              evilPlayers = alive.filter(
-                (p) => p.role.alignment != "Mafia" && p.role.alignment != "Cult"
-              );
-            }
-
-            if (evilPlayers.length <= 0) {
-              return;
-            }
+            
+            
 
             /* We need to handle a list of visits in order to know if the target should receieve a carol
                if the list is empty, that means the target doesn't visit and should get the carol. */
@@ -44,26 +31,18 @@ module.exports = class NightCaroler extends Card {
             if (visits.length > 0) {
               return;
             } else {
-              // guarantee no repeats in carol
-              var chosenThree = [Random.randArrayVal(evilPlayers)];
-              alive = alive.filter((p) => p !== chosenThree[0]);
-              alive = Random.randomizeArray(alive);
-              chosenThree.push(alive[0]);
-              chosenThree.push(alive[1]);
-              chosenThree = Random.randomizeArray(chosenThree);
+              let info = this.game.createInformation(
+              "ThreePlayersOneEvilInfo",
+              this.actor,
+              this.game
+            );
+            info.processInfo();
+            var carol = info.getInfoRaw();
+            if(carol == "No Evil Players Exist"){
+              return;
+            }
 
-              if (this.actor.hasEffect("FalseMode")) {
-                if (evilPlayers.length <= 2) {
-                  return;
-                }
-                evilPlayers = Random.randomizeArray(evilPlayers);
-                chosenThree = [];
-                chosenThree.push(evilPlayers[0]);
-                chosenThree.push(evilPlayers[1]);
-                chosenThree.push(evilPlayers[2]);
-              }
-
-              carol = `:carol: You see a merry Caroler outside your house! They sing you a Carol about ${chosenThree[0].name}, ${chosenThree[1].name}, ${chosenThree[2].name}, at least one of whom is evil!`;
+              carol = `:carol: You see a merry Caroler outside your house! They sing you a Carol about ${carol[0].name}, ${carol[1].name}, ${carol[2].name}, at least one of whom is evil!`;
             }
 
             this.target.queueAlert(carol);
