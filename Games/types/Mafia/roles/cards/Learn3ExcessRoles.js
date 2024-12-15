fc698827491f1c2ad63df6a34b1ea8bd1cf2f2bf
SawJester <anglelord270@gmail.com>
2024-12-15 13:25:39 -0500

    Update Learn3ExcessRoles.js

--- Games/types/Mafia/roles/cards/Learn3ExcessRoles.js
+++ Games/types/Mafia/roles/cards/Learn3ExcessRoles.js
@@ -5,79 +5,6 @@ const { PRIORITY_INVESTIGATIVE_DEFAULT } = require("../../const/Priority");
 module.exports = class Learn3ExcessRoles extends Card {
   constructor(role) {
     super(role);
-    /*
-    this.actions = [
-      {
-        priority: PRIORITY_INVESTIGATIVE_DEFAULT,
-        run: function () {
-          if (!this.actor.alive) return;
-          if (this.game.getStateName() != "Night") return;
-          if (this.actor.role.data.hasExcessRoles) return;
-          this.actor.role.data.hasExcessRoles = true;
-
-          let roles = this.game.PossibleRoles.filter((r) => r);
-          let players = this.game.players.filter((p) => p.role);
-          let currentRoles = [];
-
-          for (let x = 0; x < players.length; x++) {
-            currentRoles.push(players[x].role);
-          }
-          for (let y = 0; y < currentRoles.length; y++) {
-            roles = roles.filter(
-              (r) => r.split(":")[0] != currentRoles[y].name
-            );
-          }
-
-          if (this.actor.hasEffect("FalseMode")) {
-            roles = currentRoles.map((r) => r.name);
-          }
-
-          if (
-            this.actor.role.alignment == "Mafia" ||
-            this.actor.role.alignment == "Cult"
-          ) {
-            roles = roles.filter(
-              (r) => this.game.getRoleAlignment(r) == "Village"
-            );
-            /*
-              for (let x = 0; x < roles.length; x++) {
-                if (this.game.getRoleAlignment(roles[x]) != "Village") {
-                  roles.slice(roles.indexOf(roles[x]), 1);
-                }
-              }
-                
-          }
-
-          if (roles.length <= 0) {
-            this.actor.queueAlert(`There are 0 excess roles.`);
-          } else if (roles.length == 1) {
-            var role1 = roles[0];
-            this.actor.queueAlert(
-              `There is only 1 excess role. The Excess role is ${role1}`
-            );
-          } else if (roles.length == 2) {
-            var role1 = roles[0];
-            var role2 = roles[1];
-            this.actor.queueAlert(
-              `There is only 2 excess roles. The Excess roles are ${role1} and ${role2}`
-            );
-          } else {
-            var roleIndexes = roles.map((r, i) => i);
-            var roleIndex1 = Random.randArrayVal(roleIndexes, true);
-            var roleIndex2 = Random.randArrayVal(roleIndexes, true);
-            var roleIndex3 = Random.randArrayVal(roleIndexes, true);
-            var role1 = roles[roleIndex1];
-            var role2 = roles[roleIndex2];
-            var role3 = roles[roleIndex3];
-
-            this.actor.queueAlert(
-              `3 excess roles are ${role1}, ${role2}, and ${role3}.`
-            );
-          }
-        },
-      },
-    ];
-*/
     this.listeners = {
       roleAssigned: function (player) {
         if (player !== this.player) {
@@ -91,7 +18,21 @@ module.exports = class Learn3ExcessRoles extends Card {
         if (!this.player.alive) return;
         if (this.player.role.data.hasExcessRoles == true) return;
         this.player.role.data.hasExcessRoles = true;
+          
 
+
+                let info = this.game.createInformation(
+                "ExcessRolesInfo",
+                this.player,
+                this.game,
+                3
+              );
+              info.processInfo();
+              var alert = `:invest: ${info.getInfoFormated()}.`;
+              this.player.queueAlert(alert);
+
+
+        /*
         let roles = this.game.PossibleRoles.filter((r) => r);
         let players = this.game.players.filter((p) => p.role);
         let currentRoles = [];
@@ -141,6 +82,7 @@ module.exports = class Learn3ExcessRoles extends Card {
           this.player.queueAlert(
             `3 excess roles are ${role1}, ${role2}, and ${role3}.`
           );
+          */
         }
       },
     };
