3c87642cefac3333218e4c17b1b1e85143e0cd8a
SawJester <anglelord270@gmail.com>
2024-12-18 22:33:09 -0500

    Update TrackPlayerBoolean.js

--- Games/types/Mafia/roles/cards/TrackPlayerBoolean.js
+++ Games/types/Mafia/roles/cards/TrackPlayerBoolean.js
@@ -13,27 +13,17 @@ module.exports = class TrackPlayerBoolean extends Card {
           priority: PRIORITY_INVESTIGATIVE_DEFAULT,
           labels: ["investigate"],
           run: function () {
-            let visited = this.hasVisits(this.target);
-            if (this.actor.hasEffect("FalseMode")) {
-              if (!visited) {
+              let info = this.game.createInformation(
+              "BinaryTrackerInfo",
+              this.actor,
+              this.game,
+              this.target
+            );
+            info.processInfo();
                 this.actor.queueAlert(
-                  `:track: ${this.target.name} visited somebody`
+                  `:track: ${info.getInfoFormated()}`
                 );
-              } else {
-                this.actor.queueAlert(
-                  `:track: ${this.target.name} did not visit anybody`
-                );
-              }
-            } else {
-              if (visited) {
-                this.actor.queueAlert(
-                  `:track: ${this.target.name} visited somebody`
-                );
-              } else {
-                this.actor.queueAlert(
-                  `:track: ${this.target.name} did not visit anybody`
-                );
-              }
+              
             }
           },
         },
