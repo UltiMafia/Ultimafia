7bb16db14a1334d8f2c4d689f4fe3eb608990b56
SawJester <anglelord270@gmail.com>
2024-12-19 17:59:28 -0500

    Update WatchPlayer.js

--- Games/types/Mafia/roles/cards/WatchPlayer.js
+++ Games/types/Mafia/roles/cards/WatchPlayer.js
@@ -18,29 +18,16 @@ module.exports = class WatchPlayer extends Card {
           labels: ["investigate", "hidden"],
           priority: PRIORITY_INVESTIGATIVE_AFTER_RESOLVE_DEFAULT,
           run: function () {
-            let visitors = this.getVisitors(this.target);
-            let visitorNames = visitors.map((player) => player.name);
-
-            if (this.actor.hasEffect("FalseMode")) {
-              let players = this.game
-                .alivePlayers()
-                .filter((p) => p != this.target);
-              let playerNames = players.map((p) => p.name);
-              if (visitorNames.length == 0) {
-                visitorNames.push(Random.randArrayVal(playerNames));
-              } else {
-                visitorNames = [];
-              }
-            }
-
-            if (visitorNames.length === 0) {
-              visitorNames.push("no one");
-            }
+            let info = this.game.createInformation(
+              "WatcherInfo",
+              this.actor,
+              this.game,
+              this.target
+            );
+            info.processInfo();
 
             this.actor.queueAlert(
-              `:watch: ${this.target.name} was visited by ${visitorNames.join(
-                ", "
-              )} during the night.`
+              `:watch: ${info.getInfoFormated()}`
             );
           },
         },
