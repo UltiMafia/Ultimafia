fe56b5ea1309a05600d60a6984adaac555652835
SawJester <anglelord270@gmail.com>
2024-12-21 03:12:22 -0500

    Update MakePlayerLearnOneOfTwoPlayersOnDeath.js

--- Games/types/Mafia/roles/cards/MakePlayerLearnOneOfTwoPlayersOnDeath.js
+++ Games/types/Mafia/roles/cards/MakePlayerLearnOneOfTwoPlayersOnDeath.js
@@ -6,63 +6,21 @@ module.exports = class MakePlayerLearnOneOfTwoPlayersOnDeath extends Card {
   constructor(role) {
     super(role);
 
-    this.meetings = {
-      "Make Player Learn That One Of Two Players Is Evil On Death": {
-        actionName:
-          "Make Player Learn That One Of Two Players Is Evil On Death",
-        states: ["Night"],
-        flags: ["voting"],
-        shouldMeet: function () {
-          return this.game.graveyardParticipation == "Balling";
-        },
-        action: {
-          priority: PRIORITY_REVEAL_DEFAULT,
-          run: function () {
-            this.actor.role.data.playerToReveal = this.target;
-          },
-        },
-      },
-    };
     this.listeners = {
       death: function (player, killer, deathType) {
-        if (
-          player == this.player &&
-          (this.data.playerToReveal || this.game.graveyardParticipation)
-        ) {
+        if (player == this.player) {
           if (this.game.getStateName() != "Night") return;
-          let role = this.player.role.name;
-          let victim;
-          if (this.game.graveyardParticipation) {
-            victim = this.player;
-          } else {
-            victim = this.data.playerToReveal;
-          }
-          let players = this.game.alivePlayers().filter((p) => p != victim);
-          if (players.length <= 1) return;
-          let evilPlayers = players.filter(
-            (p) => p.role.alignment == "Cult" || p.role.alignment == "Mafia"
-          );
 
-          if (this.player.hasEffect("FalseMode")) {
-            evilPlayers = players.filter(
-              (p) => p.role.alignment != "Cult" && p.role.alignment != "Mafia"
-            );
-            players = players.filter(
-              (p) => p.role.alignment != "Cult" && p.role.alignment != "Mafia"
-            );
-          }
-          if (evilPlayers.length <= 0) return;
+          let info = this.game.createInformation(
+                "TwoPlayersOneEvilInfo",
+                this.player,
+                this.game,
+              );
+              info.processInfo();
+              var alert = info.getInfoRaw();
 
-          let evilPlayer = Random.randArrayVal(evilPlayers);
-          players = players.filter((p) => p != evilPlayer);
-          if (players.length <= 0) return;
-          let otherPlayer = Random.randArrayVal(players);
-
-          let chosen = [evilPlayer, otherPlayer];
-          let chosenRan = Random.randomizeArray(chosen);
-
-          victim.queueAlert(
-            `A dying ${role} tells you that at least 1 of ${chosenRan[0].name} and ${chosenRan[1].name} is Evil!!!`
+          this.player.queueAlert(
+            `:invest: You learn that ${alert[0].name} or ${alert[1].name} the Most Important Evil Player!!!`
           );
         }
       },
