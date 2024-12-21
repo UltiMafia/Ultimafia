e68b7727a796b3b603eb2037a3287410af3d2701
SawJester <anglelord270@gmail.com>
2024-12-20 21:11:25 -0500

    Update WatcherInfo.js

--- Games/types/Mafia/information/WatcherInfo.js
+++ Games/types/Mafia/information/WatcherInfo.js
@@ -12,7 +12,7 @@ const {
 } = require("../const/FactionList");
 
 module.exports = class WatcherInfo extends Information {
-  constructor(creator, game, target, limtMafia) {
+  constructor(creator, game, target, limtMafia, forceCount) {
     super("Watcher Info", creator, game);
     if (target == null) {
       this.randomTarget = true;
@@ -24,13 +24,19 @@ module.exports = class WatcherInfo extends Information {
     else{
       this.limtMafia = true;
     }
+     if(forceCount == null || forceCount == false){
+      this.forceCount = false;
+    }
+    else{
+      this.forceCount = true;
+    }
     this.target = target;
 
     
     let visitors = this.getVisitors(this.target);
     let MafiaKill = this.getVisitors(this.target, "mafia");
 
-              if (MafiaKill && MafiaKill.length > 1 && this.limtMafia == true) {
+              if (MafiaKill && MafiaKill.length > 1 && this.limtMafia == true && this.forceCount == false) {
                 for (let x = 1; x < MafiaKill.length; x++) {
                   visitors.splice(visitors.indexOf(MafiaKill[x]), 1);
                 }
@@ -74,14 +80,22 @@ module.exports = class WatcherInfo extends Information {
     }
   }
   isFavorable() {
-    if (this.mainInfo.length > 0) {
-      return false;
-    } else {
+    let badVisits = this.getKillVictims();
+
+    badVisits = badVisits.filter((p) => p == this.target);
+    if (this.mainInfo.length <= 0) {
       return true;
+    } else if(this.forceCount){
+      return true;
+    }
+    else {
+      return false;
     }
   }
   isUnfavorable() {
     let badVisits = this.getKillVictims();
+
+    badVisits = badVisits.filter((p) => p == this.target);
 
     if (badVisits.length <= 0 && this.mainInfo.length <= 0) {
       return true;
@@ -100,36 +114,64 @@ module.exports = class WatcherInfo extends Information {
   }
   makeFalse() {
     let visitors = this.getVisitors(this.target);
-    if (visitors.length > 0) {
+    if (visitors.length > 0 && this.forceCount == false) {
       this.mainInfo = [];
     } else {
       let possibleVisitors = this.game
         .alivePlayers()
         .filter((p) => p != this.creator && p != this.target && !visitors.includes(p));
-      this.mainInfo = [Random.randArrayVal(possibleVisitors)];
+      let fakeInfo = [];
+      if(this.forceCount){
+        Random.randomizeArray(possibleVisitors);
+        let doubles = false;
+        if(possibleVisitors.length < visitors.length){
+          doubles = true;
+        }
+      for(let x = 0; x < visitors.length; x++){
+        if(doubles){
+          fakeInfo.push(Random.randArrayVal(possibleVisitors));
+        }
+        else{
+          fakeInfo.push(possibleVisitors [x]);
+        }
+      }
+    }
+      else{
+        fakeInfo.push(Random.randArrayVal(possibleVisitors));
+      }
+      this.mainInfo = fakeInfo;
     }
   }
   makeFavorable() {
+    let visitors = this.getVisitors(this.target);
     let badVisits = this.getKillVictims();
-    let possibleVisitors = this.game
+    let possibleExtraVisitors = this.game
         .alivePlayers()
-        .filter((p) => p != this.creator && p != this.target && !visitors.includes(p));
+        .filter((p) => p != this.creator && p != this.target && !visitors.includes(p);
     badVisits = badVisits.filter((p) => p == this.target);
-    if (badVisits.length <= 0) {
+      let fakeInfo = [];
+      if(!this.forceCount){
       this.mainInfo = [];
-    } else {
-      this.mainInfo = [Random.randArrayVal(possibleVisitors)];
+    }
+    else if(this.forceCount){
+        this.mainInfo = visitors;
+        return;
+    }
+    else{
+      this.mainInfo = [];
+      return;
     }
   }
   makeUnfavorable() {
+    let visitors = this.getVisitors(this.target);
     let badVisits = this.getKillVictims();
     let possibleVisitors = this.game
         .alivePlayers()
         .filter((p) => p != this.creator && p != this.target);
     badVisits = badVisits.filter((p) => p == this.target);
-    if (badVisits.length <= 0) {
-      this.mainInfo = [];
-    } else {
+    if (this.forceCount || visitors.length > 0) {
+      this.mainInfo = visitors;
+    } else  {
       this.mainInfo = [Random.randArrayVal(possibleVisitors)];
     }
   }
