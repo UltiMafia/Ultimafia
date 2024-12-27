a1e6fe1f634e5df0779b771cac1b6afddf1f7475
SawJester <SawJester@users.noreply.github.com>
2024-12-27 04:32:00 +0000

    Prettified Code!

--- Games/types/Mafia/roles/cards/GlobalModifier.js
+++ Games/types/Mafia/roles/cards/GlobalModifier.js
@@ -1,7 +1,7 @@
 const Card = require("../../Card");
 const Action = require("../../Action");
 const Player = require("../../../../core/Player");
-const {  PRIORITY_COPY_ACTIONS } = require("../../const/Priority");
+const { PRIORITY_COPY_ACTIONS } = require("../../const/Priority");
 
 module.exports = class GlobalModifier extends Card {
   constructor(role) {
@@ -12,33 +12,36 @@ module.exports = class GlobalModifier extends Card {
         if (!stateInfo.name.match(/Dawn/)) {
           return;
         }
-          let actions = [];
-          for (let action of this.game.actions[0]) {
-              if (
-                action.actor == this.player && action.target instanceof Player && action.target != this.player && !action.hasLabels(["kill", "mafia"])
-              ) {
-                actions.push(action);
-              }
-            }
+        let actions = [];
+        for (let action of this.game.actions[0]) {
+          if (
+            action.actor == this.player &&
+            action.target instanceof Player &&
+            action.target != this.player &&
+            !action.hasLabels(["kill", "mafia"])
+          ) {
+            actions.push(action);
+          }
+        }
 
-        for(let visit of actions){
-          for(let person of this.game.alivePlayers()){
-              if(person != action.target){
-                var actionCopy = new Action({
-                  actor: action.actor,
-                  target: person,
-                  game: action.game,
-                  meeting: action.meeting,
-                  priority: action.priority,
-                  labels: action.labels,
-                  run: action.unboundRun,
-                  power: action.power,
-                  item: action.item,
-                  effect: action.effect,
-                  delay: action.delay,
-                })
-                this.game.queueAction(action);
-              }
+        for (let visit of actions) {
+          for (let person of this.game.alivePlayers()) {
+            if (person != action.target) {
+              var actionCopy = new Action({
+                actor: action.actor,
+                target: person,
+                game: action.game,
+                meeting: action.meeting,
+                priority: action.priority,
+                labels: action.labels,
+                run: action.unboundRun,
+                power: action.power,
+                item: action.item,
+                effect: action.effect,
+                delay: action.delay,
+              });
+              this.game.queueAction(action);
+            }
           }
         }
       },
