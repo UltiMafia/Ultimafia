9af103c46106f8bc8a19c8c7a6d4b917a8c391ea
SawJester <anglelord270@gmail.com>
2024-12-28 17:29:33 -0500

    Update LearnVisitors.js

--- Games/types/Mafia/roles/cards/LearnVisitors.js
+++ Games/types/Mafia/roles/cards/LearnVisitors.js
@@ -66,31 +66,16 @@ module.exports = class LearnVisitors extends Card {
           run: function () {
             if (!this.actor.alive) return;
 
-            let visitors = this.getVisitors(this.actor);
-
-            if (this.actor.hasEffect("FalseMode")) {
-              let players = this.game
-                .alivePlayers()
-                .filter((p) => p != this.actor);
-              let playerNames = players.map((p) => p.name);
-              let playerFake = Random.randArrayVal(players, true);
-              let roleFake = Random.randArrayVal(players, true);
-              if (visitors.length <= 0) {
-                this.actor.queueAlert(
-                  `:invest: You learn that ${
-                    playerFake.name
-                  }'s role is ${addArticle(roleFake.getRoleAppearance())}.`
-                );
-              }
-              return;
-            }
+              let info = this.game.createInformation(
+              "WatcherRoleInfo",
+              this.actor,
+              this.game,
+              this.actor
+            );
+            info.processInfo();
 
             for (let visitor of visitors) {
-              this.actor.queueAlert(
-                `:invest: You learn that ${visitor.name}'s role is ${addArticle(
-                  visitor.getRoleAppearance()
-                )}.`
-              );
+              this.actor.queueAlert(`:watch: ${info.getInfoWithPlayerNames()}`);
             }
           },
         });
