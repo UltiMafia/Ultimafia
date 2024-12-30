e175c0a684c6440ca7e81ff063185d41fa6cce85
SawJester <anglelord270@gmail.com>
2024-12-30 03:21:24 -0500

    Update GuessFiveRoles.js

--- Games/types/Mafia/roles/cards/GuessFiveRoles.js
+++ Games/types/Mafia/roles/cards/GuessFiveRoles.js
@@ -1,10 +1,12 @@
 const Card = require("../../Card");
+const Action = require("../../Action");
+const roles = require("../../../../../data/roles");
 const { PRIORITY_INVESTIGATIVE_DEFAULT } = require("../../const/Priority");
 
 module.exports = class GuessFiveRoles extends Card {
   constructor(role) {
     super(role);
-
+/*
     this.meetings = {
       "Select Players": {
         actionName: "Select Players (0-5)",
@@ -86,5 +88,149 @@ module.exports = class GuessFiveRoles extends Card {
         },
       },
     };
+*/
+
+
+    this.listeners = {
+      roleAssigned: function (player) {
+        if (player !== this.player) {
+          return;
+        }
+
+        this.player.role.data.ConvertOptions = this.game.PossibleRoles.filter(
+          (r) => r
+        );
+      this.player.role.data.GuessingPlayers = [];
+      this.player.role.data.GuessingRoles = [];
+      this.player.role.data.GuessingCount = 0;
+      },
+      // refresh cooldown
+      state: function (stateInfo) {
+        if (!stateInfo.name.match(/Night/)) {
+          return;
+        }
+        let guessOptions = this.player.role.data.ConvertOptions;
+        //ConvertOptions.push("None");
+        //this.meetings["Guess Role"].targets = guessOptions;
+
+          var action = new Action({
+          actor: this.player,
+          game: this.player.game,
+          priority: PRIORITY_INVESTIGATIVE_DEFAULT,
+          labels: ["investigate", "role", "hidden", "absolute"],
+          run: function () {
+            if (!this.actor.alive) return;
+            if(this.actor.role.data.GuessingPlayers.length <= 0) return;
+             if(this.actor.role.data.GuessingRoles.length <= 0) return;
+
+            let info = this.game.createInformation(
+              "WatcherRoleInfo",
+              this.actor,
+              this.game,
+              this.actor.role.data.GuessingPlayers,
+              this.actor.role.data.GuessingRoles,
+              true
+            );
+            info.processInfo();
+
+            this.actor.queueAlert(`:invest: ${info.getInfoFormated()}`);
+          },
+        });
+
+        this.game.queueAction(action);
+
+
+
+
+        
+      },
+    };
+
+
+
+    
   }
+
+
+
+
+
+  speak(message) {
+    let formatedMessage = message.content.replaceAll("(", "");
+    formatedMessage = formatedMessage.content.replaceAll(")", "");
+    formatedMessage = formatedMessage.toLowerCase()
+    if (formatedMessage.toLowerCase().includes("i will analyze if ")) {
+    formatedMessage = formatedMessage.content.replaceAll("i will analyze if ", "");
+    
+    let array = formatedMessage.split(" ");
+    let playerName = array[0];
+      if(array.length < 3) return;
+    let roleName = array[2].charAt(0).toUpperCase()+array[2].slice(1);
+      if(array.length >= 4){
+        roleName = roleName + " "+array[3].charAt(0).toUpperCase()+array[3].slice(1);
+      }
+    let roles = Object.entries(roles.Mafia).map((roleData) => roleData[0].toLowercase);
+    let playerTarget = false;
+      for(let player of this.game.players){
+        if(player.name.toLowerCase() == playerName){
+          playerTarget = player;
+        }
+      }
+      if(playerTarget == false){
+        var action = new Action({
+        actor: this.player,
+        target: this.player,
+        game: this.game,
+        effect: this,
+        labels: ["hidden"],
+        run: function () {
+          this.target.queueAlert(
+            `Invalid Player Name!`
+          );
+        },
+      });
+      this.game.instantAction(action);
+      return;
+      }
+      if(!roles.includes(roleName)){
+        var action = new Action({
+        actor: this.player,
+        target: this.player,
+        game: this.game,
+        effect: this,
+        labels: ["hidden"],
+        run: function () {
+          this.target.queueAlert(
+            `Invalid Role!`
+          );
+        },
+      });
+      this.game.instantAction(action);
+      return;
+      }
+
+      this.player.role.data.GuessingPlayers.push(playerTarget);
+      this.player.role.data.GuessingRoles.push(roleName);
+    this.player.role.data.GuessingCount++;
+      var action = new Action({
+        actor: this.player,
+        target: this.player,
+        game: this.game,
+        effect: this,
+        labels: ["hidden"],
+        run: function () {
+          this.target.queueAlert(
+            `You Guess has been logged you will learn about it tonight! You have ${5-this.actor.role.data.GuessingCount} guesses remaining.`
+          );
+        },
+      });
+      this.game.instantAction(action);
+      return;
+      
+    }
+  }
+
+
+
+  
 };
