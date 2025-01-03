4adfa26936f1177b6b06b8068dc1a68c0dc37516
SawJester <SawJester@users.noreply.github.com>
2025-01-03 00:27:59 +0000

    Prettified Code!

--- Games/types/Mafia/roles/cards/CompareBooleanTrack.js
+++ Games/types/Mafia/roles/cards/CompareBooleanTrack.js
@@ -20,12 +20,12 @@ module.exports = class CompareBooleanTrack extends Card {
           labels: ["investigate", "alerts"],
           priority: PRIORITY_INVESTIGATIVE_AFTER_RESOLVE_DEFAULT + 1,
           run: function () {
-           let info = this.game.createInformation(
+            let info = this.game.createInformation(
               "ReportsInfo",
               this.actor,
               this.game,
               this.target[0],
-             this.target[1]
+              this.target[1]
             );
 
             info.processInfo();
