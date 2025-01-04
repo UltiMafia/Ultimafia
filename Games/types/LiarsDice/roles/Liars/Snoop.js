39d919206018937f7883740aaa32b36215800574
SawJester <anglelord270@gmail.com>
2025-01-03 21:51:38 -0500

    Create Snoop.js

---
+++ Games/types/LiarsDice/roles/Liars/Snoop.js
@@ -0,0 +1,15 @@
+const Role = require("../../Role");
+
+module.exports = class Snoop extends Role {
+  constructor(player, data) {
+    super("Snoop", player, data);
+
+    this.alignment = "Liars";
+    this.cards = ["TownCore", "WinIfLastAlive"];
+    this.startItems = [
+      {
+        type: "Snooping"
+      },
+    ];
+  }
+};
