f28970c7b9186a6989cd1f96ec87ab0964f6f68d
SawJester <SawJester@users.noreply.github.com>
2025-01-04 02:50:30 +0000

    Prettified Code!

--- Games/types/LiarsDice/items/Gun.js
+++ Games/types/LiarsDice/items/Gun.js
@@ -14,11 +14,16 @@ module.exports = class Gun extends Item {
             this.item.drop();
             this.game.broadcast("gunshot");
 
-            this.game.queueAlert(`:gun: ${this.actor.name} pulls a gun and shoots at ${this.target.name}!`);
+            this.game.queueAlert(
+              `:gun: ${this.actor.name} pulls a gun and shoots at ${this.target.name}!`
+            );
             this.game.removeDice(this.target);
             let dice = this.target.rolledDice.pop();
             this.game.allDice -= 1;
-            this.game.allRolledDice.splice(this.game.allRolledDice.indexOf(dice),1);
+            this.game.allRolledDice.splice(
+              this.game.allRolledDice.indexOf(dice),
+              1
+            );
           },
         },
       },
