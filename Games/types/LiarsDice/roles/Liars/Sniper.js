c98ef9881c6e38a539031b32c14f509352b2c239
SawJester <anglelord270@gmail.com>
2025-01-03 21:52:19 -0500

    Create Sniper.js

---
+++ Games/types/LiarsDice/roles/Liars/Sniper.js
@@ -0,0 +1,15 @@
+const Role = require("../../Role");
+
+module.exports = class Sniper extends Role {
+  constructor(player, data) {
+    super("Sniper", player, data);
+
+    this.alignment = "Liars";
+    this.cards = ["TownCore", "WinIfLastAlive"];
+    this.startItems = [
+      {
+        type: "Gun"
+      },
+    ];
+  }
+};
