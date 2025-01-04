481ea6e5ff91c5caedd84c1a4f6e23fb6990fd95
SawJester <anglelord270@gmail.com>
2025-01-03 21:10:41 -0500

    Create GainDiceOnCorrectLieCall.js

---
+++ Games/types/LiarsDice/roles/cards/GainDiceOnCorrectLieCall.js
@@ -0,0 +1,20 @@
+const Card = require("../../Card");
+const Action = require("../../Action");
+
+module.exports = class GainDiceOnCorrectLieCall extends Card {
+  constructor(role) {
+    super(role);
+
+    this.listeners = {
+      LieCall: function (Player, Correct, Bidder) {
+        if(Player != this.player){
+          return;
+        }
+        if(!Correct){
+          return;
+        }
+        this.game.addDice(this.player);
+      },
+    };
+  }
+};
