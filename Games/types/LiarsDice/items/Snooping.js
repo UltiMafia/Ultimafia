4874b402a32bdad90866055b4ff965672c581872
SawJester <SawJester@users.noreply.github.com>
2025-01-04 02:37:43 +0000

    Prettified Code!

--- Games/types/LiarsDice/items/Snooping.js
+++ Games/types/LiarsDice/items/Snooping.js
@@ -3,45 +3,44 @@ const Item = require("../Item");
 module.exports = class Snooping extends Item {
   constructor() {
     super("Snooping");
-      this.meetings = {
+    this.meetings = {
       Snoop: {
         actionName: "Snoop Dice",
         states: ["Guess Dice"],
-         flags: ["voting", "instant", "noVeg"],
+        flags: ["voting", "instant", "noVeg"],
         action: {
           item: this,
           run: function () {
             this.item.drop();
 
             let info = [];
-            for(let dice of this.target.rolledDice){
-              if(dice == 1){
+            for (let dice of this.target.rolledDice) {
+              if (dice == 1) {
                 info.push(":Dice1:");
               }
-              if(dice == 2){
+              if (dice == 2) {
                 info.push(":Dice2:");
               }
-              if(dice == 3){
+              if (dice == 3) {
                 info.push(":Dice3:");
               }
-              if(dice == 4){
+              if (dice == 4) {
                 info.push(":Dice4:");
               }
-              if(dice == 5){
+              if (dice == 5) {
                 info.push(":Dice5:");
               }
-              if(dice == 6){
+              if (dice == 6) {
                 info.push(":Dice6:");
               }
             }
 
             this.actor.queueAlert(
-                `:invest: ${this.target.name} has ${info.join(" ")}`
-              );
+              `:invest: ${this.target.name} has ${info.join(" ")}`
+            );
           },
         },
       },
     };
   }
-
 };
