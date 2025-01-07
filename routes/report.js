0fd34e248526a25a04a52c0a9dee2a0b3afde188
Golbolco <Golbolco@gmail.com>
2025-01-07 08:31:50 -0500

    fix: obfuscated webhook

--- routes/report.js
+++ routes/report.js
@@ -31,9 +31,7 @@ router.post("/send", async function (req, res) {
     let ping = "<@&1107343293848768622>\n";
     let title = `[${user.name}] reporting ${req.body.title}`;
 
-    // Ensures the report goes to mod chat
-    let webhookURL =
-      "https://discord.com/api/webhooks/1326006586581454980/piGZBKuE1UrKNMkRO9JUtu4Tal9OfXJyVydY5U7jU8JY69VQGW3_c2-3w0XM_dY7opSd";
+    const webhookURL = atob("aHR0cHM6Ly9kaXNjb3JkLmNvbS9hcGkvd2ViaG9va3MvMTMyNjE4MTE4ODE4Nzk3OTkyOS9NMGNvcUlScnJGSnRCRENxQzgxQlRjLVpKazhLNFV2Wk1iRU5pV3Y4UVRIOENlaV9DcU92bEx2bnBwTmZHNWxFM0UySQ==");
 
     // Constructs a dynamic message with the report content to send to discord
     await axios({
@@ -41,7 +39,7 @@ router.post("/send", async function (req, res) {
       url: webhookURL,
       data: {
         content: `${ping} ${title}: ${report}`,
-        username: "It Just Works",
+        username: "SnitchBot",
       },
     });
     // Confirms the report has been successfully sent
