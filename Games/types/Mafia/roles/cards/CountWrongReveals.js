b1f66f476a231f3542c9591a2718c2c0455a5093
SawJester <anglelord270@gmail.com>
2025-01-10 20:10:20 -0500

    Update CountWrongReveals.js

--- Games/types/Mafia/roles/cards/CountWrongReveals.js
+++ Games/types/Mafia/roles/cards/CountWrongReveals.js
@@ -7,60 +7,7 @@ const {
 module.exports = class CountWrongReveals extends Card {
   constructor(role) {
     super(role);
-    /*
-    this.actions = [
-      {
-        priority: PRIORITY_INVESTIGATIVE_AFTER_RESOLVE_DEFAULT - 1,
-        labels: ["investigate"],
-        run: function () {
-          if (this.game.getStateName() != "Night") return;
-          if (!this.actor.alive) return;
-          //let visitors;
-          let problemCount = 0;
-          let role;
-          let appearRole;
-          let isFalse;
-
-          let players = this.game.players.filter((p) => p.role);
-          for (let x = 0; x < players.length; x++) {
-            //visitors = this.getVisits(players[x]);
-            role = players[x].role.name;
-            appearRole = players[x].getRoleAppearance().split(" (")[0];
-            isFalse = false;
-
-            for (let action of this.game.actions[0]) {
-              if (
-                action.actor == players[x] &&
-                action.hasLabel("investigate") &&
-                players[x].hasEffect("FalseMode")
-              ) {
-                isFalse = true;
-              }
-            }
-
-            if (role != appearRole) {
-              problemCount = problemCount + 1;
-            }
-            if (isFalse) {
-              problemCount = problemCount + 1;
-            }
-          }
-
-          if (this.actor.hasEffect("FalseMode")) {
-            if (problemCount == 0) {
-              problemCount = 1;
-            } else {
-              problemCount = 0;
-            }
-          }
-
-          this.actor.queueAlert(
-            `You learn that ${problemCount} problems occured during the night.`
-          );
-        },
-      },
-    ];
-*/
+    
     this.listeners = {
       state: function (stateInfo) {
         if (!this.player.alive) {
@@ -73,51 +20,19 @@ module.exports = class CountWrongReveals extends Card {
           actor: this.player,
           game: this.player.game,
           priority: PRIORITY_INVESTIGATIVE_AFTER_RESOLVE_DEFAULT + 2,
-          labels: ["investigate"],
+          labels: ["investigate", ""],
           run: function () {
             if (!this.actor.alive) return;
-            //let visitors;
-            let problemCount = 0;
-            let role;
-            let appearRole;
-            let isFalse;
+              let info = this.game.createInformation(
+                "CountFalseInfoInfo",
+                this.actor,
+                this.game
+              );
+              info.processInfo();
 
-            let players = this.game.players.filter((p) => p.role);
-            for (let x = 0; x < players.length; x++) {
-              //visitors = this.getVisits(players[x]);
-              role = players[x].role.name;
-              appearRole = players[x].getRoleAppearance().split(" (")[0];
-              isFalse = false;
-
-              for (let action of this.game.actions[0]) {
-                if (
-                  action.actor == players[x] &&
-                  action.hasLabel("investigate") &&
-                  players[x].hasEffect("FalseMode")
-                ) {
-                  isFalse = true;
-                }
-              }
-
-              if (role != appearRole) {
-                problemCount = problemCount + 1;
-              }
-              if (isFalse) {
-                problemCount = problemCount + 1;
-              }
-            }
-
-            if (this.actor.hasEffect("FalseMode")) {
-              if (problemCount == 0) {
-                problemCount = 1;
-              } else {
-                problemCount = 0;
-              }
-            }
-
-            this.actor.queueAlert(
-              `You learn that ${problemCount} problems occured during the night.`
-            );
+              this.actor.queueAlert(
+                `:journ: ${info.getInfoRaw()}`
+              );
           },
         });
 
