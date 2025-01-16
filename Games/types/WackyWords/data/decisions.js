94bc2376e9036527c6414f8692a5bc1609c8a21f
SawJester <SawJester@users.noreply.github.com>
2025-01-16 21:30:45 +0000

    Prettified Code!

--- Games/types/WackyWords/data/decisions.js
+++ Games/types/WackyWords/data/decisions.js
@@ -1,27 +1,42 @@
 module.exports = [
-//Superpowers
-["Have the ability to fly","Have the ability to teleport"],
-["Have the ability to never be late","Have the ability to make friends"],
-["Have the ability to fake claim in Mafia","Have the ability to be funny in Wacky Words"],
-//Pain
-["Have no teeth","Have no nails"],
-["Lose an arm","Lose a leg"],
-["Burn to death","Freeze to death"],
-["Swim with Orcas","Hug a Badger"],
-//Prizes
-["Gain a million dollars instantly","Gain 10 million dollars five years from now"],
-["Have number named after you","Have an asteroid named after you"],
-["Be famous now","Be famous 200 years from now"],
-//Trust in Users
-["Spend a day with $player","Spend a day with $OtherPlayer"],
-["Let $player read your search history","Be forced to read $player 's search history"], 
-["Let $player have access to your credit card","Let $OtherPlayer have access to your debit card"], 
-["Have $player be your Surgeon","Have $OtherPlayer be your Surgeon"], 
-//Travel
-["Go to $player 's basement","Go to Romania"], 
-["Travel to any point in the past","Travel to any point in the future"], 
-["Travel to Mars","Travel to Venus"], 
-//moral dilemmas
-["Play Guns n Hookers","Play CBA"], 
-["Redirect a Trolley to kill 1 person","Do nothing to let 6 people get run over by a Trolley"],
+  //Superpowers
+  ["Have the ability to fly", "Have the ability to teleport"],
+  ["Have the ability to never be late", "Have the ability to make friends"],
+  [
+    "Have the ability to fake claim in Mafia",
+    "Have the ability to be funny in Wacky Words",
+  ],
+  //Pain
+  ["Have no teeth", "Have no nails"],
+  ["Lose an arm", "Lose a leg"],
+  ["Burn to death", "Freeze to death"],
+  ["Swim with Orcas", "Hug a Badger"],
+  //Prizes
+  [
+    "Gain a million dollars instantly",
+    "Gain 10 million dollars five years from now",
+  ],
+  ["Have number named after you", "Have an asteroid named after you"],
+  ["Be famous now", "Be famous 200 years from now"],
+  //Trust in Users
+  ["Spend a day with $player", "Spend a day with $OtherPlayer"],
+  [
+    "Let $player read your search history",
+    "Be forced to read $player 's search history",
+  ],
+  [
+    "Let $player have access to your credit card",
+    "Let $OtherPlayer have access to your debit card",
+  ],
+  ["Have $player be your Surgeon", "Have $OtherPlayer be your Surgeon"],
+  //Travel
+  ["Go to $player 's basement", "Go to Romania"],
+  ["Travel to any point in the past", "Travel to any point in the future"],
+  ["Travel to Mars", "Travel to Venus"],
+  //moral dilemmas
+  ["Play Guns n Hookers", "Play CBA"],
+  [
+    "Redirect a Trolley to kill 1 person",
+    "Do nothing to let 6 people get run over by a Trolley",
+  ],
 ];
