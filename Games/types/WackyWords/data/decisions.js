f2882c98bfe9a66ca05d0cf1c848c80978f2b853
SawJester <anglelord270@gmail.com>
2025-01-18 17:30:41 -0500

    Update decisions.js

--- Games/types/WackyWords/data/decisions.js
+++ Games/types/WackyWords/data/decisions.js
@@ -1,16 +1,21 @@
 module.exports = [
   //Superpowers
   ["Have the ability to fly", "Have the ability to teleport"],
-  ["Have the ability to never be late", "Have the ability to make friends"],
+  ["Have the ability to never be late", "Have the ability to speak with confidence"],
   [
     "Have the ability to fake claim in Mafia",
     "Have the ability to be funny in Wacky Words",
   ],
-  //Pain
+  ["Be permanently invisible", "Permanently have X-ray vision"],
+  //Body Changes
+  ["Be 1 Foot (0.3048 Meters) shorter", "Be 1 Foot (0.3048 Meters) taller"],
+  ["Have feet replace your hands", "Have hands replace your feet"],
   ["Have no teeth", "Have no nails"],
   ["Lose an arm", "Lose a leg"],
+  //Pain
+  ["Sneeze every 10 Minutes", "Always be sweaty"],
   ["Burn to death", "Freeze to death"],
-  ["Swim with Orcas", "Hug a Badger"],
+  ["Go blind", "Go deaf"],
   //Prizes
   [
     "Gain a million dollars instantly",
@@ -33,10 +38,29 @@ module.exports = [
   ["Go to $player 's basement", "Go to Romania"],
   ["Travel to any point in the past", "Travel to any point in the future"],
   ["Travel to Mars", "Travel to Venus"],
+  ["Have fully paid trip to Paris","Have fully paid trip to Hawaii"],
+  ["Have fully paid trip to Boring, Oregon","Have fully paid trip to Dull, Scotland"],
   //moral dilemmas
   ["Play Guns n Hookers", "Play CBA"],
   [
     "Redirect a Trolley to kill 1 person",
     "Do nothing to let 6 people get run over by a Trolley",
   ],
+  ["Never be sad", "Never be angry"],
+  ["Have 1 random person die", "Have the 'A' key on your keyboard only work 60% of the time"],
+  ["Die", "Have 10 random people die"],
+  //Animals
+  ["Swim with Orcas", "Hug a Bear"],
+  ["Have a dog", "Have a cat"],
+  ["Have all spiders triple in size", "Have all spiders be smarter then you"],
+  ["Fight a horse sized chicken", "Fight 20 chicken sized horses"],
+  //Food
+  ["Only be able to drink coffee", "Only be able to drink tea"],
+  ["Eat a rotten egg", "Drink spoiled milk"],
+  ["Only be able to eat pizza", "Only be able to eat crab rangoon"],
+  ["Have everything you eat taste like cheese","Have everything you drink taste like milk"]
+  //Jobs
+  ["Be a Ninja", "Be a Pirate"],
+  ["Be a Pilot", "Be a Submariner"],
+  ["Be a Zookeeper", "Be a School Teacher"],
 ];
