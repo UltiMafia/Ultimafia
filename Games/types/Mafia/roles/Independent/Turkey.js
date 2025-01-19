4e186b1bc6c85011340e6b5138730c0444f1bdf6
SawJester <anglelord270@gmail.com>
2025-01-19 17:30:51 -0500

    Update Turkey.js

--- Games/types/Mafia/roles/Independent/Turkey.js
+++ Games/types/Mafia/roles/Independent/Turkey.js
@@ -9,7 +9,7 @@ module.exports = class Turkey extends Role {
       "VillageCore",
       "MeetingTurkey",
       "GiveTurkeyOnDeath",
-      "FamineStarter",
+      //"FamineStarter",
       "FamineImmune",
       "WinIfOnlyTurkeyAlive",
     ];
