fc433ca2ffc1684e39e9361d00f3678233f453a3
SawJester <anglelord270@gmail.com>
2025-01-20 10:03:18 -0500

    Create CreateEvent.js

---
+++ Games/types/Mafia/roles/cards/CreateEvent.js
@@ -0,0 +1,52 @@
+const Card = require("../../Card");
+const { PRIORITY_CONVERT_DEFAULT } = require("../../const/Priority");
+const { addArticle } = require("../../../../core/Utils");
+module.exports = class ConvertToChosenRole extends Card {
+  constructor(role) {
+    super(role);
+    //const targetOptions = this.game.PossibleRoles.filter((r) => r);
+    this.meetings = {
+      "Select Event": {
+        states: ["Night"],
+        flags: ["voting", "Instant"],
+        inputType: "custom",
+        //targets: { targetOptions },
+        action: {
+          labels: ["convert", "role"],
+          run: function () {
+        let eventMods = this.target.split(":")[1];
+        let eventName = this.target.split(":")[0];
+        let event = this.game.createGameEvent(eventName, eventMods);
+        event.doEvent();
+          },
+        },
+      },
+    };
+
+    this.listeners = {
+      roleAssigned: function (player) {
+        if (player !== this.player) {
+          return;
+        }
+
+        this.data.ConvertOptions = this.game.CurrentEvents.filter((r) => r);
+        for(let event of this.game.BanishedEvents.filter((r) => r)){
+          this.data.ConvertOptions.push(event);
+        }
+      },
+      // refresh cooldown
+      state: function (stateInfo) {
+        if (!stateInfo.name.match(/Night/)) {
+          return;
+        }
+        this.data.ConvertOptions = this.game.CurrentEvents.filter((r) => r);
+        for(let event of this.game.BanishedEvents.filter((r) => r)){
+          this.data.ConvertOptions.push(event);
+        }
+        var ConvertOptions = this.data.ConvertOptions;
+
+        this.meetings["Select Event"].targets = ConvertOptions;
+      },
+    };
+  }
+};
