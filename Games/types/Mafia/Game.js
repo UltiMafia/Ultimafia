4dcaa4ac4b8fbcc188cc4e0a8382710d7c551b15
SawJester <anglelord270@gmail.com>
2025-01-25 05:16:50 -0500

    Update Game.js

--- Games/types/Mafia/Game.js
+++ Games/types/Mafia/Game.js
@@ -170,10 +170,21 @@ module.exports = class MafiaGame extends Game {
         priority: 100,
         labels: ["hidden", "absolute"],
         run: function () {
+
+          if(this.Necronomicon == "Demonic" || this.Necronomicon == "Active"){
+            for(let player of this.game.players){
+              if(player.faction == "Cult" && player.role.alignment != "Independent"){
+                player.holdItem("NecroVoting", "Vote for Necronomicon Holder");
+              }
+            }
+          }
+
+          
           this.game.lastNightVisits = [];
           for (let action of this.game.actions[0]) {
             this.game.lastNightVisits.push(action);
           }
+          
         },
       });
 
