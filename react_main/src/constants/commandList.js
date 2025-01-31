f85e3d0850bb1c2acc8ec1331774231426e13d34
SawJester <anglelord270@gmail.com>
2025-01-31 06:20:24 -0500

    Update commandList.js

--- react_main/src/constants/commandList.js
+++ react_main/src/constants/commandList.js
@@ -24,6 +24,10 @@ export const commandList = {
     input: "Role name",
     description: "Used to display role descriptions in chat.",
   },
+  "/item": {
+    input: "Item name",
+    description: "Used to display item descriptions in chat.",
+  },
   "/diceroll": {
     input: "Amount, Dice Type",
     description: "Rolls dice, results are shown to everyone.",
