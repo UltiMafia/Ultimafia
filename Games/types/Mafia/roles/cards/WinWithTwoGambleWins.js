49a193a16ec62db38aa02301c89474cf9d9e6ce3
SawJester <anglelord270@gmail.com>
2025-01-31 21:25:40 -0500

    Update WinWithTwoGambleWins.js

--- Games/types/Mafia/roles/cards/WinWithTwoGambleWins.js
+++ Games/types/Mafia/roles/cards/WinWithTwoGambleWins.js
@@ -7,13 +7,20 @@ module.exports = class WinWithTwoGambleWins extends Card {
 
     this.killer = null;
     role.data.gamblerWins = 0;
+    if (this.game.players.length <= 7) {
+      role.data.killsToWin = 1;
+    } else if (this.game.players.length <= 11) {
+      role.data.killsToWin = 2;
+    } else {
+      role.data.killsToWin = 3;
+    }
     this.winCheck = {
       priority: PRIORITY_WIN_CHECK_DEFAULT,
       againOnFinished: true,
       check: function (counts, winners, aliveCount) {
         if (
           this.player.alive &&
-          (this.player.role.data.gamblerWins >= 2 || aliveCount === 2)
+          (this.player.role.data.gamblerWins >= this.data.killsToWin || aliveCount === 2)
         ) {
           winners.addPlayer(this.player, this.name);
         }
