7f80d2704d8c037df0afa6dfe3ea4a4555485c47
Golbolco <Golbolco@gmail.com>
2025-02-01 20:01:11 -0500

    fix: I made the buttons too close together

--- react_main/src/pages/User/Settings.jsx
+++ react_main/src/pages/User/Settings.jsx
@@ -366,17 +366,36 @@ export default function Settings() {
                   disabled={loading}
                 />
                 <Button
-                  variant="contained"
-                  onClick={handlePasswordReset}
+              variant="outlined"
+              sx={{
+                textTransform: "none",
+                fontSize: "16px",
+                minWidth: "120px",
+                ...(isPhoneDevice ? { flex: 0 } : {}),
+              }} onClick={handlePasswordReset}
                   disabled={loading || !emailForPasswordReset}
                 >
                   Reset Password
                 </Button>
                 {loading && <LinearProgress />}
-                <Button variant="outlined" onClick={onLogoutClick}>
+                <Button
+              variant="outlined"
+              sx={{
+                textTransform: "none",
+                fontSize: "16px",
+                minWidth: "120px",
+                ...(isPhoneDevice ? { flex: 0 } : {}),
+              }} onClick={onLogoutClick}>
                   Sign Out
                 </Button>
-                <Button variant="contained" onClick={onDeleteClick}>
+                <Button
+              variant="outlined"
+              sx={{
+                textTransform: "none",
+                fontSize: "16px",
+                minWidth: "120px",
+                ...(isPhoneDevice ? { flex: 0 } : {}),
+              }} onClick={onDeleteClick}>
                   Delete Account
                 </Button>
               </div>
