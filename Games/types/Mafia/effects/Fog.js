ff23dfdac8c5d0fcc4af4bf7d9ecab9e85f12b9e
SawJester <anglelord270@gmail.com>
2025-02-07 06:45:11 -0500

    Create Fog.js

---
+++ Games/types/Mafia/effects/Fog.js
@@ -0,0 +1,18 @@
+const Effect = require("../Effect");
+
+module.exports = class Fog extends Effect {
+  constructor(lifespan) {
+    super("Fog");
+    this.lifespan = lifespan || Infinity;
+  }
+
+  hear(message) {
+    if (!message.isServer && (message.sender != this.player && message.sender != this.player.getNeighbors()[0] && message.sender != this.player.getNeighbors()[1])) {
+      message.fiddled = true;
+    }
+  }
+
+  seeTyping(info) {
+    if (info.playerId != this.player.id && info.playerId != this.player.getNeighbors()[0].id && info.playerId != this.player.getNeighbors()[1].id) info.cancel = true;
+  }
+};
