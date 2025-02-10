6d94537914cf09dd2d373d7ad349fea9a35cfd94
SawJester <anglelord270@gmail.com>
2025-02-10 15:15:46 -0500

    Create protips.js

---
+++ data/protips.js
@@ -0,0 +1,38 @@
+const protips = {
+  Any:[
+    "Git Gud.",
+    "You can turn of off Protips in your profile settings.",
+    "You can spend the coins you earn in the Shop on your profile.",
+    "The setup is shown in the top right corner.",
+    "You can use /role to view a role's description during a game.",
+    "You can view a player's win rate, pronouns, and krama by clicking their name in the player list.",
+    "Click Say to use speech abilities or whisper.",
+    "",
+  ],
+  Mafia: [
+    "You can use /item to view an item's description during a game.",
+    "You can use /modifier to view a modifier's description during a game.",
+    "You can Earn Coins by winning ranked games.",
+    "Click the Text above the chat window to switch between meetings at night.",
+  ],
+  Resistance: [
+  ],
+  Ghost: [],
+  Jotto: [
+  ],
+  Acrotopia: [],
+  "Secret Dictator": [
+    "Count the Cards.",
+  ],
+  "Secret Hitler": [],
+  "Wacky Words": [
+     "Be Wacky.",
+  ],
+  "Liars Dice": [
+   "Call It!!!!",
+  "99% of gamblers quit before they win big!",
+  "It's always Spot On.",
+  ],
+};
+
+module.exports = protips;
