e9bdd269a0d093dd2dca58697d45776de21292d1
SawJester <anglelord270@gmail.com>
2025-02-12 13:02:00 -0500

    Create Achievements.js

---
+++ data/Achievements.js
@@ -0,0 +1,86 @@
+//DO NOT CHANGE THE IDs
+//DO NOT CHANGE THE IDs
+//DO NOT CHANGE THE IDs
+const modifierData = {
+  Mafia: {
+    //Killing
+    "Village Victory": {
+      ID: "Mafia1",
+      internal: ["VillageVictory"],
+      description: "Win as a Village Role.",
+      reward: 1,
+    },
+    "Mafia Victory": {
+      ID: "Mafia2",
+      internal: ["MafiaVictory"],
+      description: "Win as a Mafia Role.",
+      reward: 1,
+    },
+    "Cult Victory": {
+      ID: "Mafia3",
+      internal: ["CultVictory"],
+      description: "Win as a Cult Role.",
+      reward: 1,
+    },
+    "Independent  Victory": {
+      ID: "Mafia4",
+      internal: ["IndependentVictory"],
+      description: "Win as a Independent Role.",
+      reward: 1,
+    },
+    Scumhunter: {
+      ID: "Mafia5",
+      internal: ["Scumhunter"],
+      description: "As Villager correctly vote on evil players for 3 days.",
+      reward: 1,
+      roles: ["Villager"],
+    },
+    "Nothing to see here": {
+      ID: "Mafia6",
+      internal: ["NothingToSeeHere"],
+      description: "As Mafioso win after being checked by a Cop or Detective.",
+      reward: 1,
+      roles: ["Mafioso"],
+    },
+    "Acceptable Losses": {
+      ID: "Mafia7",
+      internal: ["AcceptableLosses"],
+      description: "As Cult Leader win with only Cult Aligned players in the Graveyard.",
+      reward: 10,
+      roles: ["Cult Leader"],
+    },
+    "April First": {
+      ID: "Mafia8",
+      internal: ["AprilFirst"],
+      description: "As Fool win Day 1.",
+      reward: 5,
+      roles: ["Fool"],
+    },
+    "Do No Harm": {
+      ID: "Mafia9",
+      internal: ["DoctorSave"],
+      description: "As a Doctor correctly save someone from being killed by the Mafia.",
+      reward: 5,
+      roles: ["Doctor"],
+    },
+    "Analytical Genius": {
+      ID: "Mafia10",
+      internal: ["ProAnalyst"],
+      description: "As an Analyst correctly guess Five Players.",
+      reward: 5,
+      roles: ["Analyst"],
+    },
+
+  },
+  Resistance: {},
+  Ghost: {},
+  Jotto: {},
+  Acrotopia: {},
+  "Secret Dictator": {},
+  "Secret Hitler": {},
+  "Wacky Words": {},
+  "Liars Dice": {
+  },
+};
+
+module.exports = modifierData;
