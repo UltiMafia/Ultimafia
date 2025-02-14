7fb2db16d15284354d8a2b174403d836ccc96ba4
SawJester <anglelord270@gmail.com>
2025-02-14 02:26:00 -0500

    Create CultVictory.js

---
+++ Games/types/Mafia/achievements/CultVictory.js
@@ -0,0 +1,23 @@
+const Achievements = require("../Achievements");
+
+module.exports = class CultVictory extends Achievements {
+  constructor(name, player) {
+    super(name, player);
+
+    this.data.EvilVotesCount = 0;
+     this.listeners = {
+      aboutToFinish: function (){
+            if(this.player.role.alignment != "Cult"){
+              return;
+            }
+            if(  Object.values(this.game.winners.groups)
+            .flat()
+            .find((p) => p === this.player)
+        )){
+           this.player.EarnedAchievements.push("Mafia3");
+          }
+      },
+    };
+    
+  }
+};
