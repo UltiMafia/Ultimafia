3b87b4d318e3a74dd0d8b5d60e8b8f17260aa132
SawJester <anglelord270@gmail.com>
2025-02-17 16:48:23 -0500

    Update Broker.js

--- Games/types/Mafia/roles/Village/Broker.js
+++ Games/types/Mafia/roles/Village/Broker.js
@@ -4,6 +4,6 @@ module.exports = class Broker extends Role {
   constructor(player, data) {
     super("Broker", player, data);
     this.alignment = "Village";
-    this.cards = ["VillageCore", "WinSwap", "MeetingFaction"];
+    this.cards = ["VillageCore", "WinWithFaction", "MeetingFaction", "WinSwap"];
   }
 };
