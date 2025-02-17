a44c8d0235d5fcb8080b6047f79b3f3b6a8efeaf
SawJester <anglelord270@gmail.com>
2025-02-17 16:59:41 -0500

    Update Achievements.js

--- react_main/src/constants/Achievements.js
+++ react_main/src/constants/Achievements.js
@@ -2,36 +2,36 @@
 //DO NOT CHANGE THE IDs
 //DO NOT CHANGE THE IDs
 export const AchievementData = {
-  Mafia: {
+   Mafia: {
     //Killing
     "Village Victory": {
       ID: "Mafia1",
       internal: ["VillageVictory"],
-      description: "Win as a Village Role.",
+      description: "Win as any Village Role.",
       reward: 5,
     },
     "Mafia Victory": {
       ID: "Mafia2",
       internal: ["MafiaVictory"],
-      description: "Win as a Mafia Role.",
+      description: "Win as any Mafia Role.",
       reward: 5,
     },
     "Cult Victory": {
       ID: "Mafia3",
       internal: ["CultVictory"],
-      description: "Win as a Cult Role.",
+      description: "Win as any Cult Role.",
       reward: 5,
     },
     "Independent  Victory": {
       ID: "Mafia4",
       internal: ["IndependentVictory"],
-      description: "Win as a Independent Role.",
+      description: "Win as any Independent Role.",
       reward: 5,
     },
     Scumhunter: {
       ID: "Mafia5",
       internal: ["Scumhunter"],
-      description: "As Villager correctly vote on evil players for 3 days.",
+      description: "As Villager, correctly vote on evil players for 3 days.",
       reward: 10,
       roles: ["Villager"],
     },
@@ -40,7 +40,7 @@ export const AchievementData = {
       ID: "Mafia6",
       internal: ["NothingToSeeHere"],
       description:
-        "As Mafioso survive and win after being checked by a Cop or Detective.",
+        "As Mafioso, survive and win after being checked by a Cop or Detective.",
       reward: 20,
       roles: ["Mafioso"],
     },
@@ -48,28 +48,28 @@ export const AchievementData = {
       ID: "Mafia7",
       internal: ["AllDeadAreCult"],
       description:
-        "As Cult Leader win with only Cult Aligned players in the Graveyard.",
+        "As Cult Leader, win with only Cult-aligned players in the Graveyard.",
       reward: 30,
       roles: ["Cult Leader"],
     },
     "April First": {
       ID: "Mafia8",
       internal: ["FoolWinDayOne"],
-      description: "As Fool win Day 1.",
+      description: "As Fool, win on Day 1.",
       reward: 10,
       roles: ["Fool"],
     },
     "New Sheriff in Town": {
       ID: "Mafia9",
       internal: ["SheriffShootEvil"],
-      description: "As a Sheriff shoot and kill an Evil Player.",
+      description: "As Sheriff, shoot and kill an Evil Player.",
       reward: 15,
       roles: ["Sheriff"],
     },
     "Analytical Genius": {
       ID: "Mafia10",
       internal: ["ProAnalyst"],
-      description: "As an Analyst correctly guess Five Players.",
+      description: "As Analyst, correctly guess the roles of five players.",
       reward: 10,
       roles: ["Analyst"],
     },
@@ -79,7 +79,6 @@ export const AchievementData = {
   Jotto: {},
   Acrotopia: {},
   "Secret Dictator": {},
-  "Secret Hitler": {},
   "Wacky Words": {},
   "Liars Dice": {},
 };
