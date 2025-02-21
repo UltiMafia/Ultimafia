99e419ac99af97e6c6f65f9510e2a05abfbc1072
SawJester <anglelord270@gmail.com>
2025-02-21 09:37:01 -0500

    Update GivePermaMindRot.js

--- Games/types/Mafia/roles/cards/GivePermaMindRot.js
+++ Games/types/Mafia/roles/cards/GivePermaMindRot.js
@@ -5,8 +5,15 @@ module.exports = class GivePermaMindRot extends Card {
   constructor(role) {
     super(role);
     this.listeners = {
+      revival: function (player){
+       if (player == this.player) {
+          if(this.DeliriousVictim != null && this.player.alive){
+          this.DeliriousVictim.remove();
+          }
+        }
+      },
       death: function (player, killer, instant) {
-        if (player == this.player) {
+        if (player == this.player && this.player.hasAbility(["Delirium","WhenDead","Modifier"])) {
           let alive = this.game.alivePlayers();
           var villagePlayers = alive.filter(
             (p) =>
@@ -15,8 +22,7 @@ module.exports = class GivePermaMindRot extends Card {
               ) == "Village"
           );
           var villageTarget = Random.randArrayVal(villagePlayers);
-          villageTarget.holdItem("PermaMindRot");
-          //villageTarget.holdItem("Gun");
+          this.DeliriousVictim = villageTarget.giveEffect("Delirious", this.actor, 1,["Delirium","WhenDead","Modifier"]);
         }
       },
     };
