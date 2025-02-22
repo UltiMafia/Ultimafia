59623828df0c1ff0fe827ac2eda2b8ed277c65ee
SawJester <anglelord270@gmail.com>
2025-02-22 08:53:58 -0500

    Create ChangelingWin.js

---
+++ Games/types/Mafia/achievements/ChangelingWin.js
@@ -0,0 +1,24 @@
+const Achievements = require("../Achievements");
+
+module.exports = class ChangelingWin extends Achievements {
+  constructor(name, player) {
+    super(name, player);
+
+    this.listeners = {
+      aboutToFinish: function () {
+        if (this.player.role.name != "Changeling") {
+          return;
+        }
+        if (
+          Object.values(this.game.winners.groups)
+            .flat()
+            .find((p) => p === this.player) &&
+          this.player.role.data.twincondemned &&
+          this.player.hasAbility(["Win-Con"])
+        ) {
+          this.player.EarnedAchievements.push("Mafia16");
+        }
+      },
+    };
+  }
+};
