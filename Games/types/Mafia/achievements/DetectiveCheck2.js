8ef78b20ec57018315beda17f11e94476c3bfdc4
SawJester <anglelord270@gmail.com>
2025-02-22 11:48:00 -0500

    Create DetectiveCheck2.js

---
+++ Games/types/Mafia/achievements/DetectiveCheck2.js
@@ -0,0 +1,33 @@
+const Achievements = require("../Achievements");
+
+module.exports = class DetectiveCheck2 extends Achievements {
+  constructor(name, player) {
+    super(name, player);
+  this.GoodChecks = 0;
+    this.hasChecked = [];
+    this.listeners = {
+      Information: function (info) {
+        if (
+          info.creator &&
+          info.creator == this.player &&
+          this.player.role.name == "Detective"
+        ) {
+          if (
+            info.isTrue() &&
+            info.name == "Role Info" &&
+            info.target.isEvil() == true && 
+            !this.hasChecked.includes(info.target);
+          ) {
+            this.GoodChecks++;
+            this.hasChecked.push(info.target);
+          }
+        }
+      },
+      aboutToFinish: function () {
+        if (this.GoodChecks >= 2 && this.player.alive && this.player.role.name == "Detective") {
+          this.player.EarnedAchievements.push("Mafia20");
+        }
+      },
+    };
+  }
+};
