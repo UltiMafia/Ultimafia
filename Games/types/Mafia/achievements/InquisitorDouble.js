80ac15b7a6040f0350311bced3ae94e25d78bd40
SawJester <anglelord270@gmail.com>
2025-02-23 05:27:43 -0500

    nearbear's protips (#1917)
    Co-authored-by: SawJester <SawJester@users.noreply.github.com>

--- Games/types/Mafia/achievements/InquisitorDouble.js
+++ Games/types/Mafia/achievements/InquisitorDouble.js
@@ -1,43 +1,41 @@
 const Achievements = require("../Achievements");
 const Action = require("../Action");
-const {  PRIORITY_KILL_DEFAULT } = require("../const/Priority");
+const { PRIORITY_KILL_DEFAULT } = require("../const/Priority");
 
 module.exports = class InquisitorDouble extends Achievements {
   constructor(name, player) {
     super(name, player);
     this.ConvertCount = 0;
     this.listeners = {
-        state: function (stateInfo) {
+      state: function (stateInfo) {
         if (!stateInfo.name.match(/Night/)) {
           return;
         }
-if(this.player.role.name == "Inquisitor" && this.ConvertCount < 2){
-        var action = new Action({
-          actor: null,
-          target: this.player,
-          game: this.player.game,
-          priority:  PRIORITY_KILL_DEFAULT + 2,
-          labels: ["hidden", "absolute"],
-          achievement: this,
-          run: function () {
-           
+        if (this.player.role.name == "Inquisitor" && this.ConvertCount < 2) {
+          var action = new Action({
+            actor: null,
+            target: this.player,
+            game: this.player.game,
+            priority: PRIORITY_KILL_DEFAULT + 2,
+            labels: ["hidden", "absolute"],
+            achievement: this,
+            run: function () {
               for (let action of this.game.actions[0]) {
-              if (
-                action.hasLabels(["kill"])
-                && action.actor == this.target &&
-                action.target.alive
-              ) {
-                if(action.target.role.name == "Cultist"){
-                this.achievement.ConvertCount++;
+                if (
+                  action.hasLabels(["kill"]) &&
+                  action.actor == this.target &&
+                  action.target.alive
+                ) {
+                  if (action.target.role.name == "Cultist") {
+                    this.achievement.ConvertCount++;
+                  }
+                  break;
                 }
-                 break;
               }
-            }
-            
-          },
-        });
+            },
+          });
 
-        this.game.queueAction(action);
+          this.game.queueAction(action);
         }
       },
       aboutToFinish: function () {
--- Games/types/Mafia/achievements/LawyerFrame.js
+++ Games/types/Mafia/achievements/LawyerFrame.js
@@ -1,13 +1,15 @@
 const Achievements = require("../Achievements");
 const Action = require("../Action");
-const { PRIORITY_MODIFY_INVESTIGATIVE_RESULT_DEFAULT } = require("../const/Priority");
+const {
+  PRIORITY_MODIFY_INVESTIGATIVE_RESULT_DEFAULT,
+} = require("../const/Priority");
 
 module.exports = class LawyerFrame extends Achievements {
   constructor(name, player) {
     super(name, player);
 
     this.listeners = {
-    state: function (stateInfo) {
+      state: function (stateInfo) {
         if (stateInfo.name.match(/Day/)) {
           this.FramedPlayer = null;
           return;
@@ -15,37 +17,38 @@ module.exports = class LawyerFrame extends Achievements {
         if (!stateInfo.name.match(/Night/)) {
           return;
         }
-      if(this.player.role.name == "Lawyer"){
-        var action = new Action({
-          actor: null,
-          target: this.player,
-          game: this.player.game,
-          priority: PRIORITY_MODIFY_INVESTIGATIVE_RESULT_DEFAULT+1,
-          labels: ["hidden", "absolute"],
-          achievement: this,
-          run: function () {
-           
+        if (this.player.role.name == "Lawyer") {
+          var action = new Action({
+            actor: null,
+            target: this.player,
+            game: this.player.game,
+            priority: PRIORITY_MODIFY_INVESTIGATIVE_RESULT_DEFAULT + 1,
+            labels: ["hidden", "absolute"],
+            achievement: this,
+            run: function () {
               for (let action of this.game.actions[0]) {
-              if (
-                action.hasLabels(["frame", "alignment", "flip"]) &&
-                 action.actor == this.target &&
-                action.dominates(action.target, false)
-              ) {
-                if(this.game.getRoleAlignment(action.target.role.name) == "Village"){
-                this.achievement.FramedPlayer = action.target;
+                if (
+                  action.hasLabels(["frame", "alignment", "flip"]) &&
+                  action.actor == this.target &&
+                  action.dominates(action.target, false)
+                ) {
+                  if (
+                    this.game.getRoleAlignment(action.target.role.name) ==
+                    "Village"
+                  ) {
+                    this.achievement.FramedPlayer = action.target;
+                  }
+                  break;
                 }
-                 break;
               }
-            }
-            
-          },
-        });
+            },
+          });
 
-        this.game.queueAction(action);
+          this.game.queueAction(action);
         }
       },
       Information: function (info) {
-        if(this.FramedPlayer == null || this.player.role.name != "Lawyer"){
+        if (this.FramedPlayer == null || this.player.role.name != "Lawyer") {
           return;
         }
         if (
