6669e1f40cacbf87caa47911b897d914458f6f5d
SawJester <anglelord270@gmail.com>
2025-02-23 20:03:16 -0500

    Update decisions.js

--- Games/types/WackyWords/data/decisions.js
+++ Games/types/WackyWords/data/decisions.js
@@ -10,15 +10,18 @@ module.exports = [
     "Have the ability to be funny in Wacky Words",
   ],
   ["Be permanently invisible", "Permanently have X-ray vision"],
+  ["Be Immortal", "Be Lucky"],
   //Body Changes
   ["Be 1 Foot (0.3048 Meters) shorter", "Be 1 Foot (0.3048 Meters) taller"],
   ["Have feet replace your hands", "Have hands replace your feet"],
   ["Have no teeth", "Have no nails"],
   ["Lose an arm", "Lose a leg"],
+  ["Have your hair grow 10 times as fast.", "Have your nails grow 10 times as fast."],
   //Pain
   ["Sneeze every 10 Minutes", "Always be sweaty"],
   ["Burn to death", "Freeze to death"],
   ["Go blind", "Go deaf"],
+  ["Be forced to wear a chicken suit at all times", "Be forced to dress as a clown at all times "],
   //Prizes
   [
     "Gain a million dollars instantly",
@@ -26,6 +29,7 @@ module.exports = [
   ],
   ["Have number named after you", "Have an asteroid named after you"],
   ["Be famous now", "Be famous 200 years from now"],
+  ["Have a song written about you", "Have a novel written about you"],
   //Trust in Users
   ["Spend a day with $player", "Spend a day with $OtherPlayer"],
   [
@@ -37,6 +41,8 @@ module.exports = [
     "Let $OtherPlayer have access to your debit card",
   ],
   ["Have $player be your Surgeon", "Have $OtherPlayer be your Surgeon"],
+  ["Only be able to listen to $player 's favorite music", "Only be able to listen to $OtherPlayer 's favorite music"],
+  ["Have $player as President", "Have $OtherPlayer as President"],
   //Travel
   ["Go to $player 's basement", "Go to Romania"],
   ["Travel to any point in the past", "Travel to any point in the future"],
@@ -46,6 +52,7 @@ module.exports = [
     "Have fully paid trip to Boring, Oregon",
     "Have fully paid trip to Dull, Scotland",
   ],
+  ["Go to Brazil", "Go to Kazakhstan"],
   //moral dilemmas
   ["Play Guns n Hookers", "Play CBA"],
   [
@@ -55,14 +62,16 @@ module.exports = [
   ["Never be sad", "Never be angry"],
   [
     "Have 1 random person die",
-    "Have the 'A' key on your keyboard only work 60% of the time",
+    "Have the 'A' key on any keyboard you use only work 60% of the time",
   ],
   ["Die", "Have 10 random people die"],
+  ["Cure Cancer", "End all wars"],
   //Animals
   ["Swim with Orcas", "Hug a Bear"],
   ["Have a dog", "Have a cat"],
   ["Have all spiders triple in size", "Have all spiders be smarter then you"],
   ["Fight a horse sized chicken", "Fight 20 chicken sized horses"],
+  ["Fight a horse sized $player", "Fight 20 chicken sized $OtherPlayer s"],
   //Food
   ["Only be able to drink coffee", "Only be able to drink tea"],
   ["Eat a rotten egg", "Drink spoiled milk"],
@@ -70,10 +79,11 @@ module.exports = [
   [
     "Have everything you eat taste like cheese",
     "Have everything you drink taste like milk",
-  ][
-    //Jobs
-    ("Be a Ninja", "Be a Pirate")
   ],
+  ["Drink Milk", "Drink Orange Juice"],
+  //Jobs
+  ["Be a Ninja", "Be a Pirate"],
   ["Be a Pilot", "Be a Submariner"],
   ["Be a Zookeeper", "Be a School Teacher"],
+  ["Be a Doctor", "Be a Lawyer"],
 ];
