a3da4c60a88cf0379ddb912d2826ece0a8401e70
SawJester <anglelord270@gmail.com>
2025-02-24 20:37:58 -0500

    Update CreateMafiaSetup.jsx

--- react_main/src/pages/Play/CreateSetup/CreateMafiaSetup.jsx
+++ react_main/src/pages/Play/CreateSetup/CreateMafiaSetup.jsx
@@ -205,6 +205,35 @@ export default function CreateMafiaSetup() {
       min: "0",
       max: "5",
     },
+    {
+      label: "States With No Death until Game End Event",
+      ref: "noDeathLimit",
+      type: "number",
+      value: "6",
+      min: "2",
+      max: "8",
+    },
+    {
+      label: "Force Must Act after No Deaths",
+      ref: "ForceMustAct",
+      value: true,
+      type: "boolean",
+    },
+    {
+      label: "Game End Event",
+      ref: "GameEndEvent",
+      type: "select",
+      options: [
+        {
+          label: "Meteor",
+          value: "Meteor",
+        },
+        {
+          label: "Volcanic Eruption",
+          value: "Volcanic Eruption",
+        },
+      ],
+    },
   ]);
 
   const formFieldValueMods = {
@@ -255,6 +284,9 @@ export default function CreateMafiaSetup() {
         PrivateShare: formFields[27].value,
         PublicShare: formFields[28].value,
         EventsPerNight: formFields[29].value,
+        noDeathLimit: formFields[30].value,
+        ForceMustAct: formFields[31].value,
+        GameEndEvent: formFields[32].value,
         editing: editing,
         id: params.get("edit"),
       })
