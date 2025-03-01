4dd9751a2823fb86038912c24a4c0633cb33c660
SawJester <SawJester@users.noreply.github.com>
2025-03-01 01:58:52 +0000

    Prettified Code!

--- Games/types/Mafia/roles/cards/VotesAnonymousOnDeath.js
+++ Games/types/Mafia/roles/cards/VotesAnonymousOnDeath.js
@@ -8,7 +8,7 @@ module.exports = class VotesAnonymousOnDeath extends Card {
       death: function (player, killer, killType) {
         if (!this.player.hasAbility(["Effect", "WhenDead"])) {
           return;
-         }
+        }
         if (player == this.player) this.data.causeVoteAnonymous = true;
       },
       state: function (stateInfo) {
