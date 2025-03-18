699e751feb95727cf776aa5f9cb11d6d9f89b006
SawJester <anglelord270@gmail.com>
2025-03-18 17:25:56 -0400

    Create VigilanteKill.js

---
+++ Games/types/Mafia/achievements/VigilanteKill.js
@@ -0,0 +1,27 @@
+const Achievements = require("../Achievements");
+const Action = require("../Action");
+const { PRIORITY_KILL_DEFAULT } = require("../const/Priority");
+
+module.exports = class VigilanteKill extends Achievements {
+  constructor(name, player) {
+    super(name, player);
+    this.KillCount = 0;
+    this.listeners = {
+      death: function (player, killer, deathType) {
+        if (
+          killer == this.player &&
+          deathType == "basic" &&
+          this.player.role.name == "Vigilante"
+        )
+          if (player.isEvil()) {
+            this.achievement.KillCount++;
+          }
+      },
+      aboutToFinish: function () {
+        if (this.KillCount >= 2) {
+          this.player.EarnedAchievements.push("Mafia23");
+        }
+      },
+    };
+  }
+};
