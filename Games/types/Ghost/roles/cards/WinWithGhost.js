edcb70c161d3c95910a027ef6e107f19af8eac85
SawJester <anglelord270@gmail.com>
2025-03-19 20:58:19 -0400

    Update WinWithGhost.js

--- Games/types/Ghost/roles/cards/WinWithGhost.js
+++ Games/types/Ghost/roles/cards/WinWithGhost.js
@@ -7,9 +7,16 @@ module.exports = class WinWithGhost extends Card {
     this.winCheck = {
       priority: 0,
       check: function (counts, winners, aliveCount) {
+        let wordHasBeenGuessed = false;
+        for(let player of this.game.players){
+          if(player.role.guessedWord == this.game.townWord){
+            wordHasBeenGuessed = true;
+            break;
+          }
+        }
         if (
           (aliveCount > 0 && counts["Ghost"] >= counts["Town"]) ||
-          this.guessedWord === this.game.townWord
+          wordHasBeenGuessed == true
         ) {
           winners.addPlayer(this.player, "Ghost");
         }
