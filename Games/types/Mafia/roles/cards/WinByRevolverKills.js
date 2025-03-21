7e9ac19447b34d1bddf2a09293adf65d9fd225d7
SawJester <anglelord270@gmail.com>
2025-03-21 14:57:28 -0400

    Create WinByRevolverKills.js

---
+++ Games/types/Mafia/roles/cards/WinByRevolverKills.js
@@ -0,0 +1,24 @@
+const Card = require("../../Card");
+const { PRIORITY_WIN_CHECK_DEFAULT } = require("../../const/Priority");
+
+module.exports = class WinByRevolverKills extends Card {
+  constructor(role) {
+    super(role);
+
+    role.timebombKills = 0;
+    role.data.killsToWin = 3;
+    this.winCheck = {
+      priority: PRIORITY_WIN_CHECK_DEFAULT,
+      check: function (counts, winners, aliveCount) {
+        if (
+          this.player.alive &&
+          !winners.groups[this.name] &&
+          (this.timebombKills >= this.data.killsToWin || aliveCount <= 2)
+        ) {
+          winners.addPlayer(this.player, this.name);
+        }
+      },
+    };
+
+  }
+};
