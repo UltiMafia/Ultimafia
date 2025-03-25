2dfaef4277fa6f2b716a04ab068b5d931ec9f3c9
SawJester <anglelord270@gmail.com>
2025-03-24 23:09:09 -0400

    Update Role.js

--- Games/types/Mafia/Role.js
+++ Games/types/Mafia/Role.js
@@ -7,6 +7,23 @@ module.exports = class MafiaRole extends Role {
 
     this.Action = MafiaAction;
 
+    getRevealText(roleName, modifiers, revealType) {
+    if((revealType == "death" || revealType == "condemn" || revealType == "self") &&
+    this.game.formatRoleInternal(roleName, modifiers) == this.game.formatRoleInternal(this.name, this.modifier)){
+      if(this.player.faction != this.alignment){
+
+      if (modifiers == null || modifiers == "" || modifiers == undefined) {
+      return `${roleName} |${this.player.faction}-Aligned |`;
+      }
+      return `${roleName}${modifiers ? ` (${modifiers})` : ""} |${this.player.faction}-Aligned |`;
+      } 
+    }
+    if (modifiers == null || modifiers == "" || modifiers == undefined) {
+      return `${roleName}`;
+    }
+    return `${roleName}${modifiers ? ` (${modifiers})` : ""}`;
+  }
+
     this.appearance = {
       self: "real",
       reveal: "real",
