e51e6c1dcb773b599af6bee5ee513232028a6163
SawJester <anglelord270@gmail.com>
2025-03-28 01:47:03 -0400

    Update CourtSession.js

--- Games/types/Mafia/roles/cards/CourtSession.js
+++ Games/types/Mafia/roles/cards/CourtSession.js
@@ -10,6 +10,25 @@ module.exports = class CourtSession extends Card {
 
     role.bangedGavel = 0;
 
+
+      this.listeners = {
+        PreVotingPowers: function (meeting) {
+          if (this.player.role.data.master == 0){
+          return;
+          }
+        if(meeting.name == "Court"){
+          this.player.role.VotePower = 3;
+        }      
+      
+      },
+      PostVotingPowers: function (meeting){
+        this.player.role.VotePower = 1;
+      },
+    };
+
+
+
+    
     this.meetings = {
       "Call Court": {
         actionName: "Call Court?",
