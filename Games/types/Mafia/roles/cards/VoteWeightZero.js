f37ea2ccfd2b395930a7167fd5fae9117c9030cc
SawJester <anglelord270@gmail.com>
2025-03-28 00:28:56 -0400

    Create VoteWeightZero.js

---
+++ Games/types/Mafia/roles/cards/VoteWeightZero.js
@@ -0,0 +1,20 @@
+const Card = require("../../Card");
+
+module.exports = class VoteWeightZero extends Card {
+  constructor(role) {
+    super(role);
+
+    this.listeners = {
+      AbilityToggle: function (player) {
+        if (player != this.player) {
+          return;
+        }
+        if (this.player.hasAbility(["Voting"])) {
+        this.player.role.VotePower = 0;
+        } else {
+        this.player.role.VotePower = 1;
+        }
+      },
+    };
+  }
+};
