df20045bdb77243ae1000d4bfe05d4ccd57e6f34
SawJester <anglelord270@gmail.com>
2025-03-29 01:21:24 -0400

    Update TurnIntoTree.js

--- Games/types/Mafia/roles/cards/TurnIntoTree.js
+++ Games/types/Mafia/roles/cards/TurnIntoTree.js
@@ -14,8 +14,9 @@ module.exports = class TurnIntoTree extends Card {
           run: function () {
             if (this.target == "No") return;
             if (this.target === "Yes") {
-              this.actor.giveEffect("Tree", 1);
+              //this.actor.giveEffect("Tree", 1);
               this.actor.role.isTree = true;
+              this.game.events.emit("AbilityToggle", this.actor);
             }
           },
         },
@@ -24,5 +25,40 @@ module.exports = class TurnIntoTree extends Card {
         },
       },
     };
+
+      this.listeners = {
+      AbilityToggle: function (player) {
+        if (player != this.player) {
+          return;
+        }
+        if (this.player.hasAbility(["Tree"]) && this.player.role.isTree == true) {
+          if (
+            this.TreeEffect == null ||
+            !this.player.effects.includes(this.TreeEffect)
+          ) {
+            this.TreeEffect = this.player.giveEffect(
+              "Tree",
+              1,
+              Infinity
+            );
+            this.player.passiveEffects.push(this.TreeEffect);
+          }
+        } else {
+          var index = this.player.passiveEffects.indexOf(
+            this.TreeEffect
+          );
+          if (index != -1) {
+            this.player.passiveEffects.splice(index, 1);
+          }
+          if (this.TreeEffect != null) {
+            this.TreeEffect.remove();
+            this.TreeEffect = null;
+          }
+        }
+      },
+    };
+
+
+    
   }
 };
