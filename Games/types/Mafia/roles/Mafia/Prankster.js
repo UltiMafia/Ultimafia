c6d240a0313fecb5ca2c9bb9bec5ebd44b7f3bd1
SawJester <anglelord270@gmail.com>
2025-03-31 22:59:34 -0400

    Create Prankster.js

---
+++ Games/types/Mafia/roles/Mafia/Prankster.js
@@ -0,0 +1,14 @@
+const Role = require("../../Role");
+
+module.exports = class Prankster extends Role {
+  constructor(player, data) {
+    super("Prankster", player, data);
+    this.alignment = "Mafia";
+    this.cards = [
+      "VillageCore",
+      "WinWithFaction",
+      "MeetingFaction",
+      "Pranked",
+    ];
+  }
+};
