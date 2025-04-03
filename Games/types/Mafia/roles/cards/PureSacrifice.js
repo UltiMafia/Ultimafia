4f93af485252b36b18352afdbb31bb9837829097
SawJester <SawJester@users.noreply.github.com>
2025-04-03 03:55:34 +0000

    Prettified Code!

--- Games/types/Mafia/roles/cards/PureSacrifice.js
+++ Games/types/Mafia/roles/cards/PureSacrifice.js
@@ -8,7 +8,7 @@ module.exports = class PureSacrifice extends Card {
       death: function (player, killer, deathType) {
         if (!this.player.hasAbility(["Protection", "WhenDead"])) {
           return;
-       }
+        }
         if (player == this.player && deathType == "condemn") {
           this.game.queueAlert("The virgin has been sacrificed!");
           for (let _player of this.game.players) {
