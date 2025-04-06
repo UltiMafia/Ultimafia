5ca61caecde85b43bce043258319ab198ae1eb0f
SawJester <anglelord270@gmail.com>
2025-04-06 10:28:17 -0400

    Update TrueModifier.js

--- Games/types/Mafia/roles/cards/TrueModifier.js
+++ Games/types/Mafia/roles/cards/TrueModifier.js
@@ -21,7 +21,7 @@ module.exports = class TrueModifier extends Card {
         if (this.player.hasAbility(["Modifier", "Information", "WhenDead"])) {
           if (
             this.TrueModeEffect == null ||
-            !this.players.effects.includes(this.TrueModeEffect)
+            !this.player.effects.includes(this.TrueModeEffect)
           ) {
             this.TrueModeEffect = this.player.giveEffect("TrueMode", Infinity);
             this.player.passiveEffects.push(this.TrueModeEffect);
