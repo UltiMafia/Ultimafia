d733b68669cefe7f34eee533a1906358e20adf92
SawJester <anglelord270@gmail.com>
2025-04-06 10:27:52 -0400

    Update FalseModifier.js

--- Games/types/Mafia/roles/cards/FalseModifier.js
+++ Games/types/Mafia/roles/cards/FalseModifier.js
@@ -20,7 +20,7 @@ module.exports = class FalseModifier extends Card {
         if (this.player.hasAbility(["Modifier", "Information", "WhenDead"])) {
           if (
             this.FalseModeEffect == null ||
-            !this.players.effects.includes(this.FalseModeEffect)
+            !this.player.effects.includes(this.FalseModeEffect)
           ) {
             this.FalseModeEffect = this.player.giveEffect(
               "FalseMode",
