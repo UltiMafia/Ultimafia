7ceab80c5d10d08015af88711986658e291fc978
SawJester <anglelord270@gmail.com>
2025-04-17 13:02:49 -0400

    Create Freischütz.js

---
+++ Games/types/Mafia/roles/Cult/Freischütz.js
@@ -0,0 +1,14 @@
+const Role = require("../../Role");
+
+module.exports = class Freischütz extends Role {
+  constructor(player, data) {
+    super("Freischütz", player, data);
+    this.alignment = "Cult";
+    this.cards = [
+      "VillageCore",
+      "WinWithFaction",
+      "MeetingFaction",
+      "CultWares",
+    ];
+  }
+};
