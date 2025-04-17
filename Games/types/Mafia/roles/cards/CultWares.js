efdbc12e96418da83eede043a08b70b02334639b
SawJester <anglelord270@gmail.com>
2025-04-17 12:56:57 -0400

    Create CultWares.js

---
+++ Games/types/Mafia/roles/cards/CultWares.js
@@ -0,0 +1,53 @@
+const Card = require("../../Card");
+const { PRIORITY_ITEM_GIVER_DEFAULT } = require("../../const/Priority");
+
+module.exports = class CultWares extends Card {
+  constructor(role) {
+    super(role);
+
+    this.meetings = {
+      "Give Corrupted Item": {
+        states: ["Night"],
+        flags: ["voting"],
+        action: {
+          labels: ["giveItem", "broken"],
+          priority: PRIORITY_ITEM_GIVER_DEFAULT - 1,
+          run: function () {
+            var itemType = this.actor.role.data.itemType;
+            if (!itemType) {
+              return;
+            }
+
+            this.target.holdItem(itemType, { magicCult: true });
+            this.target.queueGetItemAlert(itemType);
+            delete this.actor.role.data.itemType;
+          },
+        },
+      },
+      "Choose Corrupted Item": {
+        states: ["Night"],
+        flags: ["voting"],
+        inputType: "custom",
+        targets: [
+          "Gun",
+          "Armor",
+          "Knife",
+          "Whiskey",
+          "Bread",
+          "Candle",
+          "Falcon",
+          "Tract",
+          "Syringe",
+          "Envelope",
+          "Coffee",
+        ],
+        action: {
+          priority: PRIORITY_ITEM_GIVER_DEFAULT - 2,
+          run: function () {
+            this.actor.role.data.itemType = this.target;
+          },
+        },
+      },
+    };
+  }
+};
