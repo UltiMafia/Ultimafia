2c9bdb94b29fbaa8b83590c776de77a364249aa8
SawJester <SawJester@users.noreply.github.com>
2025-04-25 03:14:09 +0000

    Prettified Code!

--- Games/types/Mafia/roles/cards/FullMoonInvincible.js
+++ Games/types/Mafia/roles/cards/FullMoonInvincible.js
@@ -8,17 +8,22 @@ module.exports = class FullMoonInvincible extends Card {
       stateEvents: function (stateEvents) {
         if (stateEvents["Full Moon"] == true) {
           if (!this.player.hasAbility(["OnlyWhenAlive"])) {
-          return;
-           }
+            return;
+          }
           //this.player.setTempImmunity("kill", 1);
           this.player.queueAlert(":invincible: You are invincible tonight.");
         }
       },
-        AbilityToggle: function (player) {
+      AbilityToggle: function (player) {
         if (player != this.player) {
           return;
         }
-        if (this.player.hasAbility(["OnlyWhenAlive"]) && (this.game.getStateName() == "Night" || this.game.getStateName() == "Dawn") && (this.game.stateEvents["Full Moon"] == true)) {
+        if (
+          this.player.hasAbility(["OnlyWhenAlive"]) &&
+          (this.game.getStateName() == "Night" ||
+            this.game.getStateName() == "Dawn") &&
+          this.game.stateEvents["Full Moon"] == true
+        ) {
           if (
             this.FullMoonImmortalEffect == null ||
             !this.player.effects.includes(this.FullMoonImmortalEffect)
@@ -31,7 +36,9 @@ module.exports = class FullMoonInvincible extends Card {
             this.player.passiveEffects.push(this.FullMoonImmortalEffect);
           }
         } else {
-          var index = this.player.passiveEffects.indexOf(this.FullMoonImmortalEffect);
+          var index = this.player.passiveEffects.indexOf(
+            this.FullMoonImmortalEffect
+          );
           if (index != -1) {
             this.player.passiveEffects.splice(index, 1);
           }
