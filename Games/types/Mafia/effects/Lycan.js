904b7df74704f1ef83ca7326bcd0024ae55f1f3b
SawJester <anglelord270@gmail.com>
2025-04-25 08:15:55 -0400

    Update Lycan.js

--- Games/types/Mafia/effects/Lycan.js
+++ Games/types/Mafia/effects/Lycan.js
@@ -1,6 +1,7 @@
 const Effect = require("../Effect");
 const Action = require("../Action");
 const Random = require("../../../../lib/Random");
+const { PRIORITY_KILL_DEFAULT } = require("../const/Priority");
 
 module.exports = class Lycan extends Effect {
   constructor() {
@@ -10,33 +11,25 @@ module.exports = class Lycan extends Effect {
       state: function (stateInfo) {
         if (!this.player.alive) return;
 
-        if (stateInfo.name.match(/Night/) && stateInfo.dayCount % 2 == 0)
-          this.game.stateEvents["Full Moon"] = true;
-      },
-      actionsNext: function (actionQueue) {
-        if (!this.player.alive) return;
-
-        const stateInfo = this.game.getStateInfo();
-
-        if (!stateInfo.name.match(/Night/) || stateInfo.dayCount % 2 != 0)
-          return;
-
-        const nonCult = this.game.players.filter(
+        if (stateInfo.name.match(/Night/) && this.game.stateEvents["Full Moon"] == true){
+          const nonCult = this.game.players.filter(
           (p) => p.role.alignment != "Cult" && p.alive && p != this.player
         );
         const target = Random.randArrayVal(nonCult);
-
-        this.game.queueAction(
+          this.game.queueAction(
           new Action({
             actor: this.player,
             target: target,
             game: this.player.game,
             labels: ["kill", "lycan"],
+            priority: PRIORITY_KILL_DEFAULT,
             run: function () {
               if (this.dominates()) this.target.kill("basic", this.actor);
             },
           })
         );
+        }
+          
       },
     };
   }
