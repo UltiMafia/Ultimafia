6665cb6a06fef880565bc4bc957e325fc4be752e
SawJester <SawJester@users.noreply.github.com>
2025-04-25 12:16:17 +0000

    Prettified Code!

--- Games/types/Mafia/effects/Lycan.js
+++ Games/types/Mafia/effects/Lycan.js
@@ -11,25 +11,27 @@ module.exports = class Lycan extends Effect {
       state: function (stateInfo) {
         if (!this.player.alive) return;
 
-        if (stateInfo.name.match(/Night/) && this.game.stateEvents["Full Moon"] == true){
+        if (
+          stateInfo.name.match(/Night/) &&
+          this.game.stateEvents["Full Moon"] == true
+        ) {
           const nonCult = this.game.players.filter(
-          (p) => p.role.alignment != "Cult" && p.alive && p != this.player
-        );
-        const target = Random.randArrayVal(nonCult);
+            (p) => p.role.alignment != "Cult" && p.alive && p != this.player
+          );
+          const target = Random.randArrayVal(nonCult);
           this.game.queueAction(
-          new Action({
-            actor: this.player,
-            target: target,
-            game: this.player.game,
-            labels: ["kill", "lycan"],
-            priority: PRIORITY_KILL_DEFAULT,
-            run: function () {
-              if (this.dominates()) this.target.kill("basic", this.actor);
-            },
-          })
-        );
+            new Action({
+              actor: this.player,
+              target: target,
+              game: this.player.game,
+              labels: ["kill", "lycan"],
+              priority: PRIORITY_KILL_DEFAULT,
+              run: function () {
+                if (this.dominates()) this.target.kill("basic", this.actor);
+              },
+            })
+          );
         }
-          
       },
     };
   }
