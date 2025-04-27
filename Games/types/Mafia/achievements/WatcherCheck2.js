6f6230f70b978ffea18dd98fd460d5868b3041f8
SawJester <anglelord270@gmail.com>
2025-04-27 17:46:30 -0400

    Create WatcherCheck2.js

---
+++ Games/types/Mafia/achievements/WatcherCheck2.js
@@ -0,0 +1,36 @@
+const Achievements = require("../Achievements");
+
+module.exports = class WatcherCheck2 extends Achievements {
+  constructor(name, player) {
+    super(name, player);
+    this.GoodChecks = 0;
+    this.hasChecked = [];
+    this.listeners = {
+      Information: function (info) {
+        if (
+          info.creator &&
+          info.creator == this.player &&
+          this.player.role.name == "Watcher"
+        ) {
+          if(info.isTrue()){
+          for(let player of info.mainInfo){
+            if(player.isEvil() == true){
+              this.GoodChecks++;
+              return;
+            }
+          }
+          }
+        }
+      },
+      aboutToFinish: function () {
+        if (
+          this.GoodChecks >= 2 &&
+          this.player.alive &&
+          this.player.role.name == "Watcher"
+        ) {
+          this.player.EarnedAchievements.push("Mafia31");
+        }
+      },
+    };
+  }
+};
