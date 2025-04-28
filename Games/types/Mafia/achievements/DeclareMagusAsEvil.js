4a9adee6337f5b6c00e4212b9848ac97204b442e
SawJester <SawJester@users.noreply.github.com>
2025-04-28 03:02:13 +0000

    Prettified Code!

--- Games/types/Mafia/achievements/DeclareMagusAsEvil.js
+++ Games/types/Mafia/achievements/DeclareMagusAsEvil.js
@@ -6,13 +6,17 @@ module.exports = class DeclareMagusAsEvil extends Achievements {
 
     this.listeners = {
       aboutToFinish: function () {
-        if (this.game.getRoleAlignment(this.player.role.name) != "Mafia" && this.game.getRoleAlignment(this.player.role.name) != "Cult") {
+        if (
+          this.game.getRoleAlignment(this.player.role.name) != "Mafia" &&
+          this.game.getRoleAlignment(this.player.role.name) != "Cult"
+        ) {
           return;
         }
         if (
           Object.values(this.game.winners.groups)
             .flat()
-            .find((p) => p === this.player) && this.game.MagusGameDeclared == true
+            .find((p) => p === this.player) &&
+          this.game.MagusGameDeclared == true
         ) {
           this.player.EarnedAchievements.push("Mafia34");
         }
