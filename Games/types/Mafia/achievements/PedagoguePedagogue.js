7d7b75b513241a1872b2193fddcda74be08220ec
SawJester <anglelord270@gmail.com>
2025-04-28 18:17:05 -0400

    Create PedagoguePedagogue.js

---
+++ Games/types/Mafia/achievements/PedagoguePedagogue.js
@@ -0,0 +1,62 @@
+const Achievements = require("../Achievements");
+const Action = require("../Action");
+const { PRIORITY_CONVERT_DEFAULT } = require("../const/Priority");
+
+module.exports = class PedagoguePedagogue extends Achievements {
+  constructor(name, player) {
+    super(name, player);
+    this.ConvertCount = 0;
+    this.listeners = {
+      state: function (stateInfo) {
+        if (!stateInfo.name.match(/Night/)) {
+          return;
+        }
+        if (this.player.role.name == "Pedagogue" && this.ConvertHappened != true) {
+          var action = new Action({
+            actor: null,
+            target: this.player,
+            game: this.player.game,
+            priority: PRIORITY_CONVERT_DEFAULT - 2,
+            labels: ["hidden", "absolute"],
+            achievement: this,
+            run: function () {
+              let check1 = false;
+              let check2 = false;
+              for (let action of this.game.actions[0]) {
+                if (
+                  action.hasLabels(["convert"]) &&
+                  action.actor == this.target
+                ) {
+                  if (action.target.role.name == "Pedagogue") {
+                    check1 = true;
+                  }
+                  break;
+                }
+                else if (
+                  action.hasLabels(["convert"]) &&
+                  action.target == this.target &&
+                  action.actor.role.name == "Pedagogue"
+                ) {
+                  if (action.target.role.name == "Pedagogue") {
+                    check2 = true;
+                  }
+                  break;
+                }
+              }
+              if(check1 && check2){
+                this.ConvertHappened = true;
+              }
+            },
+          });
+
+          this.game.queueAction(action);
+        }
+      },
+      aboutToFinish: function () {
+        if (this.ConvertHappened == true) {
+          this.player.EarnedAchievements.push("Mafia35");
+        }
+      },
+    };
+  }
+};
