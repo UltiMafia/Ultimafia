cf1df1ac2cd024fa710fb5ecbcd3bbe40ac92f30
SawJester <anglelord270@gmail.com>
2025-04-28 21:50:08 -0400

    Create HunterKill.js

---
+++ Games/types/Mafia/achievements/HunterKill.js
@@ -0,0 +1,28 @@
+const Achievements = require("../Achievements");
+const Action = require("../Action");
+const { PRIORITY_KILL_DEFAULT } = require("../const/Priority");
+
+module.exports = class HunterKill extends Achievements {
+  constructor(name, player) {
+    super(name, player);
+    this.KillCount = 0;
+    this.listeners = {
+      death: function (player, killer, deathType) {
+        if (
+          killer == this.player &&
+          deathType == "condemnRevenge" &&
+          this.player.role.name == "Hunter" &&
+          this.game.alivePlayers().length <= 2
+        )
+          if (player.isEvil()) {
+            this.Kill = true ;
+          }
+      },
+      aboutToFinish: function () {
+        if (this.Kill == true) {
+          this.player.EarnedAchievements.push("Mafia40");
+        }
+      },
+    };
+  }
+};
