3b076fc814f6d5b6cd596b3c8acecf325313906f
SawJester <anglelord270@gmail.com>
2025-05-01 19:47:25 -0400

    Create StyleContest.js

---
+++ Games/types/Mafia/roles/cards/StyleContest.js
@@ -0,0 +1,46 @@
+const Card = require("../../Card");
+const Action = require("../../Action");
+const { PRIORITY_NIGHT_SAVER } = require("../../const/Priority");
+
+module.exports = class StyleContest extends Card {
+  constructor(role) {
+    super(role);
+
+
+
+    
+    this.listeners = {
+      AbilityToggle: function (player) {
+        if (this.player.hasAbility(["WhenDead"])){
+          for(let player of this.game.players){
+            if(player.faction == this.player.faction){
+              if(!player.hasEffect("StylePoints")){
+              let effect = player.giveEffect("Immortal", 5, Infinity);
+              this.player.passiveEffects.push(effect);
+              }
+            }
+          }
+        }
+      },
+      handleWinWith: function (winners) {
+        if (!this.player.hasAbility(["Win-Con", "WhenDead"])) {
+          return;
+        }
+        let stylePlayers = [];
+        let highScore = 1;
+        for(let player of this.game.players){
+          if(player.data.StylePoints == highScore){
+            stylePlayers.push(player);
+          }
+          else if(player.data.StylePoints > highScore){
+            stylePlayers = [];
+            highScore = player.data.StylePoints;
+          }
+        }
+        if(stylePlayers.length == 1){
+        winners.addPlayer(stylePlayers[0], "Style Points");
+        }
+      },
+    };
+  }
+};
