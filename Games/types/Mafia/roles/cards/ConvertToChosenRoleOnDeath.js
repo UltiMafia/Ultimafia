5181fe2c4906374189cc00bdc4c484c3e8f943df
SawJester <anglelord270@gmail.com>
2025-05-01 17:19:28 -0400

    Create ConvertToChosenRoleOnDeath.js

---
+++ Games/types/Mafia/roles/cards/ConvertToChosenRoleOnDeath.js
@@ -0,0 +1,60 @@
+const Card = require("../../Card");
+const { PRIORITY_CONVERT_DEFAULT } = require("../../const/Priority");
+const { addArticle } = require("../../../../core/Utils");
+module.exports = class ConvertToChosenRole extends Card {
+  constructor(role) {
+    super(role);
+    //const targetOptions = this.game.PossibleRoles.filter((r) => r);
+    this.meetings = {
+      "Select Player": {
+        states: ["Night"],
+        flags: ["voting"],
+        targets: { include: ["alive", "self"] },
+        action: {
+          priority: PRIORITY_CONVERT_DEFAULT - 1,
+          run: function () {
+            this.actor.role.data.targetPlayer = this.target;
+          },
+        },
+      },
+      "Role to Become": {
+        states: ["Night"],
+        flags: ["voting"],
+        inputType: "custom",
+        action: {
+          labels: ["convert", "role"],
+          priority: PRIORITY_CONVERT_DEFAULT,
+          run: function () {
+            let targetPlayer = this.actor.role.data.targetPlayer;
+            if (targetPlayer) {
+                if (this.dominates(targetPlayer)) {
+                  targetPlayer.giveEffect("BecomeRoleOnDeath", this.actor, this.target);
+                }
+              
+              delete this.actor.role.data.targetPlayer;
+            }
+          },
+        },
+      },
+    };
+
+    this.listeners = {
+      roleAssigned: function (player) {
+        if (player !== this.player) {
+          return;
+        }
+
+        this.data.ConvertOptions = this.game.PossibleRoles.filter((r) => this.game.getRoleAlignment(r) == "Cult" && r.split(":")[0] != this.player.role.name && !this.game.getRoleTags(r).includes("Demonic"));
+      },
+      // refresh cooldown
+      state: function (stateInfo) {
+        if (!stateInfo.name.match(/Night/)) {
+          return;
+        }
+        var ConvertOptions = this.data.ConvertOptions;
+
+        this.meetings["Role to Become"].targets = ConvertOptions;
+      },
+    };
+  }
+};
