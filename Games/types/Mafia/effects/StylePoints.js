7df83b0fbba9061e7a9258fa9c1eacfbf4912e40
SawJester <anglelord270@gmail.com>
2025-05-01 19:24:08 -0400

    Create StylePoints.js

---
+++ Games/types/Mafia/effects/StylePoints.js
@@ -0,0 +1,40 @@
+const Effect = require("../Effect");
+const Action = require("../Action");
+
+module.exports = class StylePoints extends Effect {
+  constructor(actor, player) {
+    super("StylePoints");
+    this.actor = actor;
+    this.word = player.role.name;
+    this.StylePointsToday = 0;
+
+    this.listeners = {
+      state: function () {
+        if(this.game.getStateName() == "Day"){
+          this.effect.StylePointsToday = 0;
+        }
+        if (this.game.getStateName() != "Night") return;
+        if (!this.player.alive) return;
+        this.actor.data.StylePoints += this.StylePointsToday;
+      },
+    };
+  }
+
+  speak(message) {
+    if (this.player && message.content.replace(" ", "").toLowerCase().includes(this.player.role.name)) {
+      var action = new Action({
+        actor: this.player,
+        target: this.player,
+        game: this.game,
+        effect: this,
+        labels: ["hidden"],
+        run: function () {
+          this.effect.StylePointsToday += 1;
+          this.target.queueAlert(`You gain 1 Style Point!`);
+        },
+      });
+
+      this.game.instantAction(action);
+    }
+  }
+};
