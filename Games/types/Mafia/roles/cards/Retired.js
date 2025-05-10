e962c8cf7c42b9ebbcede19dfb08b6096f7b460a
SawJester <anglelord270@gmail.com>
2025-05-10 16:10:42 -0400

    Update Retired.js

--- Games/types/Mafia/roles/cards/Retired.js
+++ Games/types/Mafia/roles/cards/Retired.js
@@ -5,6 +5,26 @@ module.exports = class Retired extends Card {
   constructor(role) {
     super(role);
 
+    this.meetingMods = {
+      "*": {
+        ModDisable: function (meetingName) {
+          for (let w = 0; w < IMPORTANT_MEETINGS_NIGHT.length; w++) {
+            if (meetingName == IMPORTANT_MEETINGS_NIGHT[w] || !meetingName) {
+              return true;
+            }
+          }
+          for (let w = 0; w < IMPORTANT_MEETINGS_DAY.length; w++) {
+            if (meetingName == IMPORTANT_MEETINGS_DAY[w] || !meetingName) {
+              return true;
+            }
+          }
+          if (meetingName == "Graveyard") return true;
+
+          return false;
+        },
+      },
+    };
+
     this.listeners = {
       SwitchRoleBefore: function (player) {
         if (player != this.player) return;
@@ -13,29 +33,40 @@ module.exports = class Retired extends Card {
         for (let item of this.player.items) {
           item.drop();
         }
+        let info = this.game.createInformation(
+          "RevealPlayersWithRoleInfo",
+          this.holder,
+          this.game,
+          null,
+          this.player.role.name
+        );
+        info.processInfo();
+        info.getInfoRaw();
 
-        this.player.holdItem("Retirement", this.player.role.name);
+        if (info.mainInfo.length <= 0) {
+          this.holder.queueAlert(
+            `You are a retired ${this.player.role.name}. You know that no one in this town is a ${this.player.role.name}`
+          );
+        } else {
+          this.holder.queueAlert(
+            `You are a retired ${this.player.role.name}. You remember a few people you worked with!`
+          );
+        }
+        
+        //this.player.holdItem("Retirement", this.player.role.name);
 
         let newRole = "Sidekick";
         let currRole = this.player.role.name;
 
-        if (this.player.role.alignment == "Village") {
-          newRole = "Villager";
-        } else if (this.player.role.alignment == "Mafia") {
-          newRole = "Mafioso";
-        } else if (this.player.role.alignment == "Cult") {
-          newRole = "Cultist";
-        }
-
+        if (this.player.role.alignment == "Independent") {
         this.player.setRole(
-          newRole,
+          "Sidekick",
           undefined,
           false,
           true,
           false,
           "No Change"
         );
-        if ((newRole = "Sidekick")) {
           this.player.role.data.OldRole = currRole;
         }
       },
