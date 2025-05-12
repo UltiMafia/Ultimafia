0382c0dbac6a8b68003d6c180be301199070d85d
SawJester <anglelord270@gmail.com>
2025-05-11 20:02:52 -0400

    Update GuideWithFaith.js

--- Games/types/Mafia/roles/cards/GuideWithFaith.js
+++ Games/types/Mafia/roles/cards/GuideWithFaith.js
@@ -9,71 +9,48 @@ module.exports = class GuideWithFaith extends Card {
   constructor(role) {
     super(role);
 
-    this.listeners = {
-      roleAssigned: function (player) {
-        if (player !== this.player) {
-          return;
+    this.winCheckSpecial = {
+      priority: PRIORITY_WIN_CHECK_DEFAULT,
+      againOnFinished: true,
+      check: function (counts, winners, aliveCount, confirmedFinished) {
+        if (this.ReaperWinningTeam) {
+            for(let player of this.game.players){
+              if(player.role.name == this.ReaperWinningTeam || player.faction == this.ReaperWinningTeam){
+                winners.addPlayer(player, this.ReaperWinningTeam);
+              }
+            }
         }
-
-        this.player.role.data.ConvertOptions = this.game.PossibleRoles.filter(
-          (r) => r
-        );
-        this.player.role.data.GuessingPlayers = [];
-        this.player.role.data.GuessingRoles = [];
-        this.player.role.data.GuessingCount = 0;
-        this.player.role.data.HasInformation = false;
       },
-      // refresh cooldown
+    };
+
+    this.listeners = {
       state: function (stateInfo) {
         if (!stateInfo.name.match(/Night/)) {
           return;
         }
-        let guessOptions = this.player.role.data.ConvertOptions;
-        //ConvertOptions.push("None");
-        //this.meetings["Guess Role"].targets = guessOptions;
+        
+        if(this.FaithTarget && this.FaithTarget.alive && this.player.hasAbility(["Win-Con"])){
+          this.ReaperWin = true;
+          if(this.FaithTarget.faction == "Independent"){
+          this.ReaperWinningTeam = this.FaithTarget.role.name;
+          }
+          else{
+            this.ReaperWinningTeam = this.FaithTarget.faction;
+          }
+        }
+        else{
+          this.FaithTarget = null;
+        }
 
-        var action = new Action({
-          actor: this.player,
-          game: this.player.game,
-          priority: PRIORITY_INVESTIGATIVE_AFTER_RESOLVE_DEFAULT - 10,
-          labels: ["investigate", "role", "hidden", "absolute"],
-          run: function () {
-            if (!this.actor.alive) return;
-            if (
-              this.actor.role.data.GuessingPlayers == null ||
-              this.actor.role.data.GuessingPlayers.length <= 0
-            )
-              return;
-            if (
-              this.actor.role.data.GuessingRoles == null ||
-              this.actor.role.data.GuessingRoles.length <= 0
-            )
-              return;
-            if (this.actor.role.data.HasInformation == true) return;
-            this.actor.role.data.HasInformation = true;
-
-            let info = this.game.createInformation(
-              "GuessRoleInfo",
-              this.actor,
-              this.game,
-              this.actor.role.data.GuessingPlayers,
-              this.actor.role.data.GuessingRoles,
-              true
-            );
-
-            info.processInfo();
-
-            info.getGuessMessages();
-            this.actor.queueAlert(`:invest: ${info.getInfoFormated()}`);
-          },
-        });
-
-        this.game.queueAction(action);
+       
       },
     };
   }
 
   speak(message) {
+    if(message.sender.role.FaithTarget != null){
+      return;
+    }
     let formatedMessage = message.content;
     while (
       formatedMessage.includes("(") ||
@@ -87,9 +64,9 @@ module.exports = class GuideWithFaith extends Card {
     formatedMessage = formatedMessage.toLowerCase();
     if (message.sender.role.data.GuessingCount >= 5) return;
     if (message.sender.role.data.HasInformation == true) return;
-    if (formatedMessage.includes("Let us all have faith in ")) {
+    if (formatedMessage.includes("I claim Reaper and choose ")) {
       formatedMessage = formatedMessage.replace(
-        "Let us all have faith in ",
+        "I claim Reaper and choose ",
         ""
       );
 
@@ -125,10 +102,10 @@ module.exports = class GuideWithFaith extends Card {
         labels: ["hidden"],
         run: function () {
           if (this.actor == this.target) {
-            this.actor.queueAlert(`You cannot place your faith in yourself.`);
+            this.actor.queueAlert(`You cannot choose yourself.`);
           }
           this.actor.queueAlert(
-            `You place your faith in ${this.target.name}, Their team will win if they survive today.`
+            `You choose ${this.target.name}, Their team will win if they survive today.`
           );
           this.actor.role.FaithTarget = this.target.name;
         },
