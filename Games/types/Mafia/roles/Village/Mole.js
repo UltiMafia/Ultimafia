e6b02c838dfeff13e6d1731060d8820eebaf428b
SawJester <anglelord270@gmail.com>
2025-05-12 09:46:23 -0400

    Create Mole.js

---
+++ Games/types/Mafia/roles/Village/Mole.js
@@ -0,0 +1,15 @@
+const Role = require("../../Role");
+
+module.exports = class Mole extends Role {
+  constructor(player, data) {
+    super("Mole", player, data);
+
+    this.alignment = "Village";
+    this.cards = [
+      "VillageCore",
+      "WinWithFaction",
+      "MeetingFaction",
+      "BecomeUndercoverEvil",
+    ];
+  }
+};
