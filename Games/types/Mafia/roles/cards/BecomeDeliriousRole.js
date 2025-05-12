3c781c76fd6374772959901dc50609ea03f66388
SawJester <SawJester@users.noreply.github.com>
2025-05-12 01:58:15 +0000

    Prettified Code!

--- Games/types/Mafia/roles/cards/BecomeDeliriousRole.js
+++ Games/types/Mafia/roles/cards/BecomeDeliriousRole.js
@@ -6,32 +6,30 @@ module.exports = class BecomeDeliriousRole extends Card {
     super(role);
 
     //this.startItems = ["IsTheBraggart"];
-    
+
     let banishedRoles = this.game.banishedRoles;
-        let roles = this.game.PossibleRoles.filter((r) => r);
-        let currentRoles = [];
-        let playersAll = this.game.players.filter((p) => p.role);
-        for (let x = 0; x < playersAll.length; x++) {
-          //currentRoles.push(playersAll[x].role);
-          let tempName = playersAll[x].role.name;
-          let tempModifier = playersAll[x].role.modifier;
-          currentRoles.push(`${tempName}:${tempModifier}`);
-        }
-        for (let y = 0; y < currentRoles.length; y++) {
-          roles = roles.filter(
-            (r) => r != currentRoles[y] && !currentRoles[y].includes(r)
-          );
-        }
-        roles = roles.filter((r) => !r.toLowerCase().includes("banished"));
-        roles = roles.filter((r) => this.game.getRoleAlignment(r) == "Village");
-        roles = roles.filter((r) => this.game.getRoleTags(r).includes("Humble"));
-        if (roles.length <= 0) {
-          roles = currentRoles;
-          roles = roles.filter(
-            (r) => this.game.getRoleAlignment(r) == "Village"
-          );
-          roles = roles.filter((r) => r.split(":")[0] != "Braggart");
-        }
+    let roles = this.game.PossibleRoles.filter((r) => r);
+    let currentRoles = [];
+    let playersAll = this.game.players.filter((p) => p.role);
+    for (let x = 0; x < playersAll.length; x++) {
+      //currentRoles.push(playersAll[x].role);
+      let tempName = playersAll[x].role.name;
+      let tempModifier = playersAll[x].role.modifier;
+      currentRoles.push(`${tempName}:${tempModifier}`);
+    }
+    for (let y = 0; y < currentRoles.length; y++) {
+      roles = roles.filter(
+        (r) => r != currentRoles[y] && !currentRoles[y].includes(r)
+      );
+    }
+    roles = roles.filter((r) => !r.toLowerCase().includes("banished"));
+    roles = roles.filter((r) => this.game.getRoleAlignment(r) == "Village");
+    roles = roles.filter((r) => this.game.getRoleTags(r).includes("Humble"));
+    if (roles.length <= 0) {
+      roles = currentRoles;
+      roles = roles.filter((r) => this.game.getRoleAlignment(r) == "Village");
+      roles = roles.filter((r) => r.split(":")[0] != "Braggart");
+    }
     this.newRole = Random.randArrayVal(roles);
 
     let tempApp = {
@@ -45,7 +43,7 @@ module.exports = class BecomeDeliriousRole extends Card {
         if (player != this.player) return;
         this.player.role.data.reroll = true;
         this.player.holdItem("IsTheBraggart");
-        
+
         this.player.setRole(
           this.newRole,
           undefined,
@@ -55,7 +53,7 @@ module.exports = class BecomeDeliriousRole extends Card {
           "No Change"
         );
       },
-      roleAssigned: function (player){
+      roleAssigned: function (player) {
         if (player !== this.player) {
           return;
         }
@@ -68,7 +66,6 @@ module.exports = class BecomeDeliriousRole extends Card {
           false,
           "No Change"
         );
-        
       },
     };
   }
