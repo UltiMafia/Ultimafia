c1170c5643e8430d58d73a8f325789c4ff8d8af7
SawJester <anglelord270@gmail.com>
2025-05-11 20:47:11 -0400

    Update and rename Shepherd.js to Reaper.js

--- Games/types/Mafia/roles/Cult/Reaper.js
+++ Games/types/Mafia/roles/Cult/Reaper.js
@@ -2,14 +2,14 @@ const Role = require("../../Role");
 
 module.exports = class Shepherd extends Role {
   constructor(player, data) {
-    super("Shepherd", player, data);
+    super("Reaper", player, data);
 
     this.alignment = "Cult";
     this.cards = [
       "VillageCore",
       "WinWithFaction",
       "MeetingFaction",
-      "GuideWithFaith",
+      "ChooseWinner",
     ];
   }
 };
