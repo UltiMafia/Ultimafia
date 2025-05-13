ef4de6b927f9f29aa9775c34bddfaa684f2c2596
SawJester <anglelord270@gmail.com>
2025-05-13 13:09:43 -0400

    Create Sheild.js

---
+++ Games/types/Mafia/items/Sheild.js
@@ -0,0 +1,50 @@
+const Item = require("../Item");
+const Action = require("../Action");
+const Random = require("../../../../lib/Random");
+const { PRIORITY_MODIFY_ACTION } = require("../const/Priority");
+
+module.exports = class Sheild extends Item {
+  constructor(options, lifespan) {
+    super("Sheild");
+
+    this.lifespan = lifespan || Infinity;
+    this.magicCult = options?.magicCult;
+    this.broken = options?.broken;
+    this.listeners = {
+      state: function (stateInfo) {
+        if (this.game.getStateName() != "Night") return;
+
+        if (!this.holder.alive) return;
+
+        this.action = new Action({
+          actor: this.holder,
+          target: this.holder,
+          game: this.game,
+          priority: PRIORITY_MODIFY_ACTION,
+          item: this,
+          labels: ["hidden"],
+          run: function () {
+            if (this.game.getStateName() != "Night") return;
+
+            var alive = this.game.players.filter(
+              (p) =>
+                p.alive &&
+                p != this.actor &&
+                p.role.alignment == this.actor.role.alignment
+            );
+            if (alive.length > 0) {
+              var randomTarget = Random.randArrayVal(alive);
+              for (const action of this.game.actions[0]) {
+                if (action.target === this.actor && action.hasLabel("kill")) {
+                  action.target = randomTarget;
+                }
+              }
+            }
+          },
+        });
+
+        this.game.queueAction(this.action);
+      },
+    };
+  }
+};
