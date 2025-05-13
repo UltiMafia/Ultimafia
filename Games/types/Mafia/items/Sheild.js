81af7cea9d77e81c046d12437a0969a71ebe4811
SawJester <SawJester@users.noreply.github.com>
2025-05-13 18:20:40 +0000

    Prettified Code!

--- Games/types/Mafia/items/Sheild.js
+++ Games/types/Mafia/items/Sheild.js
@@ -26,14 +26,13 @@ module.exports = class Sheild extends Item {
           run: function () {
             if (this.game.getStateName() != "Night") return;
 
-            if(this.item.magicCult == true || this.broken == true)
-
-            var alive = this.game.players.filter(
-              (p) =>
-                p.alive &&
-                p != this.actor &&
-                p.role.alignment == this.actor.role.alignment
-            );
+            if (this.item.magicCult == true || this.broken == true)
+              var alive = this.game.players.filter(
+                (p) =>
+                  p.alive &&
+                  p != this.actor &&
+                  p.role.alignment == this.actor.role.alignment
+              );
             if (alive.length > 0) {
               var randomTarget = Random.randArrayVal(alive);
               for (const action of this.game.actions[0]) {
