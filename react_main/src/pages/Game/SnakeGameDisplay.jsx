4a2320976fe3cf424d23ff8ddd00c2eb8001bdaa
Dmitrii Gaidarji <dmitriigaidarji@gmail.com>
2025-05-19 00:09:09 +0800

    fix(socket): move snake with the keyboard

--- react_main/src/pages/Game/SnakeGameDisplay.jsx
+++ react_main/src/pages/Game/SnakeGameDisplay.jsx
@@ -234,15 +234,16 @@ export default function SnakeGameDisplay({ player, players, gameSocket }) {
       d: "right",
     };
     function keyListener(e) {
-      if (keyToDir[e.key]) {
-        if (window.setDirection) {
-          window.setDirection(keyToDir[e.key]);
-        }
+      const val = keyToDir[e.key];
+      if (val) {
+        gameSocket.send("move", val);
+        e.preventDefault();
+        e.stopPropagation();
       }
     }
     window.addEventListener("keydown", keyListener);
     return () => window.removeEventListener("keydown", keyListener);
-  }, [playerId]);
+  }, [playerId, gameSocket]);
 
   return (
     <div
