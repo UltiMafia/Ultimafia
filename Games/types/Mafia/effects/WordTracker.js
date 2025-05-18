59b50409fa3e76b43f83e893ea8d9c49ebdd21b2
SawJester <anglelord270@gmail.com>
2025-05-18 12:09:55 -0400

    Create WordTracker.js

---
+++ Games/types/Mafia/effects/WordTracker.js
@@ -0,0 +1,36 @@
+const Effect = require("../Effect");
+const Action = require("../Action");
+
+module.exports = class WordTracker extends Effect {
+  constructor(lifespan, actor, player, phrase) {
+    super("WordTracker");
+    this.actor = actor;
+    this.player = player;
+    this.phrase = phrase;
+    this.lifespan = lifespan;
+
+  }
+
+  speak(message) {
+    if (message.content
+        .replace(" ", "")
+        .toLowerCase()
+        .includes(this.phrase.toLowerCase()) &&
+      this.game.getStateName() == "Day"
+    ) {
+      var action = new Action({
+        actor: this.player,
+        target: this.player,
+        game: this.game,
+        effect: this,
+        labels: ["hidden"],
+        run: function () {
+          this.effect.actor.data.PlayersWhoSaidPhrase.push(this.actor);
+          this.effect.remove();
+        },
+      });
+
+      this.game.instantAction(action);
+    }
+  }
+};
