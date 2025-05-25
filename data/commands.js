1347ad48a2587a8680753795b44a1d63e9f90dfc
SawJester <anglelord270@gmail.com>
2025-05-25 12:38:48 -0400

    Create commands.js

---
+++ data/commands.js
@@ -0,0 +1,57 @@
+const modifierData = {
+    "/kick": {
+    input: "Username",
+    description:
+      "Used during pregame to temporarily remove someone from a game lobby.",
+  },
+  "/ban": {
+    input: "Username",
+    description:
+      "Used during pregame to permanently remove someone from a game lobby.",
+  },
+  "/me": {
+    input: "Text",
+    description: "Used to speak in italics as if roleplaying.",
+  },
+  "/extend": {
+    input: "N/A",
+    description:
+      "Used during the day phase to give more time to players while deliberating on the vote.",
+  },
+  "/role": {
+    input: "Role name",
+    description: "Used to display role descriptions in chat.",
+  },
+  "/item": {
+    input: "Item name",
+    description: "Used to display item descriptions in chat.",
+  },
+  "/modifier": {
+    input: "Modifier name",
+    description: "Used to display modifier descriptions in chat.",
+  },
+  "/achievement": {
+    input: "Achievement name",
+    description:
+      "Used to display achievements descriptions in chat and check if you have the achievement.",
+  },
+  "/diceroll": {
+    input: "Amount, Dice Type",
+    description: "Rolls dice, results are shown to everyone.",
+  },
+  "/Nightorder": {
+    input: "N/A",
+    description:
+      "Lists the Night Order for the Setup (Ties are resloved by Player Order).",
+  },
+  "/special": {
+    input: "N/A",
+    description: "Lists Any Special Role Interactions for the Setup.",
+  },
+  "/help": {
+    input: "N/A",
+    description: "Lists the commands on the site.",
+  },
+};
+
+module.exports = modifierData;
