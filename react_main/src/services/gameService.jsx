af24a984c3439d95593a2b0060395c9d4eb1f723
nearbear <79229391+nearbear@users.noreply.github.com>
2025-05-24 19:08:28 -0600

    Changed most popular setups component to accept lobby as query param (#2010)

--- react_main/src/services/gameService.jsx
+++ react_main/src/services/gameService.jsx
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 export const getRecentlyPlayedSetups = async ({ daysInterval }) => {
+  const lobby = localStorage.getItem("lobby") || "Main";
   // by default, returns the 1st announcement (no DATE provided in URLfilter)
   try {
     const res = await axios.get(
-      `/game/mostPlayedRecently?daysInterval=${daysInterval}`
+      `/game/mostPlayedRecently?lobby=${lobby}`
     );
     return res?.data;
   } catch (err) {
