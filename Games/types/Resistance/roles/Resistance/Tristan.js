28578792ac9fb3b085bbd38905d2761cdb07de77
SawJester <anglelord270@gmail.com>
2025-05-25 13:09:27 -0400

    Create Tristan.js

---
+++ Games/types/Resistance/roles/Resistance/Tristan.js
@@ -0,0 +1,10 @@
+const Role = require("../../Role");
+
+module.exports = class Tristan extends Role {
+  constructor(player, data) {
+    super("Tristan", player, data);
+
+    this.alignment = "Resistance";
+    this.cards = ["TeamCore", "WinWithResistance", "KnowIsolde "];
+  }
+};
