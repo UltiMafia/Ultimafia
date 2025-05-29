b070b7c298f9e3bea180e7ba47321d716c337bf8
SawJester <anglelord270@gmail.com>
2025-05-28 22:16:23 -0400

    Create GoldenCarp.js

---
+++ Games/types/Mafia/items/GoldenCarp.js
@@ -0,0 +1,14 @@
+const Item = require("../Item");
+
+module.exports = class GoldenCarp extends Item {
+  constructor() {
+    super("Golden Carp");
+  }
+
+    hold(player) {
+    super.hold(player);
+    player.game.sendAlert(`${player.name} has caught a Golden Carp!`);
+    player.sendAlert(`If you collect 3 Golden Carps you win!`);
+    player.giveEffect("WinWith3GoldenCarps");
+  }
+};
