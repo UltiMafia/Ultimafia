212ef782228a89953bf627f669a6fa10908e5d6a
SawJester <anglelord270@gmail.com>
2025-05-29 20:30:41 -0400

    Create Fisherman.js

---
+++ Games/types/Mafia/roles/Village/Fisherman.js
@@ -0,0 +1,20 @@
+const Role = require("../../Role");
+
+module.exports = class Fisherman extends Role {
+  constructor(player, data) {
+    super("Fisherman", player, data);
+
+    this.alignment = "Village";
+    this.cards = [
+      "VillageCore",
+      "WinWithFaction",
+      "MeetingFaction",
+      "GiveFishingRod",
+    ];
+  this.startItems = [
+      {
+        type: "FishingRod"
+      },
+    ];
+  }
+};
