f1dfccc0fcea7dbbef638f259b25e77d33a2d85e
SawJester <anglelord270@gmail.com>
2025-06-03 23:47:41 -0400

    Create Riflemaster.js

---
+++ Games/types/Mafia/roles/Village/Riflemaster.js
@@ -0,0 +1,9 @@
+const Role = require("../../Role");
+
+module.exports = class Riflemaster extends Role {
+  constructor(player, data) {
+    super("Riflemaster", player, data);
+    this.alignment = "Village";
+    this.cards = ["VillageCore", "WinWithFaction", "MeetingFaction", "GiveRifle"];
+  }
+};
