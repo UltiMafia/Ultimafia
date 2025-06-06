6aa64f622735cc6e2bdfd58a0dcbc18c02ca4df7
SawJester <anglelord270@gmail.com>
2025-06-06 15:56:55 -0400

    Update Filibuster.js

--- Games/types/Mafia/roles/Mafia/Filibuster.js
+++ Games/types/Mafia/roles/Mafia/Filibuster.js
@@ -10,7 +10,7 @@ module.exports = class Filibuster extends Role {
       "WinWithFaction",
       "MeetingFaction",
 
-      "CondemnImmune",
+      //"CondemnImmune",
       "DiesWithVillageCondemn",
     ];
   }
