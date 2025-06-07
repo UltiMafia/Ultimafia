e50901498435641fe7716563cd5a9fb37899a16c
SawJester <anglelord270@gmail.com>
2025-06-07 00:35:30 -0400

    Create SetTimer.js

---
+++ Games/types/Mafia/roles/cards/SetTimer.js
@@ -0,0 +1,36 @@
+const Card = require("../../Card");
+const { PRIORITY_DAY_EFFECT_DEFAULT } = require("../../const/Priority");
+
+module.exports = class SetTimer extends Card {
+  constructor(role) {
+    super(role);
+
+    this.meetings = {
+      "Progress Timer": {
+        actionName: "Speed Up",
+        states: ["Day"],
+        flags: ["voting", "instant"],
+        inputType: "boolean",
+        shouldMeet: function () {
+          return !this.hasSpeedUp;
+        },
+        action: {
+          priority: PRIORITY_DAY_EFFECT_DEFAULT,
+          run: function () {
+            if (this.target === "Yes") {
+              this.actor.role.hasSpeedUp = true;
+              if (!this.actor.hasAbility(["Effect"])) {
+                return;
+              }
+              this.game.extensions = 99;
+              this.game.timers["main"].set(60 * 1000);
+              this.game.queueAlert(
+                ":omg: Someone has decided to set the timer to 1 minute!"
+              );
+            }
+          },
+        },
+      },
+    };
+  }
+};
