c37179b0e7313fcfbcc876a392b0a49bb0ae2aff
SawJester <anglelord270@gmail.com>
2025-06-11 22:47:54 +0000

    m-"Music fix"

--- react_main/src/pages/Game/MafiaGame.jsx
+++ react_main/src/pages/Game/MafiaGame.jsx
@@ -209,6 +209,9 @@ export default function MafiaGame() {
         }
       }
     }
+    else{
+        game.stopAudio();
+      }
   }, [history.currentState]);
 
   useSocketListeners((socket) => {
