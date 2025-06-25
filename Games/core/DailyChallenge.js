cd6e2c5a9a6cb2a657d1903e25dfc1c1348d3ea0
SawJester <anglelord270@gmail.com>
2025-06-25 02:08:16 -0400

    Create DailyChallenge.js

---
+++ Games/core/DailyChallenge.js
@@ -0,0 +1,39 @@
+const shortid = require("shortid");
+
+module.exports = class DailyChallenge {
+  constructor(name, player) {
+    this.id = shortid.generate();
+    this.name = name;
+    this.game = player.game;
+    this.player = player;
+    this.listeners = {};
+  }
+
+  start() {
+    for (let eventName in this.listeners) {
+      this.listeners[eventName] = this.listeners[eventName].bind(this);
+      this.game.events.on(eventName, this.listeners[eventName]);
+    }
+  }
+
+  remove() {
+    this.game.events.removeListener("state", this.ageListener);
+
+    for (let eventName in this.listeners)
+      this.player.events.removeListener(eventName, this.listeners[eventName]);
+  }
+
+  speak(message) {}
+
+  speakQuote(quote) {}
+
+  hear(message) {}
+
+  hearQuote(quote) {}
+
+  seeVote(vote) {}
+
+  seeUnvote(info) {}
+
+  seeTyping(info) {}
+};
