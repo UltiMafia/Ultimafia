09f64d1dad6e49ea2b1b354a04a8521d1cd45142
SawJester <anglelord270@gmail.com>
2025-06-28 10:32:20 -0400

    Create OnePlayerShowsAsEvil.js

---
+++ Games/types/Mafia/roles/cards/OnePlayerShowsAsEvil.js
@@ -0,0 +1,47 @@
+const Card = require("../../Card");
+const Random = require("../../../../../lib/Random");
+
+module.exports = class OnePlayerShowsAsEvil extends Card {
+  constructor(role) {
+    super(role);
+
+    //this.startEffects = ["UnfavorableMode"];
+    this.listeners = {
+      AbilityToggle: function (player) {
+        if (player != this.player) {
+          return;
+        }
+        if (this.player.hasAbility(["Modifier", "Information", "WhenDead"])) {
+          if(this.BiasedTarget == null){
+            this.BiasedTarget = Random.randArrayVal(this.game.players.filter((p)=> p.faction == "Village" && p != this.player);
+          }
+          else if(this.BiasedTarget.faction != "Village"){
+            this.BiasedTarget = Random.randArrayVal(this.game.players.filter((p)=> p.faction == "Village" && p != this.player);
+          }
+          if (
+            this.OnePlayerShowsAsEvilEffect == null ||
+            !this.player.effects.includes(this.OnePlayerShowsAsEvilEffect)
+          ) {
+            this.OnePlayerShowsAsEvilEffect = this.BiasedTarget.giveEffect(
+              "Biased",
+              Infinity,
+              this.player
+            );
+            this.player.passiveEffects.push(this.OnePlayerShowsAsEvilEffect);
+          }
+        } else {
+          var index = this.player.passiveEffects.indexOf(
+            this.OnePlayerShowsAsEvilEffect
+          );
+          if (index != -1) {
+            this.player.passiveEffects.splice(index, 1);
+          }
+          if (this.OnePlayerShowsAsEvilEffect != null) {
+            this.OnePlayerShowsAsEvilEffect.remove();
+            this.OnePlayerShowsAsEvilEffect = null;
+          }
+        }
+      },
+    };
+  }
+};
