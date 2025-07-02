d4f9533539ce383e0aeb313c2e212bca977bd947
SawJester <anglelord270@gmail.com>
2025-07-02 02:35:52 -0400

    Update Devotion.js

--- Games/types/Mafia/roles/cards/Devotion.js
+++ Games/types/Mafia/roles/cards/Devotion.js
@@ -4,9 +4,36 @@ const Random = require("../../../../../lib/Random");
 module.exports = class Devotion extends Card {
   constructor(role) {
     super(role);
+    /*
     this.role.data.DevotionCult = true;
     if (this.role.name == "Devotee") {
       this.role.data.BackUpConvert = true;
     }
+*/
+
+    this.listeners = {
+      AbilityToggle: function (player) {
+        if (player != this.player) {
+          return;
+        }
+        if (this.player.hasAbility(["Win-Con"])) {
+          this.role.data.DevotionCult = true;
+          if (this.role.name == "Devotee" && this.game.alivePlayers().length >= 5) {
+            this.role.data.BackUpConvert = true;
+          }
+          else{
+            this.role.data.BackUpConvert = false;
+            this.role.data.DevotionCult = false;
+          }
+          
+        } else {
+          this.role.data.BackUpConvert = false;
+          this.role.data.DevotionCult = false;
+        }
+      },
+    };
+
+
+    
   }
 };
