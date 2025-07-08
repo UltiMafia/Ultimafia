276e5bfa0ae75f4e8339d34bb99869161bc9dc35
SawJester <anglelord270@gmail.com>
2025-07-08 12:50:45 -0400

    Create ActAfterNightKilled.js

---
+++ Games/types/Mafia/roles/cards/ActAfterNightKilled.js
@@ -0,0 +1,196 @@
+const Card = require("../../Card");
+const {
+  IMPORTANT_MEETINGS_NIGHT,
+  INVITED_MEETINGS,
+  STARTS_WITH_MEETINGS,
+  IMPORTANT_MEETINGS_DAY,
+} = require("../../const/ImportantMeetings");
+
+module.exports = class ActAfterNightKilled extends Card {
+  constructor(role) {
+    super(role);
+
+
+      this.listeners = {
+      roleAssigned: function (player) {
+        if (player !== this.player) {
+          return;
+        }
+
+        const nonMafia = this.game.players.filter(
+          (p) => p.role.alignment === "Village" && p.alive && p !== this.player
+        );
+        let shuffledPlayers = Random.randomizeArray(nonMafia);
+        if (nonMafia.length <= 0) return;
+        this.player.role.targetPlayer = Random.randArrayVal(nonMafia);
+      },
+      death: function (player, killer, deathType, instant) {
+        if(player != this.player){
+          return;
+        }
+        if (this.game.getStateName() != "Night") return;
+        this.HasBeenNightKilled == true;
+      },
+      revival: function (player, killer, deathType, instant) {
+        if(player != this.player){
+          return;
+        }
+        if(!this.player.alive){
+          return;
+        }
+        this.HasBeenNightKilled == false;
+      },
+    };
+    
+
+    this.meetingMods = {
+      "*": {
+        shouldMeetDeadMod: function (meetingName) {
+          if (!this.player.alive) {
+            for (let w = 0; w < IMPORTANT_MEETINGS_NIGHT.length; w++) {
+              if (meetingName == IMPORTANT_MEETINGS_NIGHT[w] || !meetingName) {
+                return false;
+              }
+            }
+
+            for (let w = 0; w < INVITED_MEETINGS.length; w++) {
+              if (meetingName == INVITED_MEETINGS[w]) {
+                return false;
+              }
+            }
+
+            for (let w = 0; w < STARTS_WITH_MEETINGS.length; w++) {
+              if (
+                meetingName &&
+                meetingName.startsWith(STARTS_WITH_MEETINGS[w])
+              ) {
+                return false;
+              }
+            }
+
+            for (let w = 0; w < IMPORTANT_MEETINGS_DAY.length; w++) {
+              if (meetingName == IMPORTANT_MEETINGS_DAY[w]) {
+                return true;
+              }
+            }
+
+          if(this.player.role.HasBeenNightKilled == true){
+          return true;
+          }
+          else{
+          return false;
+          }
+          } else {
+            if (meetingName == "Graveyard") {
+              return false;
+            } else {
+              for (let w = 0; w < IMPORTANT_MEETINGS_NIGHT.length; w++) {
+                if (
+                  meetingName == IMPORTANT_MEETINGS_NIGHT[w] ||
+                  !meetingName
+                ) {
+                  return true;
+                }
+              }
+
+              for (let w = 0; w < INVITED_MEETINGS.length; w++) {
+                if (meetingName == INVITED_MEETINGS[w]) {
+                  return true;
+                }
+              }
+
+              for (let w = 0; w < STARTS_WITH_MEETINGS.length; w++) {
+                if (
+                  meetingName &&
+                  meetingName.startsWith(STARTS_WITH_MEETINGS[w])
+                ) {
+                  return true;
+                }
+              }
+
+              for (let w = 0; w < IMPORTANT_MEETINGS_DAY.length; w++) {
+                if (meetingName == IMPORTANT_MEETINGS_DAY[w]) {
+                  return true;
+                }
+              }
+          return false;
+            }
+          }
+        },
+
+        whileDeadMod: function (meetingName) {
+          // core meetings
+          for (let w = 0; w < IMPORTANT_MEETINGS_DAY.length; w++) {
+            if (meetingName == IMPORTANT_MEETINGS_DAY[w]) {
+              return true;
+            }
+          }
+
+          for (let w = 0; w < IMPORTANT_MEETINGS_NIGHT.length; w++) {
+            if (meetingName == IMPORTANT_MEETINGS_NIGHT[w] || !meetingName) {
+              return false;
+            }
+          }
+
+          for (let w = 0; w < INVITED_MEETINGS.length; w++) {
+            if (meetingName == INVITED_MEETINGS[w]) {
+              return false;
+            }
+          }
+
+          for (let w = 0; w < STARTS_WITH_MEETINGS.length; w++) {
+            if (
+              meetingName &&
+              meetingName.startsWith(STARTS_WITH_MEETINGS[w])
+            ) {
+              return false;
+            }
+          }
+
+          // meetings invited by others
+          if(this.player.role.HasBeenNightKilled == true){
+          return true;
+          }
+          else{
+          return false;
+          }
+        },
+        WhileAliveMod: function (meetingName) {
+          // core meetings
+
+          for (let w = 0; w < IMPORTANT_MEETINGS_DAY.length; w++) {
+            if (meetingName == IMPORTANT_MEETINGS_DAY[w]) {
+              return true;
+            }
+          }
+
+          for (let w = 0; w < IMPORTANT_MEETINGS_NIGHT.length; w++) {
+            if (meetingName == IMPORTANT_MEETINGS_NIGHT[w] || !meetingName) {
+              return true;
+            }
+          }
+
+          for (let w = 0; w < INVITED_MEETINGS.length; w++) {
+            if (meetingName == INVITED_MEETINGS[w]) {
+              return true;
+            }
+          }
+
+          for (let w = 0; w < STARTS_WITH_MEETINGS.length; w++) {
+            if (
+              meetingName &&
+              meetingName.startsWith(STARTS_WITH_MEETINGS[w])
+            ) {
+              return true;
+            }
+          }
+
+          if (meetingName == "Graveyard") return false;
+
+          // meetings invited by others
+          return false;
+        },
+      },
+    };
+  }
+};
