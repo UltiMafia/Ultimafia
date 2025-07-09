c47d8f84f0c9aaaa8febaf727cf2a1347a0db0a3
SawJester <anglelord270@gmail.com>
2025-07-08 20:30:01 -0400

    Create LearnRoleIfTargetIsFirstToVote.js

---
+++ Games/types/Mafia/roles/cards/LearnRoleIfTargetIsFirstToVote.js
@@ -0,0 +1,29 @@
+const Card = require("../../Card");
+const { PRIORITY_EFFECT_GIVER_DEFAULT } = require("../../const/Priority");
+
+module.exports = class LearnRoleIfTargetIsFirstToVote extends Card {
+  constructor(role) {
+    super(role);
+
+    this.meetings = {
+      "Wrangle Player": {
+        states: ["Night"],
+        flags: ["voting"],
+        targets: { include: ["alive"], exclude: ["self"] },
+        action: {
+          labels: ["effect"],
+          priority: PRIORITY_EFFECT_GIVER_DEFAULT,
+          run: function () {
+            if (this.dominates())
+              this.target.giveEffect(
+                "Wrangled",
+                this.actor,
+                this.actor.role.cursedWord,
+                1
+              );
+          },
+        },
+      },
+    };
+  }
+};
