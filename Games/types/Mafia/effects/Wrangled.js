849c05a5a3e5e01818d7d4e2a50060e61ef4a104
SawJester <anglelord270@gmail.com>
2025-07-08 20:58:59 -0400

    Create Wrangled.js

---
+++ Games/types/Mafia/effects/Wrangled.js
@@ -0,0 +1,101 @@
+const Effect = require("../Effect");
+const Action = require("../Action");
+
+module.exports = class Wrangled extends Effect {
+  constructor(curser, lifespan) {
+    super("Wrangled");
+    this.curser = curser;
+    this.lifespan = lifespan;
+
+    this.listeners = {
+      vote: function (vote) {
+        if(this.NotFirstVoter && this.NotFirstSpeaker){
+          this.remove();
+        }
+        if(this.NotFirstVoter == true){
+          return;
+        }
+        if (
+          (vote.meeting.name === "Village" ||
+            vote.meeting.name === "Room 1" ||
+            vote.meeting.name === "Room 2") &&
+          vote.voter === this.player &&
+          vote.target === this.target.id
+        ) {
+            if(vote.voter != this.player){
+              this.NotFirstVoter = true;
+            }
+          let action = new Action({
+            actor: this.curser,
+            target: this.player,
+            game: this.game,
+            labels: ["hidden", "investigate", "role"],
+            effect: this,
+            run: function () {
+            let info = this.game.createInformation(
+              "RoleInfo",
+              this.actor,
+              this.game,
+              this.target
+            );
+            info.processInfo();
+            var alert = `:invest: ${info.getInfoFormated()}.`;
+            this.actor.queueAlert(alert);
+
+              this.effect.remove();
+            },
+          });
+
+          this.game.instantAction(action);
+        }
+      },
+    };
+  }
+
+  hear(message){
+          if(this.NotFirstVoter && this.NotFirstSpeaker){
+          this.remove();
+        }
+        if(this.NotFirstSpeaker == true){
+          return;
+        }
+    if(!message.isServer && message.sender != this.player){
+      this.NotFirstSpeaker = true;
+    }
+  }
+
+  speak(message) {
+        if(this.NotFirstVoter && this.NotFirstSpeaker){
+          this.remove();
+        }
+        if(this.NotFirstSpeaker == true){
+          return;
+        }
+    if (this.NotFirstSpeaker != true) {
+          let action = new Action({
+            actor: this.curser,
+            target: this.player,
+            game: this.game,
+            labels: ["hidden", "investigate", "role"],
+            effect: this,
+            run: function () {
+            let info = this.game.createInformation(
+              "RoleInfo",
+              this.actor,
+              this.game,
+              this.target
+            );
+            info.processInfo();
+            var alert = `:invest: ${info.getInfoFormated()}.`;
+            this.actor.queueAlert(alert);
+
+              this.effect.remove();
+            },
+          });
+
+    this.game.instantAction(action);
+    }
+  }
+
+  
+};
