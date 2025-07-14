4dcb21106c84b3059443d22c22e4a827eaa2d5b0
SawJester <anglelord270@gmail.com>
2025-07-14 17:27:51 -0400

    Update MakeKillHidden.js

--- Games/types/Mafia/roles/cards/MakeKillHidden.js
+++ Games/types/Mafia/roles/cards/MakeKillHidden.js
@@ -42,13 +42,16 @@ module.exports = class MakeKillHidden extends Card {
           priority: PRIORITY_NIGHT_ROLE_BLOCKER + 5,
           run: function () {
             if (!this.actor.alive) return;
-
+            //this.actor.giveEffect("Astral", )
             for (let action of this.game.actions[0]) {
               if (
                 action.actors.includes(this.actor) &&
                 action.hasLabel("kill")
               ) {
-                action.labels = [...action.labels, "hidden"];
+               action.actors = action.actors.splice(action.actors.indexOf(this.actor),1);
+
+                
+                //action.labels = [...action.labels, "hidden"];
               }
             }
           },
