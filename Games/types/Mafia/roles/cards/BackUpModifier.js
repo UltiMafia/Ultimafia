e8b5d5fd4b1792a7e9915a01cb9a673fe1d83bef
SawJester <anglelord270@gmail.com>
2025-07-18 19:04:45 -0400

    BackUp modifier fix + SP interaction UI fix (#2148)
    SP now shows in Role Groups.
    
    BackUp modifier no longer uses the Student/Understudy roles.
    BackUp now gives a sidekick the correct leader always.

--- Games/types/Mafia/roles/cards/BackUpModifier.js
+++ Games/types/Mafia/roles/cards/BackUpModifier.js
@@ -1,5 +1,11 @@
 const Card = require("../../Card");
 const Random = require("../../../../../lib/Random");
+const {
+  IMPORTANT_MEETINGS_NIGHT,
+  INVITED_MEETINGS,
+  STARTS_WITH_MEETINGS,
+  IMPORTANT_MEETINGS_DAY,
+} = require("../../const/ImportantMeetings");
 
 module.exports = class BackUpModifier extends Card {
   constructor(role) {
@@ -26,11 +32,23 @@ module.exports = class BackUpModifier extends Card {
         } else if (this.player.role.alignment == "Cult") {
           newRole = "Devotee";
         }
-
+        
         this.player.queueAlert(
-          `Backup: You are the Backup for ${currRole}. If a ${currRole} is killed or Converted you become ${currRole}.`
+          `Backup: You are the Backup for ${currRole}. If a ${currRole} is killed you will gain your abilities.`
         );
 
+        this.data.RoleTargetBackup = this.name;
+        
+           this.BackUpEffect = this.player.giveEffect(
+              "BackUp",
+              this.name
+            );
+            this.player.passiveEffects.push(this.BackUpEffect);
+        
+
+
+        
+      if(newRole == "Sidekick"){
         this.player.setRole(
           newRole,
           undefined,
@@ -40,8 +58,82 @@ module.exports = class BackUpModifier extends Card {
           "No Change"
         );
         this.player.role.data.FromBackUpModifier = currRole;
-        this.data.OldRole = currRole;
+        this.player.role.data.OldRole = currRole;
+      }
+      },
+      roleAssigned: function (player) {
+        if(player != this.player){
+          return;
+        }
+        if(this.data.RoleTargetBackup != null){
+          return;
+        }
+
+        let newRole = "Sidekick";
+        let currRole = this.player.role.name;
+
+        if (this.player.role.alignment == "Village") {
+          newRole = "Student";
+        } else if (this.player.role.alignment == "Mafia") {
+          newRole = "Understudy";
+        } else if (this.player.role.alignment == "Cult") {
+          newRole = "Devotee";
+        }
+        
+        this.player.queueAlert(
+          `Backup: You are the Backup for ${currRole}. If a ${currRole} is killed you will gain your abilities.`
+        );
+
+        this.data.RoleTargetBackup = this.name;
+        
+           this.BackUpEffect = this.player.giveEffect(
+              "BackUp",
+              this.name
+            );
+            this.player.passiveEffects.push(this.BackUpEffect);
+        
+
+
+        
+      if(newRole == "Sidekick"){
+        this.player.setRole(
+          newRole,
+          undefined,
+          false,
+          true,
+          false,
+          "No Change"
+        );
+        this.player.role.data.FromBackUpModifier = currRole;
+        this.player.role.data.OldRole = currRole;
+      }
+
       },
     };
+
+
+    this.meetingMods = {
+      "*": {
+        shouldMeetMod: function (meetingName) {
+          for (let w = 0; w < IMPORTANT_MEETINGS_NIGHT.length; w++) {
+            if (meetingName == IMPORTANT_MEETINGS_NIGHT[w] || !meetingName) {
+              return true;
+            }
+          }
+          for (let w = 0; w < IMPORTANT_MEETINGS_DAY.length; w++) {
+            if (meetingName == IMPORTANT_MEETINGS_DAY[w] || !meetingName) {
+              return true;
+            }
+          }
+          if (meetingName == "Graveyard") return true;
+
+          return !this.player.hasEffect("BackUp");
+        },
+      },
+    };
+
+
+
+    
   }
 };
