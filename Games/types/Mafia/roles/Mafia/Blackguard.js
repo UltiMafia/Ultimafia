0467148565bc65cbfa8aac2d213ff81e5539104b
SawJester <anglelord270@gmail.com>
2025-07-22 11:41:11 -0400

    Create Blackguard.js

---
+++ Games/types/Mafia/roles/Mafia/Blackguard.js
@@ -0,0 +1,10 @@
+const Role = require("../../Role");
+
+module.exports = class Blackguard extends Role {
+  constructor(player, data) {
+    super("Blackguard", player, data);
+
+    this.alignment = "Mafia";
+    this.cards = ["VillageCore", "GainAbilitesIfTargetDies", "WinWithFaction", "MeetingFaction"];
+  }
+};
