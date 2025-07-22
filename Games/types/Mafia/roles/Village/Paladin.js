c98ea452bc0dc64f1c7bbe5338100cc92dac0bed
SawJester <anglelord270@gmail.com>
2025-07-22 11:38:56 -0400

    Update and rename Avatar.js to Paladin.js

--- Games/types/Mafia/roles/Village/Paladin.js
+++ Games/types/Mafia/roles/Village/Paladin.js
@@ -1,8 +1,8 @@
 const Role = require("../../Role");
 
-module.exports = class Avatar extends Role {
+module.exports = class Paladin extends Role {
   constructor(player, data) {
-    super("Avatar", player, data);
+    super("Paladin", player, data);
 
     this.alignment = "Village";
     this.cards = ["VillageCore", "GainAbilitesIfTargetDies", "WinWithFaction", "MeetingFaction"];
