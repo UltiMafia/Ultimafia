2aa4331dc9c8aedb0bfe4c438cc66c9d45062ebb
SawJester <anglelord270@gmail.com>
2025-08-11 19:11:36 -0400

    Update FixAllItems.js

--- Games/types/Mafia/roles/cards/FixAllItems.js
+++ Games/types/Mafia/roles/cards/FixAllItems.js
@@ -1,5 +1,5 @@
 const Card = require("../../Card");
-const { PRIORITY_ITEM_TAKER_DEFAULT } = require("../../const/Priority");
+const { PRIORITY_ITEM_TAKER_DEFAULT, PRIORITY_ITEM_TAKER_EARLY } = require("../../const/Priority");
 
 module.exports = class FixAllItems extends Card {
   constructor(role) {
@@ -11,8 +11,9 @@ module.exports = class FixAllItems extends Card {
         flags: ["voting"],
         action: {
           labels: ["fixItems"],
-          priority: PRIORITY_ITEM_TAKER_DEFAULT + 2,
+          priority: PRIORITY_ITEM_TAKER_EARLY + 2,
           run: function () {
+            this.role.PlayerToFixItems = this.target;
             for (let item of this.target.items) {
               item.broken = false;
               item.mafiaImmune = false;
@@ -22,5 +23,39 @@ module.exports = class FixAllItems extends Card {
         },
       },
     };
+
+      this.listeners = {
+      state: function (stateInfo) {
+        if (!this.hasAbility(["Item"])) {
+          return;
+        }
+
+        if (!stateInfo.name.match(/Night/)) {
+          return;
+        }
+
+        var action = new Action({
+          actor: this.player,
+          game: this.player.game,
+          role: this,
+          priority: PRIORITY_ITEM_TAKER_DEFAULT+2,
+          labels: ["sabotage"],
+          run: function () {
+            if(this.role.PlayerToFixItems != null){
+            for (let item of this.role.PlayerToFixItems.items) {
+              item.broken = false;
+              item.mafiaImmune = false;
+              item.magicCult = false;
+            }
+            }
+             this.role.PlayerToFixItems = null;
+          },
+        });
+
+        this.game.queueAction(action);
+      },
+    };
+
+    
   }
 };
