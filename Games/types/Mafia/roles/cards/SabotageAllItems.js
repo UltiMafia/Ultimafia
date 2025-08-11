75041e2a751facbed441d7be059c120d91b93725
SawJester <anglelord270@gmail.com>
2025-08-11 18:57:39 -0400

    Update SabotageAllItems.js

--- Games/types/Mafia/roles/cards/SabotageAllItems.js
+++ Games/types/Mafia/roles/cards/SabotageAllItems.js
@@ -1,5 +1,5 @@
 const Card = require("../../Card");
-const { PRIORITY_ITEM_TAKER_DEFAULT } = require("../../const/Priority");
+const { PRIORITY_ITEM_TAKER_DEFAULT, PRIORITY_ITEM_TAKER_EARLY } = require("../../const/Priority");
 
 module.exports = class SabotageAllItems extends Card {
   constructor(role) {
@@ -12,8 +12,10 @@ module.exports = class SabotageAllItems extends Card {
         targets: { include: ["alive"], exclude: ["Mafia"] },
         action: {
           labels: ["sabotage"],
-          priority: PRIORITY_ITEM_TAKER_DEFAULT + 1,
+          role: this.role,
+          priority: PRIORITY_ITEM_TAKER_EARLY + 1,
           run: function () {
+            this.role.PlayerToBreakItems = this.target;
             for (let item of this.target.items) {
               item.broken = true;
             }
@@ -21,5 +23,36 @@ module.exports = class SabotageAllItems extends Card {
         },
       },
     };
+
+    this.listeners = {
+      state: function (stateInfo) {
+        if (!this.hasAbility(["Item"])) {
+          return;
+        }
+
+        if (!stateInfo.name.match(/Night/)) {
+          return;
+        }
+
+        var action = new Action({
+          actor: this.player,
+          game: this.player.game,
+          role: this,
+          priority: PRIORITY_ITEM_TAKER_DEFAULT+1,
+          labels: ["stealItem"],
+          run: function () {
+            if(this.role.PlayerToBreakItems != null){
+            for (let item of this.role.PlayerToBreakItems.items) {
+              item.broken = true;
+            }
+            }
+            this.role.PlayerToBreakItems = null;
+          },
+        });
+
+        this.game.queueAction(action);
+      },
+    };
+    
   }
 };
