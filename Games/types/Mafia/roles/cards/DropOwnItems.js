3dcf6a677aa0989d5aeb23faa24ca6c832cfd1e4
SawJester <anglelord270@gmail.com>
2025-08-11 19:17:05 -0400

    Update DropOwnItems.js

--- Games/types/Mafia/roles/cards/DropOwnItems.js
+++ Games/types/Mafia/roles/cards/DropOwnItems.js
@@ -1,5 +1,5 @@
 const Card = require("../../Card");
-const { PRIORITY_ITEM_GIVER_DEFAULT } = require("../../const/Priority");
+const { PRIORITY_ITEM_GIVER_DEFAULT, PRIORITY_EFFECT_GIVER_EARLY } = require("../../const/Priority");
 
 module.exports = class DropOwnItems extends Card {
   constructor(role) {
@@ -16,5 +16,48 @@ module.exports = class DropOwnItems extends Card {
         },
       },
     ];
+
+    this.listeners = {
+      state: function (stateInfo) {
+        if (!this.hasAbility(["Item"])) {
+          return;
+        }
+
+        if (!stateInfo.name.match(/Night/)) {
+          return;
+        }
+
+        var action = new Action({
+          actor: this.player,
+          game: this.player.game,
+          role: this,
+          priority: PRIORITY_ITEM_TAKER_DEFAULT+2,
+          labels: ["dropItems", "hidden"],
+          run: function () {
+            for (let item of this.actor.items) {
+            item.drop();
+            }
+            
+          },
+        });
+
+        this.game.queueAction(action);
+        var action2 = new Action({
+          actor: this.player,
+          game: this.player.game,
+          role: this,
+          priority:PRIORITY_ITEM_TAKER_EARLY+2,
+          labels: ["dropItems", "hidden"],
+          run: function () {
+            for (let item of this.actor.items) {
+            item.drop();
+            }
+            
+          },
+        });
+        this.game.queueAction(action2);
+      },
+    };
+    
   }
 };
