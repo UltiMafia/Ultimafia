a436b18503aeeb87b2453d38c02adfbbef9fb776
SawJester <anglelord270@gmail.com>
2025-08-14 04:02:33 -0400

    Modifier Selection UI (#2196)

--- react_main/src/components/Roles.jsx
+++ react_main/src/components/Roles.jsx
@@ -374,6 +374,175 @@ userRoleSkins1 = user.settings.roleSkins.split(",");
   }
 }
 
+export function ModifierCount(props) {
+  const roleRef = useRef();
+  const popover = useContext(PopoverContext);
+  const user = useContext(UserContext);
+  const siteInfo = useContext(SiteInfoContext);
+  //const [roleData, setRoleData] = useState(null);
+  const isPhoneDevice = useIsPhoneDevice();
+
+  const {
+    popoverOpen: canOpenPopover,
+    popoverClasses,
+    anchorEl,
+    handleClick: handlePopoverClick,
+    handleMouseEnter,
+    handleMouseLeave,
+    closePopover,
+  } = usePopoverOpen();
+
+  const handleRoleCountClick = (e) => {
+    if (props.onClick) return props.onClick();
+
+    if (makeRolePrediction) {
+      makeRolePrediction(props.role);
+      popover.close();
+      return;
+    }
+
+    if (!roleName || props.showPopover == false || roleName === "null") return;
+
+    handlePopoverClick(e);
+  };
+
+  // Display predicted icon
+  const isRolePrediction = props.isRolePrediction;
+  // Choose from list of icons to predict from
+  const makeRolePrediction = props.makeRolePrediction;
+
+  var roleName;
+
+  if (typeof props.role == "string") {
+    roleName = props.role.split(":")[0];
+  } else if (props.role) {
+    roleName = props.role.name;
+  }
+  /*
+  useEffect(() => {
+    setRoleData({
+      ...siteInfo.modifiers[props.gameType][roleName],
+    });
+  }, [siteInfo, roleName]);
+*/
+let tempData = null;
+if(siteInfo.modifiers[props.gameType].filter((t) => t.name == roleName)){
+tempData = siteInfo.modifiers[props.gameType].filter((t) => t.name == roleName)[0];
+}
+else{
+  tempData = siteInfo.modifiers[props.gameType]["Disloyal"];
+}
+const roleData = tempData;
+
+  const roleClass = roleName
+    ? `${hyphenDelimit(props.gameType)}-${hyphenDelimit(roleName)}`
+    : "null";
+
+  function onRoleGroupClick() {
+    if (props.roleGroup) {
+      popover.onClick(
+        Promise.resolve({
+          data: {
+            roles: props.roleGroup,
+            gameType: props.gameType,
+            setup: props,
+            otherRoles: props.otherRoles,
+          },
+        }),
+        "modifier",
+      );
+    }
+  }
+
+  const digits =
+    props.count && !props.hideCount ? props.count.toString().split("") : "";
+
+  const popoverDisabled = Boolean(
+    props.showPopover === false
+  );
+  const popoverOpen = !popoverDisabled && canOpenPopover;
+  const roleTags = (roleData?.tags) ? roleData.tags.sort().join(", ") : "";
+  const DescriptionLines = (
+    <List dense sx={{ ...({ paddingTop: "0" }) }}>
+      {[roleData?.description].map((text, i) => (
+        <ListItem
+          key={text + i}
+          sx={{
+            paddingBottom: "0",
+            paddingTop: "0",
+            px: isPhoneDevice ? 1 : 2,
+          }}
+        >
+          <ListItemIcon
+            sx={{
+              minWidth: "0",
+              marginRight: "8px",
+            }}
+          >
+            <i className={"fas fa-info-circle"} />
+          </ListItemIcon>
+          <ListItemText
+            disableTypography
+            className={"mui-popover-text"}
+            primary={text}
+          />
+        </ListItem>
+      ))}
+    </List>
+  );
+
+    return (
+      <>
+        <div
+          className="role-count-wrap"
+          aria-owns={popoverOpen ? "mouse-over-popover" : undefined}
+          aria-haspopup="true"
+          onClick={handleRoleCountClick}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
+          <div
+            className={ `modifier modifier-${roleClass}`}
+            ref={roleRef}
+          >
+            {props.count > 1 && <DigitsCount digits={digits} />}
+          </div>
+        </div>
+        <div>
+          <Popover
+            open={props.showPopover !== false && popoverOpen}
+            sx={popoverClasses}
+            anchorEl={anchorEl}
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "center",
+            }}
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "center",
+            }}
+            onClose={closePopover}
+            disableScrollLock
+          >
+            <div className={"mui-popover"}>
+              <div className={"mui-popover-title"}>
+                <div className={`modifier modifier-${roleClass}`} />
+                &nbsp;{roleName}&nbsp;
+              </div>
+              <div style={{ margin: "3px" }}>
+                <div>
+                  <span style={{ fontWeight: "bold" }}>Tags</span>:{" "}
+                  {roleTags}
+                </div>
+                {DescriptionLines}
+              </div>
+            </div>
+          </Popover>
+        </div>
+      </>
+    );
+}
+
 function DigitsCount(props) {
   const digits = props.digits;
   return (
@@ -539,6 +708,173 @@ export function RoleSearch(props) {
         <SearchBar
           value={searchVal}
           placeholder="🔎 Role Name"
+          onInput={onSearchInput}
+        />
+      </Box>
+      <Box className="role-list">{roleCells}</Box>
+    </Box>
+  );
+}
+
+export function ModifierSearch(props) {
+  const theme = useTheme();
+  
+  const [roleListType, setRoleListType] = useState(
+    Alignments[props.gameType][0]
+  );
+  
+  const [searchVal, setSearchVal] = useState("");
+  const roleCellRefs = useRef([]);
+  const user = useContext(UserContext);
+  const siteInfo = useContext(SiteInfoContext);
+  const popover = useContext(PopoverContext);
+/*
+  function onAlignNavClick(alignment) {
+    setSearchVal("");
+    setRoleListType(alignment);
+  }
+
+  const roleAbbreviations = {
+    blue: ["Villager"],
+    nilla: ["Villager", "Mafioso"],
+    gs: ["Gunsmith"],
+    gf: ["Godfather"],
+    bs: ["Blacksmith"],
+    orc: ["Oracle"],
+    ww: ["Werewolf", "Hellhound"],
+    hh: ["Hellhound"],
+    bg: ["Bodyguard"],
+    cl: ["Cult Leader"],
+    gr: ["Graverobber"],
+    hb: ["Heartbreaker"],
+    lk: ["Lightkeeper"],
+    lm: ["Loudmouth"],
+    mm: ["Mastermind"],
+    ph: ["Party Host"],
+    sk: ["Serial Killer"],
+    sw: ["Sleepwalker"],
+    tc: ["Town Crier"],
+    tl: ["Tea Lady"],
+    rh: ["Robin Hood"],
+    hk: ["Housekeeper"],
+  };
+*/
+  function onSearchInput(query) {
+    setSearchVal(query.toLowerCase());
+
+    if (query !== "" && roleListType.length > 0) setRoleListType("");
+    else if (query === "" && roleListType.length === 0)
+      setRoleListType(Alignments[props.gameType][0]);
+  }
+
+  function onRoleCellClick(roleCellEl, role) {
+    popover.onClick(
+      Promise.resolve({
+        data: {
+          roleName: siteInfo.modifiers[props.gameType][role.name],
+        },
+      }),
+      "role",
+      roleCellEl,
+      role.name
+    );
+  }
+
+    function getCompatibleModifiersOther(mods) {
+
+    const mappedMods = siteInfo.modifiers[props.gameType].filter((t) => mods.includes(t.name));
+    let temp = [];
+    for(let mod of mappedMods){
+        if(mod && mod.incompatible){
+          temp.push(...mod.incompatible);
+        }
+    }
+    const incompatibles = temp;
+    const modifierOptions = siteInfo.modifiers[props.gameType]
+      .filter((e) => !e.hidden)
+      .filter((e) => e.allowDuplicate || !mods.includes(e.name))
+      .filter((e) => !incompatibles.includes(e.name))
+      .map((modifier) => (modifier.name));
+    return modifierOptions;
+  }
+
+  if (!siteInfo.modifiers) return <NewLoading small />;
+
+  const roleCells = siteInfo.modifiers[props.gameType].map((role, i) => {
+    const searchTerms = searchVal
+      .split(",")
+      .filter((term) => term.trim() !== "")
+      .map((term) => term.trim().toLowerCase());
+
+    const matchesSearch =
+      searchTerms.length === 0 ||
+      searchTerms.some(
+        (term) =>
+          role.name.toLowerCase().includes(term) ||
+          role.tags.join("").toLowerCase().includes(term)
+          /*
+          ||
+          Object.entries(roleAbbreviations).some(
+            ([shortcut, roleNames]) =>
+              shortcut === term && roleNames.includes(role.name)
+          )
+          */
+      );
+
+    if (
+      !role.disabled && getCompatibleModifiersOther(props.curMods).includes(role.name) &&
+      ((searchVal.length > 0 && (role.name.toLowerCase().indexOf(searchVal) !== -1 || matchesSearch)) || searchVal.length <= 0)
+    ) {
+      return (
+        <Card
+          className="role-cell"
+          key={role.name}
+          sx={{ padding: "0px", margin: "4px"}}
+        >
+          {user.loggedIn && props.onAddClick && (
+            <IconButton
+              className="add-role fa-plus-circle fas"
+              onClick={(e) => {
+                e.stopPropagation();
+                props.onAddClick(role.name);
+              }}
+              sx={{ padding: "2px", fontSize: "16px" }}
+            ></IconButton>
+          )}
+          <CardContent
+            className="role-cell-content"
+            onMouseOver={() =>
+              null && onRoleCellClick(roleCellRefs.current[i], role)
+            }
+            ref={(el) => (roleCellRefs.current[i] = el)}
+            sx={{ padding: "2px" }}
+          >
+          
+              <ModifierCount
+              role={role.name}
+              gameType={props.gameType}
+              sx={{ fontSize: "10px" }}
+            />
+            
+            <Typography variant="body2">{role.name}</Typography>
+          </CardContent>
+        </Card>
+      );
+    }
+  });
+
+  return (
+    <Box className="role-list-container">
+      <Box className="bot-bar">
+        <Tabs
+          value={roleListType}
+          onChange={(_, value) => setRoleListType(value)}
+          centered
+        >
+        </Tabs>
+        <SearchBar
+          value={searchVal}
+          placeholder="🔎 Modifier Name"
           onInput={onSearchInput}
         />
       </Box>
