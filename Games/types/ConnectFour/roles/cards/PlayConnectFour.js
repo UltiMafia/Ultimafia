d801fd163c34bb864046b1a1f6106bd6be653a44
SawJester <anglelord270@gmail.com>
2025-08-17 19:13:12 -0400

    Create PlayConnectFour.js

---
+++ Games/types/ConnectFour/roles/cards/PlayConnectFour.js
@@ -0,0 +1,33 @@
+const Card = require("../../Card");
+const {
+  IMPORTANT_MEETINGS,
+  ROLE_MEETINGS,
+} = require("../../const/ImportantMeetings");
+
+module.exports = class PlayConnectFour extends Card {
+  constructor(role) {
+    super(role);
+
+    this.meetings = {
+      "Place Chip": {
+        actionName: "Place Chip",
+        states: ["Turn"],
+        flags: ["voting", "multi"],
+        inputType: "custom",
+        targets: ["1","2","3","4","5","6","7","8"],
+        action: {
+          item: this,
+          run: function () {
+            this.game.placeChip(this.actor, this.target);
+          },
+        },
+        shouldMeet: function () {
+          return (
+            this.player.name ==
+            this.game.randomizedPlayersCopy[this.game.currentIndex].name
+          );
+        },
+      },
+    };
+  }
+};
