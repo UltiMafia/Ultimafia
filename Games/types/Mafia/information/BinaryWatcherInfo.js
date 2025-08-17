1812379a9049efa2fcb14d8521dd036483caeeb6
SawJester <anglelord270@gmail.com>
2025-08-17 13:32:34 -0400

    Update BinaryWatcherInfo.js

--- Games/types/Mafia/information/BinaryWatcherInfo.js
+++ Games/types/Mafia/information/BinaryWatcherInfo.js
@@ -20,6 +20,11 @@ module.exports = class BinaryTrackerInfo extends Information {
     }
     this.target = target;
     let visitors = this.getVisitorsAppearance(this.target);
+    if(visitors.includes(this.creator)){
+    while(visitors.includes(this.creator)){
+    visitors.splice(visitors.indexOf(this.creator), 1);
+    }
+    }
     if (visitors.length > 0) {
       this.mainInfo = "visited by somebody";
     } else {
@@ -40,6 +45,11 @@ module.exports = class BinaryTrackerInfo extends Information {
 
   isTrue() {
     let visitors = this.getVisitors(this.target);
+    if(visitors.includes(this.creator)){
+    while(visitors.includes(this.creator)){
+    visitors.splice(visitors.indexOf(this.creator), 1);
+    }
+  }
     let temp;
     if (visitors.length > 0) {
       temp = "visited by somebody";
@@ -76,6 +86,11 @@ module.exports = class BinaryTrackerInfo extends Information {
 
   makeTrue() {
     let visitors = this.getVisitors(this.target);
+    if(visitors.includes(this.creator)){
+    while(visitors.includes(this.creator)){
+    visitors.splice(visitors.indexOf(this.creator), 1);
+    }
+  }
     if (visitors.length > 0) {
       this.mainInfo = "visited by somebody";
     } else {
@@ -84,6 +99,11 @@ module.exports = class BinaryTrackerInfo extends Information {
   }
   makeFalse() {
     let visitors = this.getVisitors(this.target);
+    if(visitors.includes(this.creator)){
+    while(visitors.includes(this.creator)){
+    visitors.splice(visitors.indexOf(this.creator), 1);
+    }
+  }
     if (visitors.length > 0) {
       this.mainInfo = "not visited by anybody";
     } else {
