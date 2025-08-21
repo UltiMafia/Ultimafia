510610c034f54b860f5fb2800c6d8fa0022d1b96
SawJester <anglelord270@gmail.com>
2025-08-20 21:12:15 -0400

    Update AppearAsRandomRole.js

--- Games/types/Mafia/roles/cards/AppearAsRandomRole.js
+++ Games/types/Mafia/roles/cards/AppearAsRandomRole.js
@@ -5,6 +5,50 @@ module.exports = class AppearAsRandomRole extends Card {
   constructor(role) {
     super(role);
 
+
+    let evilRoles = role
+      .getAllRoles()
+      .filter(
+        (r) =>  if (
+            role.split(":")[0] != "Villager" &&
+            role.split(":")[0] != "Imposter" &&
+            role.split(":")[0] != "Impersonator" &&
+            role.split(":")[0] != "Skinwalker"
+          ) {
+            roles.push(roleName);
+          }
+           &&
+          !role.game.getRoleTags(r).includes("No Investigate")
+      );
+
+    let millers = [];
+
+    let temp = evilRoles.filter((p) => p);
+    if(evilRoles.length <= 0){
+    evilRoles = ["Cop:"];
+    }
+
+    const randomEvilRole = Random.randArrayVal(evilRoles);
+
+    // const roleAppearance = randomEvilRole.split(":")[0];
+
+    const roleAppearance = randomEvilRole;
+
+    const selfAppearance = role.name == "Miller" ? "Villager" : "real";
+
+    let tempApp = {
+      reveal: roleAppearance,
+      condemn: roleAppearance,
+      investigate: roleAppearance,
+    };
+    this.editAppearance(tempApp);
+    this.hideModifier = {
+      condemn: true,
+      investigate: true,
+      reveal: true,
+    };
+  }
+/*
     this.listeners = {
       roleAssigned: function (player) {
         if (player !== this.player) {
@@ -30,5 +74,6 @@ module.exports = class AppearAsRandomRole extends Card {
         this.player.queueAlert(roleAppearance);
       },
     };
+*/
   }
 };
