d72ce38f3c49ff8f558882560619d782adb93bdf
SawJester <anglelord270@gmail.com>
2025-08-23 06:28:51 +0000

    m-"Why"

--- modules/redis.js
+++ modules/redis.js
@@ -167,10 +167,10 @@ async function cacheUserInfo(userId, reset) {
 
     await client.setAsync(`user:${userId}:info:id`, userId);
     await client.setAsync(`user:${userId}:info:name`, user.name);
-    await client.setAsync(`user:${userId}:info:avatar`, user.avatar);
+    await client.setAsync(`user:${userId}:info:avatar`, user.avatar || false);
     await client.setAsync(`user:${userId}:info:nameChanged`, user.nameChanged);
     await client.setAsync(`user:${userId}:info:bdayChanged`, user.bdayChanged);
-    await client.setAsync(`user:${userId}:info:birthday`, user.birthday || 0);
+    await client.setAsync(`user:${userId}:info:birthday`, user.birthday || Date.now());
     await client.setAsync(`user:${userId}:info:pronouns`, user.pronouns);
     await client.setAsync(`user:${userId}:info:gamesPlayed`, gamesPlayed);
     await client.setAsync(`user:${userId}:info:redHearts`, user.redHearts);
