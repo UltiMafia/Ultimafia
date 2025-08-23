eeb2f1a62076435752d9994975e55c1be65eb7a1
SawJester <SawJester@users.noreply.github.com>
2025-08-23 06:03:42 +0000

    Prettified Code!

--- react_main/src/pages/Learn/role/RolePage.jsx
+++ react_main/src/pages/Learn/role/RolePage.jsx
@@ -93,7 +93,7 @@ export function RoleThings() {
         errorAlert(e);
       });
   }, []);
-  
+
   useEffect(() => {
     if (!user?.id) return;
 
@@ -161,15 +161,34 @@ export function RoleThings() {
 
   const roleSkins = temproleSkins;
 
-  let tempArtists = contributors["art"]?.filter((item, index) => item.roles.filter(r => r.split(":") == RoleName).length > 0);
+  let tempArtists = contributors["art"]?.filter(
+    (item, index) =>
+      item.roles.filter((r) => r.split(":") == RoleName).length > 0
+  );
   const artists = tempArtists.map((item, index) => {
-    return(<div>{<NameWithAvatar
-              small
-              id={item.user.id}
-              name={item.user.name}
-              avatar={item.user.avatar}
-            />} {item.roles.filter(r => r.split(":") == RoleName).map((roleToUse) => <RoleCount key={0} scheme={roleToUse.split(":")[1]} role={roleToUse.split(":")[0]} gameType={"Mafia"} />)}</div>)
-  })
+    return (
+      <div>
+        {
+          <NameWithAvatar
+            small
+            id={item.user.id}
+            name={item.user.name}
+            avatar={item.user.avatar}
+          />
+        }{" "}
+        {item.roles
+          .filter((r) => r.split(":") == RoleName)
+          .map((roleToUse) => (
+            <RoleCount
+              key={0}
+              scheme={roleToUse.split(":")[1]}
+              role={roleToUse.split(":")[0]}
+              gameType={"Mafia"}
+            />
+          ))}
+      </div>
+    );
+  });
 
   // favourites <SetupRowInfo title="Current Skins" content={roleSkins} />
 
