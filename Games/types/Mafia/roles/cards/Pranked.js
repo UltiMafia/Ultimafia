af8855999620f2a52b5096278ddb8441cf29f6ed
SawJester <anglelord270@gmail.com>
2025-08-27 05:20:01 -0400

    Update Pranked.js (#10)

--- Games/types/Mafia/roles/cards/Pranked.js
+++ Games/types/Mafia/roles/cards/Pranked.js
@@ -8,7 +8,7 @@ module.exports = class Pranked extends Card {
 
     this.listeners = {
       vote: function (vote) {
-        if (vote.meeting.name === "Village" && vote.target === this.player.id) {
+        if (vote.meeting.useVotingPower == true && vote.target === this.player.id) {
           if (this.data.hasBeenVoted == true) return;
 
           this.data.hasBeenVoted = true;
