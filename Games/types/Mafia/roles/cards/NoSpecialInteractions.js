1e83cc262ed857578cd34d7090f5e52057ff2cef
SawJester <anglelord270@gmail.com>
2025-09-01 02:12:04 -0400

    Create NoSpecialInteractions.js

---
+++ Games/types/Mafia/roles/cards/NoSpecialInteractions.js
@@ -0,0 +1,9 @@
+const Card = require("../../Card");
+const Random = require("../../../../../lib/Random");
+
+module.exports = class NoSpecialInteractions extends Card {
+  constructor(role) {
+    super(role);
+    
+  }
+};
