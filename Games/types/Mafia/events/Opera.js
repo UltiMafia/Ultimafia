37ac25661f4dbc86514211197815ee9ea3f819d1
SawJester <anglelord270@gmail.com>
2025-09-01 01:26:13 -0400

    Update Opera.js

--- Games/types/Mafia/events/Opera.js
+++ Games/types/Mafia/events/Opera.js
@@ -26,6 +26,12 @@ module.exports = class Opera extends Event {
             `Event: Opera, 3 players may attend a night at the Opera!`
           );
         }
+
+      if(this.event.modifiers.includes("Random")){
+       this.game.players[0].giveEffect("MovieNight", true); 
+      return;
+      }
+        
         for (const player of this.event.generatePossibleVictims()) {
           player.holdItem("MovieTicket");
         }
