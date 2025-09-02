4795ed03c902684ee7d6586d7546f56a020e7a08
SawJester <SawJester@users.noreply.github.com>
2025-09-02 21:12:27 +0000

    Prettified Code!

--- Games/types/Mafia/roles/cards/BecomeUndercoverEvil.js
+++ Games/types/Mafia/roles/cards/BecomeUndercoverEvil.js
@@ -102,13 +102,7 @@ module.exports = class BecomeUndercoverEvil extends Card {
           return;
         }
         this.player.holdItem("IsTheMole", this.player.faction);
-        this.player.setRole(
-          this.newRole,
-          undefined,
-          false,
-          true,
-          false
-        );
+        this.player.setRole(this.newRole, undefined, false, true, false);
         let tempApp = {
           self: "Mole",
         };
