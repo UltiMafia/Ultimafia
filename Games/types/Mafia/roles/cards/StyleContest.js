a009245ee62f1b5d55c05ddd5a5ed0be0e90a80d
SawJester <anglelord270@gmail.com>
2025-09-06 19:34:02 -0400

    Update StyleContest.js

--- Games/types/Mafia/roles/cards/StyleContest.js
+++ Games/types/Mafia/roles/cards/StyleContest.js
@@ -1,10 +1,83 @@
 const Card = require("../../Card");
 const Action = require("../../Action");
-const { PRIORITY_EFFECT_GIVER_DEFAULT } = require("../../const/Priority");
+const Random = require("../../../../../lib/Random");
+const { PRIORITY_EFFECT_GIVER_DEFAULT, PRIORITY_EFFECT_GIVER_EARLY } = require("../../const/Priority");
 
 module.exports = class StyleContest extends Card {
   constructor(role) {
     super(role);
+
+    role.AllTasksComplete = false;
+    let targets = ["Make all info false tonight", "Gain an extra mafia role ability"];
+    let roles = role.getAllRoles();
+    roles = roles.map((r) => r.split(":")[0]);
+    if(roles.includes("Butterfly") && role.canDoSpecialInteractions()){
+      targets.push("Revive all dead players as random independent roles");
+    }
+
+    this.meetings = {
+      "Prize Time": {
+        actionName: "Choose Reward",
+        states: ["Night"],
+        flags: ["voting"],
+        inputType: "custom",
+        targets: targets,
+        shouldMeet: function () {
+          return this.AllTasksComplete;
+        },
+        action: {
+          labels: ["Special"],
+          priority: PRIORITY_EFFECT_GIVER_EARLY,
+          role: this.role,
+          run: function () {
+            if(this.target == "Make all info false tonight"){
+              for(let player of this.game.players){
+                player.giveEffect("FalseMode", 1);
+              }
+            }
+            else if(this.target == "Gain an extra mafia role ability"){
+              const randomMafiaRole = Random.randArrayVal(
+              this.role
+                .getAllRoles()
+                .filter((r) => this.game.getRoleAlignment(r) == "Mafia")
+            );
+                let role = this.actor.addExtraRole(randomMafiaRole);
+              //this.GainedRoles.push(role);
+              this.actor.passiveExtraRoles.push(role);
+            }
+            else if(this.target == "Revive all dead players as random independent roles"){
+          let convert = new Action({
+          actor: this.actor,
+          game: this.actor.game,
+          labels: ["convert", "hidden"],
+          role: this.role,
+          run: function () {
+            }
+          },
+        });
+              for(let player of this.game.players){
+                if(!player.alive && convert.dominates(player)){
+                  let indieRoles = this.role.getAllRoles().filter((r) => this.game.getRoleAlignment(r) == "Independent");
+            if(indieRoles.length <= 0){
+              indieRoles = ["Survivor", "Fool", "Hitchhiker", "Nomad"]
+            }
+              const randomThirdRole = Random.randArrayVal(indieRoles);
+                
+            player.setRole(
+              randomThirdRole,
+              null,
+              false,
+              false,
+              false,
+            );
+                }
+              }
+            }
+            this.role.AllTasksComplete = false;
+          },
+        },
+      },
+    };
 
     this.listeners = {
       state: function () {
@@ -16,7 +89,7 @@ module.exports = class StyleContest extends Card {
           labels: ["role", "hidden"],
           role: this,
           run: function () {
-
+          this.role.AllTasksComplete = false;
           let subaction = new Action({
           actor: this.actor,
           game: this.actor.game,
@@ -28,6 +101,15 @@ module.exports = class StyleContest extends Card {
             }
             if(this.target){
               this.role.TaskComp.push(this.target);
+            }
+            let teammates = this.game.players.filter((p) => p.faction == this.actor.faction && p.alive);
+            if(teammates && teammates.length > 0){
+              for(let player of teammates){
+                if(!this.role.TaskComp.includes(player)){
+                  return;
+                }
+              }
+            this.role.AllTasksComplete = true
             }
           },
         });
@@ -42,6 +124,7 @@ module.exports = class StyleContest extends Card {
 
         this.game.queueAction(action);
          }
+        /*
         if (this.game.getStateName() == "Day") {
           let contest = [];
           for (let player of this.game.players) {
@@ -59,6 +142,7 @@ module.exports = class StyleContest extends Card {
             );
           }
         }
+        */
       },
     };
   }
