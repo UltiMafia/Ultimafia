ea425f44dac0e2fa09af816568b37bd00aa5bf2f
SawJester <anglelord270@gmail.com>
2025-09-11 22:56:22 -0400

    Banished Interaction Roles Description Updates (#2251)
    Co-authored-by: SawJester <SawJester@users.noreply.github.com>

--- data/modifiers.js
+++ data/modifiers.js
@@ -458,7 +458,8 @@ const modifierData = {
       category: "Other",
       internal: ["Add1Banished"],
       tags: ["Setup Changes", "Banished Interaction"],
-      description: "Adds 1 Banished Role in Closed Setups.",
+      description:
+        "In closed setups, replaces 1 non-Banished Village role with a Banished role.",
       eventDescription: "This modifier does nothing when on an Event.",
       allowDuplicate: true,
       incompatible: ["Banished", "Exclusive"],
@@ -467,7 +468,8 @@ const modifierData = {
       category: "Other",
       internal: ["Remove1Banished"],
       tags: ["Setup Changes", "Banished Interaction"],
-      description: "Removes 1 Banished Role in Closed Setups.",
+      description:
+        "In closed setups, replaces 1 Banished role with a non-Banished Village role.",
       eventDescription: "This modifier does nothing when on an Event.",
       allowDuplicate: true,
       incompatible: ["Banished", "Inclusive"],
