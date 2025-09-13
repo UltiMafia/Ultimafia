a173a595b9ce2c2e61ba212cc3e13e0447f55d04
SawJester <SawJester@users.noreply.github.com>
2025-09-13 17:04:37 +0000

    Prettified Code!

--- Games/types/Mafia/roles/cards/ReplaceWithBanishedRole.js
+++ Games/types/Mafia/roles/cards/ReplaceWithBanishedRole.js
@@ -9,23 +9,51 @@ module.exports = class ReplaceWithBanishedRole extends Card {
     this.listeners = {
       replaceWithBanished: function (player) {
         let banishedRoles = this.game.banishedRoles;
-        let currentRoles = this.game.players.map((p) => `${p.role.name}:${p.role.modifier}`);
-        let currentBanished = currentRoles.filter((r) => r.split(":")[1] && r.split(":")[1].includes("Banished"));
-        let tempBanished = banishedRoles.filter((b) => !currentBanished.includes(b));
-        if(tempBanished.length <= 0){
-          if(banishedRoles.length <= 0){
-            banishedRoles = ["Villager:Banished", "Mafioso:Banished", "Cultist:Banished", "Grouch:Banished"];
+        let currentRoles = this.game.players.map(
+          (p) => `${p.role.name}:${p.role.modifier}`
+        );
+        let currentBanished = currentRoles.filter(
+          (r) => r.split(":")[1] && r.split(":")[1].includes("Banished")
+        );
+        let tempBanished = banishedRoles.filter(
+          (b) => !currentBanished.includes(b)
+        );
+        if (tempBanished.length <= 0) {
+          if (banishedRoles.length <= 0) {
+            banishedRoles = [
+              "Villager:Banished",
+              "Mafioso:Banished",
+              "Cultist:Banished",
+              "Grouch:Banished",
+            ];
           }
-            tempBanished = banishedRoles;
+          tempBanished = banishedRoles;
         }
-          if(this.role.name != "Banished Any"){
-            tempBanished = tempBanished.filter(this.game.getRoleAlignment(r.split(":")[0]) != this.alignment);
-            if(tempBanished.length <= 0){
-              tempBanished = ["Villager:Banished", "Mafioso:Banished", "Cultist:Banished", "Grouch:Banished"].filter(this.game.getRoleAlignment(r.split(":")[0]) != this.alignment);
-            }
+        if (this.role.name != "Banished Any") {
+          tempBanished = tempBanished.filter(
+            this.game.getRoleAlignment(r.split(":")[0]) != this.alignment
+          );
+          if (tempBanished.length <= 0) {
+            tempBanished = [
+              "Villager:Banished",
+              "Mafioso:Banished",
+              "Cultist:Banished",
+              "Grouch:Banished",
+            ].filter(
+              this.game.getRoleAlignment(r.split(":")[0]) != this.alignment
+            );
           }
-          let newRole = Random.randArrayVal(tempBanished);
-          this.player.setRole(newRole, undefined, false,true,null,null,"RemoveStartingItems");
+        }
+        let newRole = Random.randArrayVal(tempBanished);
+        this.player.setRole(
+          newRole,
+          undefined,
+          false,
+          true,
+          null,
+          null,
+          "RemoveStartingItems"
+        );
       },
     };
   }
