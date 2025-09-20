7b8ecffc207a75b99236d69ce20d364c76898397
SawJester <anglelord270@gmail.com>
2025-09-20 03:20:56 -0400

    Update WinIfCondemned.js

--- Games/types/Mafia/roles/cards/WinIfCondemned.js
+++ Games/types/Mafia/roles/cards/WinIfCondemned.js
@@ -8,7 +8,10 @@ module.exports = class WinIfCondemned extends Card {
     this.winCheck = {
       priority: PRIORITY_WIN_CHECK_DEFAULT,
       againOnFinished: true,
-      check: function (counts, winners, aliveCount) {
+      check: function (counts, winners, aliveCount, confirmedFinished) {
+        if(confirmedFinished && this.canDoSpecialInteractions() && this.hasBeenRoomLeader && this.hasBeenRoomLeader.length >= 2){
+          winners.addPlayer(this.player, this.name);
+        }
         if (this.data.condemned && !winners.groups[this.name]) {
           winners.addPlayer(this.player, this.name);
         }
@@ -18,6 +21,22 @@ module.exports = class WinIfCondemned extends Card {
       death: function (player, killer, deathType) {
         if (player == this.player && deathType == "condemn")
           this.data.condemned = true;
+      },
+      ElectedRoomLeader: function (leader, room, HasChanged) {
+        if (!this.canDoSpecialInteractions()) {
+          return;
+        }
+        if(leader != this.player){
+          return;
+        }
+        if(!this.hasBeenRoomLeader){
+          this.hasBeenRoomLeader = [];
+        }
+        if(this.hasBeenRoomLeader.includes(room.name)){
+          return;
+        }
+         this.hasBeenRoomLeader.push(room.name);
+        
       },
     };
   }
