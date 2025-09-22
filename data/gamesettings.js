db4ffc3b23a561855d31fd3c5921afa8daab9316
SawJester <anglelord270@gmail.com>
2025-09-22 08:52:44 -0400

    Create gamesettings.js

---
+++ data/gamesettings.js
@@ -0,0 +1,167 @@
+const modifierData = {
+  Mafia: {
+    "Placeholder": {
+      category: "Standard",
+      internal: ["DayStart"],
+      tags: ["State", "Time", "Voting"],
+      description: "The game starts at day.",
+      eventDescription: "This modifier does nothing when on an Event.",
+      allowDuplicate: true,
+    },
+    //Standard
+    "Day Start": {
+      category: "Standard",
+      internal: ["DayStart"],
+      tags: ["State", "Time", "Voting"],
+      description: "The game starts at day.",
+    },
+    "Whispers": {
+      category: "Standard",
+      internal: ["Whispers"],
+      tags: ["Speaking", "Chat"],
+      description: "Allow players to privately contact another player in the village meeting.",
+    },
+    "Whisper Leak Chance": {
+      category: "Standard",
+      internal: ["Whispers"],
+      tags: ["Speaking", "Chat"],
+      description: "Whispers will have 1% chance to leak (Add this additional times to increase the leak chance). If a whisper leaks then everyone will see it.",
+      allowDuplicate: true,
+      requires: ["Whispers"],
+    },
+    "Must Act": {
+      category: "Standard",
+      internal: ["MustAct"],
+      tags: ["Night", "Actions"],
+      description: "Players cannot select 'no one' for their actions, not including the village meeting.",
+    },
+    "No Reveal": {
+      category: "Standard",
+      internal: ["NoReveal"],
+      tags: ["Death", "Information"],
+      description: "The roles of dead players are not revealed.",
+      incompatible: ["Alignment Only Reveal"],
+    },
+    "Alignment Only Reveal": {
+      category: "Standard",
+      internal: ["AlignmentOnlyReveal"],
+      tags: ["Death", "Information"],
+      description: "Only the alignments of dead players are revealed.",
+      incompatible: ["No Reveal"],
+    },
+    
+
+    //Voting
+    "Must Condemn": {
+      category: "Voting",
+      internal: ["MustCondemn"],
+      tags: ["Day", "Voting"],
+      description: "Players cannot condemn 'no one' during the village meeting.",
+    },
+    "Hidden Votes": {
+      category: "Voting",
+      internal: ["HiddenVotes"],
+      tags: ["Voting", "Information"],
+      description: "Players cannot see who other players are voting.",
+    },
+    "Majority Voting": {
+      category: "Voting",
+      internal: ["MajorityVotes"],
+      tags: ["Voting"],
+      description: "A player must get at least 50% of the vote in the village meeting to be condemned.",
+    },
+    "Voting Dead": {
+      category: "Voting",
+      internal: ["VotingDead"],
+      tags: ["Death", "Voting"],
+      description: "Dead players can vote in the village meeting.",
+    },
+
+    //Other
+    "Role Sharing": {
+      category: "Other",
+      internal: ["RoleSharing"],
+      tags: ["Information"],
+      description: "Players can use /roleshare to offer a Role Share with another player. If the other player accepts, both players will learn eachothers roles",
+    },
+    "Alignment Sharing": {
+      category: "Other",
+      internal: ["AlignmentSharing"],
+      tags: ["Information"],
+      description: "Players can use /alignmentshare to offer a Role Share with another player. If the other player accepts, both players will learn eachothers roles",
+    },
+    "Private Revealing": {
+      category: "Other",
+      internal: ["PrivateRevealing"],
+      tags: ["Information"],
+      description: "Players can use /privatereveal to privatly reveal their role to another player.",
+    },
+    "Public Revealing": {
+      category: "Other",
+      internal: ["PublicRevealing"],
+      tags: ["Information"],
+      description: "Players can use /publicreveal to privatly reveal their role to all players.",
+    },
+    "Talking Dead": {
+      category: "Other",
+      internal: ["TalkingDead"],
+      tags: ["Death", "Speaking", "Chat"],
+      description: "Dead players can speak in the village meeting.",
+    },
+    "Hidden Conversions": {
+      category: "Other",
+      internal: ["HiddenConverts"],
+      tags: ["Information"],
+      description: "Players who change roles will not be told about their role changing.",
+    },
+    "Last Wills": {
+      category: "Other",
+      internal: ["LastWills"],
+      tags: ["Information", "Death"],
+      description: "Allow players to write a message that will be revealed when they die.",
+    },
+    "Hostiles Vs Mafia": {
+      category: "Other",
+      internal: ["MafiaVsHostile"],
+      tags: ["Information", "Death"],
+      description: "Mafia and Cult cannot win if a Hostile Independent is alive.",
+    },
+    "Competing Evil Factions": {
+      category: "Other",
+      internal: ["MafiaVsCult"],
+      tags: ["Information", "Death"],
+      description: "Mafia and Cult cannot win if a member another Evil faction is alive.",
+    },
+    
+    Austere: {
+      category: "Other",
+      internal: ["OnlyUseInPlayRoles"],
+      tags: ["Austere"],
+      description: "This role can only reference roles currently in the game.",
+      eventDescription: "This modifier does nothing when on an Event.",
+      incompatible: ["Excessive"],
+    },
+    Bland: {
+      category: "Other",
+      internal: ["NoSpecialInteractions"],
+      tags: ["Bland"],
+      description: "Any Special Interactions this role has are disabled.",
+      eventDescription: "This modifier does nothing when on an Event.",
+    },
+    /*
+  },
+  Resistance: {},
+  Ghost: {},
+  Jotto: {},
+  Acrotopia: {},
+  "Secret Dictator": {},
+  "Secret Hitler": {},
+  "Wacky Words": {},
+  "Liars Dice": {},
+  "Texas Hold Em": {},
+  Cheat: {},
+  Battlesnakes: {},
+  "Connect Four": {},
+};
+
+module.exports = modifierData;
