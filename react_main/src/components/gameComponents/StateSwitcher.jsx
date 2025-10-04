dae400c35fb157427114c826478e5403021c0f19
Golbolco <Golbolco@users.noreply.github.com>
2025-10-04 20:38:08 +0000

    Prettified Code!

--- react_main/src/components/gameComponents/StateSwitcher.jsx
+++ react_main/src/components/gameComponents/StateSwitcher.jsx
@@ -12,7 +12,8 @@ const stateIconMap = {
 };
 
 export default function StateSwitcher(props) {
-  const { history, stateViewing, updateStateViewing, onStateNavigation } = props;
+  const { history, stateViewing, updateStateViewing, onStateNavigation } =
+    props;
   const currentState = history.states[stateViewing];
   const stateName = currentState ? currentState.name : "";
 
