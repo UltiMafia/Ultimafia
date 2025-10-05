b5ba22fe64352d692121b12c7bd906dbc0dfe7f0
SawJester <anglelord270@gmail.com>
2025-10-05 16:46:24 -0400

    Update ChangeRandomAlignment.js

--- Games/types/Mafia/roles/cards/ChangeRandomAlignment.js
+++ Games/types/Mafia/roles/cards/ChangeRandomAlignment.js
@@ -1,20 +1,39 @@
 const Card = require("../../Card");
 const Random = require("../../../../../lib/Random");
+const Action = require("../../Action");
+const {
+  PRIORITY_PREKILL_ACTION,
+} = require("../../const/Priority");
 
 module.exports = class ChangeRandomAlignment extends Card {
   constructor(role) {
     super(role);
 
-    role.methods.changeAlignment = function () {
-      let factions = [];
-      let players = this.game
-        .alivePlayers()
-        .filter((p) => p.faction != this.player.faction);
-      for (let x = 0; x < players.length; x++) {
-        if (!factions.includes(players[x].faction)) {
-          factions.push(players[x].faction);
+    this.listeners = {
+      roleAssigned: function (player) {
+        if (player !== this.player) return;
+        this.methods.changeAlignment();
+      },
+      state: function (stateInfo) {
+        if (!this.hasAbility(["Win-Con"])) {
+          return;
         }
-      }
+        if (!stateInfo.name.match(/Night/)) {
+          return;
+        }
+          var action = new Action({
+          actor: this.player,
+          game: this.player.game,
+          priority: PRIORITY_PREKILL_ACTION,
+          labels: ["hidden"],
+          run: function () {
+          let factions = [];
+          let players = this.game.alivePlayers().filter((p) => p.faction != this.player.faction);
+          for (let x = 0; x < players.length; x++) {
+            if (!factions.includes(players[x].faction)) {
+              factions.push(players[x].faction);
+            }
+          }
       factions = Random.randomizeArray(factions);
 
       for (let x = 0; x < factions.length; x++) {
@@ -24,35 +43,13 @@ module.exports = class ChangeRandomAlignment extends Card {
         ) {
           this.player.faction = factions[x];
         }
-        //this.player.queueAlert(`${factions[x]}`);
       }
-      /*
-      const alignment = {
-        Independent: Random.randArrayVal(["Village", "Mafia", "Cult"]),
-        Mafia: "Village",
-        Cult: "Village",
-        Village: ["Mafia" || "Cult"],
-      };
-      this.player.role.alignment = alignment[this.player.role.alignment];
-      */
       this.player.queueAlert(
         `:anon: You believe that siding with the ${this.player.faction} will help your career!`
       );
-    };
-
-    this.listeners = {
-      roleAssigned: function (player) {
-        if (player !== this.player) return;
-        this.methods.changeAlignment();
-      },
-      state: function (stateInfo) {
-        if (
-          this.hasAbility(["Alignment"]) &&
-          stateInfo.name.match(/Day/) &&
-          stateInfo.dayCount > 0
-        ) {
-          this.methods.changeAlignment();
-        }
+          },
+        });
+        this.game.queueAction(action);
       },
     };
   }
