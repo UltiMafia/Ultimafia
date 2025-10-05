a7639de3f48362934d6486e81abcd7f0a54ae9bb
SawJester <SawJester@users.noreply.github.com>
2025-10-05 20:46:55 +0000

    Prettified Code!

--- Games/types/Mafia/roles/cards/ChangeRandomAlignment.js
+++ Games/types/Mafia/roles/cards/ChangeRandomAlignment.js
@@ -1,9 +1,7 @@
 const Card = require("../../Card");
 const Random = require("../../../../../lib/Random");
 const Action = require("../../Action");
-const {
-  PRIORITY_PREKILL_ACTION,
-} = require("../../const/Priority");
+const { PRIORITY_PREKILL_ACTION } = require("../../const/Priority");
 
 module.exports = class ChangeRandomAlignment extends Card {
   constructor(role) {
@@ -21,32 +19,34 @@ module.exports = class ChangeRandomAlignment extends Card {
         if (!stateInfo.name.match(/Night/)) {
           return;
         }
-          var action = new Action({
+        var action = new Action({
           actor: this.player,
           game: this.player.game,
           priority: PRIORITY_PREKILL_ACTION,
           labels: ["hidden"],
           run: function () {
-          let factions = [];
-          let players = this.game.alivePlayers().filter((p) => p.faction != this.player.faction);
-          for (let x = 0; x < players.length; x++) {
-            if (!factions.includes(players[x].faction)) {
-              factions.push(players[x].faction);
+            let factions = [];
+            let players = this.game
+              .alivePlayers()
+              .filter((p) => p.faction != this.player.faction);
+            for (let x = 0; x < players.length; x++) {
+              if (!factions.includes(players[x].faction)) {
+                factions.push(players[x].faction);
+              }
             }
-          }
-      factions = Random.randomizeArray(factions);
+            factions = Random.randomizeArray(factions);
 
-      for (let x = 0; x < factions.length; x++) {
-        if (
-          factions[x] != this.player.faction &&
-          factions[x] != "Independent"
-        ) {
-          this.player.faction = factions[x];
-        }
-      }
-      this.player.queueAlert(
-        `:anon: You believe that siding with the ${this.player.faction} will help your career!`
-      );
+            for (let x = 0; x < factions.length; x++) {
+              if (
+                factions[x] != this.player.faction &&
+                factions[x] != "Independent"
+              ) {
+                this.player.faction = factions[x];
+              }
+            }
+            this.player.queueAlert(
+              `:anon: You believe that siding with the ${this.player.faction} will help your career!`
+            );
           },
         });
         this.game.queueAction(action);
