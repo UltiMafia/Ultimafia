864724c6bcad485411d37f040120a4e0a349d648
SawJester <anglelord270@gmail.com>
2025-10-12 10:22:12 -0400

    Update Effect.js

--- Games/types/Mafia/Effect.js
+++ Games/types/Mafia/Effect.js
@@ -3,6 +3,17 @@ const Effect = require("../../core/Effect");
 module.exports = class MafiaEffect extends Effect {
   constructor(name, data) {
     super(name, data);
+    this.source = "None";
+  }
+
+  shouldAutoRemove(){
+  if(this.source == "None"){
+    return false;
+  }
+  if(this.source.modifier && this.modifier.split("/").includes("Forgettable") && !this.source.hasAbility("Effect")){
+    this.remove();
+    return true;
+  }
   }
 
   age() {
