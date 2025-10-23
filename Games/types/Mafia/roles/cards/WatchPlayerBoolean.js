914b223d15d53b33276af1559e449f0b4e18f452
SawJester <SawJester@users.noreply.github.com>
2025-10-23 16:29:48 +0000

    Prettified Code!

--- Games/types/Mafia/roles/cards/WatchPlayerBoolean.js
+++ Games/types/Mafia/roles/cards/WatchPlayerBoolean.js
@@ -52,8 +52,8 @@ module.exports = class WatchPlayerBoolean extends Card {
                   v
                 );
                 info.processInfo();
-                if(info == "visited by somebody"){
-                this.actor.queueAlert(`:watch: ${info.getInfoFormated()}`);
+                if (info == "visited by somebody") {
+                  this.actor.queueAlert(`:watch: ${info.getInfoFormated()}`);
                 }
               }
             }
