54eb59e7f5d295d3d34c2c60a7d931a753d2273e
SawJester <SawJester@users.noreply.github.com>
2025-10-27 22:50:19 +0000

    Prettified Code!

--- Games/types/Mafia/roles/cards/AnnouceNeighborVisitors.js
+++ Games/types/Mafia/roles/cards/AnnouceNeighborVisitors.js
@@ -74,7 +74,7 @@ module.exports = class AnnouceNeighborVisitors extends Card {
         ],
         role: role,
         run: function () {
-          if(!this.role){
+          if (!this.role) {
             return;
           }
           if (this.role.neighborsForTrail.length <= 0) {
