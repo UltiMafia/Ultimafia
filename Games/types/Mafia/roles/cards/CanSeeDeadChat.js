5d4a49cc26c261a117d03ba5dac29df9d58b7b6b
SawJester <anglelord270@gmail.com>
2025-10-28 02:09:33 -0400

    Create CanSeeDeadChat.js

---
+++ Games/types/Mafia/roles/cards/CanSeeDeadChat.js
@@ -0,0 +1,39 @@
+const Card = require("../../Card");
+const Action = require("../../Action");
+const Random = require("../../../../../lib/Random");
+const { PRIORITY_NIGHT_SAVER } = require("../../const/Priority");
+
+module.exports = class CanSeeDeadChat extends Card {
+  constructor(role) {
+    super(role);
+
+    this.listeners = {
+      AbilityToggle: function (player) {
+        if (player != this.player) {
+          return;
+        }
+        if (this.hasAbility(["Modifier", "WhenDead"])) {
+          if (
+            (this.SeeingDeadEffect == null ||
+            !this.player.effects.includes((this.SeeingDeadEffect)
+          ) {
+            this.SeeingDeadEffect = this.player.giveEffect(
+              "CanSeeDead",
+              Infinity
+            );
+            this.passiveEffects.push(this.SeeingDeadEffect);
+          }
+        } else {
+          var index = this.passiveEffects.indexOf(this.SeeingDeadEffect);
+          if (index != -1) {
+            this.passiveEffects.splice(index, 1);
+          }
+          if (this.SeeingDeadEffect != null) {
+           this.SeeingDeadEffect.remove();
+            this.SeeingDeadEffect = null;
+          }
+        }
+      },
+    };
+  }
+};
