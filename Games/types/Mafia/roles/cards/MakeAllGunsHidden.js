fcdad147c9df3fcd7c9fa46f0b1d50327f74ae28
SawJester <anglelord270@gmail.com>
2025-10-28 01:36:56 -0400

    Create MakeAllGunsHidden.js

---
+++ Games/types/Mafia/roles/cards/MakeAllGunsHidden.js
@@ -0,0 +1,39 @@
+const Card = require("../../Card");
+const Action = require("../../Action");
+const {
+  PRIORITY_ITEM_TAKER_DEFAULT,
+  PRIORITY_ITEM_TAKER_EARLY,
+} = require("../../const/Priority");
+
+module.exports = class MakeAllGunsHidden extends Card {
+  constructor(role) {
+    super(role);
+
+    this.passiveActions = [
+      {
+        ability: ["Item"],
+        actor: role.player,
+        game: role.game,
+        role: role,
+        priority: PRIORITY_ITEM_TAKER_DEFAULT + 2,
+        labels: ["investigate"],
+        run: function () {
+          for (let item of this.actor.items) {
+            item.reveal = false;
+          }
+        },
+      },
+    ];
+
+    this.listeners = {
+      instantAction: function (action) {
+        if (!this.hasAbility(["Item"])) {
+          return;
+        }
+          for (let item of this.actor.items) {
+            item.reveal = false;
+          }
+      },
+    };
+  }
+};
