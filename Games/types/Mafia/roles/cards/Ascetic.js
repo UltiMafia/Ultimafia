be6fd97870edfaaad25da4428421b3b06bea60bc
SawJester <anglelord270@gmail.com>
2025-11-02 12:49:40 -0500

    Update Ascetic.js

--- Games/types/Mafia/roles/cards/Ascetic.js
+++ Games/types/Mafia/roles/cards/Ascetic.js
@@ -5,38 +5,23 @@ const { PRIORITY_UNTARGETABLE } = require("../../const/Priority");
 module.exports = class Ascetic extends Card {
   constructor(role) {
     super(role);
-    /*
-    this.actions = [
-      {
-        priority: PRIORITY_UNTARGETABLE,
-        labels: ["stop", "hidden"],
-        run: function () {
-          if (this.game.getStateName() != "Night") return;
 
+      this.passiveActions = [
+      {
+        ability: ["Blocking", "Modifier"],
+        state: "Night",
+        actor: role.player,
+        game: role.player.game,
+        priority: PRIORITY_UNTARGETABLE,
+        labels: [
+          "stop", "hidden"
+        ],
+        role: role,
+        run: function () {
           this.makeUntargetable(this.actor, "kill");
         },
       },
     ];
-*/
-    this.listeners = {
-      state: function (stateInfo) {
-        if (!this.hasAbility(["OnlyWhenAlive"])) {
-          return;
-        }
-        if (!stateInfo.name.match(/Night/)) {
-          return;
-        }
-        var action = new Action({
-          actor: this.player,
-          priority: PRIORITY_UNTARGETABLE,
-          game: this.player.game,
-          labels: ["stop", "hidden"],
-          run: function () {
-            this.makeUntargetable(this.actor, "kill");
-          },
-        });
-        this.game.queueAction(action);
-      },
-    };
+
   }
 };
