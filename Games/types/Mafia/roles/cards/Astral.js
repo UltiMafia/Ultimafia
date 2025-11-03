575a21163d4f23b8f680151e787a650d760f802e
SawJester <SawJester@users.noreply.github.com>
2025-11-03 01:35:54 +0000

    Prettified Code!

--- Games/types/Mafia/roles/cards/Astral.js
+++ Games/types/Mafia/roles/cards/Astral.js
@@ -6,28 +6,26 @@ module.exports = class Astral extends Card {
   constructor(role) {
     super(role);
 
-      this.passiveActions = [
+    this.passiveActions = [
       {
         ability: ["Modifier", "WhenDead"],
         state: "Night",
         actor: role.player,
         game: role.player.game,
         priority: PRIORITY_MODIFY_ACTION_LABELS,
-        labels: [
-          "absolute", "hidden"
-        ],
+        labels: ["absolute", "hidden"],
         role: role,
         run: function () {
-            for (let action of this.game.actions[0]) {
-              if (
-                action.priority > this.priority &&
-                action.actors.includes(this.actor) &&
-                !action.hasLabels(["mafia"])
-              ) {
-                action.labels = [...action.labels, "hidden"];
-              }
+          for (let action of this.game.actions[0]) {
+            if (
+              action.priority > this.priority &&
+              action.actors.includes(this.actor) &&
+              !action.hasLabels(["mafia"])
+            ) {
+              action.labels = [...action.labels, "hidden"];
             }
-          },
+          }
+        },
       },
     ];
   }
