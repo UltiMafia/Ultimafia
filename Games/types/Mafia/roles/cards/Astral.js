7baa962b2b609f9fbd2a76581a0fa5d74338f375
SawJester <anglelord270@gmail.com>
2025-11-02 20:35:21 -0500

    Update Astral.js

--- Games/types/Mafia/roles/cards/Astral.js
+++ Games/types/Mafia/roles/cards/Astral.js
@@ -5,17 +5,19 @@ const { PRIORITY_MODIFY_ACTION_LABELS } = require("../../const/Priority");
 module.exports = class Astral extends Card {
   constructor(role) {
     super(role);
-    this.listeners = {
-      state: function (stateInfo) {
-        if (!stateInfo.name.match(/Night/)) {
-          return;
-        }
-        var action = new Action({
-          actor: this.player,
-          game: this.player.game,
-          priority: PRIORITY_MODIFY_ACTION_LABELS,
-          labels: ["absolute", "hidden"],
-          run: function () {
+
+      this.passiveActions = [
+      {
+        ability: ["Modifier", "WhenDead"],
+        state: "Night",
+        actor: role.player,
+        game: role.player.game,
+        priority: PRIORITY_MODIFY_ACTION_LABELS,
+        labels: [
+          "absolute", "hidden"
+        ],
+        role: role,
+        run: function () {
             for (let action of this.game.actions[0]) {
               if (
                 action.priority > this.priority &&
@@ -26,9 +28,7 @@ module.exports = class Astral extends Card {
               }
             }
           },
-        });
-        this.game.queueAction(action);
       },
-    };
+    ];
   }
 };
