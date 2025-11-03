08c7eac0d3a248058ffb9be647ac8675907e2e16
SawJester <SawJester@users.noreply.github.com>
2025-11-03 19:19:25 +0000

    Prettified Code!

--- Games/types/Mafia/roles/cards/MummyMarkVisitors.js
+++ Games/types/Mafia/roles/cards/MummyMarkVisitors.js
@@ -16,13 +16,13 @@ module.exports = class MummyMarkVisitors extends Card {
         priority: PRIORITY_EFFECT_GIVER_DEFAULT,
         labels: ["hidden", "giveEffect"],
         run: function () {
-            let visitors = this.getVisitors();
-            for (let visitor of visitors) {
-              if (this.dominates(visitor)) {
-                this.role.giveEffect(visitor, "Entombed");
-              }
+          let visitors = this.getVisitors();
+          for (let visitor of visitors) {
+            if (this.dominates(visitor)) {
+              this.role.giveEffect(visitor, "Entombed");
             }
-          },
+          }
+        },
       },
     ];
   }
