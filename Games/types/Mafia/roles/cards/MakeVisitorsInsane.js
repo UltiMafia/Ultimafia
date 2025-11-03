ad64c5ddae8d97d26e33a480686120609754365b
SawJester <anglelord270@gmail.com>
2025-11-03 12:58:21 -0500

    Update MakeVisitorsInsane.js

--- Games/types/Mafia/roles/cards/MakeVisitorsInsane.js
+++ Games/types/Mafia/roles/cards/MakeVisitorsInsane.js
@@ -5,47 +5,17 @@ const { PRIORITY_EFFECT_GIVER_DEFAULT } = require("../../const/Priority");
 module.exports = class MakeVisitorsInsane extends Card {
   constructor(role) {
     super(role);
-    /*
-    this.actions = [
+
+    this.passiveActions = [
       {
+        ability: ["Effect"],
+        actor: role.player,
+        state: "Night",
+        game: role.game,
+        role: role,
         priority: PRIORITY_EFFECT_GIVER_DEFAULT,
-        labels: ["hidden", "absolute", "giveEffect", "insanity"],
+        labels: ["hidden", "giveEffect", "insanity"],
         run: function () {
-          if (
-            this.game.getStateName() !== "Night" &&
-            this.game.getStateName() !== "Dawn"
-          ) {
-            return;
-          }
-
-          let visitors = this.getVisitors();
-          for (let visitor of visitors) {
-            if (this.dominates(visitor)) {
-              visitor.giveEffect("Insanity");
-            }
-          }
-        },
-      },
-    ];
-*/
-
-    this.listeners = {
-      state: function (stateInfo) {
-        if (!this.hasAbility(["Effect"])) {
-          return;
-        }
-
-        if (!stateInfo.name.match(/Night/)) {
-          return;
-        }
-
-        var action = new Action({
-          actor: this.player,
-          game: this.player.game,
-          role: this,
-          priority: PRIORITY_EFFECT_GIVER_DEFAULT,
-          labels: ["hidden", "absolute", "giveEffect", "insanity"],
-          run: function () {
             let visitors = this.getVisitors();
             for (let visitor of visitors) {
               if (this.dominates(visitor)) {
@@ -53,10 +23,7 @@ module.exports = class MakeVisitorsInsane extends Card {
               }
             }
           },
-        });
-
-        this.game.queueAction(action);
       },
-    };
+    ];
   }
 };
