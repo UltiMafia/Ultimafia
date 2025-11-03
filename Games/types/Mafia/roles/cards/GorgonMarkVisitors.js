6cb43af70bfa679a5ad2fe54725aba98701072fb
SawJester <anglelord270@gmail.com>
2025-11-03 14:49:55 -0500

    Update and rename EnqueueVisitors.js to GorgonMarkVisitors.js

---
+++ Games/types/Mafia/roles/cards/GorgonMarkVisitors.js
@@ -0,0 +1,29 @@
+const Card = require("../../Card");
+const Action = require("../../Action");
+const { PRIORITY_EFFECT_GIVER_DEFAULT } = require("../../const/Priority");
+
+module.exports = class GorgonMarkVisitors extends Card {
+  constructor(role) {
+    super(role);
+
+    this.passiveActions = [
+      {
+        ability: ["Effect"],
+        actor: role.player,
+        state: "Night",
+        game: role.game,
+        role: role,
+        priority: PRIORITY_EFFECT_GIVER_DEFAULT,
+        labels: ["hidden", "giveEffect"],
+        run: function () {
+            let visitors = this.getVisitors();
+            for (let visitor of visitors) {
+              if (this.dominates(visitor)) {
+                this.role.giveEffect(visitor, "Marked", 1, [this.role.name]);
+              }
+            }
+          },
+      },
+    ];
+  }
+};
