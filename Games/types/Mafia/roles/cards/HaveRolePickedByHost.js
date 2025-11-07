5b51f2151b8da53548bcc02e5692f840ce053c7a
SawJester <anglelord270@gmail.com>
2025-11-07 00:20:37 -0500

    Create HaveRolePickedByHost.js

---
+++ Games/types/Mafia/roles/cards/HaveRolePickedByHost.js
@@ -0,0 +1,15 @@
+const Card = require("../../Card");
+const Random = require("../../../../../lib/Random");
+const { PRIORITY_CONVERT_DEFAULT } = require("../../const/Priority");
+module.exports = class HaveRolePickedByHost extends Card {
+  constructor(role) {
+    super(role);
+
+    for(let player of this.game.players){
+      if(player.role && player.role.name == "Host"){
+        player.giveItem("HostRoleSelection", role.player);
+      }
+    }
+
+  }
+};
