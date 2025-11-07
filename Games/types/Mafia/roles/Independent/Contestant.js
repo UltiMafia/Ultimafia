30ad1c55b156121f2b784ae741d3f72bb612f111
SawJester <anglelord270@gmail.com>
2025-11-07 13:55:25 -0500

    Create Contestant.js

---
+++ Games/types/Mafia/roles/Independent/Contestant.js
@@ -0,0 +1,13 @@
+const Role = require("../../Role");
+
+module.exports = class Contestant extends Role {
+  constructor(player, data) {
+    super("Contestant", player, data);
+
+    this.alignment = "Independent";
+    this.cards = [
+      "VillageCore",
+      "HaveRolePickedByHost",
+    ];
+  }
+};
