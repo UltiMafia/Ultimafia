6d8da06425f614a9d8449fe183d675964e9b0136
SawJester <anglelord270@gmail.com>
2025-11-06 21:17:50 -0500

    Update HostChosenRoles.js

--- Games/types/Mafia/roles/cards/HostChosenRoles.js
+++ Games/types/Mafia/roles/cards/HostChosenRoles.js
@@ -8,106 +8,24 @@ module.exports = class HostChooseRoles extends Card {
     ///const playerCount = this.game.players.length;
 
     this.meetings = {
-      "Choose Players": {
-        states: ["Dusk", "Dawn"],
-        flags: ["voting", "multi"],
-        multiMin: 0,
-        multiMax: 50,
-        targets: { include: ["alive", "dead"] },
-        shouldMeet: function () {
-          return this.hasBeenDusk != true;
-        },
-        action: {
-          priority: PRIORITY_CONVERT_DEFAULT - 1,
-          run: function () {
-            this.actor.role.data.targetPlayer = this.target;
-          },
-        },
-      },
-      "Convert To": {
-        states: ["Dusk", "Dawn"],
-        flags: ["voting", "multi"],
-        multiMin: 0,
-        multiMax: 50,
-        inputType: "custom",
-        //targets: { ["e"] },
-        shouldMeet: function () {
-          return this.hasBeenDusk != true;
-        },
-        action: {
-          labels: ["role", "hidden", "absolute"],
-          priority: PRIORITY_CONVERT_DEFAULT,
-          run: function () {
-            let targetPlayer = [];
-            this.hasBeenDusk == true;
-            if (!this.actor.role.data.targetPlayer) return;
-
-            this.actor.role.data.targetRole = this.target;
-          },
-        },
-      },
       "Confirm Selections": {
-        states: ["Dawn", "Dusk"],
+        states: ["Hosting"],
         flags: ["voting"],
         inputType: "boolean",
-        shouldMeet: function () {
-          return this.hasBeenDusk != true;
-        },
         action: {
           labels: ["investigate"],
           priority: PRIORITY_CONVERT_DEFAULT + 1,
           run: function () {
             if (this.target === "No") return;
-
-            if (!this.actor.role.data.targetPlayer) return;
-            if (!this.actor.role.data.targetRole) return;
-
-            let correctCount = 0;
-
-            let playerSize = this.actor.role.data.targetPlayer.length;
-            let roleSize = this.actor.role.data.targetRole.length;
-            let validSize;
-            if (roleSize > playerSize) {
-              validSize = playerSize;
-            } else {
-              validSize = roleSize;
-            }
-
-            let targetPlayer;
-            let targetRole;
-            for (let x = 0; x < validSize; x++) {
-              targetPlayer = this.actor.role.data.targetPlayer[x];
-              targetRole = this.actor.role.data.targetRole[x];
-              if (targetPlayer && targetRole) {
-                targetPlayer.setRole(`${targetRole}`);
-                this.actor.queueAlert(
-                  `:invest: You convert ${targetPlayer.name} to ${targetRole}.`
-                );
-              }
-            }
-            delete this.actor.role.data.targetPlayer;
-            delete this.actor.role.data.targetRole;
           },
         },
       },
     };
 
     this.listeners = {
-      roleAssigned: function (player) {
-        if (player !== this.player) {
-          return;
-        }
-        this.hasBeenDusk = false;
-        this.data.ConvertOptions = this.game.PossibleRoles.filter((r) => r);
-      },
-      // refresh cooldown
       state: function (stateInfo) {
-        if (stateInfo.name.match(/Day/) || stateInfo.name.match(/Night/)) {
-          this.hasBeenDusk = true;
-        }
-        if (stateInfo.name.match(/Dusk/) || stateInfo.name.match(/Dawn/)) {
-          var ConvertOptions = this.data.ConvertOptions;
-          this.meetings["Convert To"].targets = ConvertOptions;
+        if (stateInfo.name.match(/Hosting/) || stateInfo.name.match(/Night/)) {
+          this.game.HaveHostingState = false;
         }
       },
     };
