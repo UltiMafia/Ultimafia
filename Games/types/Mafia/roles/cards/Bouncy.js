f4180743d0ebb7cc1360a8c8fd21d25a578b3401
SawJester <anglelord270@gmail.com>
2025-11-08 23:17:44 -0500

    Update Bouncy.js

--- Games/types/Mafia/roles/cards/Bouncy.js
+++ Games/types/Mafia/roles/cards/Bouncy.js
@@ -6,47 +6,17 @@ const { PRIORITY_MODIFY_ACTION } = require("../../const/Priority");
 module.exports = class Bouncy extends Card {
   constructor(role) {
     super(role);
-    /*
-    this.actions = [
-      {
-        labels: ["redirect"],
-        priority: PRIORITY_MODIFY_ACTION,
-        run: function () {
-          if (this.game.getStateName() != "Night") return;
-          if (!this.actor.alive) return;
-          var alive = this.game.players.filter(
-            (p) =>
-              p.alive &&
-              p != this.actor &&
-              p.role.alignment == this.actor.role.alignment
-          );
-          if (alive.length > 0) {
-            var randomTarget = Random.randArrayVal(alive);
-            for (const action of this.game.actions[0]) {
-              if (action.target === this.actor && action.hasLabel("kill")) {
-                action.target = randomTarget;
-              }
-            }
-          }
-        },
-      },
-    ];
-*/
-    this.listeners = {
-      state: function (stateInfo) {
-        if (!this.hasAbility(["OnlyWhenAlive"])) {
-          return;
-        }
-        if (!stateInfo.name.match(/Night/)) {
-          return;
-        }
 
-        var action = new Action({
-          actor: this.player,
-          game: this.player.game,
-          labels: ["redirect"],
-          priority: PRIORITY_MODIFY_ACTION,
-          run: function () {
+    this.passiveActions = [
+      {
+        ability: ["Modifier", "OnlyWhenAlive"],
+        state: "Night",
+        actor: role.player,
+        game: role.player.game,
+        priority: PRIORITY_MODIFY_ACTION,
+        labels: ["redirect"],
+        role: role,
+        run: function () {
             var alive = this.game.players.filter(
               (p) =>
                 p.alive &&
@@ -62,10 +32,8 @@ module.exports = class Bouncy extends Card {
               }
             }
           },
-        });
-
-        this.game.queueAction(action);
       },
-    };
+    ];
+
   }
 };
