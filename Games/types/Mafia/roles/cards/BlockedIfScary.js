4661f0a38c8fdc75f112372ee5d9eaeb64133f28
SawJester <anglelord270@gmail.com>
2025-11-08 23:02:30 -0500

    Update BlockedIfScary.js

--- Games/types/Mafia/roles/cards/BlockedIfScary.js
+++ Games/types/Mafia/roles/cards/BlockedIfScary.js
@@ -6,29 +6,25 @@ module.exports = class BlockedIfScary extends Card {
   constructor(role) {
     super(role);
 
-    this.listeners = {
-      state: function (stateInfo) {
-        if (!stateInfo.name.match(/Night/)) {
-          return;
-        }
-        var action = new Action({
-          actor: this.player,
-          game: this.player.game,
-          priority: PRIORITY_BLOCK_EARLY,
-          labels: ["block", "hidden"],
-          run: function () {
+    this.passiveActions = [
+      {
+        ability: ["Modifier", "WhenDead"],
+        state: "Night",
+        actor: role.player,
+        game: role.player.game,
+        priority: PRIORITY_BLOCK_EARLY,
+        labels: ["block", "hidden"],
+        role: role,
+        run: function () {
             let ScaryPlayers = this.game
               .alivePlayers()
               .filter((p) => p.hasEffect("Scary"));
-
             if (ScaryPlayers.length > 0) {
               this.blockActions(this.actor);
             }
-          },
-        });
-
-        this.game.queueAction(action);
+        },
       },
-    };
+    ];
+    
   }
 };
