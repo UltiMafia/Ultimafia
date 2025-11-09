e00dd98ad9df89e60d74ad048a16982c90438d3c
SawJester <anglelord270@gmail.com>
2025-11-08 22:56:59 -0500

    Update BlockedIfKilled.js

--- Games/types/Mafia/roles/cards/BlockedIfKilled.js
+++ Games/types/Mafia/roles/cards/BlockedIfKilled.js
@@ -6,25 +6,23 @@ module.exports = class BlockedIfKilled extends Card {
   constructor(role) {
     super(role);
 
-    this.listeners = {
-      state: function (stateInfo) {
-        if (!stateInfo.name.match(/Night/)) {
-          return;
-        }
-        var action = new Action({
-          actor: this.player,
-          game: this.player.game,
-          priority: PRIORITY_BLOCK_SELF_IF_KILLED,
-          labels: ["block", "hidden"],
-          run: function () {
+
+      this.passiveActions = [
+      {
+        ability: ["Modifier", "WhenDead"],
+        state: "Night",
+        actor: role.player,
+        game: role.player.game,
+        priority: PRIORITY_BLOCK_SELF_IF_KILLED,
+        labels: ["block", "hidden"],
+        role: role,
+         run: function () {
             if (!this.actor.alive) {
               this.blockActions(this.actor);
             }
-          },
-        });
-
-        this.game.queueAction(action);
+        },
       },
-    };
+    ];
+
   }
 };
