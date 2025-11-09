945204efc28dfba220e5984a28b6d8b10c0b2610
SawJester <anglelord270@gmail.com>
2025-11-08 22:39:33 -0500

    Update BegumsSenses.js

--- Games/types/Mafia/roles/cards/BegumsSenses.js
+++ Games/types/Mafia/roles/cards/BegumsSenses.js
@@ -37,80 +37,18 @@ module.exports = class BegumsSenses extends Card {
         },
       },
     };
-    /*
-    this.actions = [
+
+
+    this.passiveActions = [
       {
+        ability: ["Information"],
+        state: "Night",
+        actor: role.player,
+        game: role.player.game,
         priority: PRIORITY_INVESTIGATIVE_DEFAULT,
         labels: ["investigate"],
+        role: role,
         run: function () {
-          if (!this.actor.alive) return;
-          if (this.game.getStateName() != "Night") return;
-          if (!this.actor.role.begumTarget) return;
-
-          let begumTarget = this.actor.role.begumTarget;
-          let visits = this.getVisits(begumTarget);
-          let visitNames = visits.map((p) => p.name);
-          let visitors = this.getVisitors(begumTarget);
-          let visitorNames = visitors.map((p) => p.name);
-
-          if (this.actor.hasEffect("FalseMode")) {
-            let players = this.game
-              .alivePlayers()
-              .filter((p) => p != this.actor);
-            let playerNames = players.map((p) => p.name);
-            if (visitNames.length == 0) {
-              visitNames.push(Random.randArrayVal(playerNames));
-            } else {
-              visitNames = [];
-            }
-
-            if (visitorNames.length == 0) {
-              visitorNames.push(Random.randArrayVal(playerNames));
-            } else {
-              visitorNames = [];
-            }
-          }
-
-          if (visitNames.length == 0) visitNames.push("no one");
-          if (visitorNames.length === 0) visitorNames.push("no one");
-
-          this.actor.queueAlert(
-            `:watch: Your target was visited by ${visitorNames.join(
-              ", "
-            )} during the night.`
-          );
-
-          this.actor.queueAlert(
-            `:watch: Your target visited ${visitNames.join(
-              ", "
-            )} during the night.`
-          );
-        },
-      },
-    ];
-*/
-    this.listeners = {
-      roleAssigned: function (player) {
-        if (player !== this.player) return;
-        let possibleTargets = this.game
-          .alivePlayers()
-          .filter((p) => p != this.player);
-        this.begumTarget = Random.randArrayVal(possibleTargets);
-      },
-      state: function (stateInfo) {
-        if (!this.hasAbility(["Information"])) {
-          return;
-        }
-        if (!stateInfo.name.match(/Night/)) {
-          return;
-        }
-
-        var action = new Action({
-          actor: this.player,
-          game: this.player.game,
-          priority: PRIORITY_INVESTIGATIVE_DEFAULT,
-          labels: ["investigate"],
-          run: function () {
             if (this.game.getStateName() != "Night") return;
             if (!this.actor.role.begumTarget) return;
 
@@ -149,9 +87,16 @@ module.exports = class BegumsSenses extends Card {
               )} during the night.`
             );
           },
-        });
-
-        this.game.queueAction(action);
+      },
+    ];
+    
+    this.listeners = {
+      roleAssigned: function (player) {
+        if (player !== this.player) return;
+        let possibleTargets = this.game
+          .alivePlayers()
+          .filter((p) => p != this.player);
+        this.begumTarget = Random.randArrayVal(possibleTargets);
       },
     };
   }
