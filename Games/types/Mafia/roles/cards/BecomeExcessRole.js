56d333cb95fba3924de19c6cc29bd6b7954c3171
SawJester <SawJester@users.noreply.github.com>
2025-11-09 03:29:57 +0000

    Prettified Code!

--- Games/types/Mafia/roles/cards/BecomeExcessRole.js
+++ Games/types/Mafia/roles/cards/BecomeExcessRole.js
@@ -18,46 +18,45 @@ module.exports = class BecomeExcessRole extends Card {
         labels: ["convert"],
         role: role,
         run: function () {
-            let roles = this.role.getAllRoles().filter((r) => r);
-            let players = this.game.players.filter((p) => p.role);
-            let currentRoles = [];
+          let roles = this.role.getAllRoles().filter((r) => r);
+          let players = this.game.players.filter((p) => p.role);
+          let currentRoles = [];
 
-            for (let x = 0; x < players.length; x++) {
-              currentRoles.push(players[x].role);
-            }
-            for (let y = 0; y < currentRoles.length; y++) {
-              roles = roles.filter(
-                (r) =>
-                  r.split(":")[0] != currentRoles[y].name &&
-                  (this.game.getRoleAlignment(r) ==
-                    this.game.getRoleAlignment(this.actor.role.name) ||
-                    this.game.getRoleAlignment(this.actor.role.name) ==
-                      "Independent")
-              );
-            }
-            if (roles.length <= 0) {
-              roles = Object.entries(AllRoles.Mafia)
-                .filter(
-                  (roleData) =>
-                    roleData[1].alignment ===
-                    this.game.getRoleAlignment(this.actor.role.name)
-                )
-                .map((roleData) => roleData[0]);
-            }
-
-            let newRole = Random.randArrayVal(roles);
-
-            this.actor.setRole(
-              newRole,
-              undefined,
-              false,
-              false,
-              false,
-              "No Change"
+          for (let x = 0; x < players.length; x++) {
+            currentRoles.push(players[x].role);
+          }
+          for (let y = 0; y < currentRoles.length; y++) {
+            roles = roles.filter(
+              (r) =>
+                r.split(":")[0] != currentRoles[y].name &&
+                (this.game.getRoleAlignment(r) ==
+                  this.game.getRoleAlignment(this.actor.role.name) ||
+                  this.game.getRoleAlignment(this.actor.role.name) ==
+                    "Independent")
             );
-          },
+          }
+          if (roles.length <= 0) {
+            roles = Object.entries(AllRoles.Mafia)
+              .filter(
+                (roleData) =>
+                  roleData[1].alignment ===
+                  this.game.getRoleAlignment(this.actor.role.name)
+              )
+              .map((roleData) => roleData[0]);
+          }
+
+          let newRole = Random.randArrayVal(roles);
+
+          this.actor.setRole(
+            newRole,
+            undefined,
+            false,
+            false,
+            false,
+            "No Change"
+          );
+        },
       },
     ];
-
   }
 };
