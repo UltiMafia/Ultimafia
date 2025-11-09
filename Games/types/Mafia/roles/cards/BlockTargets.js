758844bf3ed519d0edd58034d424b70ed658fbc4
SawJester <anglelord270@gmail.com>
2025-11-08 22:48:48 -0500

    Update BlockTargets.js

--- Games/types/Mafia/roles/cards/BlockTargets.js
+++ Games/types/Mafia/roles/cards/BlockTargets.js
@@ -5,38 +5,22 @@ const { PRIORITY_NIGHT_ROLE_BLOCKER } = require("../../const/Priority");
 module.exports = class BlockTargets extends Card {
   constructor(role) {
     super(role);
-    /*
-    this.actions = [
+
+    this.passiveActions = [
       {
+        ability: ["Modifier", "WhenDead"],
+        state: "Night",
+        actor: role.player,
+        game: role.player.game,
         priority: PRIORITY_NIGHT_ROLE_BLOCKER,
         labels: ["block", "hidden"],
+        role: role,
         run: function () {
-          if (this.game.getStateName() != "Night") return;
-
-          let visits = this.getVisits(this.actor);
-          visits.map((v) => this.blockActions(v));
+            let visits = this.getVisits(this.actor);
+            visits.map((v) => this.blockActions(v));
         },
       },
     ];
-    */
-    this.listeners = {
-      state: function (stateInfo) {
-        if (!stateInfo.name.match(/Night/)) {
-          return;
-        }
-        var action = new Action({
-          actor: this.player,
-          game: this.player.game,
-          priority: PRIORITY_NIGHT_ROLE_BLOCKER,
-          labels: ["block", "hidden"],
-          run: function () {
-            let visits = this.getVisits(this.actor);
-            visits.map((v) => this.blockActions(v));
-          },
-        });
-
-        this.game.queueAction(action);
-      },
-    };
+    
   }
 };
