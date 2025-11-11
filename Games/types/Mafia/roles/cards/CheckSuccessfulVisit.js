a7dcc88ecf4f457e22659f010a91d3dbd343ed56
SawJester <anglelord270@gmail.com>
2025-11-11 17:37:17 -0500

    Update CheckSuccessfulVisit.js

--- Games/types/Mafia/roles/cards/CheckSuccessfulVisit.js
+++ Games/types/Mafia/roles/cards/CheckSuccessfulVisit.js
@@ -5,44 +5,18 @@ const { PRIORITY_INVESTIGATIVE_DEFAULT } = require("../../const/Priority");
 module.exports = class CheckSuccessfulVisit extends Card {
   constructor(role) {
     super(role);
-    /*
-    this.actions = [
+
+
+    this.passiveActions = [
       {
+        ability: ["WhenDead", "Information"],
+        state: "Night",
+        actor: role.player,
+        game: role.player.game,
         priority: PRIORITY_INVESTIGATIVE_DEFAULT,
         labels: ["investigate", "hidden"],
+        role: role,
         run: function () {
-          if (this.game.getStateName() != "Night") return;
-
-          let targets = this.getVisits(this.actor);
-          let targetNames = targets.map((t) => t.name);
-
-          if (this.actor.hasEffect("FalseMode")) {
-            if (targetNames.length >= 1) return;
-          }
-
-          if (targetNames.length >= 1) {
-            this.actor.queueAlert(
-              `:invest: You learn that your visit to ${targetNames.join(
-                ", "
-              )} was successful.`
-            );
-          }
-        },
-      },
-    ];
-*/
-    this.listeners = {
-      state: function (stateInfo) {
-        if (!stateInfo.name.match(/Night/)) {
-          return;
-        }
-
-        var action = new Action({
-          actor: this.player,
-          priority: PRIORITY_INVESTIGATIVE_DEFAULT,
-          labels: ["investigate", "hidden"],
-          game: this.player.game,
-          run: function () {
             let targets = this.getVisits(this.actor);
             let targetNames = targets.map((t) => t.name);
             if (targetNames.length >= 1) {
@@ -53,10 +27,8 @@ module.exports = class CheckSuccessfulVisit extends Card {
               );
             }
           },
-        });
-
-        this.game.queueAction(action);
       },
-    };
+    ];
+    
   }
 };
