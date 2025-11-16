b34b8c12385b1bd58ba9b7f055697cd92a1ca821
SawJester <anglelord270@gmail.com>
2025-11-16 01:32:34 -0500

    Update Dream.js

--- Games/types/Mafia/roles/cards/Dream.js
+++ Games/types/Mafia/roles/cards/Dream.js
@@ -8,79 +8,17 @@ const {
 module.exports = class Dream extends Card {
   constructor(role) {
     super(role);
-    /*
-    this.actions = [
+
+    this.passiveActions = [
       {
-        labels: ["dream", "hidden"],
-        priority: PRIORITY_INVESTIGATIVE_AFTER_RESOLVE_DEFAULT,
+        ability: ["Information"],
+        state: "Night",
+        actor: role.player,
+        game: role.player.game,
+        priority: PRIORITY_INVESTIGATIVE_AFTER_RESOLVE_DEFAULT - 10,
+        labels: ["dream", "hidden", "investigate"],
+        role: role,
         run: function () {
-          if (this.game.getStateName() != "Night") return;
-          if (!this.actor.alive) return;
-
-          var aliveExceptSelf = this.game.players.filter(
-            (p) => p.alive && p != this.actor
-          );
-          if (aliveExceptSelf.length < 3) return;
-
-          if (this.hasVisitors()) return;
-
-          var dream;
-          let evilPlayers = aliveExceptSelf.filter(
-            (p) => p.role.alignment == "Mafia" || p.role.alignment == "Cult"
-          );
-          let village = aliveExceptSelf.filter(
-            (p) => p.role.alignment == "Village"
-          );
-
-          if (this.actor.hasEffect("FalseMode")) {
-            let temp = evilPlayers;
-            evilPlayers = village;
-            village = temp;
-          }
-
-          if (village.length == 0) {
-            dream = `:dream: You had a dream that you can trust no one but yourself…`;
-          } else if (evilPlayers.length == 0 || Random.randInt(0, 1) == 0) {
-            const chosenOne = Random.randArrayVal(village);
-            dream = `:dream: You had a dream that you can trust ${chosenOne.name}…`;
-          } else {
-            // guarantee no repeats in dream
-            var chosenThree = [Random.randArrayVal(evilPlayers)];
-            aliveExceptSelf = aliveExceptSelf.filter(
-              (p) => p !== chosenThree[0]
-            );
-            aliveExceptSelf = Random.randomizeArray(aliveExceptSelf);
-            chosenThree.push(aliveExceptSelf[0]);
-            chosenThree.push(aliveExceptSelf[1]);
-            chosenThree = Random.randomizeArray(chosenThree);
-            dream = `:dream: You had a dream where at least one of ${chosenThree[0].name}, ${chosenThree[1].name}, and ${chosenThree[2].name} is evil…`;
-            if (this.actor.hasEffect("FalseMode")) {
-              let wrongPlayers = Random.randomizeArray(evilPlayers);
-              dream = `:dream: You had a dream where at least one of ${wrongPlayers[0].name}, ${wrongPlayers[1].name}, and ${wrongPlayers[2].name} is evil…`;
-            }
-          }
-
-          this.actor.queueAlert(dream);
-        },
-      },
-    ];
-*/
-
-    this.listeners = {
-      state: function (stateInfo) {
-        if (!this.hasAbility(["Information"])) {
-          return;
-        }
-        if (!stateInfo.name.match(/Night/)) {
-          return;
-        }
-
-        var action = new Action({
-          actor: this.player,
-          game: this.player.game,
-          labels: ["dream", "hidden", "investigate"],
-          priority: PRIORITY_INVESTIGATIVE_AFTER_RESOLVE_DEFAULT - 10,
-          run: function () {
             var aliveExceptSelf = this.game.players.filter(
               (p) => p.alive && p != this.actor
             );
@@ -119,50 +57,9 @@ module.exports = class Dream extends Card {
             }
 
             this.actor.queueAlert(alert);
-            /*
-            var dream;
-            let evilPlayers = aliveExceptSelf.filter(
-              (p) => p.role.alignment == "Mafia" || p.role.alignment == "Cult"
-            );
-            let village = aliveExceptSelf.filter(
-              (p) => p.role.alignment == "Village"
-            );
-
-            if (this.actor.hasEffect("FalseMode")) {
-              let temp = evilPlayers;
-              evilPlayers = village;
-              village = temp;
-            }
-
-            if (village.length == 0) {
-              dream = `:dream: You had a dream that you can trust no one but yourself…`;
-            } else if (evilPlayers.length == 0 || Random.randInt(0, 1) == 0) {
-              const chosenOne = Random.randArrayVal(village);
-              dream = `:dream: You had a dream that you can trust ${chosenOne.name}…`;
-            } else {
-              // guarantee no repeats in dream
-              var chosenThree = [Random.randArrayVal(evilPlayers)];
-              aliveExceptSelf = aliveExceptSelf.filter(
-                (p) => p !== chosenThree[0]
-              );
-              aliveExceptSelf = Random.randomizeArray(aliveExceptSelf);
-              chosenThree.push(aliveExceptSelf[0]);
-              chosenThree.push(aliveExceptSelf[1]);
-              chosenThree = Random.randomizeArray(chosenThree);
-              dream = `:dream: You had a dream where at least one of ${chosenThree[0].name}, ${chosenThree[1].name}, and ${chosenThree[2].name} is evil…`;
-              if (this.actor.hasEffect("FalseMode")) {
-                let wrongPlayers = Random.randomizeArray(evilPlayers);
-                dream = `:dream: You had a dream where at least one of ${wrongPlayers[0].name}, ${wrongPlayers[1].name}, and ${wrongPlayers[2].name} is evil…`;
-              }
-            }
-
-            this.actor.queueAlert(dream);
-            */
           },
-        });
-
-        this.game.queueAction(action);
       },
-    };
+    ];
+
   }
 };
