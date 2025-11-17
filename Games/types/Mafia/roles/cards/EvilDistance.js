5e8e42049c456579fd88d58cd849f47f18f8731e
SawJester <anglelord270@gmail.com>
2025-11-16 23:53:49 -0500

    Update EvilDistance.js

--- Games/types/Mafia/roles/cards/EvilDistance.js
+++ Games/types/Mafia/roles/cards/EvilDistance.js
@@ -8,100 +8,17 @@ const {
 module.exports = class EvilDistance extends Card {
   constructor(role) {
     super(role);
-    /*
-    this.actions = [
+
+      this.passiveActions = [
       {
+        ability: ["Information", "OnlyWhenAlive"],
+        state: "Night",
+        actor: role.player,
+        game: role.player.game,
         priority: PRIORITY_INVESTIGATIVE_AFTER_RESOLVE_DEFAULT - 10,
         labels: ["investigate"],
+        role: role,
         run: function () {
-          if (this.game.getStateName() != "Night") return;
-          if (this.actor.role.hasInfo) return;
-          if (!this.actor.alive) return;
-
-          let alive = this.game.alivePlayers();
-          var evilPlayers = alive.filter(
-            (p) =>
-              this.game.getRoleAlignment(
-                p.getRoleAppearance().split(" (")[0]
-              ) == "Cult" ||
-              this.game.getRoleAlignment(
-                p.getRoleAppearance().split(" (")[0]
-              ) == "Mafia"
-          );
-
-          if (evilPlayers.length <= 1) {
-            this.actor.queueAlert(
-              `There wasn't enough evil players for your abilty to work!`
-            );
-            return;
-          }
-
-          var evilTarget = Random.randArrayVal(evilPlayers);
-          var indexOfTarget = alive.indexOf(evilTarget);
-          var rightIdx;
-          var leftIdx;
-          var leftAlign;
-          var rightAlign;
-          var distance = 0;
-          var found = false;
-
-          for (let x = 0; x < alive.length; x++) {
-            leftIdx =
-              (indexOfTarget - distance - 1 + alive.length) % alive.length;
-            rightIdx = (indexOfTarget + distance + 1) % alive.length;
-            leftAlign = this.game.getRoleAlignment(
-              alive[leftIdx].getRoleAppearance().split(" (")[0]
-            );
-            rightAlign = this.game.getRoleAlignment(
-              alive[rightIdx].getRoleAppearance().split(" (")[0]
-            );
-
-            if (rightAlign == "Cult" || rightAlign == "Mafia") {
-              found = true;
-              break;
-            } else if (leftAlign == "Cult" || leftAlign == "Mafia") {
-              found = true;
-              break;
-            } else {
-              distance = x;
-            }
-          }
-
-          if (this.actor.hasEffect("FalseMode")) {
-            if (distance == 0) {
-              distance = 1;
-            } else {
-              distance = distance - 1;
-            }
-          }
-
-          this.actor.queueAlert(
-            `You learn that there is ${distance} players between 2 of the evil players in the game!`
-          );
-
-          this.actor.role.hasInfo = true;
-        },
-      },
-    ];
-*/
-
-    this.listeners = {
-      state: function (stateInfo) {
-        if (!this.hasAbility(["Information", "OnlyWhenAlive"])) {
-          return;
-        }
-
-        if (!stateInfo.name.match(/Night/)) {
-          return;
-        }
-
-        var action = new Action({
-          actor: this.player,
-          game: this.player.game,
-          role: this,
-          priority: PRIORITY_INVESTIGATIVE_AFTER_RESOLVE_DEFAULT - 10,
-          labels: ["investigate"],
-          run: function () {
             if (this.role.hasInfo) return;
             if (!this.actor.alive) return;
 
@@ -122,74 +39,8 @@ module.exports = class EvilDistance extends Card {
             var alert = `:invest: ${info.getInfoFormated()}.`;
             this.actor.queueAlert(alert);
 
-            /*
-            let alive = this.game.alivePlayers();
-            var evilPlayers = alive.filter(
-              (p) =>
-                this.game.getRoleAlignment(
-                  p.getRoleAppearance().split(" (")[0]
-                ) == "Cult" ||
-                this.game.getRoleAlignment(
-                  p.getRoleAppearance().split(" (")[0]
-                ) == "Mafia"
-            );
-
-            if (evilPlayers.length <= 1) {
-              this.actor.queueAlert(
-                `There wasn't enough evil players for your abilty to work!`
-              );
-              return;
-            }
-
-            var evilTarget = Random.randArrayVal(evilPlayers);
-            var indexOfTarget = alive.indexOf(evilTarget);
-            var rightIdx;
-            var leftIdx;
-            var leftAlign;
-            var rightAlign;
-            var distance = 0;
-            var found = false;
-
-            for (let x = 0; x < alive.length; x++) {
-              leftIdx =
-                (indexOfTarget - distance - 1 + alive.length) % alive.length;
-              rightIdx = (indexOfTarget + distance + 1) % alive.length;
-              leftAlign = this.game.getRoleAlignment(
-                alive[leftIdx].getRoleAppearance().split(" (")[0]
-              );
-              rightAlign = this.game.getRoleAlignment(
-                alive[rightIdx].getRoleAppearance().split(" (")[0]
-              );
-
-              if (rightAlign == "Cult" || rightAlign == "Mafia") {
-                found = true;
-                break;
-              } else if (leftAlign == "Cult" || leftAlign == "Mafia") {
-                found = true;
-                break;
-              } else {
-                distance = x;
-              }
-            }
-
-            if (this.actor.hasEffect("FalseMode")) {
-              if (distance == 0) {
-                distance = 1;
-              } else {
-                distance = distance - 1;
-              }
-            }
-
-            this.actor.queueAlert(
-              `You learn that there is ${distance} players between 2 of the evil players in the game!`
-            );
-*/
-            //this.actor.role.hasInfo = true;
           },
-        });
-
-        this.game.queueAction(action);
       },
-    };
+    ];
   }
 };
