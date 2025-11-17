a5752a3cffb71f9d658603bc98c10a23fa6b68cf
SawJester <anglelord270@gmail.com>
2025-11-17 01:21:58 -0500

    Update VotesAnonymousOnDeath.js

--- Games/types/Mafia/roles/cards/VotesAnonymousOnDeath.js
+++ Games/types/Mafia/roles/cards/VotesAnonymousOnDeath.js
@@ -4,23 +4,41 @@ module.exports = class VotesAnonymousOnDeath extends Card {
   constructor(role) {
     super(role);
 
+    this.passiveActions = [
+      {
+        ability: ["Effect", "WhenDead"],
+        state: "Night",
+        actor: role.player,
+        game: role.player.game,
+        priority: PRIORITY_EFFECT_GIVER_DEFAULT,
+        labels: [
+          "effect",
+          "hidden",
+          "absolute",
+        ],
+        role: role,
+        run: function () {
+          if(this.role.data.causeVoteAnonymous != true){
+            return;
+          }
+           for (let p of this.game.players) {
+              this.role.giveEffect(p, "VoteBlind", 1);
+            }
+
+            this.game.queueAlert(
+              `The Typist has died! Nobody can publicly record the votes…`
+            );
+          this.role.data.causeVoteAnonymous = false;
+        },
+      },
+    ];
+
     this.listeners = {
       death: function (player, killer, killType) {
         if (!this.hasAbility(["Effect", "WhenDead"])) {
           return;
         }
         if (player == this.player) this.data.causeVoteAnonymous = true;
-      },
-      state: function (stateInfo) {
-        if (this.data.causeVoteAnonymous && stateInfo.name.match(/Day/)) {
-          this.game.stateEvents["VotesAnonymous"] = true;
-          this.data.causeVoteAnonymous = false;
-        }
-      },
-      stateEvents: function (stateEvents) {
-        if (stateEvents["VotesAnonymous"])
-          for (let player of this.game.players)
-            player.giveEffect("VoteBlind", 1);
       },
     };
   }
