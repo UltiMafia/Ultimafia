88c6b57630d6982ea67bc8a78520a0b5ddd344d3
SawJester <anglelord270@gmail.com>
2025-11-17 01:00:01 -0500

    Update FixOwnItems.js

--- Games/types/Mafia/roles/cards/FixOwnItems.js
+++ Games/types/Mafia/roles/cards/FixOwnItems.js
@@ -22,49 +22,41 @@ module.exports = class FixOwnItems extends Card {
         },
       },
     ];
-    this.listeners = {
-      state: function (stateInfo) {
-        if (!this.hasAbility(["Item"])) {
-          return;
-        }
 
-        if (!stateInfo.name.match(/Night/)) {
-          return;
-        }
-
-        var action = new Action({
-          actor: this.player,
-          game: this.player.game,
-          role: this,
-          priority: PRIORITY_ITEM_TAKER_DEFAULT + 2,
-          labels: ["fixItems", "hidden"],
-          run: function () {
+    this.passiveActions = [
+      {
+        ability: ["Item"],
+        state: "Night",
+        actor: role.player,
+        game: role.player.game,
+        priority: PRIORITY_ITEM_TAKER_DEFAULT + 2,
+        labels: ["fixItems", "hidden"],
+        role: role,
+        run: function () {
             for (let item of this.actor.items) {
               item.broken = false;
               item.magicCult = false;
               item.mafiaImmune = false;
             }
-          },
-        });
-
-        this.game.queueAction(action);
-        var action2 = new Action({
-          actor: this.player,
-          game: this.player.game,
-          role: this,
-          priority: PRIORITY_ITEM_TAKER_EARLY + 2,
-          labels: ["fixItems", "hidden"],
-          run: function () {
-            for (let item of this.actor.items) {
-              item.broken = false;
-              item.magicCult = false;
-              item.mafiaImmune = false;
-            }
-          },
-        });
-
-        this.game.queueAction(action2);
+        },
       },
-    };
+      {
+        ability: ["Item"],
+        state: "Night",
+        actor: role.player,
+        game: role.player.game,
+        priority: PRIORITY_ITEM_TAKER_EARLY + 2,
+        labels: ["fixItems", "hidden"],
+        role: role,
+        run: function () {
+            for (let item of this.actor.items) {
+              item.broken = false;
+              item.magicCult = false;
+              item.mafiaImmune = false;
+            }
+        },
+      },
+    ];
+    
   }
 };
