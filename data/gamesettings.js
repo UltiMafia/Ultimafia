8a1c9f7353775ba8df796889475e6c1c2b496c64
SawJester <anglelord270@gmail.com>
2025-11-24 19:44:10 -0500

    Update gamesettings.js

--- data/gamesettings.js
+++ data/gamesettings.js
@@ -94,6 +94,37 @@ const gameSettingData = {
       description: "Dead players can vote in the village meeting.",
     },
 
+    //Team
+    "Silent Mafia": {
+      category: "Team",
+      internal: ["NoChatMafia"],
+      tags: ["Information"],
+      description:
+        "Mafia cannot speak at night.",
+    },
+    "Silent Cult": {
+      category: "Team",
+      internal: ["NoChatCult"],
+      tags: ["Information"],
+      description:
+        "Cult cannot speak at night.",
+    },
+    "Disorganized Mafia": {
+      category: "Team",
+      internal: ["MafiaNotLearningRoles"],
+      tags: ["Information"],
+      description:
+        "Mafia will not learn their members until the first night.",
+    },
+    "Disorganized Cult": {
+      category: "Team",
+      internal: ["CultNotLearningRoles"],
+      tags: ["Information"],
+      description:
+        "Cult will not learn their members until the first night.",
+    },
+    
+
     //Other
     "Role Sharing": {
       category: "Other",
