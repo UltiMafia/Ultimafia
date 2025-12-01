fd7d74c59c3e66fa8c09528fc1f510ad1b0359a3
SawJester <anglelord270@gmail.com>
2025-12-01 17:17:40 -0500

    Update WinWithFaction.js

--- Games/types/Mafia/roles/cards/WinWithFaction.js
+++ Games/types/Mafia/roles/cards/WinWithFaction.js
@@ -169,7 +169,7 @@ module.exports = class WinWithFaction extends Card {
 
         //soldier conditional
         if (this.player.faction != "Village" && !ONE_NIGHT) {
-          const soldiersInGame = this.game.players.filter((p) =>
+          const soldiersInGame = this.game.alivePlayers().filter((p) =>
             p.hasEffect("SoldierEffect")
           );
 
