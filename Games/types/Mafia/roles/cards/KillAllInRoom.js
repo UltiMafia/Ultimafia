4d3da62de28297a0a58e2c22308dc066c9f474f9
SawJester <SawJester@users.noreply.github.com>
2025-12-04 06:13:58 +0000

    Prettified Code!

--- Games/types/Mafia/roles/cards/KillAllInRoom.js
+++ Games/types/Mafia/roles/cards/KillAllInRoom.js
@@ -6,7 +6,6 @@ module.exports = class KillAllInRoom extends Card {
   constructor(role) {
     super(role);
 
-    
     this.passiveActions = [
       {
         ability: ["Kill", "OnlyWhenAlive"],
@@ -17,41 +16,40 @@ module.exports = class KillAllInRoom extends Card {
         priority: PRIORITY_KILL_DEFAULT,
         labels: ["kill", "hidden", "absolute"],
         run: function () {
-              if (!this.actor.alive) return;
-              if (this.game.FinalRound < this.game.CurrentRound) {
-                for (let people of this.game.players) {
-                  if (
-                    people.role.data.RoleTargetBackup == "President" ||
-                    people.role.data.RoleTargetBackup == "Assassin"
-                  ) {
-                    people.role.data.RoleTargetBackup = null;
-                  }
-                }
-                for (let room of this.game.Rooms) {
-                  if (room.members.includes(this.actor)) {
-                    for (let member of room.members) {
-                      if (member != this.actor && this.dominates(member)) {
-                        member.kill("basic", this.actor);
-                      }
-                    }
-                  }
-                }
-                for (let player of this.game.players) {
-                  for (let item of player.items) {
-                    if (item.name == "Room") {
-                      item.drop();
-                    }
-                    if (item.name == "NoVillageMeeting") {
-                      item.drop();
-                    }
-                  }
-                }
-
-                this.actor.kill("basic", this.actor);
+          if (!this.actor.alive) return;
+          if (this.game.FinalRound < this.game.CurrentRound) {
+            for (let people of this.game.players) {
+              if (
+                people.role.data.RoleTargetBackup == "President" ||
+                people.role.data.RoleTargetBackup == "Assassin"
+              ) {
+                people.role.data.RoleTargetBackup = null;
               }
-            },
+            }
+            for (let room of this.game.Rooms) {
+              if (room.members.includes(this.actor)) {
+                for (let member of room.members) {
+                  if (member != this.actor && this.dominates(member)) {
+                    member.kill("basic", this.actor);
+                  }
+                }
+              }
+            }
+            for (let player of this.game.players) {
+              for (let item of player.items) {
+                if (item.name == "Room") {
+                  item.drop();
+                }
+                if (item.name == "NoVillageMeeting") {
+                  item.drop();
+                }
+              }
+            }
+
+            this.actor.kill("basic", this.actor);
+          }
+        },
       },
     ];
-
   }
 };
