92e73b80078a4fe2aa7a939d8f9971949052c782
SawJester <SawJester@users.noreply.github.com>
2025-12-04 22:06:46 +0000

    Prettified Code!

--- Games/types/Mafia/roles/cards/LearnVisitorsAndArm.js
+++ Games/types/Mafia/roles/cards/LearnVisitorsAndArm.js
@@ -17,28 +17,26 @@ module.exports = class LearnVisitorsAndArm extends Card {
         priority: PRIORITY_ITEM_GIVER_DEFAULT,
         labels: ["giveItem", "gun", "hidden"],
         run: function () {
+          let info = this.game.createInformation(
+            "WatcherInfo",
+            this.actor,
+            this.game,
+            this.actor,
+            false,
+            true
+          );
+          info.processInfo();
 
-            let info = this.game.createInformation(
-              "WatcherInfo",
-              this.actor,
-              this.game,
-              this.actor,
-              false,
-              true
+          let visitors = info.getInfoRaw();
+
+          for (let visitor of visitors) {
+            this.actor.queueAlert(
+              `:gun2: You still feel apprehensive about ${visitor.name} after their visit last night but with this new gun, you feel more safe.`
             );
-            info.processInfo();
-
-            let visitors = info.getInfoRaw();
-
-            for (let visitor of visitors) {
-              this.actor.queueAlert(
-                `:gun2: You still feel apprehensive about ${visitor.name} after their visit last night but with this new gun, you feel more safe.`
-              );
-              this.actor.holdItem("Gun", { reveal: false });
-            }
-          },
+            this.actor.holdItem("Gun", { reveal: false });
+          }
+        },
       },
     ];
-
   }
 };
