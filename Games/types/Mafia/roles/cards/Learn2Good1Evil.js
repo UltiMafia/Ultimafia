c1f02f5ad7935878dfbdece113641d925eb6754c
SawJester <SawJester@users.noreply.github.com>
2025-12-04 07:04:16 +0000

    Prettified Code!

--- Games/types/Mafia/roles/cards/Learn2Good1Evil.js
+++ Games/types/Mafia/roles/cards/Learn2Good1Evil.js
@@ -17,19 +17,19 @@ module.exports = class Learn2Good1Evil extends Card {
         priority: PRIORITY_INVESTIGATIVE_DEFAULT,
         labels: ["investigate"],
         run: function () {
-            if (this.role.hasMaestroInfo) return;
-            this.role.hasMaestroInfo = true;
+          if (this.role.hasMaestroInfo) return;
+          this.role.hasMaestroInfo = true;
 
-            let info = this.game.createInformation(
-              "TwoGoodOneEvilInfo",
-              this.actor,
-              this.game,
-              this.actor
-            );
-            info.processInfo();
-            var alert = `:carol: ${info.getInfoFormated()}.`;
-            this.actor.queueAlert(alert);
-          },
+          let info = this.game.createInformation(
+            "TwoGoodOneEvilInfo",
+            this.actor,
+            this.game,
+            this.actor
+          );
+          info.processInfo();
+          var alert = `:carol: ${info.getInfoFormated()}.`;
+          this.actor.queueAlert(alert);
+        },
       },
     ];
   }
