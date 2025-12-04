82dea945e1aef50edab7622ecdf9c0e9bae2938a
SawJester <anglelord270@gmail.com>
2025-12-04 01:54:19 -0500

    Update KillVisitorsWhileDead.js

--- Games/types/Mafia/roles/cards/KillVisitorsWhileDead.js
+++ Games/types/Mafia/roles/cards/KillVisitorsWhileDead.js
@@ -5,49 +5,24 @@ const { PRIORITY_KILL_DEFAULT } = require("../../const/Priority");
 module.exports = class KillVisitorsWhileDead extends Card {
   constructor(role) {
     super(role);
-    /*
-    this.actions = [
+
+    this.passiveActions = [
       {
-        priority: PRIORITY_KILL_DEFAULT - 1,
-        labels: ["kill", "hidden", "absolute"],
-        run: function () {
-          if (this.actor.alive) return;
-
-          let visitors = this.getVisitors();
-
-          for (let visitor of visitors)
-            if (this.dominates(visitor)) visitor.kill("basic", this.actor);
-        },
-      },
-    ];
-*/
-    this.listeners = {
-      state: function (stateInfo) {
-        if (this.player.alive) {
-          return;
-        }
-
-        if (!stateInfo.name.match(/Night/)) {
-          return;
-        }
-
-        var action = new Action({
-          actor: this.player,
-          game: this.player.game,
-          priority: PRIORITY_KILL_DEFAULT,
-          labels: ["kill", "hidden"],
+        ability: ["Kill", "OnlyWhenDead"],
+        actor: role.player,
+        state: "Night",
+        game: role.game,
+        role: role,
+        priority: PRIORITY_KILL_DEFAULT,
+        labels: ["kill", "hidden"],
           run: function () {
-            if (this.actor.alive) return;
-
             let visitors = this.getVisitors();
 
             for (let visitor of visitors)
               if (this.dominates(visitor)) visitor.kill("basic", this.actor);
           },
-        });
-
-        this.game.queueAction(action);
       },
-    };
+    ];
+    
   }
 };
