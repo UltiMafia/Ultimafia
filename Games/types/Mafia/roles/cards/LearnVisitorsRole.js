c61302b1ffe3c856285c136d3c4c6769c76033ca
SawJester <anglelord270@gmail.com>
2025-12-04 17:20:01 -0500

    Update LearnVisitorsRole.js

--- Games/types/Mafia/roles/cards/LearnVisitorsRole.js
+++ Games/types/Mafia/roles/cards/LearnVisitorsRole.js
@@ -7,56 +7,16 @@ const { PRIORITY_INVESTIGATIVE_DEFAULT } = require("../../const/Priority");
 module.exports = class LearnVisitorsRole extends Card {
   constructor(role) {
     super(role);
-    /*
-    this.actions = [
+
+    this.passiveActions = [
       {
+        ability: ["Information"],
+        actor: role.player,
+        state: "Night",
+        game: role.game,
+        role: role,
         priority: PRIORITY_INVESTIGATIVE_DEFAULT,
-        labels: ["investigate", "role", "hidden", "absolute"],
-        run: function () {
-          if (this.game.getStateName() != "Night") return;
-
-          if (!this.actor.alive) return;
-
-          let visitors = this.getVisitors(this.actor);
-
-          if (this.actor.hasEffect("FalseMode")) {
-            let players = this.game
-              .alivePlayers()
-              .filter((p) => p != this.actor);
-            if (visitors.length == 0) {
-              visitors.push(Random.randArrayVal(players));
-            } else {
-              visitors = [];
-            }
-          }
-
-          for (let visitor of visitors) {
-            this.actor.queueAlert(
-              `Last night, ${addArticle(
-                visitor.getRoleAppearance()
-              )} visited you and confessed their sins.`
-            );
-          }
-        },
-      },
-    ];
-*/
-
-    this.listeners = {
-      state: function (stateInfo) {
-        if (!this.hasAbility(["Information"])) {
-          return;
-        }
-
-        if (!stateInfo.name.match(/Night/)) {
-          return;
-        }
-
-        var action = new Action({
-          actor: this.player,
-          game: this.player.game,
-          priority: PRIORITY_INVESTIGATIVE_DEFAULT,
-          labels: ["investigate", "role", "hidden"],
+        labels: ["investigate", "role", "hidden"],
           run: function () {
             let info = this.game.createInformation(
               "WatcherRoleInfo",
@@ -74,10 +34,7 @@ module.exports = class LearnVisitorsRole extends Card {
               );
             }
           },
-        });
-
-        this.game.queueAction(action);
       },
-    };
+    ];
   }
 };
