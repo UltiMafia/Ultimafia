0d6c00846589bfa2970418b49e81725c7d4e7825
SawJester <anglelord270@gmail.com>
2025-12-04 17:08:51 -0500

    Update LearnVisitorsPerson.js

--- Games/types/Mafia/roles/cards/LearnVisitorsPerson.js
+++ Games/types/Mafia/roles/cards/LearnVisitorsPerson.js
@@ -6,60 +6,17 @@ const { PRIORITY_INVESTIGATIVE_DEFAULT } = require("../../const/Priority");
 module.exports = class LearnVisitorsPerson extends Card {
   constructor(role) {
     super(role);
-    /*
-    this.actions = [
+
+    this.passiveActions = [
       {
+        ability: ["Information"],
+        actor: role.player,
+        state: "Night",
+        game: role.game,
+        role: role,
         priority: PRIORITY_INVESTIGATIVE_DEFAULT,
-        labels: ["investigate", "role", "hidden", "absolute"],
+        labels: ["investigate", "role", "hidden"],
         run: function () {
-          if (this.game.getStateName() != "Night") return;
-
-          if (!this.actor.alive) return;
-
-          let visitors = this.getVisitors(this.actor);
-          let visitorNames = visitors.map((v) => v.name);
-
-          if (this.actor.hasEffect("FalseMode")) {
-            let players = this.game
-              .alivePlayers()
-              .filter((p) => p != this.actor);
-            let playerNames = players.map((p) => p.name);
-            if (visitorNames.length == 0) {
-              visitorNames.push(Random.randArrayVal(playerNames));
-            } else {
-              visitorNames = [];
-            }
-          }
-
-          if (visitors.length === 0) {
-            visitorNames = ["no one"];
-          }
-
-          this.actor.queueAlert(
-            `:watch: You were visited by ${visitorNames.join(
-              ", "
-            )} during the night.`
-          );
-        },
-      },
-    ];
-*/
-
-    this.listeners = {
-      state: function (stateInfo) {
-        if (!this.hasAbility(["Information"])) {
-          return;
-        }
-
-        if (!stateInfo.name.match(/Night/)) {
-          return;
-        }
-        var action = new Action({
-          actor: this.player,
-          game: this.player.game,
-          priority: PRIORITY_INVESTIGATIVE_DEFAULT,
-          labels: ["investigate", "role", "hidden"],
-          run: function () {
             let info = this.game.createInformation(
               "WatcherInfo",
               this.actor,
@@ -72,10 +29,7 @@ module.exports = class LearnVisitorsPerson extends Card {
 
             this.actor.queueAlert(`:watch: ${info.getInfoFormated()}`);
           },
-        });
-
-        this.game.queueAction(action);
       },
-    };
+    ];
   }
 };
