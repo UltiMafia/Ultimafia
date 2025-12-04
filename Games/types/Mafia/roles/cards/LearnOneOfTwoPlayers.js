faabb4c581153322dbbbc25a6ff0a6f75b646463
SawJester <SawJester@users.noreply.github.com>
2025-12-04 20:45:43 +0000

    Prettified Code!

--- Games/types/Mafia/roles/cards/LearnOneOfTwoPlayers.js
+++ Games/types/Mafia/roles/cards/LearnOneOfTwoPlayers.js
@@ -17,30 +17,29 @@ module.exports = class LearnOneOfTwoPlayers extends Card {
         priority: PRIORITY_INVESTIGATIVE_DEFAULT,
         labels: ["investigate"],
         run: function () {
-        if (this.role.hasInfo) return;
+          if (this.role.hasInfo) return;
 
-            var alive = this.game.players.filter(
-              (p) => p.alive && p != this.actor
+          var alive = this.game.players.filter(
+            (p) => p.alive && p != this.actor
+          );
+          if (alive.length <= 2) {
+            this.actor.queueAlert(
+              ` You learn nothing... the other two survivors don't trust you with their laundry.`
             );
-            if (alive.length <= 2) {
-              this.actor.queueAlert(
-                ` You learn nothing... the other two survivors don't trust you with their laundry.`
-              );
-              return;
-            }
-            this.role.hasInfo = true;
+            return;
+          }
+          this.role.hasInfo = true;
 
-            let info = this.game.createInformation(
-              "OneOfPlayersIsRoleInfo",
-              this.actor,
-              this.game
-            );
-            info.processInfo();
-            var alert = `:invest: ${info.getInfoFormated()}.`;
-            this.actor.queueAlert(alert);
-          },
+          let info = this.game.createInformation(
+            "OneOfPlayersIsRoleInfo",
+            this.actor,
+            this.game
+          );
+          info.processInfo();
+          var alert = `:invest: ${info.getInfoFormated()}.`;
+          this.actor.queueAlert(alert);
+        },
       },
     ];
-    
   }
 };
