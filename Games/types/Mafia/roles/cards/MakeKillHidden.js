8989a9f43e74380f2429871ea0cb1208e83c966f
SawJester <anglelord270@gmail.com>
2025-12-04 18:47:57 -0500

    Update MakeKillHidden.js

--- Games/types/Mafia/roles/cards/MakeKillHidden.js
+++ Games/types/Mafia/roles/cards/MakeKillHidden.js
@@ -5,42 +5,17 @@ const { PRIORITY_NIGHT_ROLE_BLOCKER } = require("../../const/Priority");
 module.exports = class MakeKillHidden extends Card {
   constructor(role) {
     super(role);
-    /*
-    this.actions = [
+
+    
+    this.passiveActions = [
       {
-        labels: ["hidden"],
-        // roleblockable
+        ability: ["Astral"],
+        actor: role.player,
+        state: "Night",
+        game: role.game,
+        role: role,
         priority: PRIORITY_NIGHT_ROLE_BLOCKER + 5,
         run: function () {
-          if (!this.actor.alive) return;
-
-          if (this.game.getStateName() != "Night") return;
-
-          for (let action of this.game.actions[0]) {
-            if (action.actors.includes(this.actor) && action.hasLabel("kill")) {
-              action.labels = [...action.labels, "hidden"];
-            }
-          }
-        },
-      },
-    ];
-*/
-
-    this.listeners = {
-      state: function (stateInfo) {
-        if (!this.hasAbility(["Astral"])) {
-          return;
-        }
-
-        if (!stateInfo.name.match(/Night/)) {
-          return;
-        }
-
-        var action = new Action({
-          actor: this.player,
-          game: this.player.game,
-          priority: PRIORITY_NIGHT_ROLE_BLOCKER + 5,
-          run: function () {
             if (!this.actor.alive) return;
             //this.actor.giveEffect("Astral", )
             for (let action of this.game.actions[0]) {
@@ -58,10 +33,8 @@ module.exports = class MakeKillHidden extends Card {
               }
             }
           },
-        });
-
-        this.game.queueAction(action);
       },
-    };
+    ];
+
   }
 };
