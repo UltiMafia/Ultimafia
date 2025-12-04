ff3ae8cf761e06e5a665c6dfa080133a380b72f0
SawJester <SawJester@users.noreply.github.com>
2025-12-04 23:48:25 +0000

    Prettified Code!

--- Games/types/Mafia/roles/cards/MakeKillHidden.js
+++ Games/types/Mafia/roles/cards/MakeKillHidden.js
@@ -6,7 +6,6 @@ module.exports = class MakeKillHidden extends Card {
   constructor(role) {
     super(role);
 
-    
     this.passiveActions = [
       {
         ability: ["Astral"],
@@ -16,25 +15,21 @@ module.exports = class MakeKillHidden extends Card {
         role: role,
         priority: PRIORITY_NIGHT_ROLE_BLOCKER + 5,
         run: function () {
-            if (!this.actor.alive) return;
-            //this.actor.giveEffect("Astral", )
-            for (let action of this.game.actions[0]) {
-              if (
-                action.actors.includes(this.actor) &&
-                action.hasLabel("kill")
-              ) {
-                action.actors.splice(action.actors.indexOf(this.actor), 1);
-                if (action.actors.length < 0) {
-                  action.actors = [];
-                  action.actor = null;
-                }
-
-                //action.labels = [...action.labels, "hidden"];
+          if (!this.actor.alive) return;
+          //this.actor.giveEffect("Astral", )
+          for (let action of this.game.actions[0]) {
+            if (action.actors.includes(this.actor) && action.hasLabel("kill")) {
+              action.actors.splice(action.actors.indexOf(this.actor), 1);
+              if (action.actors.length < 0) {
+                action.actors = [];
+                action.actor = null;
               }
+
+              //action.labels = [...action.labels, "hidden"];
             }
-          },
+          }
+        },
       },
     ];
-
   }
 };
