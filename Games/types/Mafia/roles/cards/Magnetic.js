1a7175e5894dc9355b2349a5bd95fe1b8a6c0e59
SawJester <SawJester@users.noreply.github.com>
2025-12-04 23:30:34 +0000

    Prettified Code!

--- Games/types/Mafia/roles/cards/Magnetic.js
+++ Games/types/Mafia/roles/cards/Magnetic.js
@@ -17,26 +17,25 @@ module.exports = class Magnetic extends Card {
         priority: PRIORITY_MODIFY_ACTION - 1,
         labels: ["redirect"],
         run: function () {
-            var alive = this.game.players.filter(
-              (p) =>
-                p.alive &&
-                p != this.actor &&
-                p.role.alignment == this.actor.role.alignment
-            );
-            if (alive.length > 0) {
-              for (const action of this.game.actions[0]) {
-                if (
-                  action.target != this.actor &&
-                  alive.includes(action.target) &&
-                  action.hasLabel("kill")
-                ) {
-                  action.target = this.actor;
-                }
+          var alive = this.game.players.filter(
+            (p) =>
+              p.alive &&
+              p != this.actor &&
+              p.role.alignment == this.actor.role.alignment
+          );
+          if (alive.length > 0) {
+            for (const action of this.game.actions[0]) {
+              if (
+                action.target != this.actor &&
+                alive.includes(action.target) &&
+                action.hasLabel("kill")
+              ) {
+                action.target = this.actor;
               }
             }
-          },
+          }
+        },
       },
     ];
-
   }
 };
