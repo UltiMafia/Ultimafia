77af20dfe386384df62120c4e8770ba8938028a9
SawJester <anglelord270@gmail.com>
2025-12-04 01:42:32 -0500

    Update KillVisitors.js

--- Games/types/Mafia/roles/cards/KillVisitors.js
+++ Games/types/Mafia/roles/cards/KillVisitors.js
@@ -5,49 +5,24 @@ const { PRIORITY_KILL_DEFAULT } = require("../../const/Priority");
 module.exports = class KillVisitors extends Card {
   constructor(role) {
     super(role);
-    /*
-    this.actions = [
+
+    this.passiveActions = [
       {
+        ability: ["Kill"],
+        actor: role.player,
+        state: "Night",
+        game: role.game,
+        role: role,
         priority: PRIORITY_KILL_DEFAULT,
-        labels: ["kill", "hidden", "absolute"],
-        run: function () {
-          if (!this.actor.alive) return;
-
-          if (this.game.getStateName() != "Night") return;
-
-          let visitors = this.getVisitors();
-
-          for (let visitor of visitors)
-            if (this.dominates(visitor)) visitor.kill("basic", this.actor);
-        },
-      },
-    ];
-*/
-    this.listeners = {
-      state: function (stateInfo) {
-        if (!this.hasAbility(["Kill"])) {
-          return;
-        }
-
-        if (!stateInfo.name.match(/Night/)) {
-          return;
-        }
-
-        var action = new Action({
-          actor: this.player,
-          game: this.player.game,
-          priority: PRIORITY_KILL_DEFAULT,
-          labels: ["kill", "hidden"],
+        labels: ["kill", "hidden"],
           run: function () {
             let visitors = this.getVisitors();
 
             for (let visitor of visitors)
               if (this.dominates(visitor)) visitor.kill("basic", this.actor);
           },
-        });
-
-        this.game.queueAction(action);
       },
-    };
+    ];
+    
   }
 };
