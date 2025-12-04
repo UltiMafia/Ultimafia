8b236b0f632ba21f3acbc866e4ada3ba66a5c6ec
SawJester <anglelord270@gmail.com>
2025-12-04 11:56:27 -0500

    Update LearnEvilDeadCount.js

--- Games/types/Mafia/roles/cards/LearnEvilDeadCount.js
+++ Games/types/Mafia/roles/cards/LearnEvilDeadCount.js
@@ -7,62 +7,17 @@ const {
 module.exports = class LearnEvilDeadCount extends Card {
   constructor(role) {
     super(role);
-    /*
-    this.actions = [
+
+    this.passiveActions = [
       {
-        priority: PRIORITY_INVESTIGATIVE_AFTER_RESOLVE_DEFAULT - 2,
+        ability: ["Information"],
+        actor: role.player,
+        state: "Night",
+        game: role.game,
+        role: role,
+        priority: PRIORITY_INVESTIGATIVE_AFTER_RESOLVE_DEFAULT - 5,
         labels: ["investigate"],
         run: function () {
-          if (this.game.getStateName() != "Night") return;
-          if (!this.actor.alive) return;
-          let evilCount;
-          let players = this.game.deadPlayers();
-
-          if (players.length <= 0) {
-            return;
-          }
-
-          var evilPlayers = players.filter(
-            (p) =>
-              this.game.getRoleAlignment(
-                p.getRoleAppearance().split(" (")[0]
-              ) == "Cult" ||
-              this.game.getRoleAlignment(
-                p.getRoleAppearance().split(" (")[0]
-              ) == "Mafia"
-          );
-          evilCount = evilPlayers.length;
-
-          if (this.actor.hasEffect("FalseMode")) {
-            if (evilCount == 0) evilCount = 1;
-            else evilCount = evilCount - 1;
-          }
-
-          this.actor.queueAlert(
-            `You learn that ${evilCount} dead players are Evil.`
-          );
-        },
-      },
-    ];
-*/
-
-    this.listeners = {
-      state: function (stateInfo) {
-        if (!this.hasAbility(["Information"])) {
-          return;
-        }
-
-        if (!stateInfo.name.match(/Night/)) {
-          return;
-        }
-
-        var action = new Action({
-          actor: this.player,
-          game: this.player.game,
-          priority: PRIORITY_INVESTIGATIVE_AFTER_RESOLVE_DEFAULT - 5,
-          labels: ["investigate"],
-          run: function () {
-            if (!this.actor.alive) return;
             let evilCount;
             let players = this.game.deadPlayers();
 
@@ -80,10 +35,7 @@ module.exports = class LearnEvilDeadCount extends Card {
             var alert = `:invest: ${info.getInfoFormated()}`;
             this.actor.queueAlert(alert);
           },
-        });
-
-        this.game.queueAction(action);
       },
-    };
+    ];
   }
 };
