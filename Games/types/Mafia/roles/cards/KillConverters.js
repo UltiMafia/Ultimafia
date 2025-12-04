77a2c9f8c5a0fd0add86651d9d44d52ee65cf632
SawJester <anglelord270@gmail.com>
2025-12-04 01:31:03 -0500

    Update KillConverters.js

--- Games/types/Mafia/roles/cards/KillConverters.js
+++ Games/types/Mafia/roles/cards/KillConverters.js
@@ -7,50 +7,17 @@ module.exports = class KillConverters extends Card {
     super(role);
 
     this.role.killLimit = 2;
-    /*
-    this.actions = [
+    
+    this.passiveActions = [
       {
+        ability: ["Kill", "Modifier"],
+        actor: role.player,
+        state: "Night",
+        game: role.game,
+        role: role,
         priority: PRIORITY_KILL_DEFAULT,
         labels: ["kill", "hidden"],
         run: function () {
-          if (!this.actor.alive) return;
-
-          if (this.game.getStateName() != "Night") return;
-
-          if (this.actor.role.killLimit <= 0) {
-            return;
-          }
-
-          var convertingVisitors = this.getVisitors(this.actor, "convert");
-
-          for (let visitor of convertingVisitors) {
-            if (this.actor.role.killLimit > 0 && this.dominates(visitor)) {
-              visitor.kill("basic", this.actor);
-              this.actor.role.killLimit--;
-            }
-          }
-        },
-      },
-    ];
-*/
-
-    this.listeners = {
-      state: function (stateInfo) {
-        if (!this.hasAbility(["Kill", "Modifier"])) {
-          return;
-        }
-
-        if (!stateInfo.name.match(/Night/)) {
-          return;
-        }
-
-        var action = new Action({
-          actor: this.player,
-          game: this.player.game,
-          role: this,
-          priority: PRIORITY_KILL_DEFAULT,
-          labels: ["kill", "hidden"],
-          run: function () {
             if (this.role.killLimit <= 0) {
               return;
             }
@@ -64,10 +31,7 @@ module.exports = class KillConverters extends Card {
               }
             }
           },
-        });
-
-        this.game.queueAction(action);
       },
-    };
+    ];
   }
 };
