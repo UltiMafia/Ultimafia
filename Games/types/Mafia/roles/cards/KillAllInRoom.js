978e4aeca9908d2dd43f8211b52b25af6fc025a6
SawJester <anglelord270@gmail.com>
2025-12-04 01:13:33 -0500

    Update KillAllInRoom.js

--- Games/types/Mafia/roles/cards/KillAllInRoom.js
+++ Games/types/Mafia/roles/cards/KillAllInRoom.js
@@ -6,21 +6,18 @@ module.exports = class KillAllInRoom extends Card {
   constructor(role) {
     super(role);
 
-    this.listeners = {
-      state: function (stateInfo) {
-        if (stateInfo.name.match(/Night/)) {
-          var action = new Action({
-            actor: this.player,
-            game: this.player.game,
-            priority: PRIORITY_KILL_DEFAULT,
-            labels: ["absolute", "hidden"],
-            run: function () {
+    
+    this.passiveActions = [
+      {
+        ability: ["Kill", "OnlyWhenAlive"],
+        actor: role.player,
+        state: "Night",
+        game: role.game,
+        role: role,
+        priority: PRIORITY_KILL_DEFAULT,
+        labels: ["kill", "hidden", "absolute"],
+        run: function () {
               if (!this.actor.alive) return;
-              if (
-                this.game.getStateName() != "Night" &&
-                this.game.getStateName() != "Dawn"
-              )
-                return;
               if (this.game.FinalRound < this.game.CurrentRound) {
                 for (let people of this.game.players) {
                   if (
@@ -53,10 +50,8 @@ module.exports = class KillAllInRoom extends Card {
                 this.actor.kill("basic", this.actor);
               }
             },
-          });
-          this.game.queueAction(action);
-        }
       },
-    };
+    ];
+
   }
 };
