de084a66df187f8c9fd84343ada06a05202bd350
SawJester <anglelord270@gmail.com>
2025-12-04 02:03:51 -0500

    Update Learn2Good1Evil.js

--- Games/types/Mafia/roles/cards/Learn2Good1Evil.js
+++ Games/types/Mafia/roles/cards/Learn2Good1Evil.js
@@ -7,23 +7,16 @@ module.exports = class Learn2Good1Evil extends Card {
   constructor(role) {
     super(role);
 
-    this.listeners = {
-      state: function (stateInfo) {
-        if (!this.hasAbility(["Information"])) {
-          return;
-        }
-
-        if (!stateInfo.name.match(/Night/)) {
-          return;
-        }
-
-        var action = new Action({
-          actor: this.player,
-          game: this.player.game,
-          role: this.role,
-          priority: PRIORITY_INVESTIGATIVE_DEFAULT,
-          labels: ["investigate"],
-          run: function () {
+    this.passiveActions = [
+      {
+        ability: ["Information"],
+        actor: role.player,
+        state: "Night",
+        game: role.game,
+        role: role,
+        priority: PRIORITY_INVESTIGATIVE_DEFAULT,
+        labels: ["investigate"],
+        run: function () {
             if (this.role.hasMaestroInfo) return;
             this.role.hasMaestroInfo = true;
 
@@ -37,10 +30,7 @@ module.exports = class Learn2Good1Evil extends Card {
             var alert = `:carol: ${info.getInfoFormated()}.`;
             this.actor.queueAlert(alert);
           },
-        });
-
-        this.game.queueAction(action);
       },
-    };
+    ];
   }
 };
