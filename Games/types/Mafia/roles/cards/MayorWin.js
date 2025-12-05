0932a43fcbf9a16449f4bd2fd56d3b0b928144fd
SawJester <SawJester@users.noreply.github.com>
2025-12-05 00:09:59 +0000

    Prettified Code!

--- Games/types/Mafia/roles/cards/MayorWin.js
+++ Games/types/Mafia/roles/cards/MayorWin.js
@@ -37,30 +37,30 @@ module.exports = class MayorWin extends Card {
         priority: PRIORITY_DAY_EFFECT_DEFAULT + 1,
         labels: ["hidden", "absolute"],
         run: function () {
-            if (!this.role.hasAbility(["Win-Con", "OnlyWhenAlive"])) return;
-            if (this.game.alivePlayers().length != 3) {
-              this.role.data.MayorWin = false;
-              return;
-            }
-            let alivePlayers = this.game.players.filter((p) => p.role);
+          if (!this.role.hasAbility(["Win-Con", "OnlyWhenAlive"])) return;
+          if (this.game.alivePlayers().length != 3) {
+            this.role.data.MayorWin = false;
+            return;
+          }
+          let alivePlayers = this.game.players.filter((p) => p.role);
 
-            for (let x = 0; x < alivePlayers.length; x++) {
-              for (let action of this.game.actions[0]) {
-                if (
-                  action.target == alivePlayers[x] &&
-                  action.hasLabel("condemn")
-                ) {
-                  this.role.data.MayorWin = false;
-                  return;
-                }
+          for (let x = 0; x < alivePlayers.length; x++) {
+            for (let action of this.game.actions[0]) {
+              if (
+                action.target == alivePlayers[x] &&
+                action.hasLabel("condemn")
+              ) {
+                this.role.data.MayorWin = false;
+                return;
               }
             }
-            this.role.data.MayorWin = true;
-            this.actor.giveEffect("MayorEffect", Infinity);
-            let MayorEffect = this.actor.giveEffect("MayorEffect", Infinity);
-            this.role.passiveEffects.push(MayorEffect);
-            return;
-          },
+          }
+          this.role.data.MayorWin = true;
+          this.actor.giveEffect("MayorEffect", Infinity);
+          let MayorEffect = this.actor.giveEffect("MayorEffect", Infinity);
+          this.role.passiveEffects.push(MayorEffect);
+          return;
+        },
       },
     ];
   }
