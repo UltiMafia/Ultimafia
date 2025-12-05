afb0210d7fbcacd9a8be0c60a6eccea47a528566
SawJester <anglelord270@gmail.com>
2025-12-05 02:20:08 -0500

    Create Toymaker.js

---
+++ Games/types/Mafia/roles/Village/Toymaker.js
@@ -0,0 +1,9 @@
+const Role = require("../../Role");
+
+module.exports = class Toymaker extends Role {
+  constructor(player, data) {
+    super("Toymaker", player, data);
+    this.alignment = "Village";
+    this.cards = ["VillageCore", "WinWithFaction", "MeetingFaction", "JackGiver"];
+  }
+};
