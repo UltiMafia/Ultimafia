06de4206694ca902b8d1b86704d815977f816458
SawJester <anglelord270@gmail.com>
2025-12-05 00:06:50 -0500

    Create JackGiver.js

---
+++ Games/types/Mafia/roles/cards/JackGiver.js
@@ -0,0 +1,23 @@
+const Card = require("../../Card");
+const { PRIORITY_ITEM_GIVER_EARLY } = require("../../const/Priority");
+
+module.exports = class JackGiver extends Card {
+  constructor(role) {
+    super(role);
+
+    this.meetings = {
+      "Give Bomb": {
+        states: ["Night"],
+        flags: ["voting"],
+        action: {
+          labels: ["giveItem", "jackinthebox"],
+          priority: PRIORITY_ITEM_GIVER_EARLY,
+          run: function () {
+            this.target.holdItem("JackInTheBox");
+            this.target.queueGetItemAlert("Jack In The Box");
+          },
+        },
+      },
+    };
+  }
+};
