55ec92ca22695f134b208b568e730c0195209798
SawJester <anglelord270@gmail.com>
2025-12-04 19:18:08 -0500

    Update ModifierBloodthirsty.js

--- Games/types/Mafia/roles/cards/ModifierBloodthirsty.js
+++ Games/types/Mafia/roles/cards/ModifierBloodthirsty.js
@@ -5,44 +5,26 @@ const { PRIORITY_KILL_DEFAULT } = require("../../const/Priority");
 module.exports = class ModifierBloodthirsty extends Card {
   constructor(role) {
     super(role);
-    /*
-    this.actions = [
+
+    this.passiveActions = [
       {
+        ability: ["Kill"],
+        actor: role.player,
+        state: "Night",
+        game: role.game,
+        role: role,
         priority: PRIORITY_KILL_DEFAULT,
         labels: ["kill"],
         run: function () {
-          if (this.game.getStateName() != "Night") return;
-
-          let visits = this.getVisits(this.actor);
-          visits.map((v) => this.kill(v));
-        },
-      },
-    ];
-*/
-
-    this.listeners = {
-      state: function (stateInfo) {
-        if (!stateInfo.name.match(/Night/)) {
-          return;
-        }
-
-        var action = new Action({
-          actor: this.player,
-          game: this.player.game,
-          priority: PRIORITY_KILL_DEFAULT,
-          labels: ["kill"],
-          run: function () {
             let visits = this.getVisits(this.actor);
             for (let v of visits) {
               if (this.dominates(v)) {
                 v.kill("basic", this.actor);
               }
             }
-          },
-        });
-
-        this.game.queueAction(action);
+        },
       },
-    };
+    ];
+
   }
 };
