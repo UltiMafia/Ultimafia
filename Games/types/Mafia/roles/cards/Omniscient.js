e32b77ef73cc09d4f268bd9b462f63140eba3dd1
SawJester <SawJester@users.noreply.github.com>
2025-12-07 20:34:22 +0000

    Prettified Code!

--- Games/types/Mafia/roles/cards/Omniscient.js
+++ Games/types/Mafia/roles/cards/Omniscient.js
@@ -17,35 +17,34 @@ module.exports = class Omiscient extends Card {
         priority: PRIORITY_INVESTIGATIVE_DEFAULT,
         labels: ["investigate"],
         run: function () {
-            let visits;
-            let visitNames;
-            let role;
-            let name;
-            let players = this.game.alivePlayers();
-            for (let x = 0; x < players.length; x++) {
-              let info = this.game.createInformation(
-                "RevealInfo",
-                this.actor,
-                this.game,
-                players[x],
-                null,
-                "Self"
-              );
-              info.processInfo();
-              info.getInfoRaw();
+          let visits;
+          let visitNames;
+          let role;
+          let name;
+          let players = this.game.alivePlayers();
+          for (let x = 0; x < players.length; x++) {
+            let info = this.game.createInformation(
+              "RevealInfo",
+              this.actor,
+              this.game,
+              players[x],
+              null,
+              "Self"
+            );
+            info.processInfo();
+            info.getInfoRaw();
 
-              let info2 = this.game.createInformation(
-                "TrackerInfo",
-                this.actor,
-                this.game,
-                players[x]
-              );
-              info2.processInfo();
-              this.actor.queueAlert(`:track: ${info2.getInfoFormated()}`);
-            }
-          },
+            let info2 = this.game.createInformation(
+              "TrackerInfo",
+              this.actor,
+              this.game,
+              players[x]
+            );
+            info2.processInfo();
+            this.actor.queueAlert(`:track: ${info2.getInfoFormated()}`);
+          }
+        },
       },
     ];
-
   }
 };
