fc0693f92a8d463d9b36b3c3bb35cd794e352538
SawJester <SawJester@users.noreply.github.com>
2025-12-08 22:16:34 +0000

    Prettified Code!

--- Games/types/Mafia/roles/cards/RevealNameToTarget.js
+++ Games/types/Mafia/roles/cards/RevealNameToTarget.js
@@ -9,7 +9,7 @@ module.exports = class RevealNameToTarget extends Card {
   constructor(role) {
     super(role);
 
-      this.passiveActions = [
+    this.passiveActions = [
       {
         ability: ["Modifier", "Information"],
         actor: role.player,
@@ -19,22 +19,21 @@ module.exports = class RevealNameToTarget extends Card {
         priority: PRIORITY_INVESTIGATIVE_AFTER_RESOLVE_DEFAULT - 10,
         labels: ["investigate", "hidden"],
         run: function () {
-            let info = this.game.createInformation(
-              "LearnTargetInfo",
-              this.actor,
-              this.game,
-              this.actor
-            );
-            info.processInfo();
-            var alert = `:mask: You learn that you were visited by ${
-              info.getInfoRaw().name
-            }.`;
+          let info = this.game.createInformation(
+            "LearnTargetInfo",
+            this.actor,
+            this.game,
+            this.actor
+          );
+          info.processInfo();
+          var alert = `:mask: You learn that you were visited by ${
+            info.getInfoRaw().name
+          }.`;
 
-            let visits = this.getVisits(this.actor);
-            visits.map((v) => v.queueAlert(alert));
-          },
+          let visits = this.getVisits(this.actor);
+          visits.map((v) => v.queueAlert(alert));
+        },
       },
     ];
-
   }
 };
