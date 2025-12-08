54d15662d87e7bf8641a0f48b5b7d810047af2dc
SawJester <anglelord270@gmail.com>
2025-12-08 16:47:31 -0500

    Update Regretful.js

--- Games/types/Mafia/roles/cards/Regretful.js
+++ Games/types/Mafia/roles/cards/Regretful.js
@@ -5,42 +5,16 @@ const { PRIORITY_KILL_DEFAULT } = require("../../const/Priority");
 module.exports = class Regretful extends Card {
   constructor(role) {
     super(role);
-    /*
-    this.actions = [
+
+    this.passiveActions = [
       {
+        ability: ["Modifier", "Kill"],
+        actor: role.player,
+        state: "Night",
+        game: role.game,
+        role: role,
         priority: PRIORITY_KILL_DEFAULT,
         labels: ["kill", "hidden", "absolute"],
-        run: function () {
-          if (this.game.getStateName() != "Night") return;
-
-          let visits = this.getVisits(this.actor);
-          let killers = visits.map((v) => this.getVisitors(v, "kill"));
-
-          if (killers.length == 0) {
-            return;
-          } else if (this.dominates(this.actor)) {
-            this.actor.kill("basic", this.actor);
-          }
-        },
-      },
-    ];
-*/
-
-    this.listeners = {
-      state: function (stateInfo) {
-        if (!this.hasAbility(["Modifier", "Kill"])) {
-          return;
-        }
-
-        if (!stateInfo.name.match(/Night/)) {
-          return;
-        }
-
-        var action = new Action({
-          actor: this.player,
-          game: this.player.game,
-          priority: PRIORITY_KILL_DEFAULT,
-          labels: ["kill", "hidden", "absolute"],
           run: function () {
             let visits = this.getVisits(this.actor);
             let killers = visits.map((v) => this.getVisitors(v, "kill"));
@@ -51,10 +25,7 @@ module.exports = class Regretful extends Card {
               this.actor.kill("basic", this.actor);
             }
           },
-        });
-
-        this.game.queueAction(action);
       },
-    };
+    ];
   }
 };
