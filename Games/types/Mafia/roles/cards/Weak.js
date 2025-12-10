47cbf14cd6c25a5a7162f55eea085f5416288e51
SawJester <SawJester@users.noreply.github.com>
2025-12-10 11:10:19 +0000

    Prettified Code!

--- Games/types/Mafia/roles/cards/Weak.js
+++ Games/types/Mafia/roles/cards/Weak.js
@@ -6,8 +6,8 @@ const { PRIORITY_KILL_DEFAULT } = require("../../const/Priority");
 module.exports = class Weak extends Card {
   constructor(role) {
     super(role);
-    
-     this.passiveActions = [
+
+    this.passiveActions = [
       {
         ability: ["Modifier", "Kill"],
         actor: role.player,
@@ -17,40 +17,38 @@ module.exports = class Weak extends Card {
         priority: PRIORITY_KILL_DEFAULT,
         labels: ["kill", "hidden"],
         run: function () {
-    
-            if (!this.actor.alive) return;
+          if (!this.actor.alive) return;
 
-            for (let action of this.game.actions[0]) {
-              if (action.hasLabel("mafia")) {
-                continue;
-              }
-              if (action.hasLabel("hidden")) {
-                continue;
-              }
+          for (let action of this.game.actions[0]) {
+            if (action.hasLabel("mafia")) {
+              continue;
+            }
+            if (action.hasLabel("hidden")) {
+              continue;
+            }
 
-              let toCheck = action.target;
-              if (!Array.isArray(action.target)) {
-                toCheck = [action.target];
-              }
+            let toCheck = action.target;
+            if (!Array.isArray(action.target)) {
+              toCheck = [action.target];
+            }
 
-              if (
-                action.actors.indexOf(this.actor) != -1 &&
-                !action.hasLabel("hidden") &&
-                action.target &&
-                toCheck[0] instanceof Player
-              ) {
-                for (let y = 0; y < toCheck.length; y++) {
-                  if (toCheck[y].role.alignment != this.actor.role.alignment) {
-                    if (this.dominates(this.actor)) {
-                      this.actor.kill("basic", this.actor);
-                    }
+            if (
+              action.actors.indexOf(this.actor) != -1 &&
+              !action.hasLabel("hidden") &&
+              action.target &&
+              toCheck[0] instanceof Player
+            ) {
+              for (let y = 0; y < toCheck.length; y++) {
+                if (toCheck[y].role.alignment != this.actor.role.alignment) {
+                  if (this.dominates(this.actor)) {
+                    this.actor.kill("basic", this.actor);
                   }
                 }
               }
             }
-          },
+          }
+        },
       },
     ];
-
   }
 };
