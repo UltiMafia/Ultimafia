44a7773742c240cbe0f5b2f5c7d2340fdbbe54dc
SawJester <anglelord270@gmail.com>
2025-12-10 05:50:58 -0500

    Update Vain.js

--- Games/types/Mafia/roles/cards/Vain.js
+++ Games/types/Mafia/roles/cards/Vain.js
@@ -6,71 +6,17 @@ const { PRIORITY_KILL_DEFAULT } = require("../../const/Priority");
 module.exports = class Vain extends Card {
   constructor(role) {
     super(role);
-    /*
-    this.actions = [
+
+    this.passiveActions = [
       {
+        ability: ["Modifier", "Kill"],
+        actor: role.player,
+        state: "Night",
+        game: role.game,
+        role: role,
         priority: PRIORITY_KILL_DEFAULT,
         labels: ["kill", "hidden"],
         run: function () {
-          if (
-            this.game.getStateName() != "Night" &&
-            this.game.getStateName() != "Dawn"
-          )
-            return;
-
-          if (!this.actor.alive) return;
-
-          for (let action of this.game.actions[0]) {
-            if (action.hasLabel("absolute")) {
-              continue;
-            }
-            if (action.hasLabel("mafia")) {
-              continue;
-            }
-            if (action.hasLabel("hidden")) {
-              continue;
-            }
-
-            let toCheck = action.target;
-            if (!Array.isArray(action.target)) {
-              toCheck = [action.target];
-            }
-
-            if (
-              action.actors.indexOf(this.actor) != -1 &&
-              !action.hasLabel("hidden") &&
-              action.target &&
-              toCheck[0] instanceof Player
-            ) {
-              for (let y = 0; y < toCheck.length; y++) {
-                if (toCheck[y].role.alignment == this.actor.role.alignment) {
-                  if (this.dominates(this.actor)) {
-                    this.actor.kill("basic", this.actor);
-                  }
-                }
-              }
-            }
-          }
-        },
-      },
-    ];
-*/
-    this.listeners = {
-      state: function (stateInfo) {
-        if (!this.hasAbility(["Kill", "Modifier"])) {
-          return;
-        }
-
-        if (!stateInfo.name.match(/Night/)) {
-          return;
-        }
-
-        var action = new Action({
-          actor: this.player,
-          game: this.player.game,
-          priority: PRIORITY_KILL_DEFAULT,
-          labels: ["kill", "hidden"],
-          run: function () {
             if (!this.actor.alive) return;
 
             for (let action of this.game.actions[0]) {
@@ -102,10 +48,8 @@ module.exports = class Vain extends Card {
               }
             }
           },
-        });
-
-        this.game.queueAction(action);
       },
-    };
+    ];
+    
   }
 };
