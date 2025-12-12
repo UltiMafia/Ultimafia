0a3f450cc68a999f8e93be1cf5307dce09856a3d
SawJester <anglelord270@gmail.com>
2025-12-12 13:14:44 -0500

    Create Game.js

---
+++ Games/types/Ratscrew/Game.js
@@ -0,0 +1,531 @@
+const Game = require("../../core/Game");
+const Player = require("./Player");
+const Action = require("./Action");
+const DrawDiscardPile = require("./DrawDiscardPile");
+const Queue = require("../../core/Queue");
+const Winners = require("../../core/Winners");
+
+const Random = require("../../../lib/Random");
+
+module.exports = class RatscrewGame extends Game {
+  constructor(options) {
+    super(options);
+
+    this.type = "Cheat";
+    this.Player = Player;
+    this.states = [
+      {
+        name: "Postgame",
+      },
+      {
+        name: "Pregame",
+      },
+      {
+        name: "Play Cards",
+        length: options.settings.stateLengths["Play Cards"],
+      },
+      {
+        name: "Call Lie",
+        length: options.settings.stateLengths["Call Lie"],
+      },
+    ];
+
+    //settings
+    /*
+    this.wildOnes = options.settings.wildOnes;
+    this.spotOn = options.settings.spotOn;
+    */
+    this.drawDiscardPile = new DrawDiscardPile();
+    this.drawDiscardPile.initCards();
+    this.MaxRounds = parseInt(options.settings.MaxRounds) || 0;
+    this.CardGameType = "Cheat";
+
+    //VARIABLES
+    this.randomizedPlayers = []; //All players after they get randomized. Used for showing players and their dice on left side of screen.
+    this.randomizedPlayersCopy = []; //copy of above, but players don't get removed on dying / leaving. Used for deciding next player's
+    // turn, since variable above would mess up indexes when players got removed.
+    this.currentIndex = 0; //Index of player's current turn.
+
+    //information about last turn's bid
+    this.lastAmountBid = 0;
+    this.lastFaceBid = 1;
+    this.lastBidder = null;
+    this.allRolledDice = []; //Used for counting dice on lie or spot on calls. player.diceRolled would remove dice if player left, so
+    // this got created.
+
+    this.allDice = 0; //all dice counted together, used for variable under this and messages sent regarding high bids
+    this.gameMasterAnnoyedByHighBidsThisRoundYet = false; //when bid is a lot higher than all dice together, this turns on, and players
+    // will only be able to bid by one amount higher each turn for the rest of the round.
+
+    this.chatName = "Casino";
+
+    this.spectatorMeetFilter = {
+      Pregame: true,
+      Casino: true,
+      "The Flying Dutchman": true,
+      Amount: false,
+      Face: false,
+      separationText: false,
+      CallLie: false,
+      SpotOn: false,
+    };
+  }
+
+  sendAlert(message, recipients, extraStyle = {}) {
+    if (this.chatName === "The Flying Dutchman") {
+      extraStyle = { color: "#718E77" };
+    }
+
+    super.sendAlert(message, recipients, extraStyle);
+  }
+
+  start() {
+    //introduction, rules messages
+    this.chatName = Math.random() < 0.03 ? "The Flying Dutchman" : "Casino"; //3% for meeting to be called The Flying Dutchman lol
+
+    if (this.chatName == "The Flying Dutchman") {
+      this.sendAlert(`Welcome aboard the Flying Dutchman, mates!`, undefined, {
+        color: "#718E77",
+      });
+      this.sendAlert(
+        `How many years of servitude be ye willin' to wager?`,
+        undefined,
+        { color: "#718E77" }
+      );
+    }
+    /*
+    if (this.wildOnes) {
+      this.sendAlert(
+        `WILD ONES are enabled. Ones will count towards any face amount.`
+      );
+    }
+    if (this.spotOn) {
+      this.sendAlert(
+        `SPOT ON is enabled. On your turn, you can guess that the previous bidder called exact amount. If you're right, everyone else will lose a die.`
+      );
+    }
+    */
+    if (this.MaxRounds >= 1) {
+      this.sendAlert(
+        `The player with the least Cards wins after Round ${this.MaxRounds}.`
+      );
+    }
+    this.sendAlert(`Good luck... You'll probably need it.`);
+
+    //start of game - randomizes player order, and gives dice to everyone.
+    this.hasHost = this.setup.roles[0]["Host:"];
+    if (this.hasHost) {
+      let hostPlayer = this.players.array()[0];
+      this.randomizedPlayers = Random.randomizeArray(
+        this.players.array()
+      ).filter((p) => p != hostPlayer);
+    } else {
+      this.randomizedPlayers = Random.randomizeArray(this.players.array());
+    }
+    this.randomizedPlayersCopy = this.randomizedPlayers;
+
+    this.randomizedPlayers.forEach((player) => {
+      player.Chips = parseInt(this.startingChips);
+    });
+
+    // super.start();
+    //this.rollDice();
+    if (this.CardGameType == "Cheat") {
+      this.TheStack = [];
+      this.RankNumber = 1;
+      this.drawDiscardPile.shuffle();
+      this.setupCheat();
+    }
+    this.startRoundRobin();
+
+    super.start();
+  }
+
+  //Start: Randomizes player order, and gives the microphone to first one.
+  setupCheat() {
+    this.ThePot = parseInt(0);
+    this.randomizedPlayers.forEach((player) => {
+      player.hasFolded = false;
+      player.hasHadTurn = false;
+      player.Score = 0;
+      player.AmountBidding = parseInt(0);
+      player.ScoreType = null;
+      player.ShowdownCards = [];
+    });
+    this.RoundNumber = 0;
+    this.Phase = "First Bets";
+    if (this.RoundNumber == 0) {
+      this.Dealer = this.randomizedPlayersCopy[0];
+      this.SmallBlind = this.randomizedPlayersCopy[1];
+      this.BigBlind =
+        this.randomizedPlayersCopy[(1 + 1) % this.randomizedPlayersCopy.length];
+    }
+
+    this.ThePot += this.minimumBet;
+    this.lastAmountBid = this.minimumBet;
+    let cardAmount = Math.floor(52 / this.randomizedPlayersCopy.length);
+
+    this.dealCards(cardAmount);
+  }
+
+  startRoundRobin() {
+    while (true) {
+      let nextPlayer = this.randomizedPlayersCopy[this.currentIndex];
+      if (nextPlayer.alive && nextPlayer.hasFolded != true) {
+        nextPlayer.howManySelected = false;
+        nextPlayer.whichFaceSelected = false;
+        nextPlayer.holdItem("Microphone");
+        return;
+      }
+    }
+  }
+
+  //Called each round, cycles between players.
+  incrementCurrentIndex() {
+    this.currentIndex =
+      (this.currentIndex + 1) % this.randomizedPlayersCopy.length;
+  }
+
+  //After someone uses microphone, it passes it to the next player.
+  incrementState() {
+    let previousState = this.getStateName();
+
+    console.log(this.spectatorMeetFilter);
+    if (previousState == "Call Lie") {
+      this.RoundNumber++;
+      this.RankNumber++;
+      if (this.RankNumber > 13) {
+        this.RankNumber = 1;
+      }
+      this.sendAlert(
+        `${
+          this.RankNumber != 1 &&
+          this.RankNumber != 11 &&
+          this.RankNumber != 12 &&
+          this.RankNumber != 13
+            ? this.RankNumber
+            : this.RankNumber == 1
+            ? "Ace"
+            : this.RankNumber == 11
+            ? "Jack"
+            : this.RankNumber == 12
+            ? "Queen"
+            : "King"
+        }s must be played!`
+      );
+      for (let player of this.randomizedPlayersCopy) {
+        player.hasLied = false;
+      }
+      this.incrementCurrentIndex();
+      this.sendAlert(
+        `${this.randomizedPlayersCopy[this.currentIndex].name}'s Turn!`
+      );
+    }
+
+    super.incrementState();
+  }
+
+  sortCards(cards) {
+    for (let x = 0; x < cards.length; x++) {
+      for (let y = 0; y < cards.length; y++) {
+        if (
+          this.readCard(cards[x], this.CardGameType)[0] >
+          this.readCard(cards[y], this.CardGameType)[0]
+        ) {
+          let temp = cards[x];
+          cards[x] = cards[y];
+          cards[y] = temp;
+        }
+      }
+    }
+
+    return cards;
+  }
+
+  readCard(card, type) {
+    let cardValue = card.split("-")[0];
+    let cardSuit = card.split("-")[1];
+    //if(type == "Texas Hold’em"){
+    if (cardValue == "Jack") {
+      cardValue = 11;
+    } else if (cardValue == "Queen") {
+      cardValue = 12;
+    } else if (cardValue == "King") {
+      cardValue = 13;
+    } else if (cardValue == "Ace") {
+      cardValue = 1;
+    }
+    //}
+    return [parseInt(cardValue), cardSuit];
+  }
+
+  //DealCards
+  dealCards(amount) {
+    this.broadcast("cardShuffle");
+    this.randomizedPlayers.forEach((player) => {
+      if (player.alive) {
+        let Cards = this.drawDiscardPile.drawMultiple(amount);
+        player.CardsInHand.push(...Cards);
+        player.sendAlert(`${Cards.join(", ")} have been added to your Hand!`);
+      }
+    });
+  }
+
+  removeHands() {
+    this.randomizedPlayers.forEach((player) => {
+      for (let card of player.CardsInHand) {
+        this.drawDiscardPile.discard(card);
+      }
+      player.CardsInHand = [];
+    });
+  }
+
+  discardCommunityCards() {
+    for (let card of this.CommunityCards) {
+      this.drawDiscardPile.discard(card);
+    }
+    this.CommunityCards = [];
+  }
+
+  //DealCommunity
+  DrawCommunityCards(amount) {
+    this.broadcast("cardShuffle");
+    let Cards = this.drawDiscardPile.drawMultiple(amount);
+    this.CommunityCards.push(...Cards);
+    this.sendAlert(
+      `${Cards.join(", ")} have been added to the Community Cards!`
+    );
+  }
+
+  addToPot(player, type, amount) {
+    let soundNum = Random.randInt(0, 4);
+    if (soundNum == 0) {
+      this.broadcast("chips_large1");
+    } else if (soundNum == 1) {
+      this.broadcast("chips_large2");
+    } else if (soundNum == 2) {
+      this.broadcast("chips_small1");
+    } else {
+      this.broadcast("chips_small2");
+    }
+
+    if (type == "Bet") {
+      this.sendAlert(`${player.name} bets ${amount} into the Pot!`);
+      player.Chips = parseInt(player.Chips) - parseInt(amount);
+      player.AmountBidding += parseInt(amount);
+      this.ThePot += parseInt(amount);
+      let activePlayers = this.players.filter((p) => p.alive && !p.hasFolded);
+      for (let person of activePlayers) {
+        person.hasHadTurn = false;
+      }
+
+      if (this.lastAmountBid < player.AmountBidding) {
+        this.lastAmountBid = player.AmountBidding;
+      }
+    }
+
+    if (type == "Call") {
+      if (player.Chips >= this.lastAmountBid - player.AmountBidding) {
+        this.sendAlert(
+          `${player.name} calls and puts ${
+            this.lastAmountBid - player.AmountBidding
+          } into the Pot!`
+        );
+        this.ThePot += parseInt(this.lastAmountBid - player.AmountBidding);
+        player.Chips =
+          parseInt(player.Chips) - (this.lastAmountBid - player.AmountBidding);
+        player.AmountBidding += parseInt(
+          this.lastAmountBid - player.AmountBidding
+        );
+      } else if (player.Chips > 0) {
+        this.sendAlert(
+          `${player.name} goes All in and puts ${player.Chips} into the Pot!`
+        );
+        player.AmountBidding += parseInt(player.Chips);
+        this.ThePot += parseInt(player.Chips);
+        player.Chips = 0;
+      } else {
+        this.sendAlert(`${player.name} has Nothing to put into the Pot!`);
+      }
+    }
+    /*
+    if(type == "Raise"){
+      if(player.Chips >= (this.lastAmountBid-player.AmountBidding)+amount){
+      this.sendAlert(`${player.name} raises and puts ${(this.lastAmountBid-player.AmountBidding)+amount} into the Pot!`);
+      player.Chips = player.Chips - ((this.lastAmountBid-player.AmountBidding)+amount);
+      player.AmountBidding += ((this.lastAmountBid-player.AmountBidding)+amount);
+      this.lastAmountBid = player.AmountBidding;
+      this.ThePot += ((this.lastAmountBid-player.AmountBidding)+amount);
+      }
+    }
+    */
+  }
+
+  addDice(player, amount, midRound, noMessage) {
+    if (amount == null || amount <= 0) {
+      amount = 1;
+    }
+    player.diceNum = player.diceNum + amount;
+    if (noMessage != true) {
+      this.sendAlert(`${player.name} has Gained a Dice!`);
+    }
+    if (midRound == true) {
+      player.queueAlert(`You gain a Dice!`);
+      let dice;
+      let info;
+      for (let x = 0; x < amount; x++) {
+        dice = Math.floor(Math.random() * 6) + 1;
+        player.rolledDice.push(dice);
+        if (dice == 1) {
+          info = ":Dice1:";
+        }
+        if (dice == 2) {
+          info = ":Dice2:";
+        }
+        if (dice == 3) {
+          info = ":Dice3:";
+        }
+        if (dice == 4) {
+          info = ":Dice4:";
+        }
+        if (dice == 5) {
+          info = ":Dice5:";
+        }
+        if (dice == 6) {
+          info = ":Dice6:";
+        }
+        player.queueAlert(`You gain a ${info} !`);
+        this.allDice += 1;
+        this.allRolledDice.push(dice);
+      }
+    }
+  }
+
+  getStateInfo(state) {
+    var info = super.getStateInfo(state);
+    const simplifiedPlayers = this.simplifyPlayers(this.randomizedPlayers);
+    info.extraInfo = {
+      randomizedPlayers: simplifiedPlayers,
+      isTheFlyingDutchman:
+        this.chatName == "The Flying Dutchman" ? true : false,
+      whoseTurnIsIt:
+        this.randomizedPlayersCopy?.[this.currentIndex]?.user.id ?? 0,
+      ThePot: this.ThePot,
+      RoundNumber: this.RoundNumber,
+      RankNumber: this.RankNumber,
+      TheStack: this.TheStack,
+    };
+    return info;
+  }
+
+  simplifyPlayers(players) {
+    const simplified = [];
+    for (const key in players) {
+      if (Object.prototype.hasOwnProperty.call(players, key)) {
+        const player = players[key];
+        simplified.push({
+          playerId: player.id,
+          userId: player.user.id,
+          playerName: player.name,
+          CardsInHand: player.CardsInHand,
+          Chips: player.Chips,
+          Bets: player.AmountBidding,
+          Folded: player.hasFolded,
+        });
+      }
+    }
+    return simplified;
+  }
+
+  // process player leaving immediately
+  async playerLeave(player) {
+    await super.playerLeave(player);
+
+    if (this.started && !this.finished) {
+      const deadPlayerIndex = this.randomizedPlayers.findIndex(
+        (randomizedPlayer) => randomizedPlayer.id === player.id
+      );
+      this.randomizedPlayers = this.randomizedPlayers.filter(
+        (rPlayer) => rPlayer.id !== player.id
+      );
+
+      let action = new Action({
+        actor: player,
+        target: player,
+        game: this,
+        run: function () {
+          this.target.kill("leave", this.actor, true);
+        },
+      });
+
+      if (player.alive && player != this.hostPlayer) {
+      } else {
+        this.sendAlert(`${player.name} left, and will surely be missed.`);
+      }
+
+      this.instantAction(action);
+    } else if (this.finished) {
+      this.sendAlert(`${player.name} left, and will surely be missed.`);
+    }
+  }
+
+  async vegPlayer(player) {
+    super.vegPlayer(player);
+
+    if (
+      this.started &&
+      !this.finished &&
+      this.randomizedPlayers.includes(player)
+    ) {
+      const deadPlayerIndex = this.randomizedPlayers.findIndex(
+        (randomizedPlayer) => randomizedPlayer.id === player.id
+      );
+      this.randomizedPlayers = this.randomizedPlayers.filter(
+        (rPlayer) => rPlayer.id !== player.id
+      );
+    }
+  }
+
+  checkWinConditions() {
+    var finished = false;
+    var counts = {};
+    var winQueue = new Queue();
+    var winners = new Winners(this);
+    var aliveCount = this.alivePlayers().length;
+
+    for (let player of this.players) {
+      let alignment = player.role.alignment;
+
+      if (!counts[alignment]) counts[alignment] = 0;
+
+      if (player.alive) counts[alignment]++;
+
+      winQueue.enqueue(player.role.winCheck);
+    }
+
+    for (let winCheck of winQueue) {
+      winCheck.check(counts, winners, aliveCount);
+    }
+
+    if (winners.groupAmt() > 0) finished = true;
+    else if (aliveCount == 0) {
+      winners.addGroup("No one");
+      finished = true;
+    }
+
+    winners.determinePlayers();
+    return [finished, winners];
+  }
+
+  async endGame(winners) {
+    await super.endGame(winners);
+  }
+
+  getGameTypeOptions() {
+    return {
+      startingChips: this.startingChips,
+      minimumBet: this.minimumBet,
+      MaxRounds: this.MaxRounds,
+    };
+  }
+};
