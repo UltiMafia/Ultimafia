ca20362a241632b8fc6c8291cb782334afd48f59
SawJester <anglelord270@gmail.com>
2025-12-16 02:30:10 -0500

    Update and rename WinIfWithNoCards.js to WinIfWithAllCards.js

--- Games/types/Ratscrew/roles/cards/WinIfWithAllCards.js
+++ Games/types/Ratscrew/roles/cards/WinIfWithAllCards.js
@@ -1,7 +1,7 @@
 const Card = require("../../Card");
 const Action = require("../../Action");
 
-module.exports = class WinIfWithNoCards extends Card {
+module.exports = class WinIfWithAllCards extends Card {
   constructor(role) {
     super(role);
 
@@ -12,23 +12,19 @@ module.exports = class WinIfWithNoCards extends Card {
           this.game.MaxRounds != 0 &&
           this.game.RoundNumber > this.game.MaxRounds
         ) {
-          let min = 10000;
-          let minPlayer;
+          let max = -1;
+          let maxPlayer;
           for (let player of this.game.players) {
             if (player.alive) {
-              if (player.CardsInHand.length < min) {
-                minPlayer = player;
-                min = player.CardsInHand.length;
+              if (player.CardsInHand.length > max) {
+                maxPlayer = player;
+                max = player.CardsInHand.length;
               }
             }
           }
-          if (minPlayer == this.player) {
+          if (maxPlayer == this.player) {
             winners.addPlayer(this.player, this.name);
           }
-        }
-
-        if (this.player.HasNoCards == true) {
-          winners.addPlayer(this.player, this.name);
         }
         let nonHostAlive = this.game
           .alivePlayers()
@@ -36,16 +32,19 @@ module.exports = class WinIfWithNoCards extends Card {
         if (nonHostAlive.length <= 1 && this.player.alive) {
           winners.addPlayer(this.player, this.name);
         }
+
+        for (let player of this.game.players) {
+            if (player.alive) {
+              if (player.CardsInHand.length > 0) {
+                return;
+              }
+            }
+          }
+          winners.addPlayer(this.player, this.name);
       },
     };
 
     this.listeners = {
-      start: function () {
-        if (!this.game.hasGovernor) return;
-        if (!this.game.enablePunctuation) {
-          this.meetings["Give Response"].textOptions.alphaOnlyWithSpaces = true;
-        }
-      },
       state: function (stateInfo) {
         if (stateInfo.name != "Call Lie") {
           return;
