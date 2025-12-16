207ff57a3cd57f05d4f861df753b92a7ec8eb28d
SawJester <anglelord270@gmail.com>
2025-12-16 01:22:38 -0500

    Create CreateRatscrewSetup.js

---
+++ react_main/src/pages/Play/CreateSetup/CreateRatscrewSetup.js
@@ -0,0 +1,63 @@
+import React, { useContext, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import axios from "axios";
+
+import CreateBrowser from "./CreateBrowser";
+import { SiteInfoContext } from "../../../Contexts";
+import { useForm } from "../../../components/Form";
+import { useErrorAlert } from "../../../components/Alerts";
+
+export default function CreateCheatSetup() {
+  const gameType = "Ratscrew";
+  const location = useLocation();
+  const params = new URLSearchParams(location.search);
+  const errorAlert = useErrorAlert();
+  const [formFields, updateFormFields, resetFormFields] = useForm([
+    {
+      label: "Setup Name",
+      ref: "name",
+      type: "text",
+    },
+  ]);
+  const formFieldValueMods = {};
+
+  const siteInfo = useContext(SiteInfoContext);
+
+  useEffect(() => {
+    document.title = "Create Ratscrew Setup | UltiMafia";
+  }, []);
+
+  function onCreateSetup(roleData, editing, setRedirect, gameSettings) {
+    axios
+      .post("/api/setup/create", {
+        gameType: gameType,
+        roles: roleData.roles,
+        gameSettings: gameSettings,
+        name: formFields[0].value,
+        startState: "Play Cards",
+        noReveal: true,
+        editing: editing,
+        id: params.get("edit"),
+      })
+      .then((res) => {
+        siteInfo.showAlert(
+          `${editing ? "Edited" : "Created"} setup '${formFields[0].value}'`,
+          "success"
+        );
+        setRedirect(res.data);
+      })
+      .catch(errorAlert);
+  }
+
+  return (
+    <CreateBrowser
+      gameType={gameType}
+      formFields={formFields}
+      updateFormFields={updateFormFields}
+      resetFormFields={resetFormFields}
+      closedField={{ value: false }}
+      formFieldValueMods={formFieldValueMods}
+      onCreateSetup={onCreateSetup}
+    />
+  );
+}
