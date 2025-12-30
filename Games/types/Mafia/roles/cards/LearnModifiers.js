9aa8cda1cc703469096b75eb4c077a1ae6a085a9
SawJester <anglelord270@gmail.com>
2025-12-30 06:07:16 -0500

    Create LearnModifiers.js

---
+++ Games/types/Mafia/roles/cards/LearnModifiers.js
@@ -0,0 +1,34 @@
+const Card = require("../../Card");
+const Random = require("../../../../../lib/Random");
+const { PRIORITY_INVESTIGATIVE_DEFAULT } = require("../../const/Priority");
+
+module.exports = class LearnModifiers extends Card {
+  constructor(role) {
+    super(role);
+
+    this.meetings = {
+      "Learn Modifiers": {
+        states: ["Night"],
+        flags: ["voting"],
+        action: {
+          role: this.role,
+          labels: ["investigate", "role"],
+          priority: PRIORITY_INVESTIGATIVE_DEFAULT,
+          run: function () {
+            let info = this.game.createInformation(
+              "ModifierInfo",
+              this.actor,
+              this.game,
+              this.target,
+              null,
+              this.role,
+            );
+            info.processInfo();
+            var alert = `:invest: ${info.getInfoFormated()}.`;
+            this.actor.queueAlert(alert);
+          },
+        },
+      },
+    };
+  }
+};
