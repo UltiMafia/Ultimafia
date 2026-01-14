226471a1171f0a9bb43f76c261cd2e462ac7c37f
SawJester <171521669+SawJester@users.noreply.github.com>
2026-01-14 01:27:45 -0500

    Update VillageCore.js

--- Games/types/Mafia/roles/cards/VillageCore.js
+++ Games/types/Mafia/roles/cards/VillageCore.js
@@ -64,6 +64,7 @@ module.exports = class VillageCore extends Card {
         flags: ["group", "speech", "Important"],
         whileDead: true,
         passiveDead: true,
+        speakDead: true,
       },
       Graveyard: {
         states: ["Night"],
