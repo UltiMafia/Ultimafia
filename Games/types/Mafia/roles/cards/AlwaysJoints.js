23ef128189f0f3b4d0dab23561ca3fd990c52870
SawJester <171521669+SawJester@users.noreply.github.com>
2026-01-19 22:35:03 -0500

    Create AlwaysJoints.js

---
+++ Games/types/Mafia/roles/cards/AlwaysJoints.js
@@ -0,0 +1,33 @@
+const Card = require("../../Card");
+const { PRIORITY_WIN_CHECK_DEFAULT } = require("../../const/Priority");
+const { CULT_FACTIONS, MAFIA_FACTIONS } = require("../../const/FactionList");
+
+module.exports = class AlwaysJoints extends Card {
+  constructor(role) {
+    super(role);
+
+    this.listeners = {
+      handleWinLater: function (winners) {
+        if (this.player.role.alignment != "Independent") {
+          return;
+        }
+
+        if(!this.game.WillWinLater){
+          this.game.WillWinLater = [];
+        }
+
+        if(winners.findPlayerGroup(this.player)){
+          let group = winners.findPlayerGroup(this.player);
+          this.game.WillWinLater.push([this.player, group]);
+          winners.remove(this.player, group);
+          this.game.sendAlert(`${this.player.name} has achieved their goals and ascends to a new plane of existance!`);
+          if(this.player.alive){
+            this.player.kill("victory", this.actor, true);
+          }
+          
+      
+        }
+      },
+    };
+  }
+};
