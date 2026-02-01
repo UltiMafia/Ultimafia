41893891d3d6e62c1e96309f81884174d0a8dc51
SawJester <171521669+SawJester@users.noreply.github.com>
2026-02-01 16:05:11 -0500

    Update BlockedIfKilled.js

--- Games/types/Mafia/roles/cards/BlockedIfKilled.js
+++ Games/types/Mafia/roles/cards/BlockedIfKilled.js
@@ -1,6 +1,6 @@
 const Card = require("../../Card");
 const Action = require("../../Action");
-const { PRIORITY_BLOCK_SELF_IF_KILLED } = require("../../const/Priority");
+const { PRIORITY_BLOCK_SELF_IF_KILLED, PRIORITY_FATAL_KILLS, PRIORITY_BLOCK_SELF_IF_KILLED_POST_FATAL } = require("../../const/Priority");
 
 module.exports = class BlockedIfKilled extends Card {
   constructor(role) {
@@ -21,6 +21,43 @@ module.exports = class BlockedIfKilled extends Card {
           }
         },
       },
+      {
+        ability: ["Modifier", "WhenDead"],
+        state: "Night",
+        actor: role.player,
+        game: role.player.game,
+        priority: PRIORITY_BLOCK_SELF_IF_KILLED_POST_FATAL,
+        labels: ["block", "hidden"],
+        role: role,
+        run: function () {
+          if (!this.actor.alive) {
+            this.blockActions(this.actor);
+          }
+        },
+      },
     ];
+
+    this.listeners = {
+      actionsNext: function (actions) {
+        if (!this.hasAbility(["Modifier", "WhenDead"])) {
+          return;
+        }
+        for(let action of actions){
+          if(action.actors && action.actors.indexOf(this.player) != -1){
+            if(!action.hasLabels(["kill"])){
+            continue;
+            }
+            else if(action.hasLabels(["kill", "mafia"])){
+            continue;
+            }
+            else{
+              action.priority = PRIORITY_FATAL_KILLS;
+            }
+          }
+        }
+      },
+    };
+
+    
   }
 };
