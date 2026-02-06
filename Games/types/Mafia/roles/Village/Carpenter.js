60e49cd673029455e4c0229fe17145c4b9780d4f
SawJester <171521669+SawJester@users.noreply.github.com>
2026-02-06 13:25:09 -0500

    Create Carpenter.js

---
+++ Games/types/Mafia/roles/Village/Carpenter.js
@@ -0,0 +1,12 @@
+const Role = require("../../Role");
+
+module.exports = class Carpenter extends Role {
+  constructor(player, data) {
+    super("Carpenter", player, data);
+    this.alignment = "Village";
+    this.cards = ["VillageCore", "WinWithFaction", "MeetingFaction", "StartingItemGiver"];
+  }
+  init(modifiers, itemStatus){
+    super(modifiers, "NoStartingItems");
+  }
+};
