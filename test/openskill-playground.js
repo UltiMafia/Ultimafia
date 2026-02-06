baa54df0facb8eb63dc0081ae82ab5ee7d0d7279
nearbear <79229391+nearbear@users.noreply.github.com>
2026-02-06 03:39:49 +0000

    Fixed two factions losing to the same victor being considered a draw

--- test/openskill-playground.js
+++ test/openskill-playground.js
@@ -15,7 +15,6 @@ const defaultScenario = {
   defaultSkillRatingSigma: 250,
   options: {
     model: plackettLuce,
-    beta: 1000,
   },
 };
 
@@ -49,16 +48,73 @@ scenario1.sequence = [
   ...village33winrate,
   ...village33winrate,
   ...village33winrate,
-  ...village33winrate,
-  ...village33winrate,
-  ...village33winrate,
-  ...village33winrate,
-  ...village33winrate,
 ];
 
-runScenario(scenario1);
+const villageWin    = toMap({ Village: 1, Zombie: 0, "Serial Killer": 0, });
+const killerWin     = toMap({ Village: 0, Zombie: 0, "Serial Killer": 1, });
+const zombieWin     = toMap({ Village: 0, Zombie: 1, "Serial Killer": 0, });
+const evilJointWin  = toMap({ Village: 0, Zombie: 1, "Serial Killer": 1, });
+
+// 21 village wins, 1 killer win, 9 zombie wins, 1 evil joint win
+// 65.625% village winrate, 31.25% zombie winrate, 6.25% killer winrate
+const scenario2Pattern = [
+  villageWin,
+  villageWin,
+  villageWin,
+  zombieWin,
+  villageWin,
+  villageWin,
+  villageWin,
+  killerWin,
+  zombieWin,
+  villageWin,
+  villageWin,
+  zombieWin,
+  villageWin,
+  evilJointWin,
+  zombieWin,
+  villageWin,
+  villageWin,
+  villageWin,
+  zombieWin,
+  villageWin,
+  villageWin,
+  zombieWin,
+  villageWin,
+  zombieWin,
+  villageWin,
+  villageWin,
+  villageWin,
+  zombieWin,
+  villageWin,
+  villageWin,
+  zombieWin,
+  villageWin,
+];
+
+let scenario2 = { ...defaultScenario };
+scenario2.sequence = [
+  ...scenario2Pattern,
+  ...scenario2Pattern,
+  ...scenario2Pattern,
+  ...scenario2Pattern,
+  ...scenario2Pattern,
+  ...scenario2Pattern,
+  ...scenario2Pattern,
+  ...scenario2Pattern,
+  ...scenario2Pattern,
+  ...scenario2Pattern,
+];
+
+//runScenario(scenario1);
+runScenario(scenario2);
 
 function runScenario(scenario) {
+  let externalStats = {
+    totalGames: scenario.sequence.length,
+    totalFactionPoints: {},
+    avgFactionPoints: {},
+  };
   let factionRatingsState = [];
 
   for (let j = 0; j < scenario.sequence.length; j++) {
@@ -86,6 +142,11 @@ function runScenario(scenario) {
           })
         );
       }
+
+      if (externalStats.totalFactionPoints[factionName] === undefined) {
+        externalStats.totalFactionPoints[factionName] = 0;
+      }
+      externalStats.totalFactionPoints[factionName] += factionScores.get(factionName);
     }
 
     const factionNames = [...factionsToBeRated.keys()];
@@ -93,9 +154,13 @@ function runScenario(scenario) {
     const factionsToBeRatedRaw = factionNames.map((factionName) => [
       factionsToBeRated.get(factionName),
     ]);
+    // the 1e-6 is to avoid zero scores which openskill refuses to consider a draw
     const factionScoresRaw = factionNames.map((factionName) =>
-      factionScores.get(factionName)
+      factionScores.get(factionName)+1e-6
     );
+
+    console.log("Factions to be rated: ", factionNames);
+    console.log("Faction scores: ", factionScoresRaw);
 
     // library code time
     const predictions = predictWin(factionsToBeRatedRaw, scenario.options);
@@ -103,6 +168,9 @@ function runScenario(scenario) {
       score: factionScoresRaw,
       ...scenario.options,
     });
+
+    console.log("Ratings before game: ", factionsToBeRatedRaw);
+    console.log("Ratings after game: ", ratedFactions);
 
     // Transform the results from the rate and predictWin functions back into their usable forms
     const pointsWonByFactions = {};
@@ -119,9 +187,7 @@ function runScenario(scenario) {
       const factionName = factionNames[i];
       const winPredictionPercent = predictions[i]; // this adds up to 1 across all factions
       const newSkillRating = ratedFactions[i];
-      console.log(
-        `${factionName} win ${100 * winPredictionPercent.toFixed(2)}%`
-      );
+      console.log(`${factionName} win% was predicted to be ${(100 * winPredictionPercent).toFixed(2)}%`);
 
       pointsWonByFactions[factionName] = Math.round(
         2 * scenario.pointsNominalAmount * (1 - winPredictionPercent)
@@ -140,14 +206,16 @@ function runScenario(scenario) {
     }
 
     for (const factionName of Object.keys(pointsWonByFactions)) {
-      console.log(
-        `${factionName} would win ${pointsWonByFactions[factionName]} points`
-      );
+      console.log(`${factionName} would win ${pointsWonByFactions[factionName]} points`);
     }
     for (const factionSkillRating of newFactionSkillRatings) {
-      console.log(
-        `${factionSkillRating.factionName}: ${factionSkillRating.skillRating.mu}μ / ${factionSkillRating.skillRating.sigma}σ`
-      );
+      console.log(`${factionSkillRating.factionName}: ${factionSkillRating.skillRating.mu}μ / ${factionSkillRating.skillRating.sigma}σ`);
     }
   }
+
+  for (const factionName of Object.keys(externalStats.totalFactionPoints)) {
+    externalStats.avgFactionPoints[factionName] = externalStats.totalFactionPoints[factionName] / externalStats.totalGames;
+  }
+  console.log("======================================= FINAL STATS");
+  console.log(externalStats);
 }
