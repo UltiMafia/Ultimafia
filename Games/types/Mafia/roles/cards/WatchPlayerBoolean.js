2307e6ad14ad3eea97b6b2fd5cd06ec048f2cb62
SawJester <171521669+SawJester@users.noreply.github.com>
2026-02-09 14:40:58 -0500

    Update WatchPlayerBoolean.js

--- Games/types/Mafia/roles/cards/WatchPlayerBoolean.js
+++ Games/types/Mafia/roles/cards/WatchPlayerBoolean.js
@@ -7,41 +7,17 @@ const {
 module.exports = class WatchPlayerBoolean extends Card {
   constructor(role) {
     super(role);
-    /*
-    this.meetings = {
-      "Watch (Boolean)": {
-        states: ["Night"],
-        flags: ["voting"],
-        targets: { include: ["alive"], exclude: [] },
-        action: {
-          labels: ["investigate"],
-          priority: PRIORITY_INVESTIGATIVE_AFTER_RESOLVE_DEFAULT - 5,
-          run: function () {
-            let info = this.game.createInformation(
-              "BinaryWatcherInfo",
-              this.actor,
-              this.game,
-              this.target
-            );
-            info.processInfo();
-            this.actor.queueAlert(`:watch: ${info.getInfoFormated()}`);
-          },
-        },
-      },
-    };
-*/
-    this.listeners = {
-      state: function (stateInfo) {
-        if (!stateInfo.name.match(/Night/)) {
-          return;
-        }
 
-        var action = new Action({
-          actor: this.player,
-          game: this.player.game,
-          priority: PRIORITY_INVESTIGATIVE_AFTER_RESOLVE_DEFAULT - 5,
-          labels: ["investigate"],
-          run: function () {
+        this.passiveActions = [
+      {
+        ability: ["Information", "Modifier"],
+        actor: role.player,
+        state: "Night",
+        game: role.game,
+        role: role,
+        priority: PRIORITY_INVESTIGATIVE_AFTER_RESOLVE_DEFAULT - 5,
+        labels: ["investigate"],
+        run: function () {
             let visits = this.getVisits(this.actor);
             for (let v of visits) {
               if (this.dominates(v)) {
@@ -58,10 +34,7 @@ module.exports = class WatchPlayerBoolean extends Card {
               }
             }
           },
-        });
-
-        this.game.queueAction(action);
       },
-    };
+    ];
   }
 };
