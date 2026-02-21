0c59fcedd0c2db7853c95673a74b79a82b06c4a1
SawJester <171521669+SawJester@users.noreply.github.com>
2026-02-20 23:58:07 -0500

    Create Mortician.js

---
+++ Games/types/Mafia/roles/Village/Mortician.js
@@ -0,0 +1,14 @@
+const Role = require("../../Role");
+
+module.exports = class Mortician extends Role {
+  constructor(player, data) {
+    super("Mortician", player, data);
+    this.alignment = "Village";
+    this.cards = [
+      "VillageCore",
+      "WinWithFaction",
+      "MeetingFaction",
+      "LearnCondemnedRole",
+    ];
+  }
+};
