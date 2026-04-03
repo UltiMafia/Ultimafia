294abe209eca94f4b802c5f7fe387c00200da895
joyce <24848927+pikulet@users.noreply.github.com>
2026-04-04 06:34:26 +0800

    fix: broken add friend button on miniprofile

--- react_main/src/components/Miniprofile.jsx
+++ react_main/src/components/Miniprofile.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useContext, useRef, useEffect } from "react";
+import axios from "axios";
 
 import { Box, Button, IconButton, Stack, Tooltip, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
