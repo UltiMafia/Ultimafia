ca28a8338ffbcc24f2a28f346f911729097fe355
BlueCannonBall <mmttiizz100@gmail.com>
2026-06-30 19:33:03 -0400

    test: isolate test database connection to ultimafia_test under NODE_ENV=test

--- db/db.js
+++ db/db.js
@@ -7,8 +7,12 @@ module.exports = {
       process.env.MONGO_URL,
       process.env.MONGO_DB
     );
+    const dbName = process.env.NODE_ENV === "test"
+      ? `${process.env.MONGO_DB}_test`
+      : process.env.MONGO_DB;
+    console.log("Using MongoDB database:", dbName);
     await mongoose.connect(
-      `mongodb://${process.env.MONGO_URL}/${process.env.MONGO_DB}?authSource=admin`,
+      `mongodb://${process.env.MONGO_URL}/${dbName}?authSource=admin`,
       {
         user: process.env.MONGO_USER,
         pass: process.env.MONGO_PW,
