c51ab5fc63fdc430035550ab7a884068ce6eb2b7
Vinman <embassy-motions0x@icloud.com>
2026-09-02 21:43:38 -0400

    feat(game): add Bulletproof Village role
    Standalone Village role that starts with one Armor vest and appears as
    Bulletproof to itself and to information roles, including Stalker.
    
    Closes #2986

---
+++ Games/types/Mafia/roles/Village/Bulletproof.js
@@ -0,0 +1,15 @@
+const Role = require("../../Role");
+
+module.exports = class Bulletproof extends Role {
+  constructor(player, data) {
+    super("Bulletproof", player, data);
+
+    this.alignment = "Village";
+    this.cards = [
+      "VillageCore",
+      "WinWithFaction",
+      "MeetingFaction",
+      "StartWithArmor",
+    ];
+  }
+};
